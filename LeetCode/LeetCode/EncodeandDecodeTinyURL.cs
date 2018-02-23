8f3e8e76857fb8b5c6082a06e1f561377d17248c
Woody-Hu <812791840@qq.com>
2018-02-23 17:01:49 +0800

    2018 02 23

---
+++ LeetCode/LeetCode/EncodeandDecodeTinyURL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class EncodeandDecodeTinyURL
+    {
+        private List<string> useValue = new List<string>();
+
+        // Encodes a URL to a shortened URL
+        public string encode(string longUrl)
+        {
+  
+            useValue.Add(longUrl);
+            return (useValue.Count - 1).ToString();
+        
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public string decode(string shortUrl)
+        {
+            int tempValue = int.Parse(shortUrl);
+            return tempValue > useValue.Count - 1 ? string.Empty : useValue[tempValue];
+        }
+    }
+}
