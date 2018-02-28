4a18595b1dc3c64ac4fe5c845ae65e4f38bd9d90
Woody-Hu <812791840@qq.com>
2018-02-28 09:43:57 +0800

    2018 02 28

---
+++ LeetCode/LeetCode/ReplaceWords.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class ReplaceWords
+    {
+        public string ReplaceWords(IList<string> dict, string sentence)
+        {
+            string[] allsubString = sentence.Split(' ');
+
+            var usedict = dict.OrderBy(k => k.Length);
+
+            for (int i = 0; i < allsubString.Length; i++)
+            {
+                foreach (var oneStr in usedict)
+                {
+                    if (IfIsSuccessor(oneStr,allsubString[i]))
+                    {
+                        allsubString[i] = oneStr;
+                        break;
+                    }
+                }
+            }
+
+            StringBuilder returnValue = new StringBuilder();
+
+            for (int i = 0; i < allsubString.Length; i++)
+            {
+                returnValue.Append(allsubString[i] + " ");
+            }
+
+            return returnValue.ToString().TrimEnd();
+
+
+
+
+        }
+
+
+        private bool IfIsSuccessor(string inputKey,string inputValue)
+        {
+            if (inputKey.Length > inputValue.Length)
+            {
+                return false;
+            }
+
+            return inputKey.Equals(inputValue.Substring(0, inputKey.Length));
+        }
+    }
+}
