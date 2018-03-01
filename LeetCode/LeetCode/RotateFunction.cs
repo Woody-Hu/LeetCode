e8429ef6434132fd4869018ca5c889f1828e1964
Woody-Hu <812791840@qq.com>
2018-03-01 13:22:47 +0800

    2018 03 01

---
+++ LeetCode/LeetCode/RotateFunction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class RotateFunction
+    {
+        public int MaxRotateFunction(int[] A)
+        {
+            int countN = A.Length;
+
+            int f = 0;
+
+            int sum = 0;
+
+            for (int i = 0; i < countN; i++)
+            {
+                f += i * A[i];
+                sum += A[i];
+            }
+
+            int max =f;
+             
+            for (int i = countN - 1; i >= 0; i--)
+            {
+                f = f + sum - countN * A[i];
+
+                max = Math.Max(max, f);
+            }
+
+            return max;
+
+        }
+    }
+}
