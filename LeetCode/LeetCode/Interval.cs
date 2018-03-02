a50c3ed153cadd0954ca8175c64707a25fdf30d0
Woody-Hu <812791840@qq.com>
2018-03-02 16:39:35 +0800

    2018 03 02

---
+++ LeetCode/LeetCode/Interval.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+ public class Interval {
+  public int start;
+   public int end;
+   public Interval() { start = 0; end = 0; }
+   public Interval(int s, int e) { start = s; end = e; }
+ }
+}
---
+++ LeetCode/LeetCode/MergeIntervals.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class MergeIntervals
+    {
+        public IList<Interval> Merge(IList<Interval> intervals)
+        {
+            if (null == intervals || 0 == intervals.Count)
+            {
+                return intervals;
+            }
+
+            List<Interval> returnValue = new List<Interval>();
+
+            var tempLst = intervals.OrderBy(k => k.start);
+
+            int start = tempLst.ElementAt(0).start;
+
+            int end = tempLst.ElementAt(0).end;
+
+            foreach (var oneInterval in tempLst)
+            {
+                if (oneInterval.start <= end)
+                {
+                    end = Math.Max(oneInterval.end, end);
+                }
+                else
+                {
+                    returnValue.Add(new Interval(start, end));
+                    start = oneInterval.start;
+                    end = oneInterval.end;
+                }
+            }
+
+            returnValue.Add(new Interval(start, end));
+
+            return returnValue;
+
+
+        }
+    }
+}
