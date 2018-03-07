fbc2096f14cd173981e70dd59f7fcfd92e35993e
Woody-Hu <812791840@qq.com>
2018-03-07 09:40:12 +0800

    2018 03 07

---
+++ LeetCode/LeetCode/FirstMissingPositive.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class FirstMissingPositive
+    {
+        public int FirstMissingPositiveMethod(int[] nums)
+        {
+            Array.Sort(nums);
+
+            int useIndex = 0;
+
+
+            int lastValue = 0;
+
+            for (; useIndex < nums.Count(); useIndex++)
+            {
+                if (nums[useIndex] <= 0)
+                {
+                    continue;
+                }
+                else 
+                {
+                    if (nums[useIndex] == lastValue + 1 || nums[useIndex] == lastValue)
+                    {
+                        lastValue = nums[useIndex];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (lastValue > 0 )
+            {
+                return lastValue + 1;
+            }
+            else
+            {
+                return 1;
+            }
+
+        }
+    }
+}
