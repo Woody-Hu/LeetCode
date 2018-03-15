ac31d2a325b40c3fa59216099350900a922bbfcd
Woody-Hu <812791840@qq.com>
2018-03-15 16:06:40 +0800

    2018 03 15

---
+++ LeetCode/LeetCode/NthDigit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class NthDigit
+    {
+        public int FindNthDigit(int n)
+        {
+            long useLength = 1;
+
+            long useCount = 9;
+
+            long useStart = 1;
+
+            while (n > useLength * useCount)
+            {
+                n = (int)(n - useLength * useCount);
+
+                useLength++;
+
+                useCount *= 10;
+                useStart *= 10;
+            }
+
+            useStart += (n - 1) / useLength;
+
+            string useStr = useStart.ToString();
+
+            return int.Parse(useStr.Substring((int)((n - 1) % useLength), 1));
+        }
+    }
+}
