de5529c16a674761dd19d6e05410dac92b0b5826
Woody-Hu <812791840@qq.com>
2018-03-17 12:24:16 +0800

    2018 03 17

---
+++ LeetCode/LeetCode/CountCompleteTreeNodes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class CountCompleteTreeNodes
+    {
+        private int countDeep(TreeNode inputNode)
+        {
+            return null == inputNode? -1 : 1+countDeep(inputNode.left);
+        }
+
+        public int CountNodes(TreeNode root)
+        {
+            int nowDeep = countDeep(root);
+
+            return 0 > nowDeep ? 0 : nowDeep - 1 == countDeep(root.right) ? (1 << nowDeep) + CountNodes(root.right) : (1 << (nowDeep - 1)) + CountNodes(root.left);
+        }
+
+      
+    }
+}
