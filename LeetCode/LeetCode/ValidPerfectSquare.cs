71d2c480179fe320580ef0a7e79e34a8dd295d41
Woody-Hu <812791840@qq.com>
2018-03-19 22:35:14 +0800

    2018 03 19

---
+++ LeetCode/LeetCode/ValidPerfectSquare.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class ValidPerfectSquare
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            long x = num;
+
+            while (x*x > num)
+            {
+                x = (x + num / x) >> 1;
+            }
+
+            return x * x == num;
+        }
+    }
+}
