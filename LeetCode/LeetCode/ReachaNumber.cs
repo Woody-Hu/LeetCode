c130093bf5deb231b8b8097a0a46cc1f630d90d8
Woody-Hu <812791840@qq.com>
2018-03-22 08:30:14 +0800

    2018 03 22

---
+++ LeetCode/LeetCode/ReachaNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class ReachaNumber
+    {
+        public int ReachNumber(int target)
+        {
+            target = Math.Abs(target);
+
+            int usestep = 0;
+
+            int usesum = 0;
+
+            while (usesum < target)
+            {
+                usestep++;
+                usesum += usestep;
+            }
+
+            while ((usesum - target)%2 != 0)
+            {
+                usestep++;
+                usesum += usestep;
+            }
+
+            return usestep;
+
+        }
+    }
+}
