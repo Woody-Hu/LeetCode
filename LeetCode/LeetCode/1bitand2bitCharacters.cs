56cd1ee6a42d45e7886d33b93f0d5291db698520
Woody-Hu <812791840@qq.com>
2018-03-23 12:50:08 +0800

    2018 03 23

---
+++ LeetCode/LeetCode/1bitand2bitCharacters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _1bitand2bitCharacters
+    {
+        public bool IsOneBitCharacter(int[] bits)
+        {
+            if ( 0 != bits[bits.Length - 1])
+            {
+                return false;
+            }
+            else
+            {
+                int useCount = 0;
+                for (int i = bits.Length - 2; i >= 0; i--)
+                {
+                    if (1 == bits[i])
+                    {
+                        useCount++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                return useCount % 2 == 0;
+            }
+        }
+    }
+}
