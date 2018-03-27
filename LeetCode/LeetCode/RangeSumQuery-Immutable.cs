e5670e0331a589eec1c3efe29a3ad0eb693e07ff
Woody-Hu <812791840@qq.com>
2018-03-27 12:59:44 +0800

    2018 03 27

---
+++ LeetCode/LeetCode/RangeSumQuery-Immutable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class NumArray
+    {
+        int[] m_useNums;
+
+        public NumArray(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+                nums[i] += nums[i - 1];
+
+            this.m_useNums = nums;
+        }
+
+        public int SumRange(int i, int j)
+        {
+            if (0 == i)
+            {
+                return m_useNums[j];
+            }
+
+            return m_useNums[j] - m_useNums[i - 1];
+        }
+    }
+}
