9c1f53057b8e8dacc43ac49a80ffc9cdf00c9132
Woody-Hu <812791840@qq.com>
2018-03-27 13:44:30 +0800

    2018 03 27

---
+++ LeetCode/LeetCode/CalculateCoins.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class CalculateCoins
+    {
+        public int Calculate(int n)
+        {
+            HashSet<int> useSet = new HashSet<int>();
+
+            var useLength = n / 2;
+
+            for (int i = 0; i < useLength; i++)
+            {
+                var result = i ^ (n - i);
+                useSet.Add(result);
+            }
+
+            return useSet.Count;
+        }
+    }
+}
