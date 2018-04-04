7ce2942b97c335ddac543f7b9956f478b9cb7d86
Woody-Hu <812791840@qq.com>
2018-04-04 09:41:03 +0800

    2018 04 04

---
+++ LeetCode/LeetCode/MinCostClimbingStairs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class MinCostClimbingStairs
+    {
+        public int MinCostClimbingStairsMethod(int[] cost)
+        {
+            int[] returnValues = new int[cost.Length +1 ];
+
+            returnValues[0] = cost[0];
+
+            returnValues[1] = cost[1];
+
+
+            for (int i = 2; i <= cost.Length; i++)
+            {
+                int useCost = i == cost.Length ? 0 : cost[i];
+
+                returnValues[i] = Math.Min(useCost + returnValues[i - 1], useCost + returnValues[i - 2]);
+            }
+
+            return returnValues[cost.Length];
+        }
+    }
+}
