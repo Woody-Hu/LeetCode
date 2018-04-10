a11241348ae646538005d8c5c68d6d685c9fe016
Woody-Hu <812791840@qq.com>
2018-04-10 13:11:47 +0800

    2018 04 10

---
+++ LeetCode/LeetCode/LargestTriangleArea.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class LargestTriangleArea
+    {
+        public double LargestTriangleAreaMethod(int[][] points)
+        {
+            double returnValue = 0.0d;
+
+            foreach (var i in points)
+            {
+                foreach (var j in points)
+                {
+                    foreach (var k in points)
+                    {
+                        returnValue = Math.Max(returnValue, 0.5 * Math.Abs(i[0] * j[1] + j[0] * k[1] + k[0] * i[1]- j[0] * i[1] - k[0] * j[1] - i[0] * k[1]));
+                    }
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
