46e0c0a7e55588752f003fb50754b9d8057f0a7f
Woody-Hu <812791840@qq.com>
2018-04-11 12:59:47 +0800

    2018 04 11

---
+++ LeetCode/LeetCode/ConstructtheRectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class ConstructtheRectangle
+    {
+        public int[] ConstructRectangle(int area)
+        {
+            int[] returnValue = new int[2];
+
+            int useValue = (int)Math.Sqrt(area);
+
+            while (area % useValue !=0)
+            {
+                useValue--;
+            }
+
+            returnValue[0] = area / useValue;
+            returnValue[1] = useValue;
+
+            return returnValue;
+        }
+    }
+}
