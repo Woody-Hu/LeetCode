dd05d99f4943f8c7abf7a6fb37e35cea1b7b0c6b
Woody-Hu <812791840@qq.com>
2018-04-13 08:30:30 +0800

    2018 04 13

---
+++ LeetCode/LeetCode/BinaryTreeTilt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class BinaryTreeTilt
+    {
+        public int FindTilt(TreeNode root)
+        {
+            int returnValue = 0;
+            FindTiltLoop(root, ref returnValue);
+            return returnValue;
+        }
+
+        private int FindTiltLoop(TreeNode input,ref int sumValue)
+        {
+            if (null == input)
+            {
+                return 0;
+            }
+
+            int useLeftValue = FindTiltLoop(input.left, ref sumValue);
+
+            int useRightValue = FindTiltLoop(input.right, ref sumValue);
+
+            var tempUseTilt = Math.Abs(useLeftValue - useRightValue);
+
+            sumValue += tempUseTilt;
+
+            return useLeftValue + useRightValue + input.val;
+
+
+        }
+    }
+}
