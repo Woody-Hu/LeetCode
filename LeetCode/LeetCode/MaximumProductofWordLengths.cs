956afc50ea15c493f926f02ad8427e8e63db3d95
Woody-Hu <812791840@qq.com>
2018-04-15 09:15:19 +0800

    2018 04 45

---
+++ LeetCode/LeetCode/MaximumProductofWordLengths.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class MaximumProductofWordLengths
+    {
+        public int MaxProduct(string[] words)
+        {
+            int returnValue = 0;
+
+            int wordLength = words.Length;
+
+            int[] bitValues = new int[wordLength];
+
+            for (int i = 0; i < wordLength; i++)
+            {
+                bitValues[i] = 0;
+
+                foreach (var oneChar in words[i])
+                {
+                    bitValues[i] |= 1 << (oneChar - 'a');
+                }
+            }
+
+            for (int i = 0; i < wordLength; i++)
+            {
+                for (int j = i + 1; j < wordLength; j++)
+                {
+                    if ((bitValues[i] & bitValues[j]) == 0)
+                    {
+                        returnValue = Math.Max(returnValue, words[i].Length * words[j].Length);
+                    }
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
