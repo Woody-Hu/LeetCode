70d5b991b3114f59014c535146b5ba6cd9b297d0
Woody-Hu <812791840@qq.com>
2018-04-16 10:30:13 +0800

    2018 04 16

---
+++ LeetCode/LeetCode/MinimumDistanceBetweenBSTNodes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class MinimumDistanceBetweenBSTNodes
+    {
+        public int MinDiffInBST(TreeNode root)
+        {
+            int returnValue = int.MaxValue;
+
+            int? frontValue = null;
+
+            CalculateLoop(root, ref returnValue, ref frontValue);
+
+            return returnValue;
+        }
+
+        public void CalculateLoop(TreeNode inputNode,ref int minDiffValue,ref int?frontValue)
+        {
+            if (null == inputNode)
+            {
+                return;
+            }
+
+            CalculateLoop(inputNode.left, ref minDiffValue, ref frontValue);
+
+            if (null != frontValue)
+            {
+                minDiffValue = Math.Min(minDiffValue, Math.Abs(frontValue.Value - inputNode.val));
+
+            }
+
+            frontValue = inputNode.val;
+
+            CalculateLoop(inputNode.right, ref minDiffValue, ref frontValue);
+        }
+    }
+}
