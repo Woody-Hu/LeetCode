b03bd02e1cb209f640c99db86e6d8a50207dfcde
Woody-Hu <812791840@qq.com>
2018-04-19 12:51:42 +0800

    2018 04 19

---
+++ LeetCode/LeetCode/DiameterofBinaryTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class DiameterofBinaryTree
+    {
+        public int DiameterOfBinaryTreeMethod(TreeNode root)
+        {
+            int returnValue = 0;
+            Length(root, ref returnValue);
+            return returnValue;
+
+
+        }
+
+        private int Length(TreeNode root ,ref int maxValue)
+        {
+            if (null == root)
+            {
+                return 0;
+            }
+
+            int leftValue = Length(root.left,ref maxValue);
+            int rightValue = Length(root.right, ref maxValue);
+
+            maxValue = Math.Max(leftValue + rightValue, maxValue);
+
+            return Math.Max( leftValue ,rightValue) + 1;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/MostCommonWord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MostCommonWord
+    {
+        public string MostCommonWordMethod(string paragraph, string[] banned)
+        {
+            paragraph = paragraph.Replace(",", string.Empty).Replace(".", string.Empty).Replace("!", string.Empty).Replace("?", string.Empty).Replace("'", string.Empty).Replace(";", string.Empty);
+            var splitedValue = paragraph.Split(' ');
+
+            var groupedValue = splitedValue.GroupBy(k => k.ToLower()).OrderBy(k=>-k.Count());
+
+            HashSet<string> useBannedStr = new HashSet<string>();
+
+            foreach (var oneStr in banned)
+            {
+                useBannedStr.Add(oneStr);
+            }
+
+            foreach (var oneValue in groupedValue)
+            {
+                if (!useBannedStr.Contains(oneValue.Key))
+                {
+                    return oneValue.Key;
+                }
+   
+            }
+
+            return string.Empty;
+        }
+    }
+}
