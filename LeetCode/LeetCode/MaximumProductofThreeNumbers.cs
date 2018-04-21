efff3a669ce30f2526e17ae4835ec7a915eced44
Woody-Hu <812791840@qq.com>
2018-04-21 10:11:40 +0800

    2018 04 21

---
+++ LeetCode/LeetCode/MaximumProductofThreeNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class MaximumProductofThreeNumbers
+    {
+        public int MaximumProduct(int[] nums)
+        {
+            var orderedValue = nums.OrderBy(k => -k);
+
+            return Math.Max(orderedValue.ElementAt(0) * orderedValue.ElementAt(1) * orderedValue.ElementAt(2)
+                , orderedValue.ElementAt(0) * orderedValue.ElementAt(orderedValue.Count() - 1) * orderedValue.ElementAt(orderedValue.Count() - 2));
+        }
+    }
+}
