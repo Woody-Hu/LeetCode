705bd2212eeb9b221487df182566c223aa75eb8a
Woody-Hu <812791840@qq.com>
2018-04-23 09:43:47 +0800

    2018 04 23

---
+++ LeetCode/LeetCode/MaximumLengthofRepeatedSubarray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class MaximumLengthofRepeatedSubarray
+    {
+        public int FindLength(int[] A, int[] B)
+        {
+            int aLength = A.Length;
+
+            int bLength = B.Length;
+
+
+            int[] useArray = new int[bLength + 1];
+            int returnValue = 0;
+            for (int i = aLength - 1; i >=0; i--)
+            {
+                for (int j = 0; j < bLength; j++)
+                {
+                    returnValue = Math.Max(useArray[j] = A[i] == B[j] ? 1 + useArray[j + 1] : 0, returnValue);
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
