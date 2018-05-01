6fe55609026d6b5ced6bd6e507896aef16bc2a21
Woody-Hu <812791840@qq.com>
2018-05-02 07:52:07 +0800

    2018 05 02

---
+++ LeetCode/LeetCode/RotateString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class RotateString
+    {
+        public bool RotateStringMethod(string A, string B)
+        {
+            if (A == null || B == null|| A.Length != B.Length)
+            {
+                return false;
+            }
+
+
+
+            string useValue = A + A;
+
+            return useValue.Contains(B);
+
+
+        }
+    }
+}
