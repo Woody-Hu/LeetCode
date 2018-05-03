448f6de2a851f1616d6fa50c0496aa018995bc37
Woody-Hu <812791840@qq.com>
2018-05-04 07:52:00 +0800

    2018 05 04

---
+++ LeetCode/LeetCode/ReverseStringII.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class ReverseStringII
+    {
+        public string ReverseStr(string s, int k)
+        {
+            var useChars = s.ToArray();
+
+            for (int i = 0; i < useChars.Length; i += 2 * k)
+            {
+                int end = (i + k - 1) > (useChars.Length - 1) ? (useChars.Length - 1) : (i + k - 1);
+                Reverse(useChars, i, end);
+            }
+            return new String(useChars);
+
+
+        }
+
+        private void Reverse(char[] inputChars,int startIndex,int endIndex)
+        {
+            while (startIndex < endIndex)
+            {
+                char temp = inputChars[startIndex];
+                inputChars[startIndex] = inputChars[endIndex];
+                inputChars[endIndex] = temp;
+                ++startIndex; --endIndex;
+            }
+        }
+    }
+}
