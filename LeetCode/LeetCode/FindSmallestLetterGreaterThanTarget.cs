9345e40473ae26c2a8c2ed442ad480d7e1c18368
Woody-Hu <812791840@qq.com>
2018-05-05 10:03:46 +0800

    2018 05 05

---
+++ LeetCode/LeetCode/FindSmallestLetterGreaterThanTarget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class FindSmallestLetterGreaterThanTarget
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            var useCharArrays = from n in letters where n > target select n;
+
+            if (useCharArrays.Count() == 0)
+            {
+                return letters.Min();
+            }
+
+            useCharArrays = useCharArrays.OrderBy(k => k);
+
+            return useCharArrays.First();
+        }
+    }
+}
