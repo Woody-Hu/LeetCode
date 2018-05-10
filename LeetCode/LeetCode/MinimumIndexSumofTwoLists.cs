866417a0d031cb5e3f58ea1090f82e39c2f8b700
Woody-Hu <812791840@qq.com>
2018-05-10 08:44:19 +0800

    2018 05 10

---
+++ LeetCode/LeetCode/MinimumIndexSumofTwoLists.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class MinimumIndexSumofTwoLists
+    {
+        public string[] FindRestaurant(string[] list1, string[] list2)
+        {
+            Dictionary<string, int> dic1 = new Dictionary<string, int>();
+
+            for (int i = 0; i < list1.Count(); i++)
+            {
+                dic1.Add(list1[i], i);
+            }
+
+            int minIndex = int.MaxValue;
+
+            List<string> returnValue = new List<string>();
+
+            for (int i = 0; i < list2.Count(); i++)
+            {
+                if (!dic1.ContainsKey(list2[i]))
+                {
+                    continue;
+                }
+
+                var tempIndex = i + dic1[list2[i]];
+
+                if (tempIndex < minIndex)
+                {
+                    minIndex = tempIndex;
+                    returnValue = new List<string>() { list2[i]};
+                }
+                else if (tempIndex == minIndex)
+                {
+                    returnValue.Add(list2[i]);
+                }
+            }
+
+            return returnValue.ToArray();
+        }
+    }
+}
---
+++ LeetCode/LeetCode/PositionsofLargeGroups.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class PositionsofLargeGroups
+    {
+        public IList<IList<int>> LargeGroupPositions(string S)
+        {
+            List<IList<int>> returnValue = new List<IList<int>>();
+
+            if (string.IsNullOrWhiteSpace(S))
+            {
+                return returnValue;
+            }
+
+            char lastChar = S[0];
+
+            int nowUseCount = 1;
+
+            int nowUseStartIndex = 0;
+
+            for (int i = 1; i < S.Length; i++)
+            {
+                if (S[i] == lastChar)
+                {
+                    nowUseCount++;
+                }
+                else
+                {
+                    if (nowUseCount >= 3)
+                    {
+                        returnValue.Add(new List<int>() { nowUseStartIndex, i - 1 });
+                    }
+                    nowUseCount = 1;
+                    nowUseStartIndex = i;
+                }
+
+                lastChar = S[i];
+            }
+
+            if (nowUseCount >= 3)
+            {
+                returnValue.Add(new List<int>() { nowUseStartIndex, S.Length - 1 });
+            }
+
+            return returnValue;
+        
+        }
+
+        
+    }
+}
