bfeb04288e0f351129affeac62d76652f2235748
Woody-Hu <812791840@qq.com>
2018-05-12 13:45:47 +0800

    2018 05 12

---
+++ LeetCode/LeetCode/StudentAttendanceRecordI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class StudentAttendanceRecordI
+    {
+        public bool CheckRecord(string s)
+        {
+            int aCount = 0;
+            int nowLCount = 0;
+
+            foreach (var oneChar in s)
+            {
+                if (oneChar == 'L')
+                {
+                    nowLCount++;
+                    if (nowLCount > 2)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    nowLCount = 0;
+
+                    if (oneChar == 'A')
+                    {
+                        aCount++;
+
+                        if (aCount > 1)
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+            }
+            return true;
+        }
+    }
+}
