810bb025cb8a19d7fbe674d7ef8925c5e529cf02
Woody-Hu <812791840@qq.com>
2018-05-14 08:22:28 +0800

    2018 05 14

---
+++ LeetCode/LeetCode/MinimumSizeSubarraySum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class MinimumSizeSubarraySum
+    {
+        public int MinSubArrayLen(int s, int[] nums)
+        {
+            int minLength = int.MaxValue;
+            int useLength = nums.Length;
+            int useIndex = 0;
+            int useStartIndex = useIndex;
+            int nowSumValue = 0;
+
+            while (useIndex < useLength)
+            {
+                while(nowSumValue < s)
+                {
+                    nowSumValue += nums[useIndex];
+                    useIndex++;
+                    if (useIndex == useLength)
+                    {
+                        break;
+                    }
+                }
+                while (nowSumValue >= s)
+                {
+                    minLength = Math.Min(minLength, useIndex - useStartIndex);
+                    nowSumValue -= nums[useStartIndex];
+                    useStartIndex++;
+                }
+
+            }
+
+            return minLength == int.MaxValue ? 0 : minLength;
+        }
+    }
+}
