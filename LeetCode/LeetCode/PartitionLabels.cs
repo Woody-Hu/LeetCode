e39062391bccbd75c673e19fce24743882786d47
Woody-Hu <812791840@qq.com>
2018-05-22 10:37:40 +0800

    2018 05 22

---
+++ LeetCode/LeetCode/PartitionLabels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class PartitionLabels
+    {
+        public IList<int> PartitionLabels(string S)
+        {
+            List<int> returnValue = new List<int>();
+
+            int[] useLastIndexArray = new int[26];
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                useLastIndexArray[S[i] - 'a'] = i;
+            }
+
+            int useStartIndex = 0;
+            int useLastIndex = 0;
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                useLastIndex = Math.Max(useLastIndex, useLastIndexArray[S[i] - 'a']);
+                if (i == useLastIndex)
+                {
+                    returnValue.Add(useLastIndex - useStartIndex + 1);
+
+                    useStartIndex = useLastIndex + 1;
+                }
+            }
+
+            return returnValue;
+
+        }
+    }
+}
