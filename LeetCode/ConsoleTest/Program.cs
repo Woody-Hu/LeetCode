94ed369181e39b85bb91be01f6310a96df71e6cb
Woody-Hu <812791840@qq.com>
2018-05-23 10:18:21 +0800

    2018 05 23

--- LeetCode/ConsoleTest/Program.cs
+++ LeetCode/ConsoleTest/Program.cs
@@ -11,9 +11,7 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            RepeatedSubstringPattern a = new RepeatedSubstringPattern();
 
-            a.RepeatedSubstringPatternMethod("abab");
         }
 
 
