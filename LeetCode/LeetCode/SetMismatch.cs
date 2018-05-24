5e6b306896d7209989be34005804bc6feb2bf84b
Woody-Hu <812791840@qq.com>
2018-05-24 08:13:54 +0800

    2018 05 24

---
+++ LeetCode/LeetCode/SetMismatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class SetMismatch
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+            int length = nums.Length;
+            int[] useArray = new int[length];
+
+            int tempIndex = 0;
+
+            foreach (var oneValue in nums)
+            {
+                useArray[oneValue-1]++;
+
+                if (2 == useArray[oneValue - 1])
+                {
+                    tempIndex = oneValue;
+                    break;
+                }
+            }
+
+            int tempSum = nums.Sum() - tempIndex;
+
+            int lostValue = length * (length + 1) / 2 - tempSum;
+
+            return new int[] { tempIndex, lostValue };
+
+        }
+
+    }
+}
