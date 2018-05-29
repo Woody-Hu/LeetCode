7a14df25a353bf65a58a81da9372fbb0f590640e
Woody-Hu <812791840@qq.com>
2018-05-29 08:00:37 +0800

    2018 05 29

---
+++ LeetCode/LeetCode/LargestNumberAtLeastTwiceofOthers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class LargestNumberAtLeastTwiceofOthers
+    {
+        public int DominantIndex(int[] nums)
+        {
+            int largestIndex = -1;
+
+            int largestValue = int.MinValue;
+
+            int nextLargestValue = int.MinValue;
+
+            int length = nums.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (nums[i] > largestValue)
+                {
+                    largestIndex = i;
+
+                    nextLargestValue = largestValue;
+
+                    largestValue = nums[i];
+                    continue;
+                }
+
+                if (nums[i] > nextLargestValue)
+                {
+                    nextLargestValue = nums[i];
+                }
+            }
+
+            return largestValue >= 2 * nextLargestValue ? largestIndex : -1;
+
+        }
+    }
+}
