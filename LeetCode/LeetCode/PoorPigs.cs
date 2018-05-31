4c74783493ce07a5729ad78b7bbe041bf78790b0
Woody-Hu <812791840@qq.com>
2018-05-31 12:59:33 +0800

    2018 05 31

---
+++ LeetCode/LeetCode/PoorPigs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class PoorPigs
+    {
+        public int PoorPigs(int buckets, int minutesToDie, int minutesToTest)
+        {
+            return (int)Math.Ceiling(Math.Log(buckets) / Math.Log(minutesToTest / minutesToDie + 1));
+        }
+    }
+}
