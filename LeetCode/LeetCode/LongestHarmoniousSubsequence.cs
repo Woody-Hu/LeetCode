3fb9f3f5c70915b501d2f8c54b4e1df633a6de6f
Woody-Hu <812791840@qq.com>
2018-06-04 08:06:35 +0800

    2018 06 04

---
+++ LeetCode/LeetCode/LongestHarmoniousSubsequence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class LongestHarmoniousSubsequence
+    {
+        public int FindLHS(int[] nums)
+        {
+
+
+            var groupedValue = nums.GroupBy(k => k).ToDictionary(k=>k.Key,k=>k.Count());
+
+            int returnValue = int.MinValue;
+
+            foreach (var OneKVP in groupedValue)
+            {
+                if (groupedValue.ContainsKey(OneKVP.Key + 1))
+                {
+                    returnValue = Math.Max(OneKVP.Value + groupedValue[OneKVP.Key + 1], returnValue);
+                }
+            }
+
+
+            returnValue = returnValue == int.MinValue ? 0 : returnValue;
+
+
+            return returnValue;
+        }
+    }
+}
