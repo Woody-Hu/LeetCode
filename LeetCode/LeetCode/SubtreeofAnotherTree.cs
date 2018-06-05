07706bb4c3c46eebf0312364ee44e4dfbf246fbc
Woody-Hu <812791840@qq.com>
2018-06-05 08:16:08 +0800

    2018 06 05

---
+++ LeetCode/LeetCode/SubtreeofAnotherTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class SubtreeofAnotherTree
+    {
+        public bool IsSubtree(TreeNode s, TreeNode t)
+        {
+            if (null == s && null == t)
+            {
+                return true;
+            }
+            else if (null == s && null != t)
+            {
+                return false;
+            }
+            return InnerMethod(s, t) || IsSubtree(s.left, t) || IsSubtree(s.right, t);
+        }
+
+        private bool InnerMethod(TreeNode s, TreeNode t)
+        {
+            if (null != s && null != t)
+            {
+                return s.val == t.val && InnerMethod(s.left, t.left) && InnerMethod(s.right, t.right);
+            }
+            else
+            {
+                return null == s && null == t;
+            }
+        }
+    }
+}
