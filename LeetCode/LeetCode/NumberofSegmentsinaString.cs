234272db675876683ef1e7e0cd5cdd5043dfd9a3
Woody-Hu <812791840@qq.com>
2018-06-11 08:06:09 +0800

    2018 06 11

---
+++ LeetCode/LeetCode/NumberofSegmentsinaString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class NumberofSegmentsinaString
+    {
+        public int CountSegments(string s)
+        {
+
+            var tempSplitevalue = s.Split(' ');
+
+            var returnValue = from n in tempSplitevalue where !string.IsNullOrWhiteSpace(n) select n;
+
+            return returnValue.Count();
+        }
+    }
+}
