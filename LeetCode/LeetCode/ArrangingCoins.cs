3c313bd82f6521fdb2b799aae2a23d2173db4076
Woody-Hu <812791840@qq.com>
2018-06-12 08:12:19 +0800

    2018 06 12

---
+++ LeetCode/LeetCode/ArrangingCoins.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class ArrangingCoins
+    {
+        public int ArrangeCoinsMethod(int n)
+        {
+            return (int)Math.Floor((Math.Sqrt((double)((long) n) * 8 + 1) -1)/2);
+        }
+    }
+}
