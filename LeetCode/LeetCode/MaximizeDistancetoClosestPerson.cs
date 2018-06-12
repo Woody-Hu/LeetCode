255d6ed54661fa890851280bd284594ae49948fd
Woody-Hu <812791840@qq.com>
2018-06-12 15:05:25 +0800

    2018 06 12

---
+++ LeetCode/LeetCode/MaximizeDistancetoClosestPerson.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MaximizeDistancetoClosestPerson
+    {
+        public int MaxDistToClosest(int[] seats)
+        {
+            int returnValue = 0;
+            int lastIndex = -1;
+            int nowIndex = 0;
+
+            for (; nowIndex < seats.Length; nowIndex++)
+            {
+                if (1 == seats[nowIndex])
+                {
+                    returnValue = Math.Max(returnValue,lastIndex ==-1?nowIndex:(nowIndex-lastIndex)/2);
+                    lastIndex = nowIndex;
+                }
+            }
+
+            return Math.Max(returnValue, nowIndex - lastIndex -1);
+
+        }
+    }
+}
