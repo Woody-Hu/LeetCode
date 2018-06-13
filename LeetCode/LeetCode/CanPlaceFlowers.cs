d032baaf7c592d8e1bbc15517d7b6a8dd65dcacf
Woody-Hu <812791840@qq.com>
2018-06-13 08:09:30 +0800

    2018 06 13

---
+++ LeetCode/LeetCode/CanPlaceFlowers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class CanPlaceFlowers
+    {
+        public bool CanPlaceFlowersMethod(int[] flowerbed, int n)
+        {
+            int lastIndex = -2;
+            int returnCount = 0;
+
+            var tempLength = flowerbed.Length;
+
+            for (int i = 0; i < tempLength; i++)
+            {
+                if (1 == flowerbed[i])
+                {
+                    returnCount = returnCount + (i - lastIndex - 2) / 2;
+                    lastIndex = i;
+                }
+            }
+
+            returnCount = returnCount + (tempLength - 1 - lastIndex) / 2;
+
+            return returnCount >= n;
+        }
+    }
+}
