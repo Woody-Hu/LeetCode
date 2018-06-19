972fcc6cd4103c67de7a035f283c0d483642c3cd
Woody-Hu <812791840@qq.com>
2018-06-19 08:10:59 +0800

    2018 06 19

---
+++ LeetCode/LeetCode/StringCompression.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class StringCompression
+    {
+        public int Compress(char[] chars)
+        {
+            int nowCharIndex = 0;
+
+            int nowIndex =0;
+
+            int nowCount = 0;
+
+            int useLength = chars.Length;
+
+            while (nowIndex < useLength)
+            {
+                nowCount = 1;
+
+                var tempChar = chars[nowIndex++];
+
+                while (nowIndex < useLength && tempChar == chars[nowIndex])
+                {
+                    nowCount++;
+                    nowIndex++;
+                }
+
+                if (nowCount > 1)
+                {
+                    chars[nowCharIndex++] = tempChar;
+                    foreach (var oneChar in nowCount.ToString())
+                    {
+                        chars[nowCharIndex++] = oneChar;
+                    }
+                }
+                else
+                {
+                    chars[nowCharIndex++] = tempChar;
+                }
+            }
+
+            return nowCharIndex;
+        }
+    }
+}
