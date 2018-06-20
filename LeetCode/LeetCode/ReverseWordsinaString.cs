535330a03343e1452fc9224de18e09a9c33ef5fe
Woody-Hu <812791840@qq.com>
2018-06-21 07:52:43 +0800

    2018 06 21

---
+++ LeetCode/LeetCode/ReverseWordsinaString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class ReverseWordsinaString
+    {
+        public string ReverseWords(string s)
+        {
+
+            StringBuilder returnValue = new StringBuilder();
+            var spliteValue = s.Split(' ');
+
+            for (int i = spliteValue.Length - 1; i >=0; i--)
+            {
+                if (string.IsNullOrWhiteSpace(spliteValue[i]))
+                {
+                    continue;
+                }
+                returnValue.Append(spliteValue[i].Trim());
+
+                if (i!=0)
+                {
+                    returnValue.Append(" ");
+                }
+            }
+
+            return returnValue.ToString().Trim();
+        }
+    }
+}
