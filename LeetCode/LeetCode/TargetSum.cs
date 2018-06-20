bf1dd93ccbcca4f714a3f48589e295b0ed9812dd
Woody-Hu <812791840@qq.com>
2018-06-20 08:16:36 +0800

    2018 06 20

---
+++ LeetCode/LeetCode/TargetSum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class TargetSum
+    {
+        public int FindTargetSumWays(int[] nums, int S)
+        {
+            int returnValue = 0;
+            int useLength = nums.Length;
+            int[] useSumValue = new int[useLength];
+            useSumValue[useLength - 1] = nums[useLength - 1];
+
+            for (int i = useLength - 2; i >=0; i--)
+            {
+                useSumValue[i] = useSumValue[i + 1] + nums[i];
+            }
+
+            InnerMethod(nums,useSumValue,S,0,ref returnValue);
+
+            return returnValue;
+        }
+
+        private void InnerMethod(int[] inputNums,int[] inputSumValue,int wantValue,int useIndex,ref int returnValue)
+        {
+            if (useIndex == inputNums.Length)
+            {
+                if (0 == wantValue)
+                {
+                    returnValue++;
+                 
+                }
+                return;
+            }
+         
+
+            if (inputSumValue[useIndex] < Math.Abs(wantValue))
+            {
+                return;
+            }
+
+            InnerMethod(inputNums, inputSumValue, wantValue + inputNums[useIndex], useIndex + 1, ref returnValue);
+
+            InnerMethod(inputNums, inputSumValue, wantValue - inputNums[useIndex], useIndex + 1, ref returnValue);
+        }
+    }
+}
