5878fb8a7d622f19a5fe7c0ff1d8504ff8da76a7
Woody-Hu <812791840@qq.com>
2018-06-22 08:13:17 +0800

    2018 06 22

---
+++ LeetCode/LeetCode/RectangleOverlap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RectangleOverlap
+    {
+        public bool IsRectangleOverlap(int[] rec1, int[] rec2)
+        {
+            if (rec1[0] < rec2[2] && rec1[1] < rec2[3] && rec1[2] > rec2[0] && rec1[3] > rec2[1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+       
+    }
+}
