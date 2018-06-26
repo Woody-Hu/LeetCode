1113ff56a5e8c9bd06b20d2392a69c90440c9e1f
Woody-Hu <812791840@qq.com>
2018-06-26 08:14:35 +0800

    2018 06 26

---
+++ LeetCode/LeetCode/FindModeinBinarySearchTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class FindModeinBinarySearchTree
+    {
+        public int[] FindMode(TreeNode root)
+        {
+            List<int> returnValue = new List<int>();
+            int tempMaxCount =1;
+            int tempNowCount = 1;
+            int? templastValue = null;
+
+            InnerMethod(root, ref tempMaxCount, ref tempNowCount, ref templastValue, ref returnValue);
+
+
+            return returnValue.ToArray();
+        }
+
+        private void InnerMethod(TreeNode inputNode,ref int nowmaxCount,ref int nowCount,ref int? lastValue,ref List<int> returnValue )
+        {
+            if (null == inputNode)
+            {
+                return;
+            }
+
+            InnerMethod(inputNode.left,ref nowmaxCount,ref nowCount,ref lastValue,ref returnValue);
+
+            if (null != lastValue && lastValue.Value == inputNode.val)
+            {
+                nowCount++;
+            }
+            else
+            {
+                nowCount = 1;
+            }
+
+            if (nowCount == nowmaxCount)
+            {
+                returnValue.Add(inputNode.val);
+            }
+            else if (nowCount > nowmaxCount)
+            {
+                nowmaxCount = nowCount;
+                returnValue = new List<int>();
+                returnValue.Add(inputNode.val);
+            }
+
+            lastValue = inputNode.val;
+
+            InnerMethod(inputNode.right, ref nowmaxCount, ref nowCount, ref lastValue, ref returnValue);
+
+        }
+
+    }
+}
