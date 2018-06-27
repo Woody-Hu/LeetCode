1c2749624c3a48f93a08015bd4dc3d4ed41ab58c
Woody-Hu <812791840@qq.com>
2018-06-27 08:25:42 +0800

    2018 06 27

---
+++ LeetCode/LeetCode/PerfectNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class PerfectNumber
+    {
+        public bool CheckPerfectNumber(int num)
+        {
+            if (1 == num)
+            {
+                return false;
+            }
+
+            int tempSum=0;
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    tempSum += i + num / i;
+                }
+            }
+            tempSum++;
+
+            return num == tempSum;
+        }
+    }
+}
