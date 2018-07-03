8a35709ec8a27c914087501d95377c2520e9c3b0
Woody-Hu <812791840@qq.com>
2018-07-03 08:12:41 +0800

    2018 07 03

---
+++ LeetCode/LeetCode/SumofSquareNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class SumofSquareNumbers
+    {
+        public bool JudgeSquareSum(int c)
+        {
+            if (0 == c)
+            {
+                return true;
+            }
+
+            var tempValue = 1;
+
+            while (c - (tempValue * tempValue) >= 0)
+            {
+                var tempSubValue = c - (tempValue * tempValue);
+
+                if (Math.Sqrt(tempSubValue) % 1 == 0)
+                {
+                    return true;
+                }
+                tempValue++;
+            }
+
+            return false;
+
+        }
+    }
+}
