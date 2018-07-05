3099e028ad8692a18898fd9e2d018f784aabf5ff
Woody-Hu <812791840@qq.com>
2018-07-05 08:13:36 +0800

    2018 07 05

---
+++ LeetCode/LeetCode/UniqueMorseCodeWords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class UniqueMorseCodeWords
+    {
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            string[] useValue = new string[] { ".-", "-...", "-.-.", "-..", ".",
+                "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--",
+                "-..-", "-.--", "--.." };
+
+            HashSet<string> returnValue = new HashSet<string>();
+
+            foreach (var oneWords in words)
+            {
+                StringBuilder tempStringBuilder = new StringBuilder();
+
+                foreach (var oneChar in oneWords)
+                {
+                    tempStringBuilder.Append(useValue[oneChar - 'a']);
+                }
+                returnValue.Add(tempStringBuilder.ToString());
+            }
+
+            return returnValue.Count;
+        }
+    }
+}
