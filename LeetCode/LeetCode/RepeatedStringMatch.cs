62b763912dc734fbf469a0a025ac55d2eee49b04
Woody-Hu <812791840@qq.com>
2018-07-06 08:22:51 +0800

    2018 07 06

---
+++ LeetCode/LeetCode/RepeatedStringMatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class RepeatedStringMatch
+    {
+        public int RepeatedStringMatchMethod(string A, string B)
+        {
+
+            StringBuilder tempValue = new StringBuilder();
+
+            
+
+            for (int i = 1; i <= (B.Length /A.Length + 2); i++)
+            {
+                tempValue.Append(A);
+
+                if (tempValue.Length < B.Length)
+                {
+                    continue;
+                }
+
+                if (tempValue.ToString().Contains(B))
+                {
+                    return i;
+                }
+
+            }
+
+            return -1;
+        }
+    }
+}
