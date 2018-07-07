ef71095881d77ebf813435a322546b3dbee981fe
Woody-Hu <812791840@qq.com>
2018-07-07 08:58:32 +0800

    2018 07 07

---
+++ LeetCode/LeetCode/BuddyStrings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class BuddyStrings
+    {
+        public bool BuddyStringsMethod(string A, string B)
+        {
+            if (A.Length != B.Length)
+            {
+                return false;
+            }
+
+            if (A.Equals(B))
+            {
+                return A.ToHashSet().Count() < A.Length;
+            }
+
+            List<int> lstDif = new List<int>();
+
+           for(int i =0; i<A.Length;i++)
+            {
+                if (A[i] != B[i])
+                {
+                    lstDif.Add(i);
+                }
+            }
+
+            return lstDif.Count == 2 && A[lstDif[0]] == B[lstDif[1]] && B[lstDif[0]] == A[lstDif[1]];
+
+   
+
+        }
+    }
+}
