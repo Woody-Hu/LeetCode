e2683d298b9e29c679b2456338476163195f4037
Woody-Hu <812791840@qq.com>
2018-07-10 07:52:53 +0800

    2018 07 10

---
+++ LeetCode/LeetCode/TransposeMatrix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class TransposeMatrix
+    {
+        public int[][] Transpose(int[][] A)
+        {
+            int[][] returnValue = new int[A[0].Length][];
+
+            for (int i = 0; i < A[0].Length; i++)
+            {
+                returnValue[i] = new int[A.Length];
+
+                for (int j = 0; j < returnValue[i].Length; j++)
+                {
+                    returnValue[i][j] = A[j][i];
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
