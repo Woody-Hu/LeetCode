e547d109be222fb0b96f9d6894fdb5257a4aff4d
Woody-Hu <812791840@qq.com>
2018-07-09 08:16:51 +0800

    2018 07 09

---
+++ LeetCode/LeetCode/NondecreasingArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class NondecreasingArray
+    {
+        public bool CheckPossibility(int[] nums)
+        {
+            var tempCount = 0;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] > nums[i + 1])
+                {
+                    tempCount++;
+                    if (i > 0 && nums[i + 1] < nums[i - 1]) nums[i + 1] = nums[i];
+                    else nums[i] = nums[i + 1];
+                }
+            }
+
+            return tempCount<=1;
+        }
+    }
+}
