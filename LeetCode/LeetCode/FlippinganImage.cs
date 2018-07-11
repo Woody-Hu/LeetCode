a152a189670d82aeafca285d29cd6d19537f1045
Woody-Hu <812791840@qq.com>
2018-07-11 08:01:32 +0800

    2018 07 11

---
+++ LeetCode/LeetCode/FlippinganImage.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class FlippinganImage
+    {
+        public int[][] FlipAndInvertImage(int[][] A)
+        {
+            int[][] returnValue = new int[A.Length][];
+
+            for (int i = 0; i < returnValue.Length; i++)
+            {
+                var tempLength = A[i].Length;
+                returnValue[i] = new int[tempLength];
+
+                for (int j = 0; j < tempLength; j++)
+                {
+                    returnValue[i][j] = Math.Abs(A[i][tempLength - 1 - j] - 1);
+                }   
+            }
+
+            return returnValue;
+        }
+    }
+}
