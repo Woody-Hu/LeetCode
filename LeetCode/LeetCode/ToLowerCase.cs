e975ef042e1f417fb5f69d5913d23ba5d0ab2cd9
Woody-Hu <812791840@qq.com>
2018-07-13 08:02:48 +0800

    2018 07 13

---
+++ LeetCode/LeetCode/ToLowerCase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class ToLowerCase
+    {
+        public string ToLowerCaseMethod(string str)
+        {
+            StringBuilder returnValue = new StringBuilder();
+
+            foreach (var oneValue in str)
+            {
+                if (char.IsLetter(oneValue) && char.IsUpper(oneValue))
+                {
+                    returnValue.Append((char)(oneValue + 32));
+                }
+                else
+                {
+                    returnValue.Append(oneValue);
+                }
+            }
+
+            return returnValue.ToString();
+
+        }
+    }
+}
