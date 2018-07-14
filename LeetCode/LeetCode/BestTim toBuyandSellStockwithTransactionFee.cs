72cef3b71ef0f81332244cd450e72204293a6092
Woody-Hu <812791840@qq.com>
2018-07-14 10:27:16 +0800

    2018 07 14

---
+++ LeetCode/LeetCode/BestTim toBuyandSellStockwithTransactionFee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class BestTim_toBuyandSellStockwithTransactionFee
+    {
+        public int MaxProfit(int[] prices, int fee)
+        {
+
+            int buy, tempBuy, sell, tempSell;
+
+            buy = tempBuy = -prices[0];
+
+            sell = tempSell = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                tempBuy = buy;
+
+                buy = Math.Max(tempBuy, sell - prices[i]);
+
+                sell = Math.Max(tempSell, tempBuy + prices[i] - fee);
+                tempSell = sell;
+            }
+
+            return sell;
+        }
+    }
+}
