5a13ff3b81a46aa331ee2eafa55d9e07b4aab909
Woody-Hu <812791840@qq.com>
2018-07-16 08:20:37 +0800

    2018 07 16

---
+++ LeetCode/LeetCode/LargestPalindromeProduct.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class LargestPalindromeProduct
+    {
+        public int LargestPalindrome(int n)
+        {
+            int[] ans = { 9, 987, 123, 597, 677, 1218, 877, 475 }; return ans[n - 1];
+        }
+    }
+}
