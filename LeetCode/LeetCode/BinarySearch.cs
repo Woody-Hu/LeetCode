08106bab07963c12d1a9e9e7a1b6908e46836c4d
Woody-Hu <812791840@qq.com>
2018-07-17 08:19:37 +0800

    2018 07 17

---
+++ LeetCode/LeetCode/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class BinarySearch
+    {
+        public int Search(int[] nums, int target)
+        {
+            int tempStartIndex = 0;
+
+            int tempEndIndex = nums.Length - 1;
+
+            while (tempStartIndex <= tempEndIndex)
+            {
+                var tempMidIndex = (tempStartIndex + tempEndIndex) / 2;
+
+                if (nums[tempMidIndex] < target )
+                {
+                    tempStartIndex = tempMidIndex + 1;
+                }
+                else if (nums[tempMidIndex] > target)
+                {
+                    tempEndIndex = tempMidIndex-1;
+                }
+                else
+                {
+                    return tempMidIndex;
+                }
+
+                
+            }
+            return -1;
+        }
+    }
+}
