8e6e8c2e37f0806ef73e879b7aaf75c77b267f9f
Woody-Hu <812791840@qq.com>
2018-07-19 08:18:22 +0800

    2018 07 19

---
+++ LeetCode/LeetCode/BinaryGap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class BinaryGap
+    {
+        public int BinaryGapMethod(int N)
+        {
+            int tempCount = 0;
+
+            int nowMaxCount = 0;
+
+            bool ifVisitedFirst = true;
+
+            while (0 != N)
+            {
+                var tempValue = N % 2;
+
+                if (1 == tempValue)
+                {
+                    if (ifVisitedFirst)
+                    {
+                        ifVisitedFirst = false;
+                    }
+                    else
+                    {
+                        nowMaxCount = Math.Max(nowMaxCount, tempCount + 1);
+                    }
+
+                    tempCount = 0;
+                }
+                else if (!ifVisitedFirst)
+                {
+                    tempCount++;
+                }
+
+                N = N / 2;
+            }
+
+            return nowMaxCount;
+
+        }
+    }
+}
---
+++ LeetCode/LeetCode/ShortestUnsortedContinuousSubarray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class ShortestUnsortedContinuousSubarray
+    {
+        public int FindUnsortedSubarray(int[] nums)
+        {
+            int tempRight = 0;
+
+            int tempValue = nums[0];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i]<tempValue)
+                {
+                    tempRight = i;
+                }
+                else
+                {
+                    tempValue = nums[i];
+                }
+            }
+
+            int tempLeft = nums.Length - 1;
+            tempValue = nums[nums.Length - 1];
+
+            for (int i = nums.Length - 1; i >=0; i--)
+            {
+                if (nums[i]>tempValue)
+                {
+                    tempLeft = i;
+                }
+                else
+                {
+                    tempValue = nums[i];
+                }
+            }
+
+            return tempRight - tempLeft <= 0 ? 0 : tempRight - tempLeft + 1;
+
+        }
+    }
+}
