cb1287fd0b413e1a050d80471d80d738aaa2be02
Woody-Hu <812791840@qq.com>
2018-07-20 07:51:37 +0800

    2018 07 20

---
+++ LeetCode/LeetCode/N-aryTreeLevelOrderTraversal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class N_aryTreeLevelOrderTraversal
+    {
+        public IList<IList<int>> LevelOrder(Node root)
+        {
+            IList<IList<int>> returnValue = new List<IList<int>>();
+
+            InnerMethod(root, returnValue, 0);
+
+            return returnValue;
+        }
+
+        private void InnerMethod(Node inputNode, IList<IList<int>> inputLst,int useIndex)
+        {
+            if (null == inputNode)
+            {
+                return;
+            }
+
+            if (useIndex == inputLst.Count)
+            {
+                inputLst.Add(new List<int>());
+            }
+
+            var useLst = inputLst[useIndex];
+
+            useLst.Add(inputNode.val);
+
+            if (null == inputNode.children)
+            {
+                return;
+            }
+
+            foreach (var item in inputNode.children)
+            {
+                InnerMethod(item, inputLst, useIndex + 1);
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}
