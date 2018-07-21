2ce3ac17a8631f22c95ff676e5a5d79073f7211a
Woody-Hu <812791840@qq.com>
2018-07-21 08:43:30 +0800

    2018 07 21

---
+++ LeetCode/LeetCode/RangeAdditionII.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class RangeAdditionII
+    {
+        public int MaxCount(int m, int n, int[,] ops)
+        {
+            int useRowLength = m;
+            int useColumnLength = n;
+
+            for (int i = 0; i < ops.GetLength(0); i++)
+            {
+                useRowLength = Math.Min(useRowLength, ops[i, 0]);
+                useColumnLength = Math.Min(useColumnLength, ops[i, 1]);
+            }
+
+            return useRowLength * useColumnLength;
+
+        
+
+        }
+    }
+}
