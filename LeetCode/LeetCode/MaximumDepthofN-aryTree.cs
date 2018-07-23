02e3f4b06357c74d1500d55b5af99ac57b6a973f
Woody-Hu <812791840@qq.com>
2018-07-23 09:58:32 +0800

    2018 07 23

---
+++ LeetCode/LeetCode/MaximumDepthofN-aryTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class MaximumDepthofN_aryTree
+    {
+        public int MaxDepth(Node root)
+        {
+            int returnValue = 0;
+
+            InnerMethod(root, ref returnValue, 1);
+
+            return returnValue;
+        }
+
+        private void InnerMethod(Node inputNode,ref int returnValue,int nowDeep)
+        {
+            if (null == inputNode)
+            {
+                return;
+            }
+
+            returnValue = Math.Max(returnValue, nowDeep);
+
+            if (null != inputNode.children)
+            {
+                foreach (var item in inputNode.children)
+                {
+                    InnerMethod(item, ref returnValue, nowDeep + 1);
+                }
+            }
+        }
+    }
+}
