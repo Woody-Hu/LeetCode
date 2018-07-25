d91363f8b11960d7110fa9cf55037c63ac8cef99
Woody-Hu <812791840@qq.com>
2018-07-26 07:43:20 +0800

    2018 07 26

---
+++ LeetCode/LeetCode/N-aryTreePostorderTraversal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class N_aryTreePostorderTraversal
+    {
+        public IList<int> Postorder(Node root)
+        {
+            List<int> returnValue = new List<int>();
+
+            InnerMethod(root, returnValue);
+
+            return returnValue;
+        }
+
+        private void InnerMethod(Node input,List<int> returnValue)
+        {
+            if (null == input)
+            {
+                return;
+            }
+
+            if (null != input.children)
+            {
+                foreach (var item in input.children)
+                {
+                    InnerMethod(item, returnValue);
+                }
+            }
+
+            returnValue.Add(input.val);
+        }
+    }
+}
