d8e9de8a27e630d084c0c179a8ee688441710fe3
Woody-Hu <812791840@qq.com>
2018-07-28 08:45:37 +0800

    2018 07 28

---
+++ LeetCode/LeetCode/ConstructQuadTree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class ConstructQuadTree
+    {
+        public QuadTreeNode Construct(int[][] grid)
+        {
+            return InnerMethod(0,0,grid.GetLength(0)-1,grid[0].GetLength(0)-1,grid);
+        }
+
+        private QuadTreeNode InnerMethod(int inputRowStart,int inputColumnStart,int inputRowEnd,int inputColumnEnd,int[][] grid)
+        {
+            if (inputRowStart > inputRowEnd || inputColumnStart > inputColumnEnd)
+                return null;
+
+            bool ifIsLeaf = true;
+
+            var tempValue = grid[inputRowStart][inputColumnStart];
+
+            for (int i = inputRowStart; i <= inputRowEnd; i++)
+            {
+                for (int j = inputColumnStart; j <= inputColumnEnd; j++)
+                {
+                    if (tempValue != grid[i][j])
+                    {
+                        ifIsLeaf = false;
+                        break;
+                    }
+                }
+            }
+
+            if (ifIsLeaf)
+            {
+                return new QuadTreeNode(tempValue == 1, true, null, null, null, null);
+            }
+
+            int rowMid = (inputRowStart + inputRowEnd) / 2;
+            int columnMid = (inputColumnStart + inputColumnEnd) / 2;
+
+            return new QuadTreeNode(false, false, InnerMethod(inputRowStart, inputColumnStart, rowMid, columnMid, grid),
+                InnerMethod(inputRowStart, columnMid + 1, rowMid, inputColumnEnd, grid),
+                InnerMethod(rowMid + 1, inputColumnStart, inputRowEnd, columnMid, grid),
+                InnerMethod(rowMid + 1, columnMid + 1, inputRowEnd, inputColumnEnd, grid)
+                );
+        }
+    }
+}
---
+++ LeetCode/LeetCode/QuadTreeNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class QuadTreeNode
+    {
+        public bool val;
+        public bool isLeaf;
+        public QuadTreeNode topLeft;
+        public QuadTreeNode topRight;
+        public QuadTreeNode bottomLeft;
+        public QuadTreeNode bottomRight;
+
+        public QuadTreeNode() { }
+        public QuadTreeNode(bool _val, bool _isLeaf, QuadTreeNode _topLeft, QuadTreeNode _topRight, QuadTreeNode _bottomLeft, QuadTreeNode _bottomRight)
+        {
+            val = _val;
+            isLeaf = _isLeaf;
+            topLeft = _topLeft;
+            topRight = _topRight;
+            bottomLeft = _bottomLeft;
+            bottomRight = _bottomRight;
+        }
+    }
+}
