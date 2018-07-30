7fb4855412ac1275e0580453dcbc967f49266ff9
Woody-Hu <812791840@qq.com>
2018-07-30 08:16:21 +0800

    2018 07 30

---
+++ LeetCode/LeetCode/N-aryTreePreorderTraversal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class N_aryTreePreorderTraversal
+    {
+        public IList<int> Preorder(Node root)
+        {
+            List<int> returnValue = new List<int>();
+
+            InnerMethod(root, returnValue);
+
+            return returnValue;
+        }
+
+        private void InnerMethod(Node inputRoot,List<int> returnValue)
+        {
+            if (null == inputRoot)
+            {
+                return;
+            }
+
+            returnValue.Add(inputRoot.val);
+
+            if (null != inputRoot.children)
+            {
+                foreach (var oneItem in inputRoot.children)
+                {
+                    InnerMethod(oneItem, returnValue);
+                }
+            }
+        }
+    }
+}
