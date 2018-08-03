083df4d31c9d55e162c7faa52a8e1fe898059fcc
Woody-Hu <812791840@qq.com>
2018-08-03 08:39:49 +0800

    2018 08 03

---
+++ LeetCode/LeetCode/SearchinaBinarySearchTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class SearchinaBinarySearchTree
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (null == root)
+            {
+                return null;
+            }
+
+            if (val == root.val)
+            {
+                return root;
+            }
+            else if (val < root.val)
+            {
+                return SearchBST(root.left, val);
+            }
+            else
+            {
+                return SearchBST(root.right, val);
+            }
+        }
+    }
+}
