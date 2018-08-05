de6dfae6f3dde528428d231c7e5ee3c53e835e9e
Woody-Hu <812791840@qq.com>
2018-08-06 07:59:14 +0800

    2018 08 06

---
+++ LeetCode/LeetCode/PerfectSquares.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class PerfectSquares
+    {
+        public int NumSquares(int n)
+        {
+            int[] returnValue = new int[n + 1];
+
+            returnValue[1] = 1;
+
+            for (int i = 2; i < n + 1; i++)
+            {
+                int tempMinValue = int.MaxValue;
+
+                int j = 1;
+
+                while (j*j <= i)
+                {
+                    tempMinValue = Math.Min(tempMinValue, returnValue[i - j * j]);
+                    j++;
+                }
+
+                returnValue[i] = tempMinValue + 1;
+            }
+
+            return returnValue[n];
+        }
+    }
+}
