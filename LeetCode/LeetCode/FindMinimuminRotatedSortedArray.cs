5211929ce4a9f42fbe0588e3951b929f034e5a00
Woody-Hu <812791840@qq.com>
2018-08-08 07:54:48 +0800

    2018 08 08

---
+++ LeetCode/LeetCode/FindMinimuminRotatedSortedArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class FindMinimuminRotatedSortedArray
+    {
+        public int FindMin(int[] nums)
+        {
+            Array.Sort(nums);
+
+            return nums[0];
+        }
+    }
+}
