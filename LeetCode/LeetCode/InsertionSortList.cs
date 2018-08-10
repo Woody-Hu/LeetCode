c48c4e54c87cbe29b47fab36f4725565b0b46e40
Woody-Hu <812791840@qq.com>
2018-08-10 08:03:45 +0800

    2018 08 10

---
+++ LeetCode/LeetCode/InsertionSortList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class InsertionSortList
+    {
+        public ListNode InsertionSortListMethod(ListNode head)
+        {
+            ListNode returnValue = new ListNode(0);
+
+            ListNode tempNode = head;
+
+            while (null != tempNode)
+            {
+                ListNode iter = returnValue;
+                while (iter.next != null && iter.next.val < tempNode.val)
+                {
+                    iter = iter.next;
+                }
+                ListNode next = tempNode.next;
+                tempNode.next = iter.next;
+                iter.next = tempNode;
+                tempNode = next;
+            }
+
+            return returnValue.next;
+
+
+
+        }
+    }
+}
