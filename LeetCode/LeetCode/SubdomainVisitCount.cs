9c8ab94844ca433381f2456f20ce447ea33808c3
Woody-Hu <812791840@qq.com>
2018-08-14 08:35:23 +0800

    2018 08 14

---
+++ LeetCode/LeetCode/SubdomainVisitCount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class SubdomainVisitCount
+    {
+        public IList<string> SubdomainVisits(string[] cpdomains)
+        {
+            Dictionary<string, int> useDic = new Dictionary<string, int>();
+
+            foreach (var oneStr in cpdomains)
+            {
+                var tempSplitValue = oneStr.Split(' ');
+                var useCount = int.Parse(tempSplitValue[0]);
+
+                var allValue = tempSplitValue[1].Split('.');
+
+                var tempStr = string.Empty;
+
+                for (int i = allValue.Length - 1; i >= 0; i--)
+                {
+                    tempStr = allValue[i] + (string.IsNullOrWhiteSpace(tempStr)? tempStr : "." + tempStr);
+
+                    useDic[tempStr] = useDic.GetValueOrDefault(tempStr) + useCount;
+                }
+            }
+
+            List<string> returnValue = new List<string>();
+
+            foreach (var oneKVP in useDic)
+            {
+                returnValue.Add(oneKVP.Value +" " + oneKVP.Key);
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UncommonWordsfromTwoSentences.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class UncommonWordsfromTwoSentences
+    {
+        public string[] UncommonFromSentences(string A, string B)
+        {
+            var arrayA = A.Split(' ');
+            var arrayB = B.Split(' ');
+
+            Dictionary<string, int> dicA = new Dictionary<string, int>();
+            Dictionary<string, int> dicB = new Dictionary<string, int>();
+
+            foreach (var oneValue in arrayA)
+            {
+                dicA[oneValue] = dicA.GetValueOrDefault(oneValue) + 1;
+            }
+
+            foreach (var oneValue in arrayB)
+            {
+                dicB[oneValue] = dicB.GetValueOrDefault(oneValue) + 1;
+            }
+
+            HashSet<string> returnValue = new HashSet<string>();
+
+            foreach (var oneKVP in dicA)
+            {
+                if (oneKVP.Value == 1 && !dicB.ContainsKey(oneKVP.Key))
+                {
+                    returnValue.Add(oneKVP.Key);
+                }
+            }
+
+            foreach (var oneKVP in dicB)
+            {
+                if (oneKVP.Value == 1 && !dicA.ContainsKey(oneKVP.Key))
+                {
+                    returnValue.Add(oneKVP.Key);
+                }
+            }
+
+            return returnValue.ToArray();
+        }
+    }
+}
