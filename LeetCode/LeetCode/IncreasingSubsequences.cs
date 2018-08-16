60c5353f4626ec0dc909d4dc3252ae8ae9cdbf0c
Woody-Hu <812791840@qq.com>
2018-08-16 09:09:38 +0800

    2018 08 16

---
+++ LeetCode/LeetCode/IncreasingSubsequences.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class IncreasingSubsequences
+    {
+        public IList<IList<int>> FindSubsequences(int[] nums)
+        {
+
+            List<IList<int>> returnValue = new List<IList<int>>();
+
+            List<int> nowLst = new List<int>();
+
+            InnerMethod(nums, returnValue, 0, nowLst);
+
+
+
+
+            return returnValue;
+        }
+
+        public void InnerMethod(int[] nums, List<IList<int>> list, int start, List<int> curlist)
+        {
+            HashSet<int> used = new HashSet<int>();
+            for (int i = start; i < nums.Length; i++)
+            {
+                if (i != start && used.Contains(nums[i]))
+                    continue;
+                used.Add(nums[i]);
+                if (curlist.Count == 0 || nums[i] >= curlist.Last())
+                {
+                    curlist.Add(nums[i]);
+                    if (curlist.Count >= 2)
+                    {
+                        list.Add(new List<int>(curlist));
+                    }
+                    InnerMethod(nums, list, i + 1, curlist);
+                    curlist.RemoveAt(curlist.Count - 1);
+                }
+            }
+
+
+
+        }
+
+
+
+
+    }
+}
--- LeetCode/TestConsole/Program.cs
+++ LeetCode/TestConsole/Program.cs
@@ -8,8 +8,6 @@ namespace TestConsole
         static void Main(string[] args)
         {
 
-
-
         }
     }
 }
