8f3cc31f79713ad94696a38037d8a054bc9b8ab7
Woody-Hu <812791840@qq.com>
2018-08-18 13:21:36 +0800

    2018 08 18

---
+++ LeetCode/LeetCode/NumberofLinesToWriteString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class NumberofLinesToWriteString
+    {
+        public int[] NumberOfLines(int[] widths, string S)
+        {
+            int nowValue = 0;
+            int nowRowCount = 1;
+
+            foreach (var oneChar in S)
+            {
+                var tempValue = widths[oneChar - 'a'];
+
+                if (nowValue + tempValue > 100)
+                {
+                    nowValue = tempValue;
+                    nowRowCount++;
+                }
+                else
+                {
+                    nowValue = nowValue + tempValue;
+                }
+            }
+
+            return new int[] { nowRowCount, nowValue };
+
+        }
+    }
+}
