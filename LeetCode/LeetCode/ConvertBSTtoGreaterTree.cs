4a9ba809a52b535deda23f1ef33210d74bb933bb
Woody-Hu <812791840@qq.com>
2018-08-22 07:56:39 +0800

    2018 08 22

---
+++ LeetCode/LeetCode/ConvertBSTtoGreaterTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class ConvertBSTtoGreaterTree
+    {
+        
+        public TreeNode ConvertBST(TreeNode root)
+        {
+            int tempSum = 0;
+
+            InnerMethod(root, ref tempSum);
+
+            return root;
+        }
+
+        private void InnerMethod(TreeNode inputNode,ref int nowSum)
+        {
+            if (null == inputNode)
+            {
+                return;
+            }
+
+            InnerMethod(inputNode.right, ref nowSum);
+            inputNode.val += nowSum;
+            nowSum = inputNode.val;
+            InnerMethod(inputNode.left, ref nowSum);
+        }
+    }
+}
