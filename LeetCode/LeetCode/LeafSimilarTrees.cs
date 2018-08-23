430395856f693eba8f9dbff9b1dbad46955efdbb
Woody-Hu <812791840@qq.com>
2018-08-23 08:16:03 +0800

    2018 08 23

---
+++ LeetCode/LeetCode/LeafSimilarTrees.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class LeafSimilarTrees
+    {
+        public bool LeafSimilar(TreeNode root1, TreeNode root2)
+        {
+            List<int> lstValue1 = new List<int>();
+            List<int> lstValue2 = new List<int>();
+
+            InnerMethod(root1, ref lstValue1);
+            InnerMethod(root2, ref lstValue2);
+
+            if (lstValue1.Count != lstValue2.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < lstValue1.Count; i++)
+            {
+                if (lstValue1[i] != lstValue2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void InnerMethod(TreeNode inputRoot,ref List<int> lstValue)
+        {
+            if (null == inputRoot)
+            {
+                return;
+            }
+
+            if (null == inputRoot.left && null == inputRoot.right)
+            {
+                lstValue.Add(inputRoot.val);
+            }
+
+            InnerMethod(inputRoot.left, ref lstValue);
+            InnerMethod(inputRoot.right, ref lstValue);
+        }
+    }
+}
