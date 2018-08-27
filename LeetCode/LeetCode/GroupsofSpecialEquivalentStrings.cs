a2db2b1928f8bb2853da88097fc38e9279ef96bd
Woody-Hu <812791840@qq.com>
2018-08-27 08:21:27 +0800

    2018 08 27

---
+++ LeetCode/LeetCode/GroupsofSpecialEquivalentStrings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class GroupsofSpecialEquivalentStrings
+    {
+        public int NumSpecialEquivGroups(string[] A)
+        {
+            HashSet<string> useSet = new HashSet<string>();
+
+            StringBuilder useOddStringBuilder = new StringBuilder();
+
+            StringBuilder useEvenStringBuilder = new StringBuilder();
+
+            foreach (var oneStr in A)
+            {
+                useOddStringBuilder.Clear();
+                useEvenStringBuilder.Clear();
+                for (int i = 0; i < oneStr.Length; i++)
+                {
+                    if (i%2 != 0)
+                    {
+                        useOddStringBuilder.Append(oneStr[i]);
+                    }
+                    else
+                    {
+                        useEvenStringBuilder.Append(oneStr[i]);
+                    }
+                }
+
+                var useArray1 = useOddStringBuilder.ToString().ToCharArray();
+                var useArray2 = useEvenStringBuilder.ToString().ToCharArray();
+
+                Array.Sort(useArray1);
+                Array.Sort(useArray2);
+
+                useSet.Add(new string(useArray1)+","+ new string(useArray2));
+            }
+
+            return useSet.Count;
+
+        }
+    }
+}
