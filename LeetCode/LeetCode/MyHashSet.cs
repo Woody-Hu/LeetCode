daea7ccd14f4629eac68a8144a3791177666f743
Woody-Hu <812791840@qq.com>
2018-08-31 07:58:21 +0800

    2018 08 31

---
+++ LeetCode/LeetCode/MyHashSet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class MyHashSet
+    {
+        int?[] useArray = new int?[1000000];
+
+        /** Initialize your data structure here. */
+        public MyHashSet()
+        {
+
+        }
+
+        public void Add(int key)
+        {
+            useArray[key] = key;
+        }
+
+        public void Remove(int key)
+        {
+            useArray[key] = null;
+        }
+
+        /** Returns true if this set contains the specified element */
+        public bool Contains(int key)
+        {
+           return useArray[key].HasValue;
+        }
+    }
+}
