cdacaa30d5c5a2b6bb502f4efb63155a7da21ed0
Woody-Hu <812791840@qq.com>
2018-09-01 08:53:51 +0800

    2018 09 01

---
+++ LeetCode/LeetCode/BeautifulArrangementII.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class BeautifulArrangementII
+    {
+        public int[] ConstructArray(int n, int k)
+        {
+            int[] returnValue = new int[n];
+
+            for (int i = 0,start = 1, end = n; start <= end; i++)
+            {
+                returnValue[i] = k > 1 ? (k-- % 2 != 0 ? start++ : end--) : start++;
+            }
+
+            return returnValue;
+        }
+    }
+}
