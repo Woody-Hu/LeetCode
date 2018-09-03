c05c3188f262a9bfa1ecf63b227f735643072dc6
Woody-Hu <812791840@qq.com>
2018-09-03 08:16:14 +0800

    2018 09 03

---
+++ LeetCode/LeetCode/MonotonicArray.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class MonotonicArray
+    {
+        public bool IsMonotonic(int[] A)
+        {
+            if (A.Length < 3)
+            {
+                return true;
+            }
+
+            bool? tag = null;
+
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                if (A[i] == A[i+1])
+                {
+                    continue;
+                }
+                else
+                {
+                    if (A[i] > A[i+1])
+                    {
+                        if (null == tag)
+                        {
+                            tag = true;
+                        }
+                        else if (tag.Value == false)
+                        {
+                            return false;
+                        }
+
+                    }
+                    else
+                    {
+                        if (null == tag)
+                        {
+                            tag = false;
+                        }
+                        else if (tag.Value == true)
+                        {
+                            return false;
+                        }
+
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
