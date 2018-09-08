34da5597db0b1012e9a7af83f1815eaae9dce342
Woody-Hu <812791840@qq.com>
2018-09-08 08:31:52 +0800

    2018 09 08

---
+++ LeetCode/LeetCode/MaxChunksToMakeSorted.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class MaxChunksToMakeSorted
+    {
+        public int MaxChunksToSorted(int[] arr)
+        {
+            if (null == arr || 0 == arr.Length)
+            {
+                return 0;
+            }
+
+            int useChunks = 0;
+            int useMaxValue = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                useMaxValue = Math.Max(useMaxValue, arr[i]);
+
+                if (useMaxValue == i)
+                {
+                    useChunks++;
+                }
+            }
+
+            return useChunks;
+
+
+        }
+    }
+}
