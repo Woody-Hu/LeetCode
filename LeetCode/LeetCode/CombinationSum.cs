118ab9c28fe783bda99ded9a95ed6687bf948698
Woody-Hu <812791840@qq.com>
2018-09-18 08:04:31 +0800

    2018 09 18

---
+++ LeetCode/LeetCode/CombinationSum.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class CombinationSum
+    {
+        public IList<IList<int>> CombinationSumMethod(int[] candidates, int target)
+        {
+            IList<IList<int>> returnValue = new List<IList<int>>();
+
+            InnerMethod(candidates, new List<int>(), target, ref returnValue);
+
+            return returnValue;
+        }
+
+        private void InnerMethod(int[] candidates,List<int> inputValue, int target,ref IList<IList<int>> returnValue)
+        {
+            if (target < 0)
+            {
+                return;
+            }
+            else if (0 == target)
+            {
+                var addedRange = new List<int>();
+                addedRange.AddRange(inputValue);
+                returnValue.Add(addedRange);
+            }
+            else
+            {
+                foreach (var oneValue in candidates)
+                {
+                    if (inputValue.Count > 0)
+                    {
+                        var tempValue = inputValue[inputValue.Count - 1];
+                        if (tempValue > oneValue)
+                        {
+                            continue;
+                        }
+                    }
+
+                    inputValue.Add(oneValue);
+                    InnerMethod(candidates, inputValue, target - oneValue, ref returnValue);
+                    inputValue.RemoveAt(inputValue.Count - 1);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ LeetCode/LeetCode/SortArrayByParity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class SortArrayByParity
+    {
+        public int[] SortArrayByParityMethod(int[] A)
+        {
+            int[] returnValue = new int[A.Length];
+
+            int useEvenIndex = 0;
+            int useOddIndex = A.Length - 1;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] % 2 == 0)
+                {
+                    returnValue[useEvenIndex++] = A[i];
+                }
+                else
+                {
+                    returnValue[useOddIndex--] = A[i];
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
