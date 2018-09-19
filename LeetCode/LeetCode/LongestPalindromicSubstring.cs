673da92877c29519badf53109ec80cac276755ac
Woody-Hu <812791840@qq.com>
2018-09-19 08:18:27 +0800

    2018 09 19

---
+++ LeetCode/LeetCode/LongestPalindromicSubstring.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class LongestPalindromicSubstring
+    {
+        public string LongestPalindrome(string s)
+        {
+            int length = s.Length;
+
+            if (length < 2)
+            {
+                return s;
+            }
+
+            int useStartIndex = 0;
+            int useLength = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                InnerMethod(s, i, i, ref useStartIndex, ref useLength);
+                InnerMethod(s, i, i+1, ref useStartIndex, ref useLength);
+            }
+
+            return s.Substring(useStartIndex, useLength);
+        }
+
+        private void InnerMethod(string s,int startIndex,int endIndex,ref int useStartIndex,ref int useLength)
+        {
+            while (startIndex >=0 && endIndex < s.Length && s[startIndex] == s[endIndex])
+            {
+                startIndex--;
+                endIndex++;
+            }
+
+            if (endIndex - startIndex - 1 > useLength)
+            {
+                useStartIndex = startIndex+1;
+                useLength = endIndex - startIndex - 1;
+            }
+        }
+    }
+}
--- LeetCode/TestConsole/Program.cs
+++ LeetCode/TestConsole/Program.cs
@@ -8,7 +8,7 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-
+    
         }
     }
 }
