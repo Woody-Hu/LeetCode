e6090d346d485c833134f685ebf174c8e23748e5
Woody-Hu <812791840@qq.com>
2018-09-20 08:20:33 +0800

    2018 09 20

---
+++ LeetCode/LeetCode/3SumClosest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class _3SumClosest
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            int returnValue = nums[0] + nums[1] + nums[nums.Length - 1];
+
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int tempIndexStart = i + 1;
+                int tempIndexEnd = nums.Length - 1;
+
+                while (tempIndexStart < tempIndexEnd)
+                {
+                    int tempValue = nums[i] + nums[tempIndexStart] + nums[tempIndexEnd];
+
+                    if (tempValue < target)
+                    {
+                        tempIndexStart++;
+                    }
+                    else 
+                    {
+                        tempIndexEnd--;
+                    }
+
+                    returnValue = Math.Abs(tempValue - target) < Math.Abs(returnValue - target) ? tempValue : returnValue;
+                }
+            }
+
+            return returnValue;
+
+        }
+    }
+}
