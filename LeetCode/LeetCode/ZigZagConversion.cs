892d5dae95df7e6a1eb73839a7dbabcb6f0ef5c0
Woody-Hu <812791840@qq.com>
2018-09-21 08:27:18 +0800

    2018 09 21

---
+++ LeetCode/LeetCode/ZigZagConversion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class ZigZagConversion
+    {
+        public string Convert(string s, int numRows)
+        {
+            StringBuilder[] useStringBuilderArray = new StringBuilder[numRows];
+
+            for (int i = 0; i < numRows; i++)
+            {
+                useStringBuilderArray[i] = new StringBuilder();
+            }
+
+            int tempCharIndex = 0;
+
+            var tempLength = s.Length;
+
+            while (tempCharIndex < tempLength)
+            {
+                for (int i = 0; i < numRows && tempCharIndex < tempLength; i++)
+                {
+                    useStringBuilderArray[i].Append(s[tempCharIndex++]);
+                }
+
+                for (int i = numRows - 2; i > 0 && tempCharIndex < tempLength; i--)
+                {
+                    useStringBuilderArray[i].Append(s[tempCharIndex++]);
+                }
+            }
+
+            StringBuilder returnValue = new StringBuilder();
+
+            foreach (var oneValue in useStringBuilderArray)
+            {
+                returnValue.Append(oneValue.ToString());
+            }
+
+            return returnValue.ToString();
+        }
+    }
+}
