8f67a2ada43ef64224233db370e1fc66aee548d2
Woody-Hu <812791840@qq.com>
2018-09-25 07:56:13 +0800

    2018 09 25

---
+++ LeetCode/LeetCode/SmallestRangeI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class SmallestRangeI
+    {
+        public int SmallestRangeIMehtod(int[] A, int K)
+        {
+            Array.Sort(A);
+
+            var tempMinValue = A[0];
+            var tempMaxValue = A[A.Length - 1];
+
+            if (tempMaxValue-tempMinValue <= 2*K)
+            {
+                return 0;
+            }
+
+            return tempMaxValue - tempMinValue - 2 * K;
+
+        }
+    }
+}
