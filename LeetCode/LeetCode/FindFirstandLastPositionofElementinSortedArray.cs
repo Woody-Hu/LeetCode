608abe236b50e0196fc17874b8990c6f41e42af5
Woody-Hu <812791840@qq.com>
2018-09-29 08:33:53 +0800

    2018 09 29

---
+++ LeetCode/LeetCode/FindFirstandLastPositionofElementinSortedArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class FindFirstandLastPositionofElementinSortedArray
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            if (null == nums||nums.Length == 0)
+            {
+                return new int[] { -1, -1 };
+            }
+
+            var tempStartIndex = InnerMethod(nums, 0, nums.Length - 1, target);
+
+            if (tempStartIndex == nums.Length || nums[tempStartIndex] != target)
+            {
+                return new int[] { -1, -1 };
+            }
+
+            var tempEndIndex = InnerMethod(nums, 0, nums.Length - 1, target+1);
+
+            return new int[] { tempStartIndex, nums[tempEndIndex] == target ? tempEndIndex : tempEndIndex - 1 };
+        }
+
+        private int InnerMethod(int[] nums,int low,int high,int target)
+        {
+            while (low < high)
+            {
+
+                int mid = low + (high - low) / 2;
+
+                if (nums[mid] < target)
+                {
+                    low = mid + 1;
+
+                }
+                else
+                    high = mid;
+            }
+
+            return high;
+
+        }
+    }
+}
