2b5f459f2e9c957b62560ffbd62307dba8806de1
Woody-Hu <812791840@qq.com>
2018-10-02 07:16:35 +0800

    2018 10 02

---
+++ LeetCode/LeetCode/ContainerWithMostWater.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            int startIndex = 0; int endIndex = height.Length - 1;
+            int returnValue = 0;
+
+            while (startIndex<endIndex)
+            {
+                if (height[startIndex]<height[endIndex])
+                {
+                    var tempValue = (endIndex - startIndex) * height[startIndex];
+                    startIndex++;
+                    returnValue = Math.Max(returnValue, tempValue);
+                }
+                else
+                {
+                    var tempValue = (endIndex - startIndex) * height[endIndex];
+                    endIndex--;
+                    returnValue = Math.Max(returnValue, tempValue);
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
