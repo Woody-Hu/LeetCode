173e995773843cc8255c3805079038826c05c764
Woody-Hu <812791840@qq.com>
2018-10-04 07:15:23 +0800

    2018 10 04

---
+++ LeetCode/LeetCode/GenerateParentheses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class GenerateParentheses
+    {
+        public GenerateParentheses()
+        {
+        }
+
+        public IList<string> GenerateParenthesis(int n)
+        {
+            char[] userArray = new char[2*n];
+
+            List<string> returnValue = new List<string>();
+
+            InnerMethod(0, 0, 0, n, userArray, returnValue);
+
+            return returnValue;
+        }
+
+        private void InnerMethod(int leftCount,int rightCount,int nowIndex,
+                                 int length,char[] useArray,List<string> returnValue){
+            if (rightCount == length)
+            {
+                returnValue.Add(new string(useArray));
+            }
+
+            if (leftCount < length)
+            {
+                useArray[nowIndex] = '(';
+                InnerMethod(leftCount + 1, rightCount, nowIndex + 1, length, useArray, returnValue);
+                    
+            }
+
+            if (rightCount<leftCount)
+            {
+                useArray[nowIndex] = ')';
+                InnerMethod(leftCount, rightCount + 1, nowIndex + 1, length, useArray, returnValue);
+            }
+        }
+    }
+}
