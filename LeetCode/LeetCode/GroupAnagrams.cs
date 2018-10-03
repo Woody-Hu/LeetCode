7e32deb3dcafe6cfab00de0daa56b1dac4b39922
Woody-Hu <812791840@qq.com>
2018-10-03 10:47:52 +0800

    2018 10 03

---
+++ LeetCode/LeetCode/GroupAnagrams.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class GroupAnagrams
+    {
+        public GroupAnagrams()
+        {
+        }
+
+        public IList<IList<string>> GroupAnagramsMethod(string[] strs)
+        {
+            var groupValue = strs.GroupBy((k) => InnerMethod(k));
+
+            List<IList<string>> returnValue = new List<IList<string>>();
+
+            foreach (var oneValue in groupValue)
+            {
+                returnValue.Add(oneValue.ToList());
+            }
+
+            return returnValue;
+        }
+
+        private string InnerMethod(string input)
+        {
+            var useArray = input.ToCharArray();
+
+            Array.Sort(useArray);
+
+            return new string(useArray);
+        }
+
+    }
+}
