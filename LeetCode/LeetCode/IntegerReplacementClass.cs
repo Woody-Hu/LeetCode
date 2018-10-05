11e190ef4fa1fb8a78873fc4df80e984e5dc94c6
Woody-Hu <812791840@qq.com>
2018-10-05 10:30:33 +0800

    2018 10 05

---
+++ LeetCode/LeetCode/IntegerReplacementClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class IntegerReplacementClass
+    {
+        public int IntegerReplacement(int n)
+        {
+            if (n == int.MaxValue)
+            {
+                return 32;
+            }
+
+            if (n == 1)
+            {
+                return 0;
+            }
+
+            if (n%2 == 0)
+            {
+                return IntegerReplacement(n / 2) + 1;
+            }
+
+            var useMin = int.MaxValue;
+
+            useMin = Math.Min(useMin, IntegerReplacement(n + 1) + 1);
+            useMin = Math.Min(useMin, IntegerReplacement(n - 1) + 1);
+
+            return useMin;
+
+        }
+    }
+}
