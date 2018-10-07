979649107d6c7f765c0d6cea3e27ab09dfa19996
Woody-Hu <812791840@qq.com>
2018-10-07 11:15:25 +0800

    2018 10 07

---
+++ LeetCode/LeetCode/ReverseOnlyLetters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class ReverseOnlyLetters
+    {
+        public string ReverseOnlyLettersMethod(string S)
+        {
+            char[] returnArray = new char[S.Length];
+
+            int leftIndex = 0;
+            int rightIndex = S.Length - 1;
+
+            while (leftIndex <= rightIndex)
+            {
+                if (Char.IsLetter(S[leftIndex]) && Char.IsLetter(S[rightIndex]))
+                {
+                    returnArray[leftIndex] = S[rightIndex];
+                    returnArray[rightIndex] = S[leftIndex];
+                    leftIndex++;
+                    rightIndex--;
+                    continue;
+                }
+
+                if (!Char.IsLetter(S[leftIndex]))
+                {
+                    returnArray[leftIndex] = S[leftIndex];
+                    leftIndex++;
+                }
+
+                if (!Char.IsLetter(S[rightIndex]))
+                {
+                    returnArray[rightIndex] = S[rightIndex];
+                    rightIndex--;
+                }
+            }
+
+            return new string(returnArray);
+        }
+    }
+}
