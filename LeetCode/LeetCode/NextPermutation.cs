487a3028c20342b176c6ddad39dc855a9e378cb7
Woody-Hu <812791840@qq.com>
2018-10-09 08:35:07 +0800

    2018 10 09

---
+++ LeetCode/LeetCode/NextPermutation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class NextPermutation
+    {
+        public void NextPermutationMethod(int[] nums)
+        {
+            if (nums.Length <= 1)
+            {
+                return;
+            }
+
+            int i = nums.Length-1;
+
+            for (; i >=1; i--)
+            {
+                if (nums[i]>nums[i-1])
+                {
+                    break;
+                }
+            }
+
+
+            if (i != 0)
+            {
+                InnerSwap(nums, i - 1);
+            }
+
+            InnerArrange(nums, i);
+        }
+
+        private void InnerSwap(int[] nums,int inputIndex)
+        {
+            for (int i = nums.Length-1; i > inputIndex; i--)
+            {
+                if (nums[i]>nums[inputIndex])
+                {
+                    var tempValue = nums[i];
+                    nums[i] = nums[inputIndex];
+                    nums[inputIndex] = tempValue;
+                    break;
+                }
+            }
+        }
+
+        private void InnerArrange(int[] nums,int inputIndex)
+        {
+            int start = inputIndex;
+            int end = nums.Length - 1;
+            while (start<end)
+            {
+                var tempValue = nums[start];
+                nums[start] = nums[end];
+                nums[end] = tempValue;
+
+                start++;
+                end--;
+            }
+        }
+    }
+}
