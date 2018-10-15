833c3e43cfb414c1e04613a438526cf620b4ac85
Woody-Hu <812791840@qq.com>
2018-10-15 08:27:22 +0800

    2018 10 15

---
+++ LeetCode/LeetCode/SortArrayByParityII.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class SortArrayByParityII
+    {
+        public int[] SortArrayByParityIIMethod(int[] A)
+        {
+            int[] returnValue = new int[A.Length];
+
+            int useEvenIndex = 0;
+            int useOddIndex = 1;
+
+            foreach (var oneValue in A)
+            {
+                if (oneValue % 2 == 0)
+                {
+                    returnValue[useEvenIndex] = oneValue;
+                    useEvenIndex = useEvenIndex + 2;
+                }
+                else
+                {
+                    returnValue[useOddIndex] = oneValue;
+                    useOddIndex = useOddIndex + 2;
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
