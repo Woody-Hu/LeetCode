0eac1fc89b0bb18269e0dbadd0b39cbb7aa9462d
Woody-Hu <812791840@qq.com>
2018-10-22 08:52:10 +0800

    2018 10 22

---
+++ LeetCode/LeetCode/LongPressedName.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class LongPressedName
+    {
+        public bool IsLongPressedName(string name, string typed)
+        {
+            var typedLength = typed.Length;
+            var nameLength = name.Length;
+            var nameIndex = 0;
+
+            for (int i = 0; i < typedLength && nameIndex < nameLength; i++)
+            {
+                if (typed[i] == name[nameIndex])
+                {
+                    nameIndex++;
+                }
+                else if (nameIndex > 0 && name[nameIndex -1] == typed[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return nameIndex == name.Length;
+        }
+    }
+}
