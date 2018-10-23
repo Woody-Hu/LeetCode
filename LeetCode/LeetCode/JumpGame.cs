e24bbfa244fb4f7683dc005e364c3a7ac504e22c
Woody-Hu <812791840@qq.com>
2018-10-23 08:26:56 +0800

    2018 10 23

---
+++ LeetCode/LeetCode/JumpGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class JumpGame
+    {
+        public bool CanJump(int[] nums)
+        {
+            int useMax = nums[0];
+            int useLength = nums.Length;
+
+            for (int i = 1; i < useLength; i++)
+            {
+                if (useMax < i)
+                {
+                    return false;
+                }
+
+                useMax = Math.Max(nums[i] + i, useMax);
+            }
+
+            return true;
+        }
+    }
+}
