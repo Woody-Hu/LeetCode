6bf54771349ce71498bfe205c81cf0c0df5c72cf
Woody-Hu <812791840@qq.com>
2018-10-25 08:32:26 +0800

    2018 10 25

---
+++ LeetCode/LeetCode/UniquePaths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class UniquePaths
+    {
+        public int UniquePathsMethod(int m, int n)
+        {
+            int[,] paths = new int[n+1,m+1];
+
+            paths[1, 0] = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    paths[i, j] = paths[i - 1, j] + paths[i, j - 1];
+                }
+            }
+
+            return paths[n, m];
+        }
+    }
+}
