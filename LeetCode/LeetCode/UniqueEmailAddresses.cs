e4f51e203490a6473b2ba447a79be3be1d36ea33
Woody-Hu <812791840@qq.com>
2018-10-29 08:37:43 +0800

    2018 10 29

---
+++ LeetCode/LeetCode/UniqueEmailAddresses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class UniqueEmailAddresses
+    {
+        public int NumUniqueEmails(string[] emails)
+        {
+            HashSet<string> useSet = new HashSet<string>();
+
+            foreach (var oneEmail in emails)
+            {
+                var tempStrArray = oneEmail.Split('@');
+
+                var tempStr1 = tempStrArray[1];
+
+                var tempStr2 = (tempStrArray[0].Split('+')[0]).Replace(".","");
+
+                useSet.Add(tempStr2 + "@" + tempStr1);
+            }
+
+            return useSet.Count;
+        }
+    }
+}
