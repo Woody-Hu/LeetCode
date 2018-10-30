4a971659cdccc7fbab6302501ff353b209ed367c
Woody-Hu <812791840@qq.com>
2018-10-30 08:34:43 +0800

    2018 10 30

---
+++ LeetCode/LeetCode/SimplifyPath.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class SimplifyPath
+    {
+        public string SimplifyPathMethod(string path)
+        {
+            string returnValue = "";
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return path;
+            }
+
+
+            Stack<string> useStack = new Stack<string>();
+
+            var useSplitValues = path.Split('/');
+
+            foreach (var oneString in useSplitValues)
+            {
+                if (oneString.Equals("."))
+                {
+                    continue;
+                }
+
+                if (oneString.Equals(".."))
+                {
+                    if (useStack.Count != 0)
+                    {
+                        useStack.Pop();
+                    }
+                }
+                else if (!oneString.Equals(""))
+                {
+                    useStack.Push(oneString);
+                }
+            }
+
+            while (useStack.Count != 0)
+            {
+                returnValue = "/" + useStack.Pop() + returnValue;
+            }
+
+            return returnValue.Equals("") ? "/" : returnValue;
+        }
+    }
+}
