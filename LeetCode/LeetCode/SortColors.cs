8e194a25103a52fedee0902c0e869df2f4f9d170
Woody-Hu <812791840@qq.com>
2018-10-31 08:56:03 +0800

    2018 10 31

---
+++ LeetCode/LeetCode/SortColors.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class SortColors
+    {
+        public void SortColorsMethod(int[] nums)
+        {
+            int index0 = 0;
+            int index2 = nums.Length - 1;
+
+            int index = 0;
+
+            while (index <= index2)
+            {
+                if (nums[index] == 0)
+                {
+                    nums[index] = nums[index0];
+                    nums[index0] = 0;
+                    index0++;
+                }
+
+                if (nums[index] == 2)
+                {
+                    nums[index] = nums[index2];
+                    nums[index2] = 2;
+                    index2--;
+                    index--;
+                }
+
+                index++;
+            }
+
+
+        }
+    }
+}
