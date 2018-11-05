059824d5cf1380ef5e7931d41771b99c6729374e
Woody-Hu <812791840@qq.com>
2018-11-05 08:44:02 +0800

    2018 11 05

---
+++ LeetCode/LeetCode/NumberofRecentCalls.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class NumberofRecentCalls
+    {
+        private Queue<int> _queue = new Queue<int>();
+
+        public NumberofRecentCalls()
+        {
+
+        }
+
+        public int Ping(int t)
+        {
+            while (_queue.Count != 0 && _queue.Peek() < t-3000)
+            {
+                _queue.Dequeue();
+            }
+
+            _queue.Enqueue(t);
+
+            return _queue.Count;
+        }
+    }
+}
