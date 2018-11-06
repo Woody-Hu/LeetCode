37be2133cbaa3e00f51575a02e5557b1fdf8f706
Woody-Hu <812791840@qq.com>
2018-11-06 08:31:14 +0800

    2018 11 06

---
+++ LeetCode/LeetCode/SearchinRotatedSortedArrayII.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class SearchinRotatedSortedArrayII
+    {
+        public bool Search(int[] nums, int target)
+        {
+            if (null == nums || nums.Length == 0)
+            {
+                return false;
+            }
+
+            return Search(nums, 0, nums.Length - 1, target);
+        }
+
+        private bool Search(int[] nums, int startIndex, int endIndex, int target)
+        {
+            if (startIndex == endIndex)
+            {
+                return nums[startIndex] == target;
+            }
+
+            if ((nums[startIndex] < nums[endIndex]) && (nums[startIndex] > target || nums[endIndex] < target) )
+            {
+                return false;
+            }
+
+            return Search(nums, startIndex, (startIndex + endIndex) / 2, target) ||
+                   Search(nums, (startIndex + endIndex) / 2+1, endIndex, target);
+        }
+    }
+}
