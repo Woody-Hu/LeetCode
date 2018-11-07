9bc18d54fdd4330f7ab91173ec3371ac21da7c86
Woody-Hu <812791840@qq.com>
2018-11-07 08:54:25 +0800

    2018 11 07

---
+++ LeetCode/LeetCode/RemoveDuplicatesfromSortedListII.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class RemoveDuplicatesfromSortedListII
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode useTag = new ListNode(-1);
+
+            useTag.next = head;
+
+            ListNode frontNode = useTag;
+
+            ListNode nowNode = head;
+
+            int? duplicateValue = null;
+
+            while (null != nowNode)
+            {
+                var nextNode = nowNode.next;
+
+                if (null != nextNode && nowNode.val == nextNode.val)
+                {
+                    if (null == duplicateValue)
+                    {
+                        duplicateValue = nowNode.val;
+                    }
+                    nowNode = nextNode;
+                    continue;
+                }
+
+                if (null != duplicateValue)
+                {
+                    duplicateValue = null;
+                    nowNode = nextNode;
+                    frontNode.next = nowNode;
+                    continue;
+                }
+
+                frontNode.next = nowNode;
+
+                frontNode = nowNode;
+
+                nowNode = nowNode.next;
+            }
+
+            return useTag.next;
+
+        }
+    }
+}
