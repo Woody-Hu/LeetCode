a71a530ee92e534fd9c8358b99b8c19de16bb02b
Woody-Hu <812791840@qq.com>
2018-11-09 08:36:29 +0800

    2018 11 09

---
+++ LeetCode/LeetCode/GrayCode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class GrayCode
+    {
+        public IList<int> GrayCodeMethod(int n)
+        {
+            IList<int> returnValue = new List<int>();
+
+            for (int i = 0; i < 1<<n; i++)
+            {
+                returnValue.Add(i^(i>>1));
+            }
+
+            return returnValue;
+        }
+    }
+}
