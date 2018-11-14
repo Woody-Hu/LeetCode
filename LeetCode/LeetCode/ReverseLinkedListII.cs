714e92fa989a15866d2ca1550dbc21d6317b6009
Woody-Hu <812791840@qq.com>
2018-11-14 09:01:27 +0800

    2018 10 14

---
+++ LeetCode/LeetCode/ReverseLinkedListII.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class ReverseLinkedListII
+    {
+        public ListNode ReverseBetween(ListNode head, int m, int n)
+        {
+            ListNode tag = new ListNode(-1);
+            tag.next = head;
+            ListNode[] useArray = new ListNode[n-m+1];
+            ListNode frontNode = null;
+            ListNode currentNode = tag;
+            int index = 0;
+
+            while (null != currentNode)
+            {
+                if (index == m-1)
+                {
+                    frontNode = currentNode;
+                }
+                else if (index >= m && index <= n)
+                {
+                    useArray[index - m] = currentNode;
+                }
+                else if (index > n)
+                {
+                    break;
+                }
+
+                currentNode = currentNode.next;
+                index++;
+            }
+
+            if (null != frontNode)
+            {
+                var rearNode = useArray[n - m].next;
+                currentNode = frontNode;
+
+                for (int i = n - m; i >=0; i--)
+                {
+                    currentNode.next = useArray[i];
+                    currentNode = currentNode.next;
+                }
+
+                currentNode.next = rearNode;
+            }
+
+            return tag.next;
+        }
+    }
+}
