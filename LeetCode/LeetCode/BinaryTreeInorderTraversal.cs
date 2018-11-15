922396f77d3410cc4fe7dc85b43160b3a689dc55
Woody-Hu <812791840@qq.com>
2018-11-15 08:45:33 +0800

    2018 11 15

---
+++ LeetCode/LeetCode/BinaryTreeInorderTraversal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class BinaryTreeInorderTraversal
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            IList<int> returnValue = new List<int>();
+            InorderTraversal(root, returnValue);
+            return returnValue;
+        }
+
+        private void InorderTraversal(TreeNode node, IList<int> visited)
+        {
+            if (null == node)
+            {
+                return;
+            }
+
+            InorderTraversal(node.left,visited);
+            visited.Add(node.val);
+            InorderTraversal(node.right,visited);
+        }
+    }
+}
