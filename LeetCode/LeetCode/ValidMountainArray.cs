e99a5d51e19bd484a4303355ad6dc64c16995808
Woody-Hu <812791840@qq.com>
2018-11-19 08:50:27 +0800

    2018 11 19

---
+++ LeetCode/LeetCode/ValidMountainArray.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class ValidMountainArray
+    {
+        public bool ValidMountainArrayMethod(int[] A)
+        {
+            int? frontValue = null;
+
+            bool front = false;
+            bool back = false;
+
+            foreach (var oneValue in A)
+            {
+                if (null != frontValue)
+                {
+                    if (frontValue.Value < oneValue)
+                    {
+                        if (back)
+                        {
+                            return false;
+                        }
+
+                        front = true;
+                    }
+                    else if (frontValue.Value > oneValue)
+                    {
+                        if (!front)
+                        {
+                            return false;
+                        }
+
+                        back = true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                frontValue = oneValue;
+            }
+
+            return front && back;
+
+        }
+    }
+}
