f3e18f428136f8dfa8a4a01ae79a9bec5e5fbed7
Woody-Hu <812791840@qq.com>
2018-11-22 08:51:48 +0800

    2018 11 22

---
+++ LeetCode/LeetCode/ValidateBinarySearchTree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class ValidateBinarySearchTree
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            return IsValidBST(root, null, null);
+        }
+
+        private bool IsValidBST(TreeNode root, TreeNode lowNode, TreeNode upNode)
+        {
+            if (null == root)
+            {
+                return true;
+            }
+
+            if (null != lowNode && root.val <= lowNode.val)
+            {
+                return false;
+            }
+
+            if (null != upNode && root.val >= upNode.val)
+            {
+                return false;
+            }
+
+            var leftValue = IsValidBST(root.left, lowNode, root);
+            var rightValue = IsValidBST(root.right, root, upNode);
+
+            return leftValue && rightValue;
+        }
+    }
+}
