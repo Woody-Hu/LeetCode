68c13438afa68cefefcaece0ae7709fcb8e9c7df
Woody-Hu <812791840@qq.com>
2018-11-26 09:14:27 +0800

    2018 11 26

---
+++ LeetCode/LeetCode/ConstructBinaryTreefromPreorderandInorderTraversal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class ConstructBinaryTreefromPreorderandInorderTraversal
+    {
+        private int preIndex = 0;
+        private int inIndex = 0;
+
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            return dfs(preorder, inorder, null);
+        }
+
+        public TreeNode dfs(int[] preorder, int[] inorder, TreeNode end)
+        {
+            if (inIndex > inorder.Length - 1 || (end != null && end.val == inorder[inIndex]))
+            {
+                return null;
+            }
+
+            TreeNode root = new TreeNode(preorder[preIndex++]);
+            root.left = dfs(preorder, inorder, root);
+
+            inIndex++;
+            root.right = dfs(preorder, inorder, end);
+
+            return root;
+        }
+    }
+}
