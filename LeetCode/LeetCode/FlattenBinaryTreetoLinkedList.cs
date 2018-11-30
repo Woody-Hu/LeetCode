6da52e9567082de3adf3829d283ac88c0cfea080
Woody-Hu <812791840@qq.com>
2018-11-30 08:51:16 +0800

    2018 11 30

---
+++ LeetCode/LeetCode/FlattenBinaryTreetoLinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class FlattenBinaryTreetoLinkedList
+    {
+        public void Flatten(TreeNode root)
+        {
+            if (null == root)
+            {
+                return;
+            }
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while (stack.Count != 0)
+            {
+                var node = stack.Pop();
+                if (null != node.right)
+                {
+                    stack.Push(node.right);
+                    node.right = null;
+                }
+                if (null != node.left)
+                {
+                    stack.Push(node.left);
+                    node.left = null;
+                }
+
+     
+
+                if (stack.Count != 0)
+                {
+                    node.right = stack.Peek();
+                }
+            }
+        }
+    }
+}
