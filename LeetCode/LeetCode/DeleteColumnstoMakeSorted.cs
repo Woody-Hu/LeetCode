4b3009fe6866416985b6ffc0582d6f99b64a3cd6
Woody-Hu <812791840@qq.com>
2018-12-03 08:23:10 +0800

    2018 12 3

---
+++ LeetCode/LeetCode/DeleteColumnstoMakeSorted.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class DeleteColumnstoMakeSorted
+    {
+        public int MinDeletionSize(string[] A)
+        {
+            if (0 == A.Length || 0 == A[0].Length)
+            {
+                return 0;
+            }
+
+            var arrayLength = A.Length;
+            var stringLength = A[0].Length;
+            var count = 0;
+            for (var i = 0; i < stringLength; i++)
+            {
+                for (var j = 0; j < arrayLength - 1; j++)
+                {
+                    if (A[j][i] > A[j+1][i])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
