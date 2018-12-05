c55479651713b1d415c67331947bfcb9c9f6aafc
Woody-Hu <812791840@qq.com>
2018-12-05 09:13:38 +0800

    2018 12 05

---
+++ LeetCode/LeetCode/Triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Triangle
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            var length = triangle.Count;
+
+            for (var i = length - 2; i >=0; i--)
+            {
+                for (var j = 0; j <= i; j++)
+                {
+                    var perValue = Math.Min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+                    var cureValue = perValue + triangle[i][j];
+                    triangle[i][j] = cureValue;
+                }
+            }
+
+            return triangle[0][0];
+
+        }
+    }
+}
