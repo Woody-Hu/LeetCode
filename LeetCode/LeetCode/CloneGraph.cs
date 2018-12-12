b4c715514be05d57c620524d6837fa491a51c429
Woody-Hu <812791840@qq.com>
2018-12-12 08:48:03 +0800

    2018 12 12

---
+++ LeetCode/LeetCode/CloneGraph.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class CloneGraph
+    {
+        public UndirectedGraphNode CloneGraphMethod(UndirectedGraphNode node)
+        {
+            if (null == node)
+            {
+                return null;
+            }
+            var queue = new Queue<UndirectedGraphNode>();
+            var dic = new Dictionary<UndirectedGraphNode, UndirectedGraphNode>();
+            queue.Enqueue(node);
+            dic.Add(node,new UndirectedGraphNode(node.label));
+
+            while (queue.Count != 0)
+            {
+                var useNode = queue.Dequeue();
+
+                foreach (var oneNode in useNode.neighbors)
+                {
+                    if (!dic.ContainsKey(oneNode))
+                    {
+                        dic.Add(oneNode,new UndirectedGraphNode(oneNode.label));
+                        queue.Enqueue(oneNode);
+                    }
+                    dic[useNode].neighbors.Add(dic[oneNode]);
+                }
+            }
+
+            return dic[node];
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UndirectedGraphNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class UndirectedGraphNode
+    {
+          public int label;
+          public IList<UndirectedGraphNode> neighbors;
+          public UndirectedGraphNode(int x) { label = x; neighbors = new List<UndirectedGraphNode>(); }
+    }
+}
