a2c49196604e8f5bc875d2b5c42021d7130442e8
Woody-Hu <812791840@qq.com>
2018-12-13 08:21:58 +0800

    2018 12 23

---
+++ LeetCode/LeetCode/GasStation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class GasStation
+    {
+        public int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            var length = gas.Length;
+            var sum = 0;
+            var calculatedValue = 0;
+            var useIndex = 0;
+
+            for (var i = 0; i < length; i++)
+            {
+                calculatedValue += gas[i] - cost[i];
+                if (calculatedValue < 0)
+                {
+                    sum += calculatedValue;
+                    calculatedValue = 0;
+                    useIndex = i + 1;
+                }
+            }
+            sum += calculatedValue;
+            return sum < 0 ? -1 : useIndex;
+
+        }
+    }
+}
