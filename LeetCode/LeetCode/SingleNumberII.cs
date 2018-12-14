16202459af16e7a25de587d39425fd790be73f9f
Woody-Hu <812791840@qq.com>
2018-12-14 08:32:10 +0800

    2018 12 14

---
+++ LeetCode/LeetCode/SingleNumberII.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class SingleNumberII
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var firstValue = 0;
+            var secondValue = 0;
+
+            foreach (var oneValue in nums)
+            {
+                firstValue = firstValue ^ oneValue & ~secondValue;
+                secondValue = secondValue ^ oneValue & ~firstValue;
+            }
+
+            return firstValue;
+
+        }
+    }
+}
