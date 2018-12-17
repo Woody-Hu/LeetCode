463d689c6d86d153ed251376eeb70e317b44ab38
Woody-Hu <812791840@qq.com>
2018-12-17 09:18:17 +0800

    2018 12 17

---
+++ LeetCode/LeetCode/CopyListwithRandomPointer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class CopyListwithRandomPointer
+    {
+        public RandomListNode CopyRandomList(RandomListNode head)
+        {
+            if (null == head)
+            {
+                return null;
+            }
+            var dic = new Dictionary<RandomListNode, RandomListNode>();
+            var createdNode = new RandomListNode(head.label);
+            var useQueue = new Queue<RandomListNode>();
+            useQueue.Enqueue(head);
+            dic.Add(head,createdNode);
+
+            while (useQueue.Count != 0)
+            {
+                var node = useQueue.Dequeue();
+                var dicNode = dic[node];
+
+                if (null != node.next)
+                {
+                    useQueue.Enqueue(node.next);
+                    if (!dic.ContainsKey(node.next))
+                    {
+                        dic.Add(node.next,new RandomListNode(node.next.label));
+                    }
+
+                    dicNode.next = dic[node.next];
+                }
+
+                if (null != node.random)
+                {
+                    if (!dic.ContainsKey(node.random))
+                    {
+                        dic.Add(node.random, new RandomListNode(node.random.label));
+                    }
+
+                    dicNode.random = dic[node.random];
+                }
+
+            }
+
+            return dic[head];
+        }
+    }
+}
---
+++ LeetCode/LeetCode/RandomListNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class RandomListNode
+    {
+        public int label;
+        public RandomListNode next, random;
+        public RandomListNode(int x) { this.label = x; }
+    }
+}
