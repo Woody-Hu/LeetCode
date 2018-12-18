b6fa5d94ac12f54e3b153664ec6c60808547e539
Woody-Hu <812791840@qq.com>
2018-12-18 08:42:14 +0800

    2018 12 18

---
+++ LeetCode/LeetCode/WordBreak.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class WordBreak
+    {
+        public bool WordBreakMethod(string s, IList<string> wordDict)
+        {
+            var n = s.Length;
+
+            var dp = new bool[n];
+
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j <= i; j++)
+                {
+                    var sub = s.Substring(j, i + 2 - j);
+
+                    if (wordDict.Contains(sub) && (j == 0 || dp[j - 1]))
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return dp[n - 1];
+        }
+    }
+}
