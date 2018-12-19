48f6d12fff239eb24f7be0622d33ed04d214cc6a
Woody-Hu <812791840@qq.com>
2018-12-19 08:29:12 +0800

    2018 12 19

---
+++ LeetCode/LeetCode/LinkedListCycleII.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class LinkedListCycleII
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            var set = new HashSet<ListNode>();
+
+            var node = head;
+            while (null != node)
+            {
+                if (null != node.next && set.Contains(node.next))
+                {
+                    return node.next;
+                }
+
+                set.Add(node);
+                node = node.next;
+            }
+
+            return null;
+        }
+    }
+}
