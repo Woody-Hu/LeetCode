eb0e47a498d5de304ae253005e4c56fa2c5416bc
Woody-Hu <812791840@qq.com>
2018-12-28 08:26:53 +0800

    2018 12 28

---
+++ LeetCode/LeetCode/Find Peak Element.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Find_Peak_Element
+    {
+        public int FindPeakElement(int[] nums)
+        {
+            var length = nums.Length;
+            if (0 == length || 1 == length)
+            {
+                return 0;
+            }
+
+            var leftIndex = 0;
+            var rightIndex = length - 1;
+
+            while (leftIndex < rightIndex)
+            {
+                var mid = (leftIndex + rightIndex) / 2;
+                if (nums[mid] < nums[mid + 1])
+                {
+                    leftIndex = mid + 1;
+                }
+                else
+                {
+                    rightIndex = mid;
+                }
+            }
+
+            return leftIndex;
+
+        }
+    }
+}
