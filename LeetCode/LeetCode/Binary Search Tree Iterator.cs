de0a2a0cb50910855331c37b85e2586456cb6362
Woody-Hu <812791840@qq.com>
2019-01-03 08:45:32 +0800

    2019 01 03

---
+++ LeetCode/LeetCode/Binary Search Tree Iterator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class BSTIterator
+    {
+        private readonly Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        public BSTIterator(TreeNode root)
+        {
+            SetNext(root);
+        }
+
+        /** @return the next smallest number */
+        public int Next()
+        {
+            var node = stack.Pop();
+            if (null == node)
+            {
+                return -1;
+            }
+
+            SetNext(node.right);
+            return node.val;
+        }
+
+        /** @return whether we have a next smallest number */
+        public bool HasNext()
+        {
+            return stack.Count != 0;
+        }
+
+        private void SetNext(TreeNode node)
+        {
+            while (null != node)
+            {
+                stack.Push(node);
+                node = node.left;
+            }
+        }
+    }
+}
