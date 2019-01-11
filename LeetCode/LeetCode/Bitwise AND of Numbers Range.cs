b401575ac8af1942ecaaeed462ebf4eb5cc49c1d
Woody-Hu <812791840@qq.com>
2019-01-11 08:35:34 +0800

    2018 01 11

---
+++ LeetCode/LeetCode/Bitwise AND of Numbers Range.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Bitwise_AND_of_Numbers_Range
+    {
+        public int RangeBitwiseAnd(int m, int n)
+        {
+            if (m == 0)
+            {
+                return 0;
+            }
+
+            var count = 1;
+
+            while (m != n)
+            {
+                m >>= 1;
+                n >>= 1;
+
+                count <<= 1;
+            }
+
+            return m * count;
+        }
+    }
+}
