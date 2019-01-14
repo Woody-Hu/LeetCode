2d7e6e3eea282b621f42f34c4f9b94c4f06c5184
Woody-Hu <812791840@qq.com>
2019-01-14 08:15:14 +0800

    2019 01 14

---
+++ LeetCode/LeetCode/Fibonacci Number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Fibonacci_Number
+    {
+        public int Fib(int N)
+        {
+            if (N <= 1)
+            {
+                return N;
+            }
+
+            var value1 = 0;
+            var value2 = 1;
+
+            for (int i = 2; i <= N; i++)
+            {
+                var sum = value2 + value1;
+                value1 = value2;
+                value2 = sum;
+            }
+
+            return value2;
+        }
+    }
+}
