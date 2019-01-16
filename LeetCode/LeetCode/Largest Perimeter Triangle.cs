1cdb48472625aec3b20717c21a7974c07db111ad
Stephenhu <stephenhu@seismic.com>
2019-01-16 08:45:29 +0800

    2019 01 16

---
+++ LeetCode/LeetCode/Largest Perimeter Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Largest_Perimeter_Triangle
+    {
+        public int LargestPerimeter(int[] A)
+        {
+            Array.Sort(A);
+
+            for (int i = A.Length - 1; i >1; i--)
+            {
+                if (A[i] < A[i-1] + A[i-2])
+                {
+                    return A[i] + A[i - 1] + A[i - 2];
+                }
+            }
+
+            return 0;
+        }
+    }
+}
