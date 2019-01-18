0b1dc9e545287cfc35046318852058fd185d58a8
Woody-Hu <812791840@qq.com>
2019-01-18 08:26:19 +0800

    2018 01 18

---
+++ LeetCode/LeetCode/Implement Trie (Prefix Tree).cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Trie
+    {
+        private TrieNode root;
+
+        /** Initialize your data structure here. */
+        public Trie()
+        {
+            root = new TrieNode
+            {
+                Val = ' '
+            };
+        }
+
+        /** Inserts a word into the trie. */
+        public void Insert(string word)
+        {
+            TrieNode ws = root;
+            foreach (var c in word)
+            {
+                if (ws.ChildrenNodes[c - 'a'] == null)
+                {
+                    ws.ChildrenNodes[c - 'a'] = new TrieNode(c);
+                }
+                ws = ws.ChildrenNodes[c - 'a'];
+            }
+            ws.IsWord = true;
+        }
+
+        /** Returns if the word is in the trie. */
+        public bool Search(string word)
+        {
+            TrieNode ws = root;
+            foreach (var c in word)
+            {
+                if (ws.ChildrenNodes[c - 'a'] == null) return false;
+                ws = ws.ChildrenNodes[c - 'a'];
+            }
+            return ws.IsWord;
+        }
+
+        /** Returns if there is any word in the trie that starts with the given prefix. */
+        public bool StartsWith(string prefix)
+        {
+            TrieNode ws = root;
+            foreach (var c in prefix)
+            {
+                if (ws.ChildrenNodes[c - 'a'] == null) return false;
+                ws = ws.ChildrenNodes[c - 'a'];
+            }
+            return true;
+        }
+
+        private class TrieNode
+        {
+            public char Val;
+            public bool IsWord;
+            public TrieNode[] ChildrenNodes = new TrieNode[26];
+            public TrieNode() { }
+            public TrieNode(char c)
+            {
+                TrieNode node = new TrieNode();
+                node.Val = c;
+            }
+        }
+    }
+}
