f96f783434771867b4eea562fdc99433be7ad17a
Stephenhu <stephenhu@seismic.com>
2019-01-21 08:30:25 +0800

    2019 01 21

---
+++ LeetCode/LeetCode/Squares of a Sorted Array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Squares_of_a_Sorted_Array
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            var squaredArray = new int[A.Length];
+            for (int i = 0; i < A.Length; i++)
+            {
+                squaredArray[i] = A[i] * A[i];
+            }
+
+            Array.Sort(squaredArray);
+            return squaredArray;
+        }
+    }
+}
