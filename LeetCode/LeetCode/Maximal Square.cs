86f2b97217f795d809998631f71a143ed3eba4f4
Woody-Hu <812791840@qq.com>
2019-01-29 08:29:07 +0800

    2019 01 29

---
+++ LeetCode/LeetCode/Maximal Square.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Maximal_Square
+    {
+        public int MaximalSquare(char[,] matrix)
+        {
+            var rowLength = matrix.GetLength(0);
+            var columnLength = matrix.GetLength(1);
+
+            var dp = new int[rowLength + 1, columnLength + 1];
+            var maxLength = 0;
+
+            for (int i = 1; i <= rowLength; i++)
+            {
+                for (int j = 1; j <= columnLength; j++)
+                {
+                    if (matrix[i-1,j-1] == '1')
+                    {
+                        dp[i, j] = Math.Min(dp[i - 1, j - 1], Math.Min(dp[i - 1, j], dp[i, j - 1])) + 1;
+                        maxLength = Math.Max(maxLength, dp[i, j]);
+                    }
+                }
+            }
+
+            return maxLength * maxLength;
+        }
+    }
+}
