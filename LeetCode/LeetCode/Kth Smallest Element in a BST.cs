413f59b2c8061d2f64c25641b24cee631001ce6b
Woody-Hu <812791840@qq.com>
2019-02-01 08:39:26 +0800

    2019 02 01

---
+++ LeetCode/LeetCode/Kth Smallest Element in a BST.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Kth_Smallest_Element_in_a_BST
+    {
+        public int KthSmallest(TreeNode root, int k)
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            var current = root;
+            int minCount = 0;
+
+            while (stack.Count != 0 || null != current)
+            {
+                if (current != null)
+                {
+                    stack.Push(current);
+                    current = current.left;
+                }
+                else
+                {
+                    var node = stack.Pop();
+                    if (++minCount == k)
+                    {
+                        return node.val;
+                    }
+
+                    current = node.right;
+                }
+            }
+
+            return Int32.MinValue;
+        }
+    }
+}
