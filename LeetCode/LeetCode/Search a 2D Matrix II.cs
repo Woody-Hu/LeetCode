7db9cc3ba8b499fd0870ad5b91fa87e7fceabc0a
Woody-Hu <812791840@qq.com>
2019-02-03 08:18:59 +0800

    2019 02 03

---
+++ LeetCode/LeetCode/Search a 2D Matrix II.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Search_a_2D_Matrix_II
+    {
+        public bool SearchMatrix(int[,] matrix, int target)
+        {
+            var rowCount = matrix.GetLength(0);
+            var columnCount = matrix.GetLength(1);
+            var rowIndex = 0;
+            var columnIndex = columnCount - 1;
+            while (columnIndex >=0 && rowIndex < rowCount)
+            {
+                var value = matrix[rowIndex, columnIndex];
+                if (target == value)
+                {
+                    return true;
+                }
+
+                if (target > value)
+                {
+                    rowIndex++;
+                }
+                else
+                {
+                    columnIndex--;
+                }
+            }
+
+
+            return false;
+        }
+    }
+}
