b382e7da98bc4ae3b7dc6284cdcdb9091c75758c
Woody-Hu <812791840@qq.com>
2019-02-12 08:25:57 +0800

    2019 02 12

---
+++ LeetCode/LeetCode/Add to Array-Form of Integer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Add_to_Array_Form_of_Integer
+    {
+        public IList<int> AddToArrayForm(int[] A, int K)
+        {
+            var list = new List<int>();
+
+            for (int i = A.Length - 1; i >=0 || K > 0; i--)
+            {
+                list.Add((i >= 0 ? A[i] + K : K) % 10);
+                K = (i >= 0 ? A[i] + K : K) / 10;
+            }
+
+            list.Reverse();
+
+            return list;
+        }
+    }
+}
