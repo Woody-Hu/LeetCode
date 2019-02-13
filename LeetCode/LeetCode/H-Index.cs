88954ee3ba1fe9c980e24276d8c5b224e83cedd0
Woody-Hu <812791840@qq.com>
2019-02-13 08:46:50 +0800

    2019 02 13

---
+++ LeetCode/LeetCode/H-Index.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class H_Index
+    {
+        public int HIndex(int[] citations)
+        {
+            var length = citations.Length;
+            var buckets = new int[length + 1];
+            foreach (var value in citations)
+            {
+                if (value> length)
+                {
+                    buckets[length]++;
+                }
+                else
+                {
+                    buckets[value]++;
+                }
+            }
+
+            var sumCount = 0;
+
+            for (int i = length; i >= 0; i--)
+            {
+                sumCount += buckets[i];
+
+                if (sumCount >= i)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
