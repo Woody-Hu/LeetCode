3ea0219b63bd850579025a50be9856648aa8636f
Woody-Hu <812791840@qq.com>
2019-02-14 10:02:36 +0800

    2019 02 14

---
+++ LeetCode/LeetCode/Single Number III.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Single_Number_III
+    {
+        public int[] SingleNumber(int[] nums)
+        {
+            var xorResult = 0;
+            foreach (var value in nums)
+            {
+                xorResult ^= value;
+            }
+
+            xorResult &= -xorResult;
+            var returnValue = new int[2];
+            foreach (var value in nums)
+            {
+                if ((xorResult & value) == 0)
+                {
+                    returnValue[0] ^= value;
+                }
+                else
+                {
+                    returnValue[1] ^= value;
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
