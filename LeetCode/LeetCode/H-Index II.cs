d2da313014c4c22239c542b8dafff5581f00eccf
Woody-Hu <812791840@qq.com>
2019-02-20 08:31:31 +0800

    2019 02 20

---
+++ LeetCode/LeetCode/H-Index II.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class H_Index_II
+    {
+        public int HIndex(int[] citations)
+        {
+            var length = citations.Length;
+            var left = 0;
+            var right = length - 1;
+            var mid = 0;
+
+            while (left <= right)
+            {
+                mid = (left + right) / 2;
+
+                if (citations[mid] == length - mid)
+                {
+                    return citations[mid];
+                }
+                else if (citations[mid] > length - mid)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return length - (right + 1);
+        }
+    }
+}
