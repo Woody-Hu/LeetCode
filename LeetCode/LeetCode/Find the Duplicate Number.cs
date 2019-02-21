03c281f29ff2d680a9a7b94414daba5e934effd3
Woody-Hu <812791840@qq.com>
2019-02-21 08:40:52 +0800

    2019 02 21

---
+++ LeetCode/LeetCode/Find the Duplicate Number.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Find_the_Duplicate_Number
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            if (nums.Length > 1)
+            {
+                var slow = nums[0];
+                var fast = nums[nums[0]];
+                while (slow != fast)
+                {
+                    slow = nums[slow];
+                    fast = nums[nums[fast]];
+                }
+
+                fast = 0;
+                while (fast != slow)
+                {
+                    fast = nums[fast];
+                    slow = nums[slow];
+                }
+                return slow;
+            }
+            return -1;
+        }
+    }
+}
