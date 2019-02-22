b1391d8fe1f070212eb900b3ce99928b44d8db08
Woody-Hu <812791840@qq.com>
2019-02-22 08:31:17 +0800

    2019 02 22

---
+++ LeetCode/LeetCode/Longest Increasing Subsequence.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Longest_Increasing_Subsequence
+    {
+        public int LengthOfLIS(int[] nums)
+        {
+            var array = new int[nums.Length];
+            var size = 0;
+            foreach (var value in nums)
+            {
+                var i = 0;
+                var j = size;
+                while (i != j)
+                {
+                    var mid = (i + j) / 2;
+                    if (array[mid] < value)
+                    {
+                        i = mid + 1;
+                    }
+                    else
+                    {
+                        j = mid;
+                    }
+                }
+
+                array[i] = value;
+                if (i == size)
+                {
+                    size++;
+                }
+            }
+
+            return size;
+        }
+    }
+}
