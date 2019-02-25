6b0613f9a2b32574189bad88eb75df1af7f58691
Woody-Hu <812791840@qq.com>
2019-02-25 08:36:17 +0800

    2019 02 25

---
+++ LeetCode/LeetCode/Find the Town Judge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Find_the_Town_Judge
+    {
+        public int FindJudge(int N, int[][] trust)
+        {
+            var array = new int[N + 1];
+
+            foreach (var onePair in trust)
+            {
+                array[onePair[0]]--;
+                array[onePair[1]]++;
+            }
+
+            for (int i = 1; i <= N; ++i)
+            {
+                if (array[i] == N - 1) return i;
+            }
+
+            return -1;
+        }
+    }
+}
