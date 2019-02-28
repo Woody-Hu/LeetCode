0c84eb5accca0bcd39b8bee484897074d0fbc8f5
Woody-Hu <812791840@qq.com>
2019-02-28 08:26:08 +0800

    2019 02 28

---
+++ LeetCode/LeetCode/Coin Change.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Coin_Change
+    {
+        public int CoinChange(int[] coins, int amount)
+        {
+            if (amount < 1) return 0;
+            int[] dp = new int[amount + 1];
+            int sum = 0;
+
+            while (++sum <= amount)
+            {
+                int min = -1;
+                foreach (var coin in coins)
+                {
+                    if (sum >= coin && dp[sum - coin] != -1)
+                    {
+                        int temp = dp[sum - coin] + 1;
+                        min = min < 0 ? temp : (temp < min ? temp : min);
+                    }
+                }
+
+                dp[sum] = min;
+            }
+            return dp[amount];
+        }
+    }
+}
