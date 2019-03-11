e4e0086eb69c14eed8d1f31e510dd7328e08a411
Stephenhu <stephenhu@seismic.com>
2019-03-11 08:31:11 +0800

    2019 03 11

---
+++ LeetCode/LeetCode/Maximize Sum Of Array After K Negations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Maximize_Sum_Of_Array_After_K_Negations
+    {
+        public int LargestSumAfterKNegations(int[] A, int K)
+        {
+            Array.Sort(A);
+
+            var sum = 0;
+            var count = 0;
+            var minPost = int.MaxValue;
+            foreach (var oneValue in A)
+            {
+                var useValue = oneValue;
+                if (oneValue < 0 && ++count <= K)
+                {
+                    useValue = -oneValue;
+                }
+
+                sum += useValue;
+                minPost = Math.Min(minPost, useValue);
+            }
+
+            if (count > K || (K - count) % 2 == 0)
+            {
+                return sum;
+            }
+            else
+            {
+                return sum - 2 * minPost;
+            }
+        }
+    }
+}
