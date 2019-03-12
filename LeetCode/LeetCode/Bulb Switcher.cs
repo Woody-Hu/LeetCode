aa46d4bbd2e9dce552dab24324eb7ce2a966d245
Woody-Hu <812791840@qq.com>
2019-03-12 08:44:09 +0800

    2019 03 12

---
+++ LeetCode/LeetCode/Bulb Switcher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Bulb_Switcher
+    {
+        public int BulbSwitch(int n)
+        {
+            return (int) Math.Sqrt(n);
+        }
+    }
+}
