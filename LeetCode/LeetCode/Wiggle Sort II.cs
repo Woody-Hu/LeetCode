a0d18caa22f471635dc9f2d10744c731a4ac6c53
Woody-Hu <812791840@qq.com>
2019-03-13 08:43:28 +0800

    2019 03 13

---
+++ LeetCode/LeetCode/Wiggle Sort II.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Wiggle_Sort_II
+    {
+        public void WiggleSort(int[] nums)
+        {
+            var array = new int[nums.Length];
+            Array.Copy(nums, array, nums.Length);
+            Array.Sort(array);
+            for (int i = 0, j = 0, n = nums.Length, k = (n - 1) / 2; i < n;)
+            {
+                nums[i++] = array[k + j];
+                if (i < n)
+                {
+                    nums[i++] = array[n - 1 + (j--)];
+                }
+            }
+
+        }
+    }
+}
