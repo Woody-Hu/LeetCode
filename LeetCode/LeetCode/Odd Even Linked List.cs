f169c67ffa0f20e6b5aedf9fc992185cdcf31a47
Woody-Hu <812791840@qq.com>
2019-03-14 08:30:50 +0800

    2019 03 14

---
+++ LeetCode/LeetCode/Odd Even Linked List.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Odd_Even_Linked_List
+    {
+        public ListNode OddEvenList(ListNode head)
+        {
+            var cur = head;
+            ListNode saved = null;
+            ListNode savedCur = null;
+            while (cur != null)
+            {
+                var next = cur.next;
+                if (saved != null)
+                {
+                    savedCur.next = next;
+                    savedCur = next;
+                }
+                else
+                {
+                    saved = next;
+                    savedCur = saved;
+                }
+
+                if (next == null)
+                {
+                    break;
+                }
+
+                next = next.next;
+                if (next == null)
+                {
+                    break;
+                }
+
+                cur.next = next;
+                cur = next;
+            }
+
+            if (cur != null)
+            {
+                cur.next = saved;
+            }
+
+            return head;
+        }
+    }
+}
