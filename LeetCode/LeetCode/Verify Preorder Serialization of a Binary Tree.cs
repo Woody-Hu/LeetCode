ba4350cd0780deb4b3baed9cdaeaba33335c797d
Woody-Hu <812791840@qq.com>
2019-03-15 08:33:46 +0800

    2019 03 15

---
+++ LeetCode/LeetCode/Verify Preorder Serialization of a Binary Tree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Verify_Preorder_Serialization_of_a_Binary_Tree
+    {
+        public bool IsValidSerialization(string preorder)
+        {
+            var array = preorder.Split(',');
+            var degree = 1;
+            foreach (var oneStr in array)
+            {
+                if (--degree < 0)
+                {
+                    return false;
+                }
+
+                if (oneStr != "#")
+                {
+                    degree += 2;
+                }
+            }
+
+            return degree == 0;
+        }
+    }
+}
