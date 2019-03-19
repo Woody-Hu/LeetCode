f84dd94ed5f4ee96dc28acf58bec5d0ad1b2a82c
Woody-Hu <812791840@qq.com>
2019-03-19 08:19:57 +0800

    2019 03 19

---
+++ LeetCode/LeetCode/Pairs of Songs With Total Durations Divisible by 60.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Pairs_of_Songs_With_Total_Durations_Divisible_by_60
+    {
+        public int NumPairsDivisibleBy60(int[] time)
+        {
+            var array = new int[60];
+            var count = 0;
+            foreach (var item in time)
+            {
+                count += array[(60 - item % 60) % 60];
+                array[item % 60]++;
+            }
+
+            return count;
+        }
+    }
+}
