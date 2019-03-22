07fe738e83e46492a66c401f2df537b212eabb00
Stephenhu <stephenhu@seismic.com>
2019-03-22 08:19:00 +0800

    2019 03 22

---
+++ LeetCode/LeetCode/Increasing Triplet Subsequence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Increasing_Triplet_Subsequence
+    {
+        public bool IncreasingTriplet(int[] nums)
+        {
+            var first = int.MaxValue;
+            var second = int.MaxValue;
+
+            foreach (var value in nums)
+            {
+                if (value <= first)
+                {
+                    first = value;
+                }
+                else if (value <= second)
+                {
+                    second = value;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+    }
+}
