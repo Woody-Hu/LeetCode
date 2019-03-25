42e5d7769f347bad41ad2677c6f0578a2de91ca9
Woody-Hu <812791840@qq.com>
2019-03-25 08:29:26 +0800

    2019 03 25

---
+++ LeetCode/LeetCode/Partition Array Into Three Parts With Equal Sum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class Partition_Array_Into_Three_Parts_With_Equal_Sum
+    {
+        public bool CanThreePartsEqualSum(int[] A)
+        {
+            var sum = A.Sum();
+            if (sum % 3 != 0)
+            {
+                return false;
+            }
+
+            sum = sum / 3;
+            var count = 0;
+            var tempSum = 0;
+
+            foreach (var value in A)
+            {
+                tempSum += value;
+                if (tempSum == sum)
+                {
+                    tempSum = 0;
+                    count++;
+                }
+            }
+
+            return count >= 3;
+        }
+    }
+}
