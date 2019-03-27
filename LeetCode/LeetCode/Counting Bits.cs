7c8f0dbb3dcb19c5df57e85828268f657d594a9c
Woody-Hu <812791840@qq.com>
2019-03-27 08:32:02 +0800

    2019 03 27

---
+++ LeetCode/LeetCode/Counting Bits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Counting_Bits
+    {
+        public int[] CountBits(int num)
+        {
+            var array = new int[num + 1];
+            for (int i = 1; i <= num; i++)
+            {
+                array[i] = array[i >> 1] + (i & 1);
+            }
+
+            return array;
+        }
+    }
+}
