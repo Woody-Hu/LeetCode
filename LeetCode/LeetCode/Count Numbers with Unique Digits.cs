3d14badc4ab87db71fef7dd66dba3e61eebbbe23
Woody-Hu <812791840@qq.com>
2019-04-03 08:37:09 +0800

    2019 04 03

---
+++ LeetCode/LeetCode/Count Numbers with Unique Digits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Count_Numbers_with_Unique_Digits
+    {
+        public int CountNumbersWithUniqueDigits(int n)
+        {
+            if (n == 0) return 1;
+
+            int res = 10;
+            int uniqueDigits = 9;
+            int availableNumber = 9;
+            while (n-- > 1 && availableNumber > 0)
+            {
+                uniqueDigits = uniqueDigits * availableNumber;
+                res += uniqueDigits;
+                availableNumber--;
+            }
+            return res;
+        }
+    }
+}
