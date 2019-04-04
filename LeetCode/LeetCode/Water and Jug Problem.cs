e846e0d45f3f27f00efc873696ddf7a184c41a07
Woody-Hu <812791840@qq.com>
2019-04-04 08:23:40 +0800

    2019 04 04

---
+++ LeetCode/LeetCode/Water and Jug Problem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Water_and_Jug_Problem
+    {
+        public bool CanMeasureWater(int x, int y, int z)
+        {
+            if (x + y < z)
+            {
+                return false;
+            }
+
+            if (x == z || y == z || x + y == z)
+            {
+                return true;
+            }
+
+            return z % GetGCD(x, y) == 0;
+        }
+
+        private int GetGCD(int x, int y)
+        {
+            while (y != 0)
+            {
+                var temp = y;
+                y = x % y;
+                x = temp;
+            }
+
+            return x;
+        }
+    }
+}
