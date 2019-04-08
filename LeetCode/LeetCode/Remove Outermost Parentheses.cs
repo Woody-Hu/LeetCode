87d28d4f315163dba9de3ba87f6628394657eca1
Woody-Hu <812791840@qq.com>
2019-04-08 08:16:52 +0800

    2019 04 08

---
+++ LeetCode/LeetCode/Remove Outermost Parentheses.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Remove_Outermost_Parentheses
+    {
+        public string RemoveOuterParentheses(string S)
+        {
+            var stringBuilder = new StringBuilder();
+            var operationCount = 0;
+
+            foreach (var oneChar in S)
+            {
+                if (oneChar == '(' && operationCount++ > 0)
+                {
+                    stringBuilder.Append(oneChar);
+                }
+                else if (oneChar == ')' && operationCount-- > 1)
+                {
+                    stringBuilder.Append(oneChar);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
