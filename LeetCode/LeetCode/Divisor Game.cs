9ce30d528a9abc98e252c6ffdc8e9de227c81614
Woody-Hu <812791840@qq.com>
2019-04-15 08:16:07 +0800

    2019 04 15

---
+++ LeetCode/LeetCode/Divisor Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Divisor_Game
+    {
+        public bool DivisorGame(int N)
+        {
+            return N % 2 == 0;
+        }
+    }
+}
