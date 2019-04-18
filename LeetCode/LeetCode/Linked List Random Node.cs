d8f440ac0a64b1c1b7baf1dfe2f893b6508114b5
Woody-Hu <812791840@qq.com>
2019-04-18 08:30:50 +0800

    2019 04 18

---
+++ LeetCode/LeetCode/Linked List Random Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Linked_List_Random_Node
+    {
+        private ListNode _head;
+        private Random _random;
+
+        public Linked_List_Random_Node(ListNode head)
+        {
+            _head = head;
+            _random = new Random();
+        }
+
+        public int GetRandom()
+        {
+            var current = _head;
+            var value = current.val;
+            var index = 0;
+            while (current != null)
+            {
+                if (_random.Next(index+1) == index)
+                {
+                    value = current.val;
+                }
+
+                current = current.next;
+                index++;
+            }
+
+            return value;
+
+        }
+    }
+}
