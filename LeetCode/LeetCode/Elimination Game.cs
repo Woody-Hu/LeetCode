7cc2c4977069dd0c4696bb3068d073edd9f33f71
Woody-Hu <812791840@qq.com>
2019-04-26 08:33:20 +0800

    2019 04 26

---
+++ LeetCode/LeetCode/Elimination Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Elimination_Game
+    {
+        public int LastRemaining(int n)
+        {
+            var start = 1;
+            var step = 1;
+            var fromLeft = true;
+            while (n != 1)
+            {
+                if (fromLeft || (n % 2) == 1)
+                {
+                    start = start + step;
+                }
+
+                n = n / 2;
+                step = step * 2;
+                fromLeft = !fromLeft;
+            }
+
+            return start;
+        }
+    }
+}
