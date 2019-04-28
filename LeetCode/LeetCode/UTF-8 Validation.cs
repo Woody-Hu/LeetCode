29516333f2b06f607af1fde0310558af18e91f64
Woody-Hu <812791840@qq.com>
2019-04-28 08:24:36 +0800

    2019 04 28

---
+++ LeetCode/LeetCode/UTF-8 Validation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class UTF_8_Validation
+    {
+        public bool ValidUtf8(int[] data)
+        {
+            var count = 0;
+            foreach (var oneValue in data)
+            {
+                if (count == 0)
+                {
+                    var shift = 1 << 7;
+                    while ((shift & oneValue) == shift && shift > 1)
+                    {
+                        count++;
+                        shift = shift >> 1;
+                    }
+
+                    if (count == 1|| count > 4)
+                    {
+                        return false;
+                    }
+                    count = (count == 0) ? count : --count;
+                }
+                else
+                {
+                    var shift1 = 1 << 7;
+                    var shift2 = 1 << 6;
+                    if (count > 0 && (shift1 & oneValue) == shift1 && (shift2 & oneValue) == 0)
+                    {
+                        count--;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return count == 0;
+
+        }
+    }
+}
