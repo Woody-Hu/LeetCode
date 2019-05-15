0670ec16f66ebffa3d2664253e39a6b2dbccfd70
Woody-Hu <812791840@qq.com>
2019-05-15 08:33:03 +0800

    2019 05 15

---
+++ LeetCode/LeetCode/Battleships in a Board.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace LeetCode
+{
+    class Battleships_in_a_Board
+    {
+        public int CountBattleships(char[][] board)
+        {
+            var count = 0;
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    if (board[i][j] == '.')
+                    {
+                        continue;
+                    }
+                    else if (i >0 && board[i-1][j] == 'X')
+                    {
+                        continue;
+                    }
+                    else if (j > 0 && board[i][j - 1] == 'X')
+                    {
+                        continue;
+                    }
+
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
