cfe56333380acb8f8970ef7a47123d45c5d9a696
Woody-Hu <812791840@qq.com>
2019-05-17 08:11:17 +0800

    2019 05 17

---
+++ LeetCode/LeetCode/Find All Duplicates in an Array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Find_All_Duplicates_in_an_Array
+    {
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            var list = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var index = Math.Abs(nums[i]) - 1;
+                if (nums[index] < 0)
+                {
+                    list.Add(index + 1);
+                    continue;
+                }
+
+                nums[index] = -nums[index];
+            }
+
+            return list;
+        }
+    }
+}
