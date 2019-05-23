fc30c58f202a7239956acf1a8031f65f2be2936d
Woody-Hu <812791840@qq.com>
2019-05-23 08:31:02 +0800

    2019 05 23

---
+++ LeetCode/LeetCode/Find Right Interval.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace LeetCode
+{
+    class Find_Right_Interval
+    {
+        public int[] FindRightInterval(int[][] intervals)
+        {
+            var list = new List<KeyValuePair<int, int>>();
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                list.Add(new KeyValuePair<int, int>(intervals[i][0], i));
+            }
+
+            list.Sort(new DefaultCompare());
+
+            var res = new List<int>();
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                var index = BinarySearchCeiling(list, intervals[i][1]);
+                res.Add(index == -1 ? index : list[index].Value);
+            }
+
+            return res.ToArray();
+        }
+
+        private int BinarySearchCeiling(List<KeyValuePair<int, int>> list, int value)
+        {
+            int cur, m, l;
+            m = 0;
+            l = list.Count - 1;
+            if (value < list[m].Key || list[l].Key < value)
+                return -1;
+            while (l - m > 1)
+            {
+                cur = Convert.ToInt16(Math.Ceiling((m + l) / 2.0));
+                if (list[cur].Key < value)
+                    m = cur;
+                else if (list[cur].Key > value)
+                    l = cur;
+                else
+                    return cur;
+            }
+
+            return l;
+        }
+
+        private class DefaultCompare : IComparer<KeyValuePair<int, int>>
+        {
+            public int Compare(KeyValuePair<int, int> x, KeyValuePair<int, int> y)
+            {
+                return x.Key - y.Key;
+            }
+        }
+    }
+}
