817396f872f12efaf8f7a3f6d0e02ff9b163b043
Woody-Hu <812791840@qq.com>
2019-05-27 08:09:46 +0800

    2019 05 27

---
+++ LeetCode/LeetCode/Height Checker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Height_Checker
+    {
+        public int HeightChecker(int[] heights)
+        {
+            var copiedArray = new int[heights.Length];
+            Array.Copy(heights, copiedArray, heights.Length);
+            Array.Sort(copiedArray);
+            var count = 0;
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if (heights[i] != copiedArray[i])
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
