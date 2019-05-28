115636fcd4b652743be44fa5a06c1ee8590d1650
Woody-Hu <812791840@qq.com>
2019-05-28 08:23:20 +0800

    2019 05 28

---
+++ LeetCode/LeetCode/Sort Characters By Frequency.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class Sort_Characters_By_Frequency
+    {
+        public string FrequencySort(string s)
+        {
+            var dict = new Dictionary<char, int>();
+            foreach (var oneChar in s)
+            {
+                dict[oneChar] = dict.GetValueOrDefault(oneChar) + 1;
+            }
+
+            var list = dict.ToList();
+            list.Sort(new KeyValueComparer());
+
+            var stringBuilder = new StringBuilder();
+            foreach (var onePair in list)
+            {
+                for (int i = 1; i <= onePair.Value; i++)
+                {
+                    stringBuilder.Append(onePair.Key);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private class KeyValueComparer : IComparer<KeyValuePair<char, int>>
+        {
+            public int Compare(KeyValuePair<char, int> x, KeyValuePair<char, int> y)
+            {
+                return y.Value - x.Value;
+            }
+        }
+    }
+}
