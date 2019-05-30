8ba7b2c06574e2129b59b25b0c583bcbe23b0ec5
Woody-Hu <812791840@qq.com>
2019-05-30 08:13:34 +0800

    2019 05 30

---
+++ LeetCode/LeetCode/Minimum Number of Arrows to Burst Balloons.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Minimum_Number_of_Arrows_to_Burst_Balloons
+    {
+        public int FindMinArrowShots(int[][] points)
+        {
+            if (points.Length < 2)
+            {
+                return points.Length;
+            }
+
+            Array.Sort(points, (ints, ints1) => ints[1] - ints1[1] );
+
+            var res = 1;
+            var currentIndex = 0;
+            for (int i = 1; i < points.Length; i++)
+            {
+                if (points[i][0] > points[currentIndex][1])
+                {
+                    currentIndex = i;
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
