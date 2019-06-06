ca02022126e12775cd2f556dadf1d644ac00afe0
Woody-Hu <812791840@qq.com>
2019-06-06 08:55:06 +0800

    2019 06 06

---
+++ LeetCode/LeetCode/Unique Substrings in Wraparound String.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Unique_Substrings_in_Wraparound_String
+    {
+        public int FindSubstringInWraproundString(string p)
+        {
+            int res = 0;
+            int i = 0;
+            int[] letters = new int[26];
+            while (i < p.Length)
+            {
+                int endIndex = i;
+                while (endIndex + 1 < p.Length && p[endIndex + 1] % 26 == (p[endIndex] + 1) % 26) endIndex++;
+                for (int j = i; j <= endIndex; j++)
+                {
+                    letters[p[j] - 'a'] = Math.Max(letters[p[j] - 'a'], endIndex - j + 1);
+                }
+
+                i = endIndex + 1;
+            }
+
+            for (int j = 0; j < 26; j++)
+            {
+                res += letters[j];
+            }
+            return res;
+        }
+    }
+}
