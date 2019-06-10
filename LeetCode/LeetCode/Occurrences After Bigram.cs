5e1011d1b6a3e306010c487814f1415c088501e4
Woody-Hu <812791840@qq.com>
2019-06-10 08:15:03 +0800

    2019 06 10

---
+++ LeetCode/LeetCode/Occurrences After Bigram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Occurrences_After_Bigram
+    {
+        public string[] FindOcurrences(string text, string first, string second)
+        {
+            var array = text.Split(' ');
+            var list = new List<string>();
+            for (int i = 2; i < array.Length; i++)
+            {
+                if (array[i - 2] == first && array[i - 1] == second)
+                {
+                    list.Add(array[i]);
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+}
