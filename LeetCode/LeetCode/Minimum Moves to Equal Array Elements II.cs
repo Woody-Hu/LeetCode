8344dfa6bd587eae3b16b38f5aab2f232f1ac147
Woody-Hu <812791840@qq.com>
2019-06-12 08:13:29 +0800

    2019 06 12

---
+++ LeetCode/LeetCode/Minimum Moves to Equal Array Elements II.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Minimum_Moves_to_Equal_Array_Elements_II
+    {
+        public int MinMoves2(int[] nums)
+        {
+            Array.Sort(nums);
+            var mid = nums[nums.Length / 2];
+            var res = 0;
+
+            foreach (var oneValue in nums)
+            {
+                res += Math.Abs(oneValue - mid);
+            }
+
+            return res;
+        }
+    }
+}
