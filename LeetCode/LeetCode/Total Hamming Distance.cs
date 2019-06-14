6c2a3abc08bc86167a2be5c1b843e68d2b63143e
Woody-Hu <812791840@qq.com>
2019-06-14 08:21:40 +0800

    2016 06 14

---
+++ LeetCode/LeetCode/Total Hamming Distance.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Total_Hamming_Distance
+    {
+        public int TotalHammingDistance(int[] nums)
+        {
+            var arrayOne = new int[32];
+            var arrayZero = new int[32];
+            var res = 0;
+            foreach (var oneNum in nums)
+            {
+                var n = oneNum;
+                for (int i = 0; i < 32; ++i)
+                {
+                    if ((n & 1) == 0)
+                    {
+                        res += arrayZero[i];
+                        arrayOne[i]++;
+                    }
+                    else
+                    {
+                        res += arrayOne[i];
+                        arrayZero[i]++;
+                    }
+                    n = n >> 1;
+                }
+            }
+
+            return res;
+        }
+    }
+}
