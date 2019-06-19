95230ecb08df9b2d1e0200fdecd07f84d069ab28
Woody-Hu <812791840@qq.com>
2019-06-19 08:17:24 +0800

    2019 06 19

---
+++ LeetCode/LeetCode/Range Sum of BST.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Range_Sum_of_BST
+    {
+        public int RangeSumBST(TreeNode root, int L, int R)
+        {
+            return RangeSumBST(root, L, R, 0);
+        }
+
+        private int RangeSumBST(TreeNode root, int L, int R, int sum)
+        {
+            if (root == null)
+            {
+                return sum;
+            }
+
+            if (!(root.val < L || root.val > R))
+            {
+                sum = sum + root.val;
+            }
+
+            if (root.val > L)
+            {
+                sum = RangeSumBST(root.left, L, R, sum);
+            }
+
+            if (root.val < R)
+            {
+                sum = RangeSumBST(root.right, L, R, sum);
+            }
+
+            return sum;
+        }
+    }
+}
