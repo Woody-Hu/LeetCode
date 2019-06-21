f96ef95ec280169a2400867e5a89a7b12899f345
Woody-Hu <812791840@qq.com>
2019-06-21 08:52:18 +0800

    2019 06 21

---
+++ LeetCode/LeetCode/Magical String.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Magical_String
+    {
+        public int MagicalString(int n)
+        {
+            int res = 0;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(1);
+            for (int i = 0, j = 0; j < n; i++)
+            {
+                if (sb[j] == '1')
+                {
+                    res++;
+                    if (sb[i] == '1')
+                    {
+                        sb.Append('2');
+                        j++;
+                    }
+                    else
+                    {
+                        sb.Append('1');
+                        if (j + 1 < n) res++;
+                        sb.Append('2');
+                        j += 2;
+                    }
+                }
+                else
+                {
+                    if (sb[i] == '1')
+                    {
+                        sb.Append('1');
+                        j++;
+                    }
+                    else
+                    {
+                        sb.Append('2');
+                        sb.Append('1');
+                        j += 2;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
