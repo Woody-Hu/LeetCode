5172027a9fbe1d32e5cea949ebd230435a1d2920
Woody-Hu <812791840@qq.com>
2019-06-25 08:22:46 +0800

    2019 06 25

---
+++ LeetCode/LeetCode/Ones and Zeroes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Ones_and_Zeroes
+    {
+        public int FindMaxForm(string[] strs, int m, int n)
+        {
+            var dp = new int[m + 1, n + 1];
+            foreach (var s in strs)
+            {
+                int zero = 0, one = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == '0')
+                    {
+                        zero++;
+                    }
+                    else
+                    {
+                        one++;
+                    }
+                }
+                for (int i = m; i > zero - 1; i--)
+                {
+                    for (int j = n; j > one - 1; j--)
+                    {
+                        dp[i, j] = Math.Max(dp[i, j], dp[i - zero, j - one] + 1);
+                    }
+                }
+            }
+            return dp[m, n];
+        }
+    }
+}
