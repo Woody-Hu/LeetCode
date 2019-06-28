65110286d3fcb1166918bafe301e17eab529e685
Woody-Hu <812791840@qq.com>
2019-06-28 08:35:50 +0800

    2019 06 28

---
+++ LeetCode/LeetCode/Next Greater Element II.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Next_Greater_Element_II
+    {
+        public int[] NextGreaterElements(int[] nums)
+        {
+            var length = nums.Length;
+            var res = new int[nums.Length];
+            var stack = new Stack<KeyValuePair<int,int>>();
+            UpdateArray(nums, length, stack, res,true);
+            UpdateArray(nums, length, stack, res,false);
+            while (stack.Count != 0)
+            {
+                var pair = stack.Pop();
+                res[pair.Key] = -1;
+            }
+
+            return res;
+        }
+
+        private static void UpdateArray(int[] nums, int length, Stack<KeyValuePair<int, int>> stack, int[] res, bool pushValue)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                var num = nums[i];
+                while (stack.Count != 0 && stack.Peek().Value < num)
+                {
+                    var pair = stack.Pop();
+                    res[pair.Key] = num;
+                }
+
+                if (pushValue)
+                {
+                    stack.Push(new KeyValuePair<int, int>(i, num));
+                }
+            }
+        }
+    }
+}
