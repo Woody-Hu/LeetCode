8b6d3cebdc82a1a7818ee3e7774d0f0d8588f6fc
Woody-Hu <812791840@qq.com>
2019-07-01 08:30:30 +0800

    2019 07 01

---
+++ LeetCode/LeetCode/Distribute Candies to People.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Distribute_Candies_to_People
+    {
+        public int[] DistributeCandies(int candies, int num_people)
+        {
+            var res = new int[num_people];
+
+            for (int i = 0; candies > 0; i++)
+            {
+                res[i % num_people] += Math.Min(candies, i + 1);
+                candies -= i + 1;
+            }
+
+            return res;
+        }
+    }
+}
