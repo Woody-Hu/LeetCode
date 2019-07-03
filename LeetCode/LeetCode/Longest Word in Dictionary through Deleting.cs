bfbbc4d774f4b7934df7f0b11d3948c7b7dd0c4f
Woody-Hu <812791840@qq.com>
2019-07-03 08:33:04 +0800

    2019 07 03

---
+++ LeetCode/LeetCode/Longest Word in Dictionary through Deleting.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Longest_Word_in_Dictionary_through_Deleting
+    {
+        public string FindLongestWord(string s, IList<string> d)
+        {
+            var res = string.Empty;
+
+            foreach (var oneItem in d)
+            {
+                if (oneItem.Length < res.Length || (oneItem.Length == res.Length && string.Compare(oneItem, res) >= 0))
+                {
+                    continue;
+                }
+
+                if (IsSubString(oneItem, s))
+                {
+                    res = oneItem;
+                }
+            }
+
+            return res;
+        }
+
+        private bool IsSubString(string s, string target)
+        {
+            var i = 0;
+            var j = 0;
+            if (s.Length > target.Length)
+            {
+                return false;
+            }
+
+            while (i < s.Length && j< target.Length)
+            {
+                if (s[i] == target[j])
+                {
+                    i++;
+                }
+
+                j++;
+            }
+
+            return i == s.Length;
+        }
+    }
+}
