1a304e15664b049b035844853980e8986ee43ac6
Woody-Hu <812791840@qq.com>
2019-07-08 08:23:28 +0800

    2019 07 08

---
+++ LeetCode/LeetCode/Defanging an IP Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Defanging_an_IP_Address
+    {
+        public string DefangIPaddr(string address)
+        {
+            var array = address.Split('.');
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < array.Length; i++)
+            {
+                sb.Append(array[i]);
+                if (i != array.Length - 1)
+                {
+                    sb.Append("[.]");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
