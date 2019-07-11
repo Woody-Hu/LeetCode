a9f8e201a6e10c769ddb76befef584cdf960853a
Woody-Hu <812791840@qq.com>
2019-07-11 08:18:55 +0800

    2019 07 11

---
+++ LeetCode/LeetCode/Longest Palindromic Subsequence.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Longest_Palindromic_Subsequence
+    {
+        public int LongestPalindromeSubseq(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return 0;
+            }
+
+            var dp = new int[s.Length, s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                dp[i, i] = 1;
+            }
+
+            for (int i = s.Length - 1; i >=0; i--)
+            {
+                for (int j = i+1; j < s.Length; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        dp[i,j] = 2+dp[i+1,j-1];
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Max(dp[i + 1, j], dp[i, j - 1]);
+                    }
+                }
+            }
+
+            return dp[0, s.Length - 1];
+        }
+    }
+}
