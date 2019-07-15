5712cb9ad015a561cd4ee9ce9ab12ce86df426bd
Woody-Hu <812791840@qq.com>
2019-07-15 08:27:18 +0800

    2019 07 15

---
+++ LeetCode/LeetCode/Relative Sort Array.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Relative_Sort_Array
+    {
+        public int[] RelativeSortArray(int[] arr1, int[] arr2)
+        {
+            var k = 0;
+            var array = new int[1001];
+            var res = new int[arr1.Length];
+
+            foreach (var oneValue in arr1)
+            {
+                array[oneValue]++;
+            }
+
+            foreach (var oneValue in arr2)
+            {
+                while (array[oneValue]-- > 0)
+                {
+                    res[k++] = oneValue;
+                }
+            }
+
+            for (int i = 0; i < 1001; i++)
+            {
+                while (array[i]-- > 0)
+                {
+                    res[k++] = i;
+                }
+            }
+
+            return res;
+        }
+    }
+}
