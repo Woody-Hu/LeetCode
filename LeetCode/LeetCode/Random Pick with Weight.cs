536451cc97a1b2f05db22ec0d519799a2711af77
Woody-Hu <812791840@qq.com>
2019-07-25 08:32:58 +0800

    2019 07 25

---
+++ LeetCode/LeetCode/Random Pick with Weight.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Random_Pick_with_Weight
+    {
+        private int[] _w;
+        private int _sum;
+        private Random _random = new Random();
+
+
+        public Random_Pick_with_Weight(int[] w)
+        {
+            _w = new int[w.Length];
+            var sum = 0;
+            for (int i = 0; i < w.Length; i++)
+            {
+                _w[i] = sum;
+                sum += w[i];
+            }
+
+            _sum = sum;
+        }
+
+        public int PickIndex()
+        {
+            var index = _random.Next(_sum);
+            int lo = 0, hi = _w.Length - 1;
+            while (lo < hi)
+            {
+                int mid = lo + (hi - lo) / 2 + 1;
+                if (_w[mid] == index)
+                    return mid;
+
+                if (_w[mid] > index)
+                {
+                    hi = mid - 1;
+                }
+                else
+                {
+                    lo = mid;
+                }
+            }
+            return lo;
+        }
+    }
+}
