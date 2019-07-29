23cdee2d887628f7d26b4e11bd906807ca0153e3
Woody-Hu <812791840@qq.com>
2019-07-29 08:25:15 +0800

    2019 07 29

---
+++ LeetCode/LeetCode/N-th Tribonacci Number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class N_th_Tribonacci_Number
+    {
+        public int Tribonacci(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+
+            var a = 0;
+            var b = 1;
+            var c = 1;
+            while (n-->2)
+            {
+                var d = a + b + c;
+                a = b;
+                b = c;
+                c = d;
+            }
+
+            return c;
+        }
+    }
+}
