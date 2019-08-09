e7ba0c4ad059250b057817f2dbd4bbbf1302b3b1
Woody-Hu <812791840@qq.com>
2019-08-09 08:26:15 +0800

    2019 08 09

---
+++ LeetCode/LeetCode/2 Keys Keyboard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class _2_Keys_Keyboard
+    {
+        public int MinSteps(int n)
+        {
+            if (n <= 1)
+            {
+                return 0;
+            }
+
+            var dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+
+            for (int i = 2; i < dp.Length; i++)
+            {
+                dp[i] = i;
+                for (int j = 1; j <= i/2; j++)
+                {
+                    if (i%j == 0)
+                    {
+                        dp[i] = Math.Min(dp[i], dp[j] + i / j);
+                    }
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
