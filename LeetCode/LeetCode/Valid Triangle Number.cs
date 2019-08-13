8db80fc81a56d23ae623a997e24197a0ff40a7c6
Woody-Hu <812791840@qq.com>
2019-08-13 08:34:48 +0800

    2019 08 13

---
+++ LeetCode/LeetCode/Valid Triangle Number.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Valid_Triangle_Number
+    {
+        public int TriangleNumber(int[] nums)
+        {
+            if (nums.Length < 3)
+            {
+                return 0;
+            }
+
+            Array.Sort(nums);
+            var res = 0;
+            for (var i = nums.Length - 1; i >=2; i--)
+            {
+                var max = nums[i];
+                var low = 0;
+                var high = i - 1;
+                while (low < high)
+                {
+                    if (nums[low] + nums[high] > max)
+                    {
+                        res+= high -low;
+                        high--;
+                    }
+                    else
+                    {
+                        low++;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
