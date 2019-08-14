ffdbc8162d01f5f03c1547e3ec87ad37a36ecb44
Woody-Hu <812791840@qq.com>
2019-08-14 08:27:07 +0800

    2019 08 14

---
+++ LeetCode/LeetCode/Split Array into Consecutive Subsequences.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Split_Array_into_Consecutive_Subsequences
+    {
+        public bool IsPossible(int[] nums)
+        {
+            int first = nums[0], len = nums[nums.Length - 1] - first + 1;
+            int[] count = new int[len + 1];
+            foreach (var num in nums)
+            {
+                count[num - first + 1]++;
+            }
+
+            int start = 1, end = 1;
+            while (end <= len)
+            {
+                while (end < len && count[end] <= count[end + 1]) end++;
+                if (end - start < 2) return false;
+
+                for (int i = start; i <= end; i++)
+                {
+                    count[i] = count[i] - 1;
+                    if (count[i] == 0) start = i + 1;
+                }
+                while (end <= len && count[end] == 0)
+                {
+                    end++;
+                    start = end;
+                }
+            }
+            return true;
+        }
+    }
+}
