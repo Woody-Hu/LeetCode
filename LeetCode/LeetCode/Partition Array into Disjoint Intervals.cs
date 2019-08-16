2f3eaf764337079fa217f494eae6ef48f76191dc
Woody-Hu <812791840@qq.com>
2019-08-16 08:28:16 +0800

    2019 08 16

---
+++ LeetCode/LeetCode/Partition Array into Disjoint Intervals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Partition_Array_into_Disjoint_Intervals
+    {
+        public int PartitionDisjoint(int[] A)
+        {
+            int globalMax = A[0];
+            int leftMax = A[0];
+            int boundary = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                globalMax = Math.Max(globalMax, A[i]);
+                if (A[i] < leftMax)
+                {
+                    boundary = i;
+                    leftMax = Math.Max(globalMax, leftMax);
+                }
+            }
+            return boundary + 1;
+        }
+    }
+}
