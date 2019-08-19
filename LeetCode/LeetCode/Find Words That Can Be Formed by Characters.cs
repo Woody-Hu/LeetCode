0238d0eb0bcaa9e394699d33857bf6e7f7206ec7
Woody-Hu <812791840@qq.com>
2019-08-19 08:43:15 +0800

    2019 08 19

---
+++ LeetCode/LeetCode/Find Words That Can Be Formed by Characters.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Find_Words_That_Can_Be_Formed_by_Characters
+    {
+        public int CountCharacters(string[] words, string chars)
+        {
+            var array = new int[26];
+            var res = 0;
+            foreach (var oneChar in chars)
+            {
+                array[oneChar - 'a']++;
+            }
+
+            foreach (var oneWord in words)
+            {
+                var index = 0;
+                var tag = true;
+                for (; index < oneWord.Length && tag; index++)
+                {
+                    if (--array[oneWord[index] - 'a'] < 0)
+                    {
+                        tag = false;
+                        break;
+                    }
+                }
+
+                if (index == oneWord.Length)
+                {
+                    res += oneWord.Length;
+                    index = oneWord.Length - 1;
+                }
+
+                for (; index>=0; index--)
+                {
+                    array[oneWord[index] - 'a']++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
