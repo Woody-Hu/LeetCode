4e7f340e435d800e771bc2fd0715bf51db0f4cae
Woody-Hu <812791840@qq.com>
2019-08-27 08:33:54 +0800

    2019 08 27

---
+++ LeetCode/LeetCode/Validate Stack Sequences.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Validate_Stack_Sequences
+    {
+        public bool ValidateStackSequences(int[] pushed, int[] popped)
+        {
+            if (pushed == null || popped == null || pushed.Length != popped.Length) return false;
+            var s = new Stack<int>();
+            int i = 0, j = 0;
+            while (i < pushed.Length && j < popped.Length)
+            {
+                if (s.Count == 0 || !s.Peek().Equals(popped[j]))
+                {
+                    s.Push(pushed[i]);
+                    i++;
+                }
+                else
+                {
+                    s.Pop();
+                    j++;
+                }
+            }
+
+            while (j < popped.Length && s.Count != 0 && s.Peek().Equals(popped[j]))
+            {
+                s.Pop();
+                j++;
+            }
+
+            return (s.Count == 0 && j == popped.Length);
+        }
+    }
+}
