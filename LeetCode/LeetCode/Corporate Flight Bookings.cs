32a95df3085a62d45c34695571b8e0826e8210a2
Woody-Hu <812791840@qq.com>
2019-08-28 08:28:08 +0800

    2019 08 29

---
+++ LeetCode/LeetCode/Corporate Flight Bookings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Corporate_Flight_Bookings
+    {
+        public int[] CorpFlightBookings(int[][] bookings, int n)
+        {
+            var res = new int[n];
+            foreach (var oneArray in bookings)
+            {
+                res[oneArray[0] - 1] += oneArray[2];
+                if (oneArray[1] < n)
+                {
+                    res[oneArray[1]] -= oneArray[2];
+                }
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                res[i] += res[i - 1];
+            }
+
+            return res;
+        }
+    }
+}
