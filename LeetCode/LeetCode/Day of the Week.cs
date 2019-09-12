6e624c48432bde007f2ab0036a4e58e56407489e
Woody-Hu <812791840@qq.com>
2019-09-12 08:39:57 +0800

    2019 09 12

---
+++ LeetCode/LeetCode/Day of the Week.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Day_of_the_Week
+    {
+        public string DayOfTheWeek(int day, int month, int year)
+        {
+            var days = new string[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            if (month < 3)
+            {
+                month += 12;
+                year -= 1;
+            }
+
+            int c = year / 100;
+            year = year % 100;
+            int w = (c / 4 - 2 * c + year + year / 4 + 13 * (month + 1) / 5 + day - 1) % 7;
+            return days[(w + 7) % 7];
+
+        }
+    }
+}
