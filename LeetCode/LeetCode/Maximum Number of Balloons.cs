e7c60592d5ff498ee68026f43ce47f51f2ab22f0
Woody-Hu <812791840@qq.com>
2019-10-24 08:01:52 +0800

    2019 10 24

---
+++ LeetCode/LeetCode/Maximum Number of Balloons.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Maximum_Number_of_Balloons
+    {
+        public int MaxNumberOfBalloons(string text)
+        {
+            var res = 0;
+            var array = new int[26];
+            foreach (var oneChar in text)
+            {
+                array[oneChar - 'a']++;
+            }
+
+            var str = "balloon";
+            var ifBreak = false;
+            while (!ifBreak)
+            {
+                foreach (var oneChar in str)
+                {
+                    var index = oneChar - 'a';
+                    if (array[index] > 0)
+                    {
+                        array[index]--;
+                    }
+                    else
+                    {
+                        ifBreak = true;
+                        break;
+                    }
+                }
+
+                if (!ifBreak)
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
