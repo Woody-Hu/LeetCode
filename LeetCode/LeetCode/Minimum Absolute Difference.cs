59b07a7d0a5a71637a038b7f0bc745901d8daea4
Woody-Hu <812791840@qq.com>
2019-11-19 08:38:56 +0800

    Create Minimum Absolute Difference.cs

---
+++ LeetCode/LeetCode/Minimum Absolute Difference.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Minimum_Absolute_Difference
+    {
+        public IList<IList<int>> MinimumAbsDifference(int[] arr)
+        {
+            var res = new List<IList<int>>();
+            Array.Sort(arr);
+            var min = int.MaxValue;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                var dif = Math.Abs(arr[i] - arr[i + 1]);
+                if (dif > min)
+                {
+                    continue;
+                }
+                else if (dif == min)
+                {
+                    res.Add(new List<int>() { arr[i], arr[i + 1] });
+                }
+                else
+                {
+                    min = dif;
+                    res.Clear();
+                    res.Add(new List<int>() { arr[i], arr[i + 1] });
+                }
+            }
+
+            return res;
+        }
+    }
+}
