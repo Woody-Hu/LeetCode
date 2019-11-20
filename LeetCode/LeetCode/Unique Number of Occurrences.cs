4bb6a2ac94810f5825fec795910eb28f56879624
Woody-Hu <812791840@qq.com>
2019-11-20 09:01:38 +0800

    Create Unique Number of Occurrences.cs

---
+++ LeetCode/LeetCode/Unique Number of Occurrences.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Unique_Number_of_Occurrences
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            var dict = new Dictionary<int, int>();
+
+            foreach (var item in arr)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict.Add(item, 0);
+                }
+
+                dict[item] = dict[item] + 1;
+            }
+
+            var set = new HashSet<int>();
+
+            foreach (var keyValuePair in dict)
+            {
+                if (set.Contains(keyValuePair.Value))
+                {
+                    return false;
+                }
+
+                set.Add(keyValuePair.Value);
+            }
+
+            return true;
+        }
+    }
+}
