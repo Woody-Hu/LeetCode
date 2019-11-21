d9770c85dddd01b3849b1b6a5526f39da5c8c076
Woody-Hu <812791840@qq.com>
2019-11-21 08:26:10 +0800

    Create Play with Chips.cs

---
+++ LeetCode/LeetCode/Play with Chips.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Play_with_Chips
+    {
+        public int MinCostToMoveChips(int[] chips)
+        {
+            var even = 0;
+            var odd = 0;
+            foreach (var item in chips)
+            {
+                if (item % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd++;
+                }
+            }
+
+            return Math.Min(even, odd);
+        }
+    }
+}
