0330c2e6ba691041505e37fb321b30e02a2a51ab
Woody-Hu <812791840@qq.com>
2019-11-27 07:55:12 +0800

    Create Shift 2D Grid.cs

---
+++ LeetCode/LeetCode/Shift 2D Grid.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Shift_2D_Grid
+    {
+        public IList<IList<int>> ShiftGrid(int[][] grid, int k)
+        {
+            int sizeN = grid.Length, sizeM = grid[0].Length;
+            int index = (sizeM * sizeN - (k % (sizeM * sizeN))) % (sizeM * sizeN);
+
+            var shifted = new List<IList<int>>();
+            for (int i = 0; i < sizeN; ++i)
+            {
+                var row = new List<int>();
+                for (int j = 0; j < sizeM; ++j)
+                {
+                    row.Add(grid[index / sizeM][index % sizeM]);
+                    index = (index + 1) % (sizeN * sizeM);
+                }
+                shifted.Add(row);
+            }
+
+            return shifted;
+        }
+    }
+}
