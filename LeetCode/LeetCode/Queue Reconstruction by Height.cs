d149d90061662691e8df36ce9370cf3c36831f1c
Woody-Hu <812791840@qq.com>
2019-12-04 08:43:30 +0800

    Create Queue Reconstruction by Height.cs

---
+++ LeetCode/LeetCode/Queue Reconstruction by Height.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class Queue_Reconstruction_by_Height
+    {
+        public int[][] ReconstructQueue(int[][] people)
+        {
+            var peopleList = new List<int[]>();
+            var output = new List<int[]>();
+            peopleList = people.OrderByDescending(a => a[0]).ThenBy(a => a[1]).ToList();
+            for (int i = 0; i < peopleList.Count; i++)
+            {
+                output.Insert(peopleList[i][1], peopleList[i]);
+            }
+
+            return output.ToArray();
+        }
+    }
+}
