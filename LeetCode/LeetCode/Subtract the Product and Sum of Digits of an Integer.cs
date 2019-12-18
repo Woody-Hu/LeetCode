2d443d468d376c1ed12b47e954178dc0ad19394e
Woody-Hu <812791840@qq.com>
2019-12-19 07:46:09 +0800

    Create Subtract the Product and Sum of Digits of an Integer.cs

---
+++ LeetCode/LeetCode/Subtract the Product and Sum of Digits of an Integer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Subtract_the_Product_and_Sum_of_Digits_of_an_Integer
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            var sum = 0;
+            var pro = 1;
+
+            while (n != 0)
+            {
+                var digt = n % 10;
+                sum += digt;
+                pro *= digt;
+                n /= 10;
+            }
+
+            return pro - sum;
+        }
+    }
+}
