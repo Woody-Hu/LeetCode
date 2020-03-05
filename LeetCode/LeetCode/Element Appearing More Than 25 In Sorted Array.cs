272e06c7236c85315ddcd3d2b9be1d4adbdb7ec2
Stephenhu <stephenhu@seismic.com>
2020-03-05 15:32:59 +0800

    Update Element Appearing More Than 25 In Sorted Array.cs

--- LeetCode/LeetCode/Element Appearing More Than 25 In Sorted Array.cs
+++ LeetCode/LeetCode/Element Appearing More Than 25 In Sorted Array.cs
@@ -6,5 +6,25 @@ namespace LeetCode
 {
     class Element_Appearing_More_Than_25_In_Sorted_Array
     {
+        public int FindSpecialInteger(int[] arr)
+        {
+            int max = arr.Length / 4;
+            int i = 0;
+            while (i < arr.Length)
+            {
+                int num = arr[i];
+                if (i + max < arr.Length && arr[i + max] == num)
+                {
+                    return num;
+                }
+
+                while (arr[i] == num)
+                {
+                    i++;
+                }
+            }
+
+            return 1;
+        }
     }
 }
