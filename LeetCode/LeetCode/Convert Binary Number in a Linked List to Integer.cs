5074b6984ff4b1649ebcc1f92e4624e19c1a8b03
Stephenhu <stephenhu@seismic.com>
2020-03-12 09:52:26 +0800

    Update Convert Binary Number in a Linked List to Integer.cs

--- LeetCode/LeetCode/Convert Binary Number in a Linked List to Integer.cs
+++ LeetCode/LeetCode/Convert Binary Number in a Linked List to Integer.cs
@@ -6,5 +6,17 @@ namespace LeetCode
 {
     class Convert_Binary_Number_in_a_Linked_List_to_Integer
     {
+        public int GetDecimalValue(ListNode head)
+        {
+            var stringBuilder = new StringBuilder();
+            var current = head;
+            while (current != null)
+            {
+                stringBuilder.Append(current.val);
+                current = current.next;
+            }
+
+            return Convert.ToInt32(stringBuilder.ToString(), 2);
+        }
     }
 }
