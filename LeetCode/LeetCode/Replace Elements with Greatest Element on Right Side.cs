9b69b37de855cd1862ee903564ae820b3bed088f
Stephenhu <stephenhu@seismic.com>
2020-03-16 14:33:06 +0800

    Create Replace Elements with Greatest Element on Right Side.cs

---
+++ LeetCode/LeetCode/Replace Elements with Greatest Element on Right Side.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Replace_Elements_with_Greatest_Element_on_Right_Side
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            var max = -1;
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                var temp = arr[i];
+                arr[i] = max;
+                max = Math.Max(temp, max);
+            }
+
+            return arr;
+        }
+    }
+}
