1e53ae151fc808f66d374ee4ed2ac4e5c1162113
Stephenhu <stephenhu@seismic.com>
2020-03-17 10:53:16 +0800

    Create Find N Unique Integers Sum up to Zero.cs

---
+++ LeetCode/LeetCode/Find N Unique Integers Sum up to Zero.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Find_N_Unique_Integers_Sum_up_to_Zero
+    {
+        public int[] SumZero(int n)
+        {
+            if (n == 1)
+            {
+                return new int[] { 0 };
+            }
+
+            int[] res;
+            int value = 1;
+            var i = 0;
+            res = new int[n];
+            if (n % 2 != 0)
+            {
+                res[0] = 0;
+                i = 1;
+            }
+
+            for ( ; i < n; i = i +2)
+            {
+                res[i] = value;
+                res[i + 1] = -value;
+                value++;
+            }
+
+            return res;
+        }
+    }
+}
