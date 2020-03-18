a588176e0113ac039ebfd9614c0c00ae21974b9d
Stephenhu <stephenhu@seismic.com>
2020-03-18 09:12:02 +0800

    Create Decrypt String from Alphabet to Integer Mapping.cs

---
+++ LeetCode/LeetCode/Decrypt String from Alphabet to Integer Mapping.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Decrypt_String_from_Alphabet_to_Integer_Mapping
+    {
+        public string FreqAlphabets(string s)
+        {
+            var stringBuilder = new StringBuilder();
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                int next = 'a' - 1, offset = 0;
+                if (i + 2 < s.Length && s[i + 2] == '#')
+                {
+                    offset = (s[i++] - '0') * 10 + (s[i++] - '0');
+                }
+                else
+                {
+                    offset = s[i] - '0';
+                }
+
+                stringBuilder.Append((char)(next + offset));
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
