6bf910e1e4dd1921e640c929bef6a15fd84952e2
Stephenhu <stephenhu@seismic.com>
2020-03-19 08:17:57 +0800

    Create Decompress Run-Length Encoded List.cs

---
+++ LeetCode/LeetCode/Decompress Run-Length Encoded List.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Decompress_Run_Length_Encoded_List
+    {
+        public int[] DecompressRLElist(int[] nums)
+        {
+            var res = new List<int>();
+            for (int i = 0; i < nums.Length; i = i + 2)
+            {
+                for (int j = 0; j < nums[i]; j++)
+                {
+                    res.Add(nums[i + 1]);
+                }
+            }
+
+            return res.ToArray();
+        }
+    }
+}
