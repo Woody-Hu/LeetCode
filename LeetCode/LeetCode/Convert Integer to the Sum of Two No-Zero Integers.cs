36a0cd56679f307c5439d20ea1ab7b2bfe61d3fa
Stephenhu <stephenhu@seismic.com>
2020-03-20 08:32:50 +0800

    Create Convert Integer to the Sum of Two No-Zero Integers.cs

---
+++ LeetCode/LeetCode/Convert Integer to the Sum of Two No-Zero Integers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Convert_Integer_to_the_Sum_of_Two_No_Zero_Integers
+    {
+        public int[] GetNoZeroIntegers(int n)
+        {
+            if (n < 10)
+                return new[] { 1, n - 1 };
+
+            int x = n;
+            int i = 0;
+            int a = 0;
+            while (n >= 10)
+            {
+                int r = n % 10;
+                int d = (r == 1) ? 2 : 1;
+                int pow10 = 1;
+                int j = i;
+                while (j-- > 0)
+                    pow10 *= 10;
+                a += d * pow10;
+                i += 1;
+                n -= d;
+                n /= 10;
+            }
+
+            return new[] { a, x - a };
+        }
+    }
+}
