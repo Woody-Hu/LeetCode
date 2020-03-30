35560cc5400b63293a0540004d50093803ff7961
Stephenhu <stephenhu@seismic.com>
2020-03-30 16:45:37 +0800

    Create Maximum 69 Number.cs

---
+++ LeetCode/LeetCode/Maximum 69 Number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Maximum_69_Number
+    {
+        public int Maximum69Number(int num)
+        {
+            var temp = num;
+            var max = num;
+            var multi = 1;
+            while (temp > 0)
+            {
+                if (temp % 10 == 6)
+                {
+                    var updated = num;
+                    updated -= 6 * multi;
+                    updated += 9 * multi;
+                    max = Math.Max(updated, max);
+                }
+                temp /= 10;
+                multi *= 10;
+            }
+            return max;
+        }
+    }
+}
