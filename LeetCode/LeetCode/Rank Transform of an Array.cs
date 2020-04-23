4a5cff711ecfa6ca440511cf42b8a63beea7293d
Stephenhu <stephenhu@seismic.com>
2020-04-23 09:48:32 +0800

    Create Rank Transform of an Array.cs

---
+++ LeetCode/LeetCode/Rank Transform of an Array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Rank_Transform_of_an_Array
+    {
+        public int[] ArrayRankTransform(int[] arr)
+        {
+            var res = new int[arr.Length];
+            arr.CopyTo(res, 0);
+            Array.Sort(res);
+            var dict = new Dictionary<int, int>();
+            var currentRank = 1;
+            foreach (var item in res)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict.Add(item, currentRank++);
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res[i] = dict[arr[i]];
+            }
+
+            return res;
+        }
+
+    }
+}
