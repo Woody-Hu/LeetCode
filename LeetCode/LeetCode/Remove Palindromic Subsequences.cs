67b8b67d42f28861efc4eb943ee62f8285a3417c
Stephenhu <stephenhu@seismic.com>
2020-04-24 14:08:15 +0800

    Create Remove Palindromic Subsequences.cs

---
+++ LeetCode/LeetCode/Remove Palindromic Subsequences.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Remove_Palindromic_Subsequences
+    {
+        public int RemovePalindromeSub(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return 0;
+            }
+
+            if (Check(s))
+            {
+                return 1;
+            }
+
+            return 2;
+        }
+
+        private bool Check(string s)
+        {
+            var i = 0;
+            var j = s.Length - 1;
+            while (i < j)
+            {
+                if (s[i] != s[j])
+                {
+                    return false; 
+                }
+
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
