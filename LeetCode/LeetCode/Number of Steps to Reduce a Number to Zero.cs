139efe83a65842996366374d82e7f918b07ca4c1
Stephenhu <stephenhu@seismic.com>
2020-04-28 09:10:20 +0800

    Create Number of Steps to Reduce a Number to Zero.cs

---
+++ LeetCode/LeetCode/Number of Steps to Reduce a Number to Zero.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Number_of_Steps_to_Reduce_a_Number_to_Zero
+    {
+        public int NumberOfSteps(int num)
+        {
+            var res = 0;
+            while (num != 0)
+            {
+                res++;
+                if (num % 2 == 0)
+                {
+                    num = num / 2;
+                }
+                else
+                {
+                    num = num - 1;
+                }
+            }
+
+            return res;
+        }
+    }
+}
