332b7a4bbb08feaec11524f9ed09b3e3cef65dfb
Stephenhu <stephenhu@seismic.com>
2020-04-29 08:48:25 +0800

    Create Check If N and Its Double Exist.cs

---
+++ LeetCode/LeetCode/Check If N and Its Double Exist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Check_If_N_and_Its_Double_Exist
+    {
+        public bool CheckIfExist(int[] arr)
+        {
+            Array.Sort(arr);
+            var set = new HashSet<int>();
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                if (set.Contains(arr[i] * 2) || (arr[i] % 2 == 0 && set.Contains(arr[i] / 2)))
+                {
+                    return true;
+                }
+
+                set.Add(arr[i]);
+            }
+
+            return false;
+        }
+    }
+}
