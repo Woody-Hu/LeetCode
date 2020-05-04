47cffe9342df3f1cbfc67916ba7328d73cf55f59
Stephenhu <stephenhu@seismic.com>
2020-05-04 08:02:05 +0800

    Create Sort Integers by The Number of 1 Bits.cs

---
+++ LeetCode/LeetCode/Sort Integers by The Number of 1 Bits.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Sort_Integers_by_The_Number_of_1_Bits
+    {
+        public int[] SortByBits(int[] arr)
+        {
+            Array.Sort(arr, (n1, n2) =>
+            {
+                var count1 = GetCount(n1);
+                var count2 = GetCount(n2);
+                var onesCmp = count1.CompareTo(count2);
+                if (onesCmp != 0)
+                {
+                    return onesCmp;
+                }
+
+                return n1.CompareTo(n2);
+            });
+
+            return arr;
+        }
+
+        private int GetCount(int value)
+        {
+            var res = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                if ((value & (1 << i)) != 0)
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
