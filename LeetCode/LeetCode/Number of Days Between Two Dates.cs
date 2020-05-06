a0a8f30413c87a2dd58d212577fe3cb2b246f65c
Stephenhu <stephenhu@seismic.com>
2020-05-06 10:01:32 +0800

    Create Number of Days Between Two Dates.cs

---
+++ LeetCode/LeetCode/Number of Days Between Two Dates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Number_of_Days_Between_Two_Dates
+    {
+        public int DaysBetweenDates(string date1, string date2)
+        {
+            var dateTime1 = DateTime.Parse(date1);
+            var dateTime2 = DateTime.Parse(date2);
+
+            return Math.Abs((int)((dateTime2 - dateTime1).TotalDays));
+        }
+    }
+}
