9dbc2e1af73932df39c68100ef674b6cd46a9e91
Stephenhu <stephenhu@seismic.com>
2020-05-11 09:06:44 +0800

    Create How Many Numbers Are Smaller Than the Current Number.cs

---
+++ LeetCode/LeetCode/How Many Numbers Are Smaller Than the Current Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class How_Many_Numbers_Are_Smaller_Than_the_Current_Number
+    {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            var copy = new int[nums.Length];
+            nums.CopyTo(copy, 0);
+            Array.Sort(copy);
+            var list = new List<int>();
+            foreach (var item in nums)
+            {
+                list.Add(Array.IndexOf(copy, item));
+            }
+
+            return list.ToArray();
+        }
+    }
+}
