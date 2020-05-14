dec766e8cebc70d2f6906bc0fb09760f942fc1f2
Stephenhu <stephenhu@seismic.com>
2020-05-14 09:52:57 +0800

    Create Generate a String With Characters That Have Odd Counts.cs

---
+++ LeetCode/LeetCode/Generate a String With Characters That Have Odd Counts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Generate_a_String_With_Characters_That_Have_Odd_Counts
+    {
+        public string GenerateTheString(int n)
+        {
+            var res = new StringBuilder();
+            if (n % 2 == 0)
+            {
+                for (int i = 0; i < n - 1 ; i++)
+                {
+                    res.Append('a');
+                }
+
+                res.Append('b');
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    res.Append('a');
+                }
+            }
+
+            return res.ToString();
+        }
+    }
+}
