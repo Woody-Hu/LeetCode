8f75a95af505fb358e439e5e0057aacb134883ec
Stephenhu <stephenhu@seismic.com>
2020-05-15 12:49:16 +0800

    Update Find Lucky Integer in an Array.cs

--- LeetCode/LeetCode/Find Lucky Integer in an Array.cs
+++ LeetCode/LeetCode/Find Lucky Integer in an Array.cs
@@ -6,5 +6,30 @@ namespace LeetCode
 {
     class Find_Lucky_Integer_in_an_Array
     {
+        public int FindLucky(int[] arr)
+        {
+            var dict = new Dictionary<int, int>();
+            foreach (var item in arr)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict.Add(item, 0);
+                }
+
+                dict[item]++;
+            }
+
+            var res = -1;
+
+            foreach (var item in dict)
+            {
+                if (item.Key == item.Value)
+                {
+                    res = Math.Max(item.Key, res);
+                }
+            }
+
+            return res;
+        }
     }
 }
