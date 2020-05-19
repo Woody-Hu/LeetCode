f63c386f7428e56f20033306f0082d90fcf8d750
Stephenhu <stephenhu@seismic.com>
2020-05-19 09:06:13 +0800

    Create Count Largest Group.cs

---
+++ LeetCode/LeetCode/Count Largest Group.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Count_Largest_Group
+    {
+        public int CountLargestGroup(int n)
+        {
+			int largest = 0, numLargest = 0;
+			int[] arr = new int[37];
+			for (int i = 1; i <= n; i++)
+			{
+				int temp = i, digitSum = 0;
+				while (temp > 0)
+				{
+					digitSum += temp % 10;
+					temp /= 10;
+				}
+
+				arr[digitSum]++;
+				if (arr[digitSum] > largest)
+				{
+					largest = arr[digitSum];
+					numLargest = 1;
+				}
+				else if (arr[digitSum] == largest) numLargest++;
+			}
+
+			return numLargest;
+		}
+    }
+}
