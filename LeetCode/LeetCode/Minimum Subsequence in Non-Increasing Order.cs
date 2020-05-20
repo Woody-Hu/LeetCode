bcb2c48a567289ab90305868650d8d0461e3073f
Stephenhu <stephenhu@seismic.com>
2020-05-20 13:26:42 +0800

    Update Minimum Subsequence in Non-Increasing Order.cs

--- LeetCode/LeetCode/Minimum Subsequence in Non-Increasing Order.cs
+++ LeetCode/LeetCode/Minimum Subsequence in Non-Increasing Order.cs
@@ -6,5 +6,28 @@ namespace LeetCode
 {
     class Minimum_Subsequence_in_Non_Increasing_Order
     {
+        public IList<int> MinSubsequence(int[] nums)
+        {
+            Array.Sort(nums);
+            var sum = 0;
+            foreach (var oneNumber in nums)
+            {
+                sum += oneNumber;
+            }
+
+            var res = new List<int>();
+            var temp = 0;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                res.Add(nums[i]);
+                temp += nums[i];
+                if (temp > sum - temp)
+                {
+                    break;
+                }
+            }
+
+            return res;
+        }
     }
 }
