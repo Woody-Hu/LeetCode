30c8a2e23d82e8e362ed3b62430e1996cdabf8ff
Stephenhu <stephenhu@seismic.com>
2020-05-21 09:35:54 +0800

    Create Create Target Array in the Given Order.cs

---
+++ LeetCode/LeetCode/Create Target Array in the Given Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Create_Target_Array_in_the_Given_Order
+    {
+        public int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            var res = new int[nums.Length];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (int j = res.Length - 1; j >= index[i] + 1; j--)
+                {
+                    res[j] = res[j - 1];
+                }
+
+                res[index[i]] = nums[i];
+            }
+
+            return res;
+        }
+    }
+}
