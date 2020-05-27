ec59680eb070c874e78d6914a77af765f83007f0
Stephenhu <stephenhu@seismic.com>
2020-05-27 08:54:40 +0800

    Create Minimum Value to Get Positive Step by Step Sum.cs

---
+++ LeetCode/LeetCode/Minimum Value to Get Positive Step by Step Sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Minimum_Value_to_Get_Positive_Step_by_Step_Sum
+    {
+        public int MinStartValue(int[] nums)
+        {
+            var min = int.MaxValue;
+            var sum = 0;
+            foreach (var num in nums)
+            {
+                sum += num;
+                min = Math.Min(min, sum);
+            }
+
+            if (min > 0)
+            {
+                return 1;
+            }
+
+            return -min + 1;
+        }
+    }
+}
