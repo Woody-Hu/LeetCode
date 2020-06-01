659ada3b2acdd3185a57df84146a1eecfa1d0fa3
Stephenhu <stephenhu@seismic.com>
2020-06-01 15:36:57 +0800

    Create Maximum Score After Splitting a String.cs

---
+++ LeetCode/LeetCode/Maximum Score After Splitting a String.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Maximum_Score_After_Splitting_a_String
+    {
+        public int MaxScore(string s)
+        {
+            int left = 0;
+            int right = 0;
+            int total = 0;
+            if (s.Length == 0) return total;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '1') right++;
+            }
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] == '0') left++;
+                if (s[i] == '1') right--;
+                total = Math.Max(total, left + right);
+            }
+
+            return total;
+        }
+    }
+}
