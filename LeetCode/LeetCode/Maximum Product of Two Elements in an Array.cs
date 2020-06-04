7c6195a9a03baa99116cea07de1e0323037648ee
Stephenhu <stephenhu@seismic.com>
2020-06-04 08:23:50 +0800

    Create Maximum Product of Two Elements in an Array.cs

---
+++ LeetCode/LeetCode/Maximum Product of Two Elements in an Array.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Maximum_Product_of_Two_Elements_in_an_Array
+    {
+        public int MaxProduct(int[] nums)
+        {
+            Array.Sort(nums);
+            return (nums[nums.Length - 1] - 1) * (nums[nums.Length - 2] - 1);
+        }
+    }
+}
