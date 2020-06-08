5715aa7508822944a943880efcbcd98fdac77bbb
Stephenhu <stephenhu@seismic.com>
2020-06-08 09:06:08 +0800

    Create Shuffle the Array.cs

---
+++ LeetCode/LeetCode/Shuffle the Array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Shuffle_the_Array
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+            var res = new int[2 * n];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    res[i] = nums[i / 2];
+                }
+                else
+                {
+                    res[i] = nums[n + i / 2];
+                }
+            }
+
+            return res;
+        }
+    }
+}
