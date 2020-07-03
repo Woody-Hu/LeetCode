a365a6f0e88289fbfd69ba6630cff07df02b43e6
Stephenhu <stephenhu@seismic.com>
2020-07-03 18:23:48 +0800

    Create Running Sum of 1d Array.cs

---
+++ LeetCode/LeetCode/Running Sum of 1d Array.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Running_Sum_of_1d_Array
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+
+            return nums;
+        }
+    }
+}
