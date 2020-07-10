bc3f63d9b9bd6825cbac730c422a8f5092dcf6be
Stephenhu <stephenhu@seismic.com>
2020-07-10 14:19:11 +0800

    Create XOR Operation in an Array.cs

---
+++ LeetCode/LeetCode/XOR Operation in an Array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class XOR_Operation_in_an_Array
+    {
+        public int XorOperation(int n, int start)
+        {
+            int res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                var num = start + 2 * (i);
+                res = res ^ num;
+            }
+            return res;
+        }
+    }
+}
