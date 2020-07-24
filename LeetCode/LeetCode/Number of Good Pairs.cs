c679deb7ab616a1b98290e53a6ea71b9f0dfde19
Stephenhu <stephenhu@seismic.com>
2020-07-24 15:27:54 +0800

    Update Number of Good Pairs.cs

--- LeetCode/LeetCode/Number of Good Pairs.cs
+++ LeetCode/LeetCode/Number of Good Pairs.cs
@@ -6,5 +6,25 @@ namespace LeetCode
 {
     class Number_of_Good_Pairs
     {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                dict[item] = dict.GetValueOrDefault(item) + 1;
+            }
+
+            var res = 0;
+            foreach (var onePair in dict)
+            {
+                var temp = (onePair.Value) * (onePair.Value - 1) / 2;
+                if (temp > 0)
+                {
+                    res += temp;
+                }
+            }
+
+            return res;
+        }
     }
 }
