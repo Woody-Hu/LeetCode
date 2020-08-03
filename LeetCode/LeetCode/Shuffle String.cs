da9591e82d0f6bb0040dcd0af55e84e30e0bd086
Stephenhu <stephenhu@seismic.com>
2020-08-03 09:15:09 +0800

    Create Shuffle String.cs

---
+++ LeetCode/LeetCode/Shuffle String.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Shuffle_String
+    {
+        public string RestoreString(string s, int[] indices)
+        {
+            var length = s.Length;
+            char[] res = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                res[indices[i]] = s[i];
+            }
+
+            return new string(res);
+        }
+    }
+}
