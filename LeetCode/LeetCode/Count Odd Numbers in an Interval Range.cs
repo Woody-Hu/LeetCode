9feccd0e1d59c5a511c605225e5d8f994dc3e4db
Stephenhu <stephenhu@seismic.com>
2020-08-07 09:14:16 +0800

    Create Count Odd Numbers in an Interval Range.cs

---
+++ LeetCode/LeetCode/Count Odd Numbers in an Interval Range.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Count_Odd_Numbers_in_an_Interval_Range
+    {
+        public int CountOdds(int low, int high)
+        {
+            int interval = 0;
+            if (low % 2 != 0 || high % 2 != 0)
+                interval = (high - low) / 2 + 1;
+            else
+                interval = (high - low) / 2;
+            return interval;
+        }
+    }
+}
