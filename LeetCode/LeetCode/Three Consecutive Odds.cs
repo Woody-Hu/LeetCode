3622f9c31b62d8ebd776042262b6350dc89689f0
Stephen Hu <stephenhu@seismic.com>
2020-09-18 15:34:37 +0800

    2020 09 18

---
+++ LeetCode/LeetCode/Three Consecutive Odds.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Three_Consecutive_Odds
+    {
+        public Three_Consecutive_Odds()
+        {
+        }
+
+        public bool ThreeConsecutiveOdds(int[] arr)
+        {
+            var count = 0;
+            foreach (var item in arr)
+            {
+                if (item % 2 == 0)
+                {
+                    count = 0;
+                }
+                else
+                {
+                    count++;
+                }
+
+                if (count == 3)
+                {
+                    return true;
+                }
+            }
+
+            return count >= 3;
+        }
+    }
+}
