e29abf98d1a7762489e76dfb01f6e4c0a8ee9234
Stephen Hu <stephenhu@seismic.com>
2020-09-24 09:12:49 +0800

    2020 09 24

---
+++ LeetCode/LeetCode/ThousandSeparator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    public class ThousandSeparator
+    {
+        public ThousandSeparator()
+        {
+        }
+
+        public string ThousandSeparatorMethod(int n)
+        {
+            var s = n.ToString();
+            var b = new StringBuilder();
+
+            int j = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (j == 3)
+                {
+                    b.Insert(0, ".");
+                    j = 0;
+                }
+                b.Insert(0, s[i]);
+                j++;
+            }
+
+            return b.ToString();
+        }
+    }
+}
