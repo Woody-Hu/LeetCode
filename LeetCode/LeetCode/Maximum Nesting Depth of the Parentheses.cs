1916e7265177d3471b70eee898898b6e1f8fadd5
Stephen Hu <stephenhu@seismic.com>
2020-10-23 15:37:16 +0800

    2020 10 23

---
+++ LeetCode/LeetCode/Maximum Nesting Depth of the Parentheses.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Maximum_Nesting_Depth_of_the_Parentheses
+    {
+        public Maximum_Nesting_Depth_of_the_Parentheses()
+        {
+        }
+
+        public int MaxDepth(string s)
+        {
+            var res = int.MinValue;
+            var count = 0;
+            foreach (var item in s)
+            {
+                if (item == '(')
+                {
+                    count++;
+                }
+                else if (item == ')')
+                {
+                    count--;
+                }
+
+                res = Math.Max(count, res);
+
+            }
+
+            return res;
+        }
+    }
+}
