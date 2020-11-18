9fa42eeb4ef9ad6890ca7aa56df3382668f6fdc8
Stephen Hu <stephenhu@seismic.com>
2020-11-18 08:36:40 +0800

    Update Sort Array by Increasing Frequency.cs

--- LeetCode/LeetCode/Sort Array by Increasing Frequency.cs
+++ LeetCode/LeetCode/Sort Array by Increasing Frequency.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Linq;
+
 namespace LeetCode
 {
     public class Sort_Array_by_Increasing_Frequency
     {
         public Sort_Array_by_Increasing_Frequency()
         {
+        }
+
+        public int[] FrequencySort(int[] nums)
+        {
+            var groups = nums.GroupBy(k => k).OrderBy(k => k.Count()).ThenByDescending(k => k.Key);
+            var res = new int[nums.Length];
+            var index = 0;
+            foreach (var item in groups)
+            {
+                item.ToArray().CopyTo(res, index);
+                index = index + item.Count();
+            }
+
+            return res;
         }
     }
 }
