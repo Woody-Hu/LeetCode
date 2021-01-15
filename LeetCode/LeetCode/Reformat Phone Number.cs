3e166b7e5a6bd4ae2c4d86836692ca8a9d1d4eff
Stephen Hu <stephenhu@seismic.com>
2021-01-15 09:49:26 +0800

    Create Reformat Phone Number.cs

---
+++ LeetCode/LeetCode/Reformat Phone Number.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Reformat_Phone_Number
+    {
+        public string ReformatNumber(string number)
+        {
+            StringBuilder st = new StringBuilder();
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] >= '0' && number[i] <= '9')
+                {
+                    st.Append(number[i]);
+                }
+            }
+
+            int len = st.Length;
+            int end = len - 1;
+
+            if (len % 3 != 0 && len > 2)
+            {
+                st.Insert(end - 1, '-');
+                end -= 2;
+
+                if (len % 3 == 1 && len > 4)
+                {
+                    st.Insert(end - 1, '-');
+                    end -= 2;
+                }
+            }
+
+            for (int i = end - 2; i > 0; i -= 3)
+            {
+                st.Insert(i, '-');
+            }
+
+            return st.ToString();
+        }
+    }
+}
