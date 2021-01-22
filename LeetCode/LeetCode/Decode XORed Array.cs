ff1a65f96d168c20a477d24ea5213b957fcb6700
Stephen Hu <stephenhu@seismic.com>
2021-01-22 09:41:37 +0800

    Create Decode XORed Array.cs

---
+++ LeetCode/LeetCode/Decode XORed Array.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Decode_XORed_Array
+    {
+        public int[] Decode(int[] encoded, int first)
+        {
+            var res = new int[encoded.Length + 1];
+            res[0] = first;
+            for (int i = 0; i < encoded.Length; i++)
+            {
+                res[i + 1] = res[i] ^ encoded[i];
+            }
+
+            return res;
+        }
+    }
+}
