06874bc5176b22d02a3893f2966649e5942d13e8
Stephen Hu <stephenhu@seismic.com>
2021-02-02 09:58:57 +0800

    Create Find the Highest Altitude.cs

---
+++ LeetCode/LeetCode/Find the Highest Altitude.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Find_the_Highest_Altitude
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            var res = 0;
+            var current = res;
+            foreach (var item in gain)
+            {
+                var next = current + item;
+                res = Math.Max(res, next);
+                current = next;
+            }
+
+            return res;
+        }
+    }
+}
