f13aa907f2a9f5c7319ef31e8ed39ff0b3f5aa9c
Stephen Hu <stephenhu@seismic.com>
2021-02-07 09:33:26 +0800

    Create Sum of Unique Elements.cs

---
+++ LeetCode/LeetCode/Sum of Unique Elements.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Sum_of_Unique_Elements
+    {
+        public int SumOfUnique(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                dict[item] = dict.GetValueOrDefault(item) + 1;
+            }
+
+            var res = 0;
+            foreach (var item in dict)
+            {
+                if (item.Value == 1)
+                {
+                    res += item.Key;
+                }
+            }
+
+            return res;
+        }
+    }
+}
