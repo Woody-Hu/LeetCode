7312e6a417cbe58663cdf2f0f050161eda6ee25c
Stephen Hu <stephenhu@seismic.com>
2021-02-26 09:16:28 +0800

    Create Minimum Changes To Make Alternating Binary String.cs

---
+++ LeetCode/LeetCode/Minimum Changes To Make Alternating Binary String.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Minimum_Changes_To_Make_Alternating_Binary_String
+    {
+        public int MinOperations(string s)
+        {
+            var zeroones = 0;
+            var onezeros = 0;
+            int i = 0;
+            foreach (var ch in s)
+            {
+                int n = ch - '0';
+                zeroones += Math.Abs(i % 2 - n);
+                onezeros += Math.Abs((i + 1) % 2 - n);
+                i++;
+            }
+
+
+            return Math.Min(zeroones, onezeros);
+        }
+    }
+}
