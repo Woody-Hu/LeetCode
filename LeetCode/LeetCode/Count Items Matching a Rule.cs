8dbf308e5406b963a1f43aa615fdb38e8ba025ab
Stephen Hu <stephenhu@seismic.com>
2021-03-02 16:45:53 +0800

    2022 03 02

---
+++ LeetCode/LeetCode/Count Items Matching a Rule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Count_Items_Matching_a_Rule
+    {
+        public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            var res = 0;
+            var index = 0;
+            if (ruleKey[0] == 't')
+            {
+                index = 0;
+            }
+            else if (ruleKey[0] == 'c')
+            {
+                index = 1;
+            }
+            else
+            {
+                index = 2;
+            }
+
+            foreach (var item in items)
+            {
+                if (item[index].Equals(ruleValue, StringComparison.OrdinalIgnoreCase))
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
