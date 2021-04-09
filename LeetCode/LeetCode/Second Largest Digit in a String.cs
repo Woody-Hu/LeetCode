533fc4c285e177160aaebff016283f25b552056b
Stephen Hu <stephenhu@seismic.com>
2021-04-09 12:30:18 +0800

    Create Second Largest Digit in a String.cs

---
+++ LeetCode/LeetCode/Second Largest Digit in a String.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Second_Largest_Digit_in_a_String
+    {
+        public int SecondHighest(string s)
+        {
+            var res = -1;
+            var max = -1;
+            foreach (var item in s)
+            {
+                if (!char.IsDigit(item))
+                {
+                    continue;
+                }
+
+                var value = item - '0';
+                if (value > max)
+                {
+                    res = max;
+                    max = value;
+                }
+                else if (value > res && value < max)
+                {
+                    res = value;
+                }
+            }
+
+            return res;
+        }
+    }
+}
