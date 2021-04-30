7430b4371c41a578d15138fb826514f1112f8ba5
Stephen Hu <stephenhu@seismic.com>
2021-04-30 09:43:56 +0800

    Create Maximum Ascending Subarray Sum.cs

---
+++ LeetCode/LeetCode/Maximum Ascending Subarray Sum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Maximum_Ascending_Subarray_Sum
+    {
+        public int MaxAscendingSum(int[] nums)
+        {
+            var res = nums[0];
+            var currentSum = nums[0];
+            var top = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] <= top)
+                {
+                    res = Math.Max(res, currentSum);
+                    currentSum = nums[i];
+                }
+                else
+                {
+                    currentSum += nums[i];
+                }
+
+                top = nums[i];
+            }
+
+            res = Math.Max(res, currentSum);
+            return res;
+
+        }
+    }
+}
