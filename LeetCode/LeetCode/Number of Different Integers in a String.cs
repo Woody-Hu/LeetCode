d42f32c5b14ae4a2600b1d3b8ef8f5008bf98d06
Stephen Hu <stephenhu@seismic.com>
2021-05-28 12:24:50 +0800

    Create Number of Different Integers in a String.cs

---
+++ LeetCode/LeetCode/Number of Different Integers in a String.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Number_of_Different_Integers_in_a_String
+    {
+        public int NumDifferentIntegers(string word)
+        {
+            var set = new HashSet<string>();
+            var current = new StringBuilder();
+            foreach (var item in word)
+            {
+                if (!char.IsDigit(item))
+                {
+                    if (current.Length != 0)
+                    {
+                        set.Add(current.ToString().TrimStart('0'));
+                    }
+
+                    current = new StringBuilder();
+                }
+                else
+                {
+                    current.Append(item);
+                }
+            }
+
+            if (current.Length != 0)
+            {
+                set.Add(current.ToString().TrimStart('0'));
+            }
+
+            return set.Count;
+        }
+    }
+}
