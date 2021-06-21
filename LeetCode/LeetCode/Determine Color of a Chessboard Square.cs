b57721a25e37156e167d23cb3dfaf5bd16cac45d
Stephen Hu <stephenhu@seismic.com>
2021-06-21 12:40:53 +0800

    Create Determine Color of a Chessboard Square.cs

---
+++ LeetCode/LeetCode/Determine Color of a Chessboard Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Determine_Color_of_a_Chessboard_Square
+    {
+        public bool SquareIsWhite(string coordinates)
+        {
+            var columnKind = (coordinates[0] - 'a') % 2;
+            var rowKind = (coordinates[1] - '0') % 2;
+            if (columnKind == 0 && rowKind == 1)
+            {
+                return false;
+            }
+            else if (columnKind == 0 && rowKind == 0)
+            {
+                return true;
+            }
+            else if (columnKind == 1 && rowKind == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
