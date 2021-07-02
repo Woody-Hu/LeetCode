823ef9c81c2a66ac8416e96a6c14f179c7f74dd4
Stephen Hu <stephenhu@seismic.com>
2021-07-02 09:08:03 +0800

    Create Truncate Sentence.cs

---
+++ LeetCode/LeetCode/Truncate Sentence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Truncate_Sentence
+    {
+        public string TruncateSentence(string s, int k)
+        {
+            var splited = s.Split(' ');
+            if (k >= splited.Length)
+            {
+                return s;
+            }
+
+            var stringBuilder = new StringBuilder();
+            for (int i = 1; i <= k; i++)
+            {
+                if (i != 1)
+                {
+                    stringBuilder.Append(" ");
+                }
+
+                stringBuilder.Append(splited[i - 1]);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
