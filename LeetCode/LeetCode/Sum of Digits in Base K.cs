f377cecd25ae79f3eeb6ef29bbd66d4dde03172f
Stephen Hu <stephenhu@seismic.com>
2021-07-09 08:14:31 +0800

    Create Sum of Digits in Base K.cs

---
+++ LeetCode/LeetCode/Sum of Digits in Base K.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Sum_of_Digits_in_Base_K
+    {
+        public int SumBase(int n, int k)
+        {
+            var res = 0;
+            while (n > 0)
+            {
+                res += n % k;
+                n /= k;
+            }
+
+            return res;
+        }
+    }
+}
