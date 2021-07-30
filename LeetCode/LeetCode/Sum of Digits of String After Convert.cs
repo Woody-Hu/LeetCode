9601fafa270eb1355bcaae56ca5a2f522d0b3780
Stephen Hu <stephenhu@seismic.com>
2021-07-30 09:48:28 +0800

    Create Sum of Digits of String After Convert.cs

---
+++ LeetCode/LeetCode/Sum of Digits of String After Convert.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Sum_of_Digits_of_String_After_Convert
+    {
+        public int GetLucky(string s, int k)
+        {
+            int res = 0;
+            foreach (var item in s)
+            {
+                var value = item + 1 - 'a';
+                if (value < 10)
+                {
+                    res += value;
+                }
+                else
+                {
+                    res += value / 10 + value % 10;
+                }
+            }
+
+            k--;
+            while (k-- > 0)
+            {
+                res = Calculate(res);
+            }
+
+            return res;
+        }
+
+        private int Calculate(int value)
+        {
+            int sum = 0;
+            while (value > 0)
+            {
+                var temp = value % 10;
+                sum = sum + temp;
+                value = value / 10;
+            }
+            return sum;
+        }
+    }
+}
