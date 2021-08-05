e57061b8cda379893881492a3340bbd09c1f3fcc
Stephen Hu <stephenhu@seismic.com>
2021-08-05 16:46:32 +0800

    Create Replace All Digits with Characters.cs

---
+++ LeetCode/LeetCode/Replace All Digits with Characters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Replace_All_Digits_with_Characters
+    {
+        public string ReplaceDigits(string s)
+        {
+            var array = s.ToCharArray();
+            for (int i = 0; i < array.Length -1; i = i + 2)
+            {
+                var letter = array[i];
+                var value = array[i + 1] - '0' + letter;
+                array[i + 1] = (char)value;
+            }
+
+            return new string(array);
+        }
+    }
+}
