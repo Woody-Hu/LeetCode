fd1f284c8e001913e2c8febfb7cbf8bc2c1e279c
Stephen Hu <stephenhu@seismic.com>
2021-08-13 08:17:31 +0800

    Create Sorting the Sentence.cs

---
+++ LeetCode/LeetCode/Sorting the Sentence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Sorting_the_Sentence
+    {
+        public string SortSentence(string s)
+        {
+            var array = s.Split(' ');
+            var resArray = new string[array.Length];
+            foreach (var item in array)
+            {
+                var index = item[item.Length - 1] - '0';
+                resArray[index - 1] = item.Substring(0, item.Length - 1);
+            }
+
+            return string.Join(' ', resArray);
+        }
+    }
+}
