230366e3a8506c3110a4416ca2b76dcd4aae34c5
Stephen Hu <stephenhu@seismic.com>
2021-08-27 08:17:00 +0800

    Create Check If String Is a Prefix of Array.cs

---
+++ LeetCode/LeetCode/Check If String Is a Prefix of Array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Check_If_String_Is_a_Prefix_of_Array
+    {
+        public bool IsPrefixString(string s, string[] words)
+        {
+            var res = new StringBuilder();
+            foreach (var word in words)
+            {
+                res.Append(word);
+                if (s.Equals(res.ToString()))
+                    return true;
+                if (s.IndexOf(res.ToString()) == -1)
+                    return false;
+            }
+
+            return false;
+        }
+    }
+}
