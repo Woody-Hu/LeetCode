18a42f6c6b2d28fcb13e94975137b0ce33ae5968
Stephen Hu <stephenhu@seismic.com>
2021-09-03 09:40:06 +0800

    Create Check if the Sentence Is Pangram.cs

---
+++ LeetCode/LeetCode/Check if the Sentence Is Pangram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Check_if_the_Sentence_Is_Pangram
+    {
+        public bool CheckIfPangram(string sentence)
+        {
+            var hashSet = new HashSet<char>(26);
+            foreach (var item in sentence)
+            {
+                hashSet.Add(item);
+            }
+
+            return hashSet.Count == 26;
+        }
+    }
+}
