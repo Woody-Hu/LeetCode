0c44b39e1ada3b07992923a7d0908768358f34b1
Stephen Hu <stephenhu@seismic.com>
2021-09-24 15:25:44 +0800

    Create Reverse Prefix of Word.cs

---
+++ LeetCode/LeetCode/Reverse Prefix of Word.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Reverse_Prefix_of_Word
+    {
+        public string ReversePrefix(string word, char ch)
+        {
+            var i = word.IndexOf(ch);
+            if (i < 0)
+            {
+                return word;
+            }
+
+            var array = new char[word.Length];
+            var j = 0;
+            for (; i >= 0; i--, j++)
+            {
+                array[j] = word[i];
+
+            }
+
+            for (; j < word.Length; j++)
+            {
+                array[j] = word[j];
+            }
+
+            return new string(array);
+        }
+    }
+}
