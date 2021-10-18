d012738bf0d69f4ed6c602862bd279820f735fd2
Stephen Hu <stephenhu@seismic.com>
2021-10-19 07:58:51 +0800

    Create Check if Numbers Are Ascending in a Sentence.cs

---
+++ LeetCode/LeetCode/Check if Numbers Are Ascending in a Sentence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Check_if_Numbers_Are_Ascending_in_a_Sentence
+    {
+        public bool AreNumbersAscending(string s)
+        {
+            var splited = s.Split(' ');
+            var tag = 0;
+            foreach (var item in splited)
+            {
+                if (string.IsNullOrWhiteSpace(item))
+                {
+                    continue;
+                }
+
+                if (!char.IsDigit(item[0]))
+                {
+                    continue;
+                }
+
+                var currentValue = int.Parse(item);
+                if (currentValue <= tag)
+                {
+                    return false;
+                }
+
+                tag = currentValue;
+            }
+
+            return true;
+        }
+    }
+}
