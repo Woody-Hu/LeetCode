8e79c8c1308ffaa8593dcae140f7c8bb43f93d35
Stephen Hu <stephenhu@seismic.com>
2021-10-29 10:59:01 +0800

    Create Concatenation of Array.cs

---
+++ LeetCode/LeetCode/Concatenation of Array.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Concatenation_of_Array
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            var res = new int[2 * nums.Length];
+            Array.Copy(nums, res, nums.Length);
+            Array.Copy(nums, 0,  res, nums.Length, nums.Length);
+            return res;
+        }
+    }
+}
