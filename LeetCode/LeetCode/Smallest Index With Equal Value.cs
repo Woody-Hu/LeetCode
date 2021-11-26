dd37df1e33aae9ad4c7f4c74650ae6606b143039
Stephen Hu <stephenhu@seismic.com>
2021-11-26 11:22:09 +0800

    2021 11 26

---
+++ LeetCode/LeetCode/Smallest Index With Equal Value.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Smallest_Index_With_Equal_Value
+    {
+        public int SmallestEqual(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % 10 == nums[i])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
