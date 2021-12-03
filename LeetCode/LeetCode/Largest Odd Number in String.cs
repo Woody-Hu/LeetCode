01272b926c47dd652639989d7b737fb23c2ef1eb
Stephen Hu <stephenhu@seismic.com>
2021-12-03 09:48:03 +0800

    Create Largest Odd Number in String.cs

---
+++ LeetCode/LeetCode/Largest Odd Number in String.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Largest_Odd_Number_in_String
+    {
+        public string LargestOddNumber(string num)
+        {
+            for (int i = num.Length - 1; i >= 0; i--)
+                if (num[i] % 2 != 0) return num.Substring(0, i + 1);
+            return string.Empty;
+        }
+    }
+}
