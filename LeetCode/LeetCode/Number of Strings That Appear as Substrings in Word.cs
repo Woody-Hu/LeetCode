3a90e627ed7b8644572c56d6dfa2fd979794303c
Stephen Hu <stephenhu@seismic.com>
2021-12-09 14:00:56 +0800

    Create Number of Strings That Appear as Substrings in Word.cs

---
+++ LeetCode/LeetCode/Number of Strings That Appear as Substrings in Word.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Number_of_Strings_That_Appear_as_Substrings_in_Word
+    {
+        public int NumOfStrings(string[] patterns, string word)
+        {
+            var res = 0;
+            var set = new HashSet<string>();
+            foreach (var item in patterns)
+            {
+                if (set.Contains(item))
+                {
+                    res++;
+                }
+                else if (word.Contains(item))
+                {
+                    res++;
+                    set.Add(item);
+                }
+            }
+
+            return res;
+        }
+    }
+}
