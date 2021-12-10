364e9290a7c61130bae6f02e5799465e8f98d6c8
Stephen Hu <stephenhu@seismic.com>
2021-12-10 15:07:22 +0800

    Create Find the Middle Index in Array.cs

---
+++ LeetCode/LeetCode/Find the Middle Index in Array.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Find_the_Middle_Index_in_Array
+    {
+        public int FindMiddleIndex(int[] nums)
+        {
+            int totalSum = 0, leftSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+                totalSum += nums[i];
+            for (int i = 0; i < nums.Length; leftSum += nums[i++])
+                if (leftSum * 2 == totalSum - nums[i])
+                    return i;
+            return -1;
+        }
+    }
+}
