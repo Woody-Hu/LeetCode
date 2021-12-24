2825d6a8f2cb433455b2a9167883fb522ed9cf73
Stephen Hu <stephenhu@seismic.com>
2021-12-24 09:37:42 +0800

    Update Slowest Key.cs

--- LeetCode/LeetCode/Slowest Key.cs
+++ LeetCode/LeetCode/Slowest Key.cs
@@ -3,8 +3,21 @@ namespace LeetCode
 {
     public class Slowest_Key
     {
-        public Slowest_Key()
+        public char SlowestKey(int[] releaseTimes, string keysPressed)
         {
+            var max = releaseTimes[0];
+            var res = keysPressed[0];
+            for (int i = 1; i < keysPressed.Length; i++)
+            {
+                var current = releaseTimes[i] - releaseTimes[i - 1];
+                if (current > max || (current == max && (keysPressed[i] > res )))
+                {
+                    max = current;
+                    res = keysPressed[i];
+                }
+            }
+
+            return res;
         }
     }
 }
