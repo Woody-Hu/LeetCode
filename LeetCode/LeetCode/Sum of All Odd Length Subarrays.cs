b1617b63d0a938e5d99f119ae9b7d062acf8b0f7
Stephen Hu <stephenhu@seismic.com>
2022-01-14 09:45:18 +0800

    Create Sum of All Odd Length Subarrays.cs

---
+++ LeetCode/LeetCode/Sum of All Odd Length Subarrays.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Sum_of_All_Odd_Length_Subarrays
+    {
+        public int SumOddLengthSubarrays(int[] arr)
+        {
+            var sum = 0;
+            for (var i = 0; i < arr.Length; i++)
+                sum += (arr[i] * ((((i + 1) * (arr.Length - i)) + 1) / 2));
+            return sum;
+        }
+    }
+}
