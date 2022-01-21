34e20d59a81975ebaa6bb96a212a78c2ed18b296
Stephen Hu <stephenhu@seismic.com>
2022-01-21 10:16:08 +0800

    Create Maximum Number of Words Found in Sentences.cs

---
+++ LeetCode/LeetCode/Maximum Number of Words Found in Sentences.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class Maximum_Number_of_Words_Found_in_Sentences
+    {
+        public int MostWordsFound(string[] sentences)
+        {
+            var res = -1;
+            foreach (var item in sentences)
+            {
+                var current = item.Where(k => k == ' ').Count();
+                res = Math.Max(current, res);
+            }
+
+            return res + 1;
+        }
+    }
+}
