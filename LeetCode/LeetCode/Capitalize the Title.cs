0dd35be7bc4f67a359438ae5275b03cdf3f1474c
Stephen Hu <stephenhu@seismic.com>
2022-01-28 14:41:48 +0800

    Create Capitalize the Title.cs

---
+++ LeetCode/LeetCode/Capitalize the Title.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Capitalize_the_Title
+    {
+
+        public string CapitalizeTitle(string title)
+        {
+            var res = title.ToCharArray();
+            for (int i = 0, j = 0; i <= title.Length; ++i)
+            {
+                if (i == title.Length || title[i] == ' ')
+                {
+                    if (i - j > 2)
+                        res[j] = char.ToUpper(title[j]);
+                    j = i + 1;
+                }
+                else
+                    res[i] = char.ToLower(title[i]);
+            }
+
+            return new string(res);
+        }
+    }
+}
