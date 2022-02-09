9cf56420ea874c41e7b2c945e673239d0baf4b8e
Stephen Hu <stephenhu@seismic.com>
2022-02-09 11:01:42 +0800

    Create Minimum Sum of Four Digit Number After Splitting Digits.cs

---
+++ LeetCode/LeetCode/Minimum Sum of Four Digit Number After Splitting Digits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits
+    {
+        public int MinimumSum(int num)
+        {
+            var values = new int[4];
+            var current = 0;
+            while (num > 0) 
+            {
+                values[current++] = num % 10;
+                num /= 10;
+            }
+
+            Array.Sort(values);
+            return values[0] * 10 + values[2] + values[1] * 10 + values[3];
+        }
+    }
+}
