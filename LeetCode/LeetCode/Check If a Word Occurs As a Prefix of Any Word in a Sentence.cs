f12ef94ca4a188ded0d1746ab6d31a511258719b
Stephen Hu <stephenhu@seismic.com>
2022-02-17 10:54:02 +0800

    Create Check If a Word Occurs As a Prefix of Any Word in a Sentence.cs

---
+++ LeetCode/LeetCode/Check If a Word Occurs As a Prefix of Any Word in a Sentence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence
+    {
+        public Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence()
+        {
+        }
+
+        public int IsPrefixOfWord(string sentence, string searchWord)
+        {
+            var array = sentence.Split(' ');
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].StartsWith(searchWord, StringComparison.OrdinalIgnoreCase))
+                {
+                    return i + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
