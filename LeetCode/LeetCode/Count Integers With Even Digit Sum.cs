e713c0ce9c7e90625ec89401845018944bdb2679
Stephen Hu <stephenhu@seismic.com>
2022-02-25 09:54:44 +0800

    Create Count Integers With Even Digit Sum.cs

---
+++ LeetCode/LeetCode/Count Integers With Even Digit Sum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Count_Integers_With_Even_Digit_Sum
+    {
+        public int CountEven(int num)
+        {
+            int sum = num % 10 + (num / 10) % 10 + (num / 100) % 10 + (num / 1000) % 10;
+            return (num - (sum & 1)) / 2;
+        }
+    }
+}
