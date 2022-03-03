2e0ca305147015eb5ef05c29d2e47577cb6c1db8
Stephen Hu <stephenhu@seismic.com>
2022-03-03 17:31:22 +0800

    Create Counting Words With a Given Prefix.cs

---
+++ LeetCode/LeetCode/Counting Words With a Given Prefix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Counting_Words_With_a_Given_Prefix
+    {
+        public int PrefixCount(string[] words, string pref)
+        {
+            var res = 0;
+            foreach (var item in words)
+            {
+                if (item.Length < pref.Length)
+                {
+                    continue;
+                }
+
+                if (item.StartsWith(pref, StringComparison.Ordinal))
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
