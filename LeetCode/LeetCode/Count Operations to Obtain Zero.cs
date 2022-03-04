2bba95a91035ec8e7673a1c830c564403a844d67
Stephen Hu <stephenhu@seismic.com>
2022-03-04 14:04:54 +0800

    Create Count Operations to Obtain Zero.cs

---
+++ LeetCode/LeetCode/Count Operations to Obtain Zero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Count_Operations_to_Obtain_Zero
+    {
+        public int CountOperations(int num1, int num2)
+        {
+            int res = 0;
+            while (num1 > 0 && num2 >0)
+            {
+                if(num1 < num2)
+                {
+                    var tmp = num1;
+                    num1 = num2;
+                    num2 = tmp;
+                }
+
+                res += num1 / num2;
+                num1 %= num2;
+            }
+
+            return res;
+        }
+    }
+}
