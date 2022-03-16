52a1b5bc35f3f14b675b214d690b37e7462c8dd8
Stephen Hu <stephenhu@seismic.com>
2022-03-16 11:52:03 +0800

    Create Number of Valid Words in a Sentence.cs

---
+++ LeetCode/LeetCode/Number of Valid Words in a Sentence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LeetCode
+{
+    public class Number_of_Valid_Words_in_a_Sentence
+    {
+        public int CountValidWords(string sentence)
+        {
+            var regex1 = new Regex("^([a-z]+(-?[a-z]+)?)?(!|\\.|,)?$");
+            var regex2 = new Regex("[^0-9]+");
+            var items = sentence.Split(' ');
+            var res = 0;
+            foreach (var item in items)
+            {
+                if (regex1.IsMatch(item) && regex2.IsMatch(item))
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
