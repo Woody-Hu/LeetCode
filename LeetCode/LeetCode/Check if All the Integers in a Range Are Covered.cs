9b9a3c501ff2d90d05615f490576078081f49600
Stephen Hu <stephenhu@seismic.com>
2022-03-31 16:45:10 +0800

    Create Check if All the Integers in a Range Are Covered.cs

---
+++ LeetCode/LeetCode/Check if All the Integers in a Range Are Covered.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Check_if_All_the_Integers_in_a_Range_Are_Covered
+    {
+        public bool IsCovered(int[][] ranges, int left, int right)
+        {
+            Array.Sort(ranges, (x, y) => x[0] - y[0] );
+            foreach (var item in ranges)
+            {
+                if (left >= item[0] && left <= item[1])
+                {
+                    left = item[1] + 1;
+                }
+            }
+
+            return left > right;
+        }
+    }
+}
