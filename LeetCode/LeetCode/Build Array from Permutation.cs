6a7e9d4b77697e37ee51a424060489f106ca5850
Stephen Hu <stephenhu@seismic.com>
2022-04-01 10:37:40 +0800

    Create Build Array from Permutation.cs

---
+++ LeetCode/LeetCode/Build Array from Permutation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Build_Array_from_Permutation
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            int n = nums.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = nums[i] + n * (nums[nums[i]] % n);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = nums[i] / n;
+            }
+
+            return nums;
+        }
+    }
+}
