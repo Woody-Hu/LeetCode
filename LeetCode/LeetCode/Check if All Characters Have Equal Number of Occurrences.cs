72b208e97da1b50ddaf32eef62309341dbeea451
Stephen Hu <stephenhu@seismic.com>
2022-04-08 15:58:45 +0800

    Create Check if All Characters Have Equal Number of Occurrences.cs

---
+++ LeetCode/LeetCode/Check if All Characters Have Equal Number of Occurrences.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Check_if_All_Characters_Have_Equal_Number_of_Occurrences
+    {
+        public bool AreOccurrencesEqual(string s)
+        {
+            var array = new int[26];
+            foreach (var item in s)
+            {
+                array[item - 'a']++;
+            }
+
+            var pre = array[s[0] - 'a'];
+            foreach (var item in array)
+            {
+                if (item != 0 && item != pre)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
