09143cd5cf0044479fb7dd6ccf85237c5e2bc08f
Stephen Hu <stephenhu@seismic.com>
2022-04-19 11:19:21 +0800

    Update Jump Game II.cs

--- LeetCode/LeetCode/Jump Game II.cs
+++ LeetCode/LeetCode/Jump Game II.cs
@@ -3,8 +3,19 @@ namespace LeetCode
 {
     public class Jump_Game_II
     {
-        public Jump_Game_II()
+        public int Jump(int[] nums)
         {
-        }
+			int jumps = 0, curEnd = 0, curFarthest = 0;
+			for (int i = 0; i < nums.Length - 1; i++)
+			{
+				curFarthest = Math.Max(curFarthest, i + nums[i]);
+				if (i == curEnd)
+				{
+					jumps++;
+					curEnd = curFarthest;
+				}
+			}
+			return jumps;
+		}
     }
 }
