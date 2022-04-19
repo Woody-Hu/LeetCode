10362c627e4596fbf26603caa5db91830bc9db45
Stephen Hu <stephenhu@seismic.com>
2022-04-19 12:42:54 +0800

    Create PeekingIterator.cs

---
+++ LeetCode/LeetCode/PeekingIterator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    class PeekingIterator
+    {
+        private IEnumerator<int> _iterator;
+        private bool _next;
+        // iterators refers to the first element of the array.
+        public PeekingIterator(IEnumerator<int> iterator)
+        {
+            // initialize any member here.
+            _iterator = iterator;
+            _next = true;
+        }
+
+        // Returns the next element in the iteration without advancing the iterator.
+        public int Peek()
+        {
+            return _iterator.Current;
+        }
+
+        // Returns the next element in the iteration and advances the iterator.
+        public int Next()
+        {
+            var res = _iterator.Current;
+            _next = _iterator.MoveNext();
+            return res;
+        }
+
+        // Returns false if the iterator is refering to the end of the array of true otherwise.
+        public bool HasNext()
+        {
+            return _next;
+        }
+    }
+}
