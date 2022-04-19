2f1f326f385162217cf6f727fb62c242c2db0947
Stephen Hu <stephenhu@seismic.com>
2022-04-19 11:19:23 +0800

    Create Longest Consecutive Sequence.cs

---
+++ LeetCode/LeetCode/Longest Consecutive Sequence.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Longest_Consecutive_Sequence
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            var set = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                set.Add(item);
+            }
+
+            var res = 0;
+            foreach (var item in nums)
+            {
+                if (set.Remove(item))
+                {
+                    var num = item;
+                    var sum = 1;
+                    var value = item;
+                    while (set.Remove(value-1))
+                    {
+                        value--;
+                        sum++;
+                    }
+
+                    value = item;
+                    while (set.Remove(value + 1))
+                    {
+                        value++;
+                        sum++;
+                    }
+
+                    res = Math.Max(sum, res);
+                }
+            }
+
+            return res;
+        }
+    }
+}
