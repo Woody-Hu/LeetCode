020c228f4a93c5a6f3ae58a85ca1c9b2fef05f54
Stephen Hu <stephenhu@seismic.com>
2022-04-19 17:03:05 +0800

    Create Longest Substring with At Most Two Distinct Characters.cs

---
+++ LeetCode/LeetCode/Longest Substring with At Most Two Distinct Characters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class Longest_Substring_with_At_Most_Two_Distinct_Characters
+    {
+        public int LengthOfLongestSubstringTwoDistinct(string s)
+        {
+            var length = s.Length;
+            var left = 0;
+            var right = 0;
+            var dict = new Dictionary<char, int>();
+            var res = 0;
+            while (right < length)
+            {
+                if (dict.Count < 3)
+                {
+                    dict[s[right]] = right++;
+                }
+
+                // slidewindow contains 3 characters
+                if (dict.Count() == 3)
+                {
+                    var del_idx = dict.Min(k => k.Value);
+                    dict.Remove(s[del_idx]);
+                    // move left pointer of the slidewindow
+                    left = del_idx + 1;
+                }
+
+                res = Math.Max(res, right - left);
+            }
+
+            return res;
+        }
+    }
+}
