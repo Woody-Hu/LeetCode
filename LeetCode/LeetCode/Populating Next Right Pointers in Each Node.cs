c4ab51b270b85cc82a9bea157323a7e14b37e721
Stephen Hu <stephenhu@seismic.com>
2022-04-19 14:10:18 +0800

    Create Populating Next Right Pointers in Each Node.cs

---
+++ LeetCode/LeetCode/Populating Next Right Pointers in Each Node.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Populating_Next_Right_Pointers_in_Each_Node
+    {
+        public class Node
+        {
+            public int val;
+            public Node left;
+            public Node right;
+            public Node next;
+
+            public Node() { }
+
+            public Node(int _val)
+            {
+                val = _val;
+            }
+
+            public Node(int _val, Node _left, Node _right, Node _next)
+            {
+                val = _val;
+                left = _left;
+                right = _right;
+                next = _next;
+            }
+        }
+
+
+        public Node Connect(Node root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            var level_start = root;
+            while (level_start != null)
+            {
+                var cur = level_start;
+                while (cur != null)
+                {
+                    if (cur.left != null) cur.left.next = cur.right;
+                    if (cur.right != null && cur.next != null) cur.right.next = cur.next.left;
+
+                    cur = cur.next;
+                }
+                level_start = level_start.left;
+            }
+
+            return root;
+        }
+    }
+}
