a9f118bae384796f45bb12e196395b4a2515b2d6
Stephen Hu <stephenhu@seismic.com>
2022-04-20 16:55:33 +0800

    Create minimum-swaps-to-group-all-1s-together.cs

---
+++ LeetCode/LeetCode/minimum-swaps-to-group-all-1s-together.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_swaps_to_group_all_1s_together
+    {
+        public int MinSwaps(int[] data)
+        {
+            var totalOneCount = 0;
+            foreach (var item in data)
+            {
+                totalOneCount += item;
+            }
+
+            if (totalOneCount == 0 || totalOneCount == 1)
+            {
+                return 0;
+            }
+
+            var current = 0;
+            for (int i = 0; i < totalOneCount; i++)
+            {
+                current += data[i];
+            }
+
+            var temp = current;
+            for (int i = totalOneCount; i < data.Length ; i++)
+            {
+                current = current - data[i - totalOneCount] + data[i];
+                temp = Math.Max(current, temp);
+            }
+
+            return totalOneCount - temp;
+        }
+    }
+}
