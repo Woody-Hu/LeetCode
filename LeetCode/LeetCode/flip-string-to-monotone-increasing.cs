c7664dc9b7536596031784f40e3b606a3df1dc5a
Stephen Hu <stephenhu@seismic.com>
2022-04-20 10:46:19 +0800

    Create flip-string-to-monotone-increasing.cs

---
+++ LeetCode/LeetCode/flip-string-to-monotone-increasing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+    public class flip_string_to_monotone_increasing
+    {
+        public int MinFlipsMonoIncr(string s)
+        {
+            var res = 0;
+            var count = 0;
+            foreach (var item in s)
+            {
+                if (item == '1')
+                {
+                    count++;
+                }
+                else
+                {
+                    res = Math.Min(res + 1, count);
+                }
+            }
+
+            return res;
+
+        }
+    }
+}
