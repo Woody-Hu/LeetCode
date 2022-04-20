3ad672ea9cbe85ff7fdeb2950c9c60e1ed3f590f
Stephen Hu <stephenhu@seismic.com>
2022-04-20 08:18:41 +0800

    Create top-k-frequent-words.cs

---
+++ LeetCode/LeetCode/top-k-frequent-words.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class top_k_frequent_words
+    {
+        public IList<string> TopKFrequent(string[] words, int k)
+        {
+            var dict = new Dictionary<string, int>();
+            foreach (var item in words)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+            }
+
+            var ordered = dict.OrderByDescending(k => k.Value).ThenBy(k => k.Key);
+            var res = new List<string>();
+            foreach (var item in ordered)
+            {
+                if (k <= 0)
+                {
+                    break;
+                }
+
+                res.Add(item.Key);
+                k--;
+            }
+
+            return res;
+        
+        }
+    }
+}
