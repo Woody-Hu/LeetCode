f018f5b13a319d6102576a8516507008969ce6b3
Stephen Hu <stephenhu@seismic.com>
2022-04-20 08:00:56 +0800

    Create MeetingRoomsII.cs

---
+++ LeetCode/LeetCode/MeetingRoomsII.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class MeetingRoomsII
+    {
+        public int MinMeetingRooms(int[][] intervals)
+        {
+            var ordered =  intervals.OrderBy(k => k[0]);
+            var list = new List<int[]>();
+            foreach (var item in ordered)
+            {
+                var found = false;
+                var currentStart = item[0];
+                for (int i = 0; i < list.Count(); i++)
+                {
+                    if (list[i] != null && list[i][1] <= currentStart)
+                    {
+                        list[i] = null;
+                    }
+
+                    if (list[i] == null)
+                    {
+                        list[i] = item;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    list.Add(item);
+                }
+            }
+
+            return list.Count();
+        }
+    }
+}
