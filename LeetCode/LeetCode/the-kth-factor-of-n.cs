c96216a2d43936040b2c2c4a17d2bde0f65d7de4
Stephen Hu <stephenhu@seismic.com>
2022-04-20 11:10:44 +0800

    Create the-kth-factor-of-n.cs

---
+++ LeetCode/LeetCode/the-kth-factor-of-n.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class the_kth_factor_of_n
+    {
+        public int KthFactor(int n, int k)
+        {
+            var list = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    list.Add(i);
+                }
+            }
+
+            if (k - 1 > list.Count -1)
+            {
+                return -1;
+            }
+
+            return list[k - 1];
+        }
+    }
+}
