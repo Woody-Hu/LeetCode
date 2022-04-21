73a489e8079f371d502713af51dde3fbe9680c76
Stephen Hu <stephenhu@seismic.com>
2022-04-21 13:08:42 +0800

    Create maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cs

---
+++ LeetCode/LeetCode/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts
+    {
+        public maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts()
+        {
+        }
+    }
+}
