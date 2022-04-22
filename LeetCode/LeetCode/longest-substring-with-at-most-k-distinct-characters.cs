c8c6fce2455310e73cb42003df371effc65cb00e
Stephen Hu <stephenhu@seismic.com>
2022-04-23 07:19:49 +0800

    Create longest-substring-with-at-most-k-distinct-characters.cs

---
+++ LeetCode/LeetCode/longest-substring-with-at-most-k-distinct-characters.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class longest_substring_with_at_most_k_distinct_characters
+    {
+        public int LengthOfLongestSubstringKDistinct(string s, int k)
+        {
+            var length = s.Length;
+            if (length * k == 0)
+            {
+                return 0;
+            }
+
+            var location = new Dictionary<char, int>();
+            var l = 0;
+            var r = 0;
+            var res = 1;
+            while (r < length)
+            {
+                location[s[r]] = r;
+                r = r + 1;
+                if (location.Count > k)
+                {
+                    var remove = location.OrderBy(k => k.Value).First().Key;
+                    var removeIndex = location[remove];
+                    location.Remove(remove);
+                    l = removeIndex + 1;
+                }
+
+                res = Math.Max(res, r - l);
+            }
+
+            return res;
+        }
+    }
+}
+
