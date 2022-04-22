73d9cbf2d79b1aa65312357dbf6ac7af50e3c2a3
Stephen Hu <stephenhu@seismic.com>
2022-04-22 13:18:00 +0800

    Create capacity-to-ship-packages-within-d-days.cs

---
+++ LeetCode/LeetCode/capacity-to-ship-packages-within-d-days.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class capacity_to_ship_packages_within_d_days
+    {
+        public int ShipWithinDays(int[] weights, int days)
+        {
+            var max = weights.Sum();
+            var min = weights.Max();
+            while (min < max)
+            {
+                var mid = (max + min) / 2;
+                var useDays = 1;
+                var current = 0;
+                foreach (var item in weights)
+                {
+                    if (current + item > mid)
+                    {
+                        useDays++;
+                        current = item;
+                    }
+                    else
+                    {
+                        current += item;
+                    }
+                }
+
+                if (useDays > days)
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid;
+                }
+            }
+
+            return min;
+        }
+    }
+}
