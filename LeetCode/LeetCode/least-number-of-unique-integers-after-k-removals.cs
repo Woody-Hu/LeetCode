9b1fee2f3e0f5a7fbe6a1acf1549107a4d5b1501
Stephen Hu <stephenhu@seismic.com>
2022-04-22 09:50:00 +0800

    Create least-number-of-unique-integers-after-k-removals.cs

---
+++ LeetCode/LeetCode/least-number-of-unique-integers-after-k-removals.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class least_number_of_unique_integers_after_k_removals
+    {
+        public int FindLeastNumOfUniqueInts(int[] arr, int k)
+        {
+            var dic = new Dictionary<int, int>();
+            foreach (var item in arr)
+            {
+                if (!dic.ContainsKey(item))
+                {
+                    dic.Add(item, 0);
+                }
+
+                dic[item]++;
+            }
+
+            var sortList = new SortedList<string, int>();
+            foreach (var item in dic)
+            {
+                var record = $"{item.Value}_{item.Key}";
+                sortList.Add(record, item.Value);
+            }
+
+            while (k > 0)
+            {
+                var first = sortList.First();
+                var value = first.Value;
+                var subValue = Math.Min(value, k);
+                k = k - subValue;
+                value = value - subValue;
+                if (value == 0)
+                {
+                    sortList.RemoveAt(0);
+                }
+            }
+
+            return sortList.Count();
+        }
+    }
+}
