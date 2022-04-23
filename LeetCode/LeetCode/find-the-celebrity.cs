4f929743a06175c461d9772e3878b36cdfdb6a63
Stephen Hu <stephenhu@seismic.com>
2022-04-23 16:39:10 +0800

    Create find-the-celebrity.cs

---
+++ LeetCode/LeetCode/find-the-celebrity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace LeetCode
+{
+    public class find_the_celebrity
+    {
+        public int FindCelebrity(int n)
+        {
+            var resut = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (!Knows(i, resut))
+                {
+                    resut = i;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i == resut)
+                {
+                    continue;
+                }
+
+                if (!Knows(i, resut))
+                {
+                    return -1;
+                }
+
+                if (Knows(resut, i))
+                {
+                    return -1;
+                }
+            }
+
+            return resut;
+
+        }
+
+        private bool Knows(int a, int b)
+        {
+            return false;
+        }
+    }
+}
+
