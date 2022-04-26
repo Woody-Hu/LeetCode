3fc354f8397d07d194fa43fc524c28f74d93f23f
Stephen Hu <stephenhu@seismic.com>
2022-04-26 13:45:03 +0800

    Create meeting-rooms.cs

---
+++ LeetCode/LeetCode/meeting-rooms.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class meeting_rooms
+    {
+        public bool CanAttendMeetings(int[][] intervals)
+        {
+            Array.Sort(intervals, (k1, k2) => k1[0] - k2[0]);
+            for (int i = 0; i < intervals.Count() - 1; i++)
+            {
+                if (intervals[i][1] > intervals[i + 1][0])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
