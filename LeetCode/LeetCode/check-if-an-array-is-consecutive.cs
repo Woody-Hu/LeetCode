3e5ed8414c04369118ba7a8a26ea64cd8914f49e
Stephen Hu <stephenhu@seismic.com>
2022-04-27 17:36:55 +0800

    Create check-if-an-array-is-consecutive.cs

---
+++ LeetCode/LeetCode/check-if-an-array-is-consecutive.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class check_if_an_array_is_consecutive
+    {
+        public bool IsConsecutive(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return true;
+            }
+
+            Array.Sort(nums);
+            var min = nums[0];
+            var max = nums[nums.Length - 1];
+            return max - min == nums.Length - 1 && nums.Distinct().Count() == nums.Length;
+        }
+    }
+}
