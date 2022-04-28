23aa47025d798bb4c7293680403c7f89d76e5002
Stephen Hu <stephenhu@seismic.com>
2022-04-28 12:08:49 +0800

    Create largest-subarray-length-k.cs

---
+++ LeetCode/LeetCode/largest-subarray-length-k.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeetCode
+{
+    public class largest_subarray_length_k
+    {
+        public int[] LargestSubarray(int[] nums, int k)
+        {
+            if (nums.Length < k)
+            {
+                return new int[0];
+            }
+            else if (nums.Length == k)
+            {
+                return nums;
+            }
+
+            var res = new int[k];
+            var maxValue = nums[0];
+            var maxIndex = 0;
+
+            for (int i = 0; i + k - 1 < nums.Length; i++)
+            {
+                var value = nums[i];
+                if (value > maxValue)
+                {
+                    maxValue = value;
+                    maxIndex = i;
+                }
+            }
+
+            Array.Copy(nums, maxIndex, res, 0, k);
+            return res;
+        }
+    }
+}
