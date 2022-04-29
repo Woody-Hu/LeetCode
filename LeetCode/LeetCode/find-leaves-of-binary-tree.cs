2381f8d10adec0c50917053aad4cfb7a7ab2ed35
Stephen Hu <stephenhu@seismic.com>
2022-04-30 07:04:38 +0800

    Create find-leaves-of-binary-tree.cs

---
+++ LeetCode/LeetCode/find-leaves-of-binary-tree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class find_leaves_of_binary_tree
+	{
+		public IList<IList<int>> FindLeaves(TreeNode root)
+		{
+			var res = new List<IList<int>>();
+			var dict = new Dictionary<int, IList<int>>();
+			Visit(root, dict);
+            foreach (var item in dict)
+            {
+				res.Add(item.Value);
+            }
+
+			return res;
+		}
+
+		private int Visit(TreeNode root, Dictionary<int, IList<int>> dict)
+		{
+            if (root == null)
+            {
+				return -1;
+            }
+
+			var left = Visit(root.left, dict);
+			var right = Visit(root.right, dict);
+			var current = Math.Max(left, right) + 1;
+            if (!dict.ContainsKey(current))
+            {
+				dict.Add(current, new List<int>());
+            }
+
+			dict[current].Add(root.val);
+			return current;
+		}
+	}
+}
+
