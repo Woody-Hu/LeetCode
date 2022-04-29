f535f31676685e6beaaa1766e0b157e6d9326b1f
Stephen Hu <stephenhu@seismic.com>
2022-04-29 09:22:45 +0800

    Create two-sum-less-than-k.cs

---
+++ LeetCode/LeetCode/two-sum-less-than-k.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace LeetCode
+{
+    public class two_sum_less_than_k
+    {
+        public int TwoSumLessThanK(int[] nums, int k)
+        {
+            if (nums.Length == 1)
+            {
+                return -1;
+            }
+
+            Array.Sort(nums);
+            if (nums[0] + nums[1] > k)
+            {
+                return -1;
+            }
+
+            var res = -1;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    var current = nums[i] + nums[j];
+                    if (current > res && current < k)
+                    {
+                        res = current;
+                    }
+                }
+
+            }
+
+            return res;
+        }
+    }
+}
