ef5ed5c702b6dc50b4e62df94bf921e532bc4943
Stephen Hu <stephenhu@seismic.com>
2022-04-29 11:13:03 +0800

    Create number-of-distinct-substrings-in-a-string.cs

---
+++ LeetCode/LeetCode/number-of-distinct-substrings-in-a-string.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace LeetCode
+{
+    public class number_of_distinct_substrings_in_a_string
+    {
+        public number_of_distinct_substrings_in_a_string()
+        {
+        }
+    }
+}
