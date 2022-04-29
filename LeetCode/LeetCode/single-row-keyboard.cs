27d6d2c64f8883206edae15703f796297523fdaa
Stephen Hu <stephenhu@seismic.com>
2022-04-30 07:11:09 +0800

    Create single-row-keyboard.cs

---
+++ LeetCode/LeetCode/single-row-keyboard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class single_row_keyboard
+	{
+		public int CalculateTime(string keyboard, string word)
+		{
+			var chars = new Dictionary<char, int>();
+            for (int i = 0; i < keyboard.Length; i++)
+            {
+				chars[keyboard[i]] = i;
+            }
+
+			var res = 0;
+			var currentIndex = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+				var i1 = chars[word[i]];
+				res += Math.Abs(currentIndex - i1);
+				currentIndex = i1;
+            }
+
+			return res;
+		}
+	}
+}
+
