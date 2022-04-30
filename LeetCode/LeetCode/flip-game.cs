576c6f3809f128cd4c89151fae5a4d371f8e742f
Stephen Hu <stephenhu@seismic.com>
2022-05-01 07:11:12 +0800

    Create flip-game.cs

---
+++ LeetCode/LeetCode/flip-game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class flip_game
+	{
+		public IList<string> GeneratePossibleNextMoves(string currentState)
+		{
+			var res = new List<string>();
+			var array = currentState.ToCharArray();
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] == '+' && array[i + 1] == '+')
+                {
+					array[i] = '-';
+					array[i + 1] = '-';
+					res.Add(new string(array));
+					array[i] = '+';
+					array[i + 1] = '+';
+				}
+            }
+
+			return res;
+		}
+	}
+}
+
