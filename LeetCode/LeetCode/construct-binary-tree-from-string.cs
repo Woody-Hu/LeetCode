06d449268820f2f24d688cb4ab01768298998f99
Stephen Hu <stephenhu@seismic.com>
2022-05-02 05:52:02 +0800

    Create construct-binary-tree-from-string.cs

---
+++ LeetCode/LeetCode/construct-binary-tree-from-string.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace LeetCode
+{
+	public class construct_binary_tree_from_string
+	{
+		public TreeNode Str2tree(string s)
+		{
+            if (string.IsNullOrWhiteSpace(s))
+            {
+				return null;
+            }
+
+			var i1 = s.IndexOf('(');
+            if (i1 == -1)
+            {
+				return new TreeNode(int.Parse(s));
+            }
+
+            var root = new TreeNode(int.Parse(s.Substring(0, i1)));
+			var start1 = i1;
+			var count = 0;
+            for (int i = i1; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    count++;
+                }
+                else if (s[i] == ')')
+                {
+                    count--;
+                }
+
+                if (count == 0 && start1 == i1)
+                {
+                    root.left = Str2tree(s.Substring(start1 + 1, i - start1 - 1));
+                    start1 = i + 1;
+                }
+                else if (count == 0)
+                {
+                    root.right = Str2tree(s.Substring(start1 + 1, i - start1 - 1));
+                }
+            }
+
+            return root;
+		}
+	}
+}
+
