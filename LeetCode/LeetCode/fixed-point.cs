b79efc51a60fdf47370dc31cf887fe7ded01d94d
Stephen Hu <stephenhu@seismic.com>
2022-05-02 05:53:04 +0800

    Create fixed-point.cs

---
+++ LeetCode/LeetCode/fixed-point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LeetCode
+{
+	public class fixed_point
+	{
+		public int FixedPoint(int[] arr)
+		{
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == i)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+		}
+	}
+}
+
