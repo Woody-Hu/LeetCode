5725c97ac95a3b2224de2e532f68a78bdea8c990
Stephen Hu <stephenhu@seismic.com>
2022-05-03 07:40:34 +0800

    Update binary-tree-upside-down.cs

--- LeetCode/LeetCode/binary-tree-upside-down.cs
+++ LeetCode/LeetCode/binary-tree-upside-down.cs
@@ -3,8 +3,21 @@ namespace LeetCode
 {
     public class binary_tree_upside_down
     {
-        public binary_tree_upside_down()
+        public TreeNode UpsideDownBinaryTree(TreeNode root)
         {
+            TreeNode right = null;
+            TreeNode parent = null;
+            while (root != null)
+            {
+                var left = root.left;
+                root.left = right;
+                right = root.right;
+                root.right = parent;
+                parent = root;
+                root = left;
+            }
+
+            return parent;
         }
     }
 }
