9e44e487498530a5a26d8cd36e5706e3060c0391
Stephen Hu <stephenhu@seismic.com>
2022-05-03 11:30:12 +0800

    Create moving-average-from-data-stream.cs

---
+++ LeetCode/LeetCode/moving-average-from-data-stream.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class moving_average_from_data_stream
+    {
+        public class MovingAverage
+        {
+            private Queue<int> _queue = new Queue<int>();
+            private int _size = 0;
+
+            public MovingAverage(int size)
+            {
+                _size = size;
+            }
+
+            public double Next(int val)
+            {
+                if (_queue.Count >= _size)
+                {
+                    _queue.Dequeue();
+                }
+
+                _queue.Enqueue(val);
+                return _queue.Average();
+            }
+        }
+    }
+}
