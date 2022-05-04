2be9ba40be035139b027d40b9be7645dbb9d63e3
Stephen Hu <stephenhu@seismic.com>
2022-05-04 17:17:16 +0800

    Create remove-vowels-from-a-string.cs

---
+++ LeetCode/LeetCode/remove-vowels-from-a-string.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    public class remove_vowels_from_a_string
+    {
+        public string RemoveVowels(string s)
+        {
+            var stringBuilder = new StringBuilder(s.Length);
+            foreach (var item in s)
+            {
+                if (item != 'a' && item != 'e' && item != 'i' && item != 'o' && item != 'u')
+                {
+                    stringBuilder.Append(item);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
