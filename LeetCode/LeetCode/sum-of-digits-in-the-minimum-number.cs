4fdd41a3b5ff8caeb3d26356bd1c45368d27f035
Stephen Hu <stephenhu@seismic.com>
2022-05-04 12:21:55 +0800

    Create sum-of-digits-in-the-minimum-number.cs

---
+++ LeetCode/LeetCode/sum-of-digits-in-the-minimum-number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class sum_of_digits_in_the_minimum_number
+    {
+        public int SumOfDigits(int[] nums)
+        {
+            var min = nums.Min();
+            var sum = 0;
+            while (min > 0)
+            {
+                sum += min % 10;
+                min = min / 10;
+            }
+
+            if (sum % 2 == 0)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
