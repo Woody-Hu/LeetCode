44a77c1a05adf5fd64f36fddcf7138f45792b77d
Stephen Hu <stephenhu@seismic.com>
2022-05-04 11:02:22 +0800

    Create logger-rate-limiter.cs

---
+++ LeetCode/LeetCode/logger-rate-limiter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class logger_rate_limiter
+    {
+        public logger_rate_limiter()
+        {
+        }
+
+        public class Logger
+        {
+            private Dictionary<string, int> _dict = new Dictionary<string, int>();
+
+            public Logger()
+            {
+
+            }
+
+            public bool ShouldPrintMessage(int timestamp, string message)
+            {
+                if (!_dict.TryGetValue(message, out var exist))
+                {
+                    _dict.Add(message, timestamp);
+                    return true;
+                }
+
+                if (timestamp - exist >= 10)
+                {
+                    _dict[message] = timestamp;
+                    return true;
+                }
+
+                return false;
+            }
+        }
+    }
+}
