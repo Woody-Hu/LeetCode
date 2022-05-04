8faaff8e1701ba09a093a98bb390f090d0df045e
Stephen Hu <stephenhu@seismic.com>
2022-05-04 17:51:59 +0800

    Create wiggle-sort.cs

---
+++ LeetCode/LeetCode/wiggle-sort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+    public class wiggle_sort
+    {
+        public void WiggleSort(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length - 1; i = i + 2)
+            {
+                var value1 = nums[i];
+                var value2 = nums[i + 1];
+                nums[i + 1] = value1;
+                nums[i] = value2;
+            }
+        }
+    }
+}
