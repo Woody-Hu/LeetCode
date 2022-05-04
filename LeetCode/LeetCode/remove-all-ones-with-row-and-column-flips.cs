ff11f52d07f4d51cdaca9b1e16d12f284abf3984
Stephen Hu <stephenhu@seismic.com>
2022-05-04 18:33:05 +0800

    Create remove-all-ones-with-row-and-column-flips.cs

---
+++ LeetCode/LeetCode/remove-all-ones-with-row-and-column-flips.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+    public class remove_all_ones_with_row_and_column_flips
+    {
+        public bool RemoveOnes(int[][] grid)
+        {
+            var n = grid.Length;
+            var m = grid[0].Length;
+            for (int i = 1; i < n; i++)
+            {
+                var value = grid[i][0] ^ grid[0][0];
+                for (int j = 1; j < m; j++)
+                {
+                    if ((grid[i][j] ^ grid[0][j]) != value)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
