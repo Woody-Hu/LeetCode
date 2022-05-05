7559b919c069cbb306f0e7087556fdbcd81892da
Stephen Hu <stephenhu@seismic.com>
2022-05-05 14:22:30 +0800

    Create maximum-distance-in-arrays.cs

---
+++ LeetCode/LeetCode/maximum-distance-in-arrays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class maximum_distance_in_arrays
+    {
+        public int MaxDistance(IList<IList<int>> arrays)
+        {
+            var res = 0;
+            var max = arrays[0][arrays[0].Count - 1];
+            var min = arrays[0][0];
+            for (int i = 1; i < arrays.Count; i++)
+            {
+                res = Math.Max(res, Math.Max(Math.Abs(max - arrays[i][0]), Math.Abs(arrays[i][arrays[i].Count - 1] - min)));
+                max = Math.Max(max, arrays[i][arrays[i].Count - 1]);
+                min = Math.Min(min, arrays[i][0]);
+            }
+
+            return res;
+        }
+    }
+}
