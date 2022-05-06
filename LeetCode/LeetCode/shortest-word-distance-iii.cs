094e1e89e30c17da88a6796e9db4507867ca7ad6
Stephen Hu <stephenhu@seismic.com>
2022-05-06 09:13:39 +0800

    Create shortest-word-distance-iii.cs

---
+++ LeetCode/LeetCode/shortest-word-distance-iii.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+    public class shortest_word_distance_iii
+    {
+        public int ShortestWordDistance(string[] wordsDict, string word1, string word2)
+        {
+            var lastId = -1;
+            var res = int.MaxValue;
+            for (int i = 0; i < wordsDict.Length; i++)
+            {
+                if (wordsDict[i].Equals(word1) || wordsDict[i].Equals(word2))
+                {
+                    if (lastId != -1 && (!wordsDict[i].Equals(wordsDict[lastId]) ||word1.Equals(word2) ))
+                    {
+                        res = Math.Min(res, i - lastId);
+                    }
+
+                    lastId = i;
+                }
+            }
+
+            return res;
+        }
+    }
+}
