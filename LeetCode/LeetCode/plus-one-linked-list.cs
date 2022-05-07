7b879efbe98dcfc72d47abb24475b1cdf66358b1
Stephen Hu <stephenhu@seismic.com>
2022-05-07 17:34:50 +0800

    Create plus-one-linked-list.cs

---
+++ LeetCode/LeetCode/plus-one-linked-list.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace LeetCode
+{
+	public class plus_one_linked_list
+	{
+		public ListNode PlusOne(ListNode head)
+		{
+			var sentiel = new ListNode(0);
+			sentiel.next = head;
+			var find = sentiel;
+            while (head != null)
+            {
+                if (head.val != 9)
+                {
+					find = head;
+                }
+
+				head = head.next;
+            }
+
+			find.val = find.val + 1;
+			find = find.next;
+            while (find != null)
+            {
+				find.val = 0;
+				find = find.next;
+            }
+
+            if (sentiel.val == 0)
+            {
+				return sentiel.next;
+            }
+
+			return sentiel;
+		}
+	}
+}
+
