c3ab762ac5e9754cfdd0844a9d589ae6279cb271
Stephen Hu <stephenhu@seismic.com>
2022-05-07 08:08:47 +0800

    Create number-of-days-in-a-month.cs

---
+++ LeetCode/LeetCode/number-of-days-in-a-month.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LeetCode
+{
+	public class number_of_days_in_a_month
+	{
+		public int NumberOfDays(int year, int month)
+		{
+			var current = new DateTime(year, month, 1);
+			var nextMonth = current.AddMonths(1);
+			return (int)(nextMonth - current).TotalDays;
+		}
+	}
+}
+
