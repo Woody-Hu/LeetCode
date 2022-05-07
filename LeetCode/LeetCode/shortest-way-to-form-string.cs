51753960e7fff6d2bd8012b3b4e9d3490f1239a5
Stephen Hu <stephenhu@seismic.com>
2022-05-08 06:56:46 +0800

    Create shortest-way-to-form-string.cs

---
+++ LeetCode/LeetCode/shortest-way-to-form-string.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class shortest_way_to_form_string
+	{
+		public int ShortestWay(string source, string target)
+		{
+			var sp = 0;
+			var tp = 0;
+			var res = 0;
+            while (tp < target.Length)
+            {
+				var currentTp = tp;
+                while (tp < target.Length && sp < source.Length)
+                {
+                    if (source[sp] == target[tp])
+                    {
+                        sp++;
+                        tp++;
+                    }
+                    else
+                    {
+                        sp++;
+                    }
+                }
+
+                if (tp > currentTp)
+                {
+                    res++;
+                }
+                else
+                {
+                    return -1;
+                }
+
+                sp = 0;
+            }
+
+            return res;		
+		}
+	}
+}
+
