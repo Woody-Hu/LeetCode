f0dec24d218f5dc2d79775ed2cc422196d59aa13
Stephen Hu <stephenhu@seismic.com>
2022-05-11 06:54:51 +0800

    Create design-most-recently-used-queue.cs

---
+++ LeetCode/LeetCode/design-most-recently-used-queue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class design_most_recently_used_queue
+    {
+        public class MRUQueue
+        {
+            private List<int> _list;
+
+            public MRUQueue(int n)
+            {
+                _list = new List<int>(n);
+                for (int i = 1; i <= n; i++)
+                {
+                    _list.Add(n);
+                }
+            }
+
+            public int Fetch(int k)
+            {
+                k--;
+                var res = _list[k];
+                _list.RemoveAt(k);
+                _list.Add(res);
+                return res;
+            }
+        }
+    }
+}
