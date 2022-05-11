ebe96921a1a08d5585ca9b2dea8ab2dcf920916d
Stephen Hu <stephenhu@seismic.com>
2022-05-11 16:23:21 +0800

    Create array-transformation.cs

---
+++ LeetCode/LeetCode/array-transformation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class array_transformation
+    {
+        public IList<int> TransformArray(int[] arr)
+        {
+            while (true)
+            {
+                var count = 0;
+                var temp = arr.ToList();
+                for (int i = 1; i < arr.Length - 1; i++)
+                {
+                    if (arr[i - 1] < arr[i] && arr[i] > arr[i+1])
+                    {
+                        temp[i]--;
+                        count++;
+                    }
+
+                    if (arr[i - 1] > arr[i] && arr[i] < arr[i + 1])
+                    {
+                        temp[i]++;
+                        count++;
+                    }
+                }
+
+                if (count == 0)
+                {
+                    return temp;
+                }
+
+                arr = temp.ToArray();
+            }
+
+            return arr;
+        }
+    }
+}
