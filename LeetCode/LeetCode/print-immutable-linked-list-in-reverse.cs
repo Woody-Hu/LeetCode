285308e86664953818533223e4481f8f919832c7
Stephen Hu <stephenhu@seismic.com>
2022-05-11 10:18:36 +0800

    Create print-immutable-linked-list-in-reverse.cs

---
+++ LeetCode/LeetCode/print-immutable-linked-list-in-reverse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class print_immutable_linked_list_in_reverse
+    {
+        public void PrintLinkedListInReverse(ImmutableListNode head)
+        {
+            if (head == null)
+            {
+                return;
+            }
+
+            var list = new List<ImmutableListNode>();
+            while (head != null)
+            {
+                list.Add(head);
+                head = head.GetNext();
+            }
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                list[i].PrintValue();
+            }
+        }
+
+        public abstract class ImmutableListNode
+        {
+           public abstract  void PrintValue(); // print the value of this node.
+           public abstract ImmutableListNode GetNext(); // return the next node.
+        }
+    }
+}
