ef2ff8c5281733bdb7661da842ff09a896ba6c85
Stephen Hu <stephenhu@seismic.com>
2022-05-11 16:04:45 +0800

    Create sparse-matrix-multiplication.cs

---
+++ LeetCode/LeetCode/sparse-matrix-multiplication.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeetCode
+{
+    public class sparse_matrix_multiplication
+    {
+        public int[][] Multiply(int[][] mat1, int[][] mat2)
+        {
+            var n = mat1.Length;
+            var m = mat2[0].Length;
+            var k = mat1[0].Length;
+            var res = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                res[i] = new int[m];
+            }
+
+            for (int i = 0; i < n; i++)
+            { 
+                for (int c = 0; c < k; c++)
+                { 
+                    if (mat1[i][c] == 0)
+                    { 
+                        continue; 
+                    }
+                    for (int j = 0; j < m; j++)
+                    {
+                        res[i][j] += mat1[i][c] * mat2[c][j];
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
