6b4a44ec33219b55e068367bea9fd6bd8a749aab
Stephen Hu <stephenhu@seismic.com>
2022-05-12 11:13:11 +0800

    Create count-substrings-with-only-one-distinct-letter.cs

---
+++ LeetCode/LeetCode/count-substrings-with-only-one-distinct-letter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+    public class count_substrings_with_only_one_distinct_letter
+    {
+        public int CountLetters(string s)
+        {
+            var res = 0;
+            var currentCount = 1;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    res += currentCount * (currentCount + 1) / 2;
+                    currentCount = 1;
+                }
+            }
+
+            res += currentCount * (currentCount + 1) / 2;
+            return res;
+        }
+    }
+}
