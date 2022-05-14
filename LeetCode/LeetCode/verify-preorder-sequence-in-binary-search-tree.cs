cbec586e030a76ece690323fef62d5b650a6d9e8
Stephen Hu <stephenhu@seismic.com>
2022-05-15 07:16:37 +0800

    Create verify-preorder-sequence-in-binary-search-tree.cs

---
+++ LeetCode/LeetCode/verify-preorder-sequence-in-binary-search-tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class verify_preorder_sequence_in_binary_search_tree
+	{
+		public bool VerifyPreorder(int[] preorder)
+		{
+			var stack = new Stack<int>();
+			var lastMin = int.MinValue;
+            foreach (var item in preorder)
+            {
+                if (item < lastMin)
+                {
+					return false;
+                }
+
+                while (stack.Count != 0 && stack.Peek() < item)
+                {
+					lastMin = stack.Pop();
+                }
+
+				stack.Push(item);
+            }
+
+			return true;
+		}
+	}
+}
+
