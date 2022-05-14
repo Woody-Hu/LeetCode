61f2f2b27140d81bdbcc99b14676f7efa9d178c3
Stephen Hu <stephenhu@seismic.com>
2022-05-15 07:46:44 +0800

    Create armstrong-numbe.cs

---
+++ LeetCode/LeetCode/armstrong-numbe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode
+{
+	public class armstrong_numbe
+	{
+		public bool IsArmstrong(int n)
+		{
+			var length = n.ToString().Length;
+			var res = 0;
+			var temp = n;
+            while (temp > 0)
+            {
+				var value = temp % 10;
+				temp = temp / 10;
+				res += (int)Math.Pow(value, length);
+            }
+
+			return res == n;
+		}
+	}
+}
+
