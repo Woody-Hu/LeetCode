f5e8e5661398fa8814f1c844578b7eb45b3e1f5b
Stephen Hu <stephenhu@seismic.com>
2022-05-14 08:38:06 +0800

    Create count-univalue-subtrees.cs

---
+++ LeetCode/LeetCode/count-univalue-subtrees.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace LeetCode
+{
+	public class count_univalue_subtrees
+	{
+		public int CountUnivalSubtrees(TreeNode root)
+		{
+			var res = 0;
+			Visit(root, ref res);
+			return res;
+		}
+
+		private bool Visit(TreeNode root, ref int res)
+		{
+            if (root == null)
+            {
+				return false;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+				res++;
+				return true;
+            }
+
+			var isUnival = true;
+            if (root.left != null)
+            {
+				var leftRes = Visit(root.left, ref res) && root.val == root.left.val;
+				isUnival = isUnival && leftRes;
+            }
+
+            if (root.right != null)
+            {
+				var rightRes = Visit(root.right, ref res) && root.val == root.right.val;
+				isUnival = isUnival && rightRes;
+			}
+
+            if (!isUnival)
+            {
+				return false;
+            }
+
+			res++;
+			return true;
+		}
+	}
+}
+
