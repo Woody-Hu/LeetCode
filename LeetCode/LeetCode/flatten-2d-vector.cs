8ed2de035fd99649331c210f16f7d532f94e2a80
Stephen Hu <stephenhu@seismic.com>
2022-05-15 06:51:54 +0800

    Create flatten-2d-vector.cs

---
+++ LeetCode/LeetCode/flatten-2d-vector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class flatten_2d_vector
+	{
+		public flatten_2d_vector()
+		{
+		}
+
+        public class Vector2D
+        {
+            private int[][] _data;
+            private int _row = 0;
+            private int _column = 0;
+
+            public Vector2D(int[][] vec)
+            {
+                _data = vec;
+                SkipEmptyRow();
+            }
+
+            public int Next()
+            {
+                var res = _data[_row][_column];
+                _column++;
+                if (_column >= _data[_row].Length)
+                {
+                    _row++;
+                    SkipEmptyRow();
+                    _column = 0;
+                }
+
+                return res;
+            }
+
+            public bool HasNext()
+            {
+                return _row < _data.Length && _column < _data[_row].Length;
+            }
+
+            private void SkipEmptyRow()
+            {
+                while (_row < _data.Length && !_data[_row].Any())
+                {
+                    _row++;
+                }
+            }
+        }
+    }
+}
+
