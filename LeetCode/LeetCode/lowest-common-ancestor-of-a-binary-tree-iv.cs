7ef0b7fe8f0688a53778f30fb77aaaff16284aa6
Stephen Hu <stephenhu@seismic.com>
2022-05-16 11:01:14 +0800

    Create lowest-common-ancestor-of-a-binary-tree-iv.cs

---
+++ LeetCode/LeetCode/lowest-common-ancestor-of-a-binary-tree-iv.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace LeetCode
+{
+    public class lowest_common_ancestor_of_a_binary_tree_iv
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode[] nodes)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            foreach (var item in nodes)
+            {
+                if (root == item)
+                {
+                    return root;
+                }
+            }
+
+            var left = LowestCommonAncestor(root.left, nodes);
+            var right = LowestCommonAncestor(root.right, nodes);
+            if (left != null & right != null )
+            {
+                return root;
+            }
+
+            if (left != null)
+            {
+                return left;
+            }
+
+            return right;
+        }
+    }
+}
