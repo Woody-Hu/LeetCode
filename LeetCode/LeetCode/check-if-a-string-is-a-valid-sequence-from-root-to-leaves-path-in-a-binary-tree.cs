295f6e8ef70a081c685e3b7ae59c0af9d7ff6098
Stephen Hu <stephenhu@seismic.com>
2022-05-18 13:10:31 +0800

    Create check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.cs

---
+++ LeetCode/LeetCode/check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace LeetCode
+{
+    public class check_if_a_string_is_a_valid_sequence_from_root_to_leaves_path_in_a_binary_tree
+    {
+        public bool IsValidSequence(TreeNode root, int[] arr)
+        {
+            return Visit(root, arr, 0);
+        }
+
+        private bool Visit(TreeNode root, int[] arr, int index)
+        {
+            if (root == null || root.val != arr[index])
+            {
+                return false;
+            }
+
+            if (index == arr.Length - 1)
+            {
+                if (root.left == null && root.right == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return Visit(root.left, arr, index + 1) || Visit(root.right, arr, index + 1);
+        }
+    }
+}
