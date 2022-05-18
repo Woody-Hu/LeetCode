f2a7cbca120a94914954a773e1a5e652e6fab42f
Stephen Hu <stephenhu@seismic.com>
2022-05-18 13:03:49 +0800

    Create counting-elements.cs

---
+++ LeetCode/LeetCode/counting-elements.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class counting_elements
+    {
+        public int CountElements(int[] arr)
+        {
+            var set = new HashSet<int>();
+            foreach (var item in arr)
+            {
+                set.Add(item);
+            }
+
+            var res = 0;
+            foreach (var item in arr)
+            {
+                if (set.Contains(item + 1))
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
