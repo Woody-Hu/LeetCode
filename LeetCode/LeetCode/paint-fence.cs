47adf7af93ed956b0035e21e58e62d8fa2a165df
Stephen Hu <stephenhu@seismic.com>
2022-05-19 10:29:31 +0800

    Create paint-fence.cs

---
+++ LeetCode/LeetCode/paint-fence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+    public class paint_fence
+    {
+        public int NumWays(int n, int k)
+        {
+            if (n == 1)
+            {
+                return k;
+            }
+
+            var first = k;
+            var second = k * k;
+            for (int i = 2; i < n; i++)
+            {
+                var current = (first + second) * (k - 1);
+                first = second;
+                second = current;
+            }
+
+            return second;
+        }
+    }
+}
