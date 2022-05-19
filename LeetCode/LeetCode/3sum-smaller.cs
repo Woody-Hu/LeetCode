e171a95c004e864ddd73bdea876e8dafc550d912
Stephen Hu <stephenhu@seismic.com>
2022-05-19 10:18:07 +0800

    Create 3sum-smaller.cs

---
+++ LeetCode/LeetCode/3sum-smaller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace LeetCode
+{
+    public class _sum_smaller
+    {
+        public int ThreeSumSmaller(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            var res = 0;
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (nums[i] > target)
+                {
+                    break;
+                }
+
+                res += TwoSumSmaller(nums, i + 1, target - nums[i]);
+            }
+
+            return res;
+        }
+
+        private int TwoSumSmaller(int[] nums, int start, int target)
+        {
+            var left = start;
+            var right = nums.Length - 1;
+            var res = 0;
+            while (left < right)
+            {
+                if (nums[left] + nums[right] < target)
+                {
+                    res += right - left;
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return res;
+
+        }
+    }
+}
