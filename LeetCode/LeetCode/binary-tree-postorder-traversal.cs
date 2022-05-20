3098864e91958067d4e42762925fdcb451183a61
Stephen Hu <stephenhu@seismic.com>
2022-05-21 07:33:13 +0800

    Create binary-tree-postorder-traversal.cs

---
+++ LeetCode/LeetCode/binary-tree-postorder-traversal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class binary_tree_postorder_traversal
+	{
+		public IList<int> PostorderTraversal(TreeNode root)
+		{
+			var res = new List<int>();
+			Visit(root, res);
+			return res;
+		}
+
+		private void Visit(TreeNode root, IList<int> list)
+		{
+            if (root == null)
+            {
+				return;
+            }
+
+			Visit(root.left, list);
+			Visit(root.right, list);
+			list.Add(root.val);
+		}
+	}
+}
+
