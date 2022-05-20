002c40ad40f7793d1cbd1ecf169ce75fb754144f
Stephen Hu <stephenhu@seismic.com>
2022-05-21 07:27:11 +0800

    Create optimal-division.cs

---
+++ LeetCode/LeetCode/optimal-division.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+	public class optimal_division
+	{
+		public string OptimalDivision(int[] nums)
+		{
+			var n = nums.Length;
+            if (n == 1)
+            {
+				return nums[0].ToString();
+            }
+            else if (n == 2)
+            {
+				return $"{nums[0]}/{nums[1]}";
+            }
+
+			var stringBuilder = new StringBuilder();
+			stringBuilder.Append($"{nums[0]}/({nums[1]}");
+			for (int i = 2; i < n; i++)
+			{
+				stringBuilder.Append($"/{nums[i]}");
+			}
+
+			stringBuilder.Append(")");
+			return stringBuilder.ToString();
+            {
+
+            }
+		}
+	}
+}
+
