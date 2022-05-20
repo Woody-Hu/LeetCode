dbbaa1bacd7a0b7f29c3f436b097ba8b67be6dc5
Stephen Hu <stephenhu@seismic.com>
2022-05-21 06:19:43 +0800

    Create flatten-a-multilevel-doubly-linked-list.cs

---
+++ LeetCode/LeetCode/flatten-a-multilevel-doubly-linked-list.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace LeetCode
+{
+	public class flatten_a_multilevel_doubly_linked_list
+	{
+        public Node Flatten(Node head)
+        {
+            if (head == null)
+            {
+                return head;
+            }
+
+            FlatternAndReturnLast(head);
+            return head;
+        }
+
+        private Node FlatternAndReturnLast(Node head)
+        {
+            var current = head;
+            Node last = null;
+            while (current != null)
+            {
+                var next = current.next;
+                if (current.child != null)
+                {
+                    var childLast = FlatternAndReturnLast(current.child);
+                    current.next = current.child;
+                    current.child.prev = current;
+                    childLast.next = next;
+                    if (next != null)
+                    {
+                        next.prev = childLast;
+                    }
+
+                    current.child = null;
+                    last = childLast;
+                }
+                else
+                {
+                    last = current;
+                }
+
+                current = next;
+            }
+
+            return last;
+        }
+
+        public class Node
+        {
+            public int val;
+            public Node prev;
+            public Node next;
+            public Node child;
+        }
+    }
+}
+
