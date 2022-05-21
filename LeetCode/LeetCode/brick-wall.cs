9257e2a94574293cb1dcc3ea42e81df619702c6d
Stephen Hu <stephenhu@seismic.com>
2022-05-22 06:36:17 +0800

    Create brick-wall.cs

---
+++ LeetCode/LeetCode/brick-wall.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class brick_wall
+	{
+		public int LeastBricks(IList<IList<int>> wall)
+		{
+			var dict = new Dictionary<int, int>();
+            foreach (var item in wall)
+            {
+				var sum = 0;
+                for (int i = 0; i < item.Count - 1; i++)
+                {
+					sum += item[i];
+                    if (!dict.ContainsKey(sum))
+                    {
+                        dict[sum] = 0;
+                    }
+
+                    dict[sum]++;
+                }
+            }
+
+            if (!dict.Any())
+            {
+                return wall.Count;
+            }
+
+            var max = dict.Values.Max();
+            return wall.Count - max;
+		}
+	}
+}
+
