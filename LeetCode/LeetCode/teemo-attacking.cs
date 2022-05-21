d84499b8b2c25dadca1407792645ff57e9f1d835
Stephen Hu <stephenhu@seismic.com>
2022-05-22 06:42:11 +0800

    Create teemo-attacking.cs

---
+++ LeetCode/LeetCode/teemo-attacking.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeetCode
+{
+	public class teemo_attacking
+	{
+		public int FindPoisonedDuration(int[] timeSeries, int duration)
+		{
+			var res = 0;
+			var expired = 0;
+            foreach (var item in timeSeries)
+            {
+				if (item >= expired)
+				{
+					res += duration;
+				}
+				else
+				{
+					res += item + duration - expired;
+				}
+
+				expired = item + duration;
+            }
+
+			return res;
+		}
+	}
+}
+
