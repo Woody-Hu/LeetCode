fc19d532b12eae7d2b001dafa656f2efc51ab6b8
Stephen Hu <stephenhu@seismic.com>
2022-05-24 10:14:51 +0800

    Create maximum-binary-tree.cs

---
+++ LeetCode/LeetCode/maximum-binary-tree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_binary_tree
+    {
+        public TreeNode ConstructMaximumBinaryTree(int[] nums)
+        {
+            return ConstructMaximumBinaryTree(nums, 0, nums.Length);
+        }
+
+        private TreeNode ConstructMaximumBinaryTree(int[] nums, int left, int right)
+        {
+            if (left == right)
+            {
+                return null;
+            }
+
+            var max_i = left;
+            var maxValue = nums[max_i];
+            for (int i = left; i < right; i++)
+            {
+                var currentValue = nums[i];
+                if (currentValue > maxValue)
+                {
+                    max_i = i;
+                    maxValue = currentValue;
+                }
+            }
+
+            var res = new TreeNode(nums[max_i]);
+            res.left = ConstructMaximumBinaryTree(nums, left, max_i);
+            res.right = ConstructMaximumBinaryTree(nums, max_i + 1, right);
+            return res;
+        }
+    }
+}
