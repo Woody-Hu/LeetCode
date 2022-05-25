93a24dce53fd493a16f5ce231e3b4d4ee0ad1c52
Stephen Hu <stephenhu@seismic.com>
2022-05-25 16:16:40 +0800

    Create find-duplicate-subtrees.cs

---
+++ LeetCode/LeetCode/find-duplicate-subtrees.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class find_duplicate_subtrees
+    {
+        public IList<TreeNode> FindDuplicateSubtrees(TreeNode root)
+        {
+            var dict = new Dictionary<string, int>();
+            var res = new List<TreeNode>();
+            Visit(root, dict, res);
+            return res;
+        }
+
+        private string Visit(TreeNode root, Dictionary<string, int> dict, List<TreeNode> res)
+        {
+            if (root == null)
+            {
+                return "n";
+            }
+
+            var thumbnail = $"{root.val}:{Visit(root.left, dict, res)}:{Visit(root.right, dict, res)}";
+            if (!dict.ContainsKey(thumbnail))
+            {
+                dict.Add(thumbnail, 0);
+            }
+
+            dict[thumbnail]++;
+            if (dict[thumbnail] == 2)
+            {
+                res.Add(root);
+            }
+
+            return thumbnail;
+        }
+    }
+}
