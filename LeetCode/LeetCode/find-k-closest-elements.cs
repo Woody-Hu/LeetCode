603e7cff12e1acdb71b043a8135ef75c1f511554
Stephen Hu <stephenhu@seismic.com>
2022-05-25 16:29:31 +0800

    Create find-k-closest-elements.cs

---
+++ LeetCode/LeetCode/find-k-closest-elements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class find_k_closest_elements
+    {
+        public IList<int> FindClosestElements(int[] arr, int k, int x)
+        {
+            var lists = arr.ToList();
+            var len = arr.Length - 1;
+            if (x <= lists[0])
+                return lists.Take(k).ToList(); 
+            if (x >= lists[len])
+                return lists.TakeLast(k).ToList();
+
+            var lastIndex = lists.BinarySearch(0, len, x, Comparer<int>.Default);
+            if (lastIndex < 0)
+                lastIndex = -lastIndex - 1;
+            var low = Math.Max(0, lastIndex - k - 1);
+            var high = Math.Min(len, lastIndex + k - 1);
+
+            while (high - low > k - 1)
+            {
+                if ((x - lists[low]) <= (lists[high] - x))
+                    high--;
+                else if ((x - lists[low]) > (lists[high] - x))
+                    low++;
+                
+            }
+
+            return lists.Skip(low).Take(k).ToList();
+        }
+    }
+}
