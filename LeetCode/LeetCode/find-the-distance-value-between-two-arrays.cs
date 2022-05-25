979e68badc339be1c07a2a2403f1ab3f6b224e83
Stephen Hu <stephenhu@seismic.com>
2022-05-25 11:14:01 +0800

    Create find-the-distance-value-between-two-arrays.cs

---
+++ LeetCode/LeetCode/find-the-distance-value-between-two-arrays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class find_the_distance_value_between_two_arrays
+    {
+        public int FindTheDistanceValue(int[] arr1, int[] arr2, int d)
+        {
+            var res = 0;
+            foreach (var item in arr1)
+            {
+                if (arr2.All(k => Math.Abs(item - k) > d))
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
