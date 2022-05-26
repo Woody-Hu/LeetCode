a081b149d168d6031856bfe8f86f860ba715e0ac
Stephen Hu <stephenhu@seismic.com>
2022-05-26 11:06:42 +0800

    Create maximum-length-of-pair-chain.cs

---
+++ LeetCode/LeetCode/maximum-length-of-pair-chain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class maximum_length_of_pair_chain
+    {
+        public int FindLongestChain(int[][] pairs)
+        {
+            Array.Sort(pairs, new InnerComarper());
+            var res = 0;
+            var pre = int.MinValue;
+            foreach (var item in pairs)
+            {
+                if (item[0] > pre)
+                {
+                    res++;
+                    pre = item[1];
+                }
+            }
+
+            return res;
+        }
+
+        private sealed class InnerComarper : IComparer<int[]>
+        {
+            public int Compare(int[] x, int[] y)
+            {
+                return x[1] - y[1];
+            }
+        }
+    }
+}
