e285f8d503ff8106cfe95f81becb1b4a30ba6db4
Stephen Hu <stephenhu@seismic.com>
2022-05-27 11:44:06 +0800

    Create bulb-switcher-ii.cs

---
+++ LeetCode/LeetCode/bulb-switcher-ii.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LeetCode
+{
+    public class bulb_switcher_ii
+    {
+        public int FlipLights(int n, int presses)
+        {
+            n = Math.Min(n, 3);
+            if (presses == 0) return 1;
+            if (presses == 1) return n == 1 ? 2 : n == 2 ? 3 : 4;
+            if (presses == 2) return n == 1 ? 2 : n == 2 ? 4 : 7;
+            return n == 1 ? 2 : n == 2 ? 4 : 8;
+        }
+    }
+}
