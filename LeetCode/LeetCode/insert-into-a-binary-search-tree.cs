7dd65daa02639a5fd3642c5b9df9177377f251eb
Stephen Hu <stephenhu@seismic.com>
2022-05-29 06:59:01 +0800

    Create insert-into-a-binary-search-tree.cs

---
+++ LeetCode/LeetCode/insert-into-a-binary-search-tree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace LeetCode
+{
+	public class insert_into_a_binary_search_tree
+	{
+		public TreeNode InsertIntoBST(TreeNode root, int val)
+		{
+            if (root == null)
+            {
+				return new TreeNode(val);
+            }
+
+			var current = root;
+            while (current != null)
+            {
+                if (val < current.val)
+                {
+                    if (current.left == null)
+                    {
+                        current.left = new TreeNode(val);
+                        break;
+                    }
+                    else
+                    {
+                        current = current.left;
+                    }
+                }
+                else
+                {
+                    if (current.right == null)
+                    {
+                        current.right = new TreeNode(val);
+                        break;
+                    }
+                    else
+                    {
+                        current = current.right;
+                    }
+                }
+            }
+
+            return root;
+		}
+	}
+}
+
