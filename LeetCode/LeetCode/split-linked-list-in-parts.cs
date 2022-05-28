1b97eeaafd6dab8232bfe180ebc8b07bbd83bea1
Stephen Hu <stephenhu@seismic.com>
2022-05-29 06:58:57 +0800

    Create split-linked-list-in-parts.cs

---
+++ LeetCode/LeetCode/split-linked-list-in-parts.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace LeetCode
+{
+	public class split_linked_list_in_parts
+	{
+		public ListNode[] SplitListToParts(ListNode head, int k)
+		{
+			var length = 0;
+			var temp = head;
+            while (temp != null)
+            {
+				length++;
+				temp = temp.next;
+            }
+
+			var average = length / k;
+			var modRes = length % k;
+			var res = new ListNode[k];
+			var current = head;
+            for (int i = 0; i < k && current != null; i++)
+            {
+				res[i] = current;
+				var useLength = average + (i < modRes ? 1 : 0);
+                for (int j = 1; j < useLength; j++)
+                {
+					current = current.next;
+                }
+
+				var next = current.next;
+				current.next = null;
+				current = next;
+            }
+
+			return res;
+		}
+	}
+}
+
