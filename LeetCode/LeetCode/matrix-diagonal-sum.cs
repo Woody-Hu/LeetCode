46e04367e11a655959b65ebf926e7144e80efa7b
Stephen Hu <stephenhu@seismic.com>
2022-05-29 07:04:04 +0800

    Create matrix-diagonal-sum.cs

---
+++ LeetCode/LeetCode/matrix-diagonal-sum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LeetCode
+{
+	public class matrix_diagonal_sum
+	{
+		public int DiagonalSum(int[][] mat)
+		{
+			var n = mat.Length;
+			var mid = n / 2;
+			var res = 0;
+            for (int i = 0; i < n; i++)
+            {
+				res += mat[i][i] + mat[i][n - i - 1];
+            }
+
+			return res - mat[mid][mid] * (n % 2 == 0? 0 : 1);
+		}
+	}
+}
+
