e453f38310b78fb31d2060a031f5300374bac73b
Stephen Hu <stephenhu@seismic.com>
2022-05-30 10:37:34 +0800

    Create employee-importance.cs

---
+++ LeetCode/LeetCode/employee-importance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class employee_importance
+    {
+        public int GetImportance(IList<Employee> employees, int id)
+        {
+            var dic = new Dictionary<int, Employee>();
+            foreach (var item in employees)
+            {
+                dic[item.id] = item;
+            }
+
+            return Visit(dic, id);
+        }
+
+        private int Visit(Dictionary<int, Employee> dic, int id)
+        {
+            if (!dic.ContainsKey(id))
+            {
+                return 0;
+            }
+
+            var current = dic[id];
+            var res = current.importance;
+            foreach (var item in current.subordinates)
+            {
+                res += Visit(dic, item);
+            }
+
+            return res;
+        }
+
+
+        public class Employee
+        {
+            public int id;
+            public int importance;
+            public IList<int> subordinates;
+        }
+    }
+}
