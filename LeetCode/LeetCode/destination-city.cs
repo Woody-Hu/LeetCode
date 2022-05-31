d8bddb2e6d078bef6c922c392e2822049095bc50
Stephen Hu <stephenhu@seismic.com>
2022-05-31 10:48:52 +0800

    Create destination-city.cs

---
+++ LeetCode/LeetCode/destination-city.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class destination_city
+    {
+        public string DestCity(IList<IList<string>> paths)
+        {
+            var set = new HashSet<string>();
+            foreach (var item in paths)
+            {
+                set.Add(item[0]);
+            }
+
+            foreach (var item in paths)
+            {
+                if (!set.Contains(item[1]))
+                {
+                    return item[1];
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
