9d1a099355ca85490f5e23a44dc4e8c2e671fc14
Stephen Hu <stephenhu@seismic.com>
2022-06-02 10:42:36 +0800

    Create my-calendar-i.cs

---
+++ LeetCode/LeetCode/my-calendar-i.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class my_calendar_i
+    {
+        public class MyCalendar
+        {
+            private readonly List<(int, int)> _values = new List<(int, int)>();
+
+            public MyCalendar()
+            {
+
+            }
+
+            public bool Book(int start, int end)
+            {
+                foreach (var item in _values)
+                {
+                    if (start < item.Item2 && item.Item1 < end)
+                    {
+                        return false;
+                    }
+                }
+
+                _values.Add((start, end));
+                return true;
+            }
+        }
+    }
+}
