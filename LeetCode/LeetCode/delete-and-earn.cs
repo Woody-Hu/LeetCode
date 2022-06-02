9cf456010065373280f5a9da253cf9a7397c3faa
Stephen Hu <stephenhu@seismic.com>
2022-06-02 11:13:45 +0800

    Create delete-and-earn.cs

---
+++ LeetCode/LeetCode/delete-and-earn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class delete_and_earn
+    {
+        public int DeleteAndEarn(int[] nums)
+        {
+            var max = nums.Max();
+            var array = new int[max + 1];
+            foreach (var item in nums)
+            {
+                array[item] += item;
+            }
+
+            var first = array[0];
+            var second = Math.Max(array[0], array[1]);
+
+            for (int i = 2; i < array.Length; i++)
+            {
+                var temp = second;
+                second = Math.Max(first + array[i], second);
+                first = temp;
+            }
+
+            return second;
+        }
+    }
+}
