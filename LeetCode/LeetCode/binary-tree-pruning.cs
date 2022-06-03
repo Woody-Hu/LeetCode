01e028bcd9a417ee53706a996f0eac8b10cfa2f6
Stephen Hu <stephenhu@seismic.com>
2022-06-03 09:46:16 +0800

    Create binary-tree-pruning.cs

---
+++ LeetCode/LeetCode/binary-tree-pruning.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace LeetCode
+{
+	public class binary_tree_pruning
+	{
+		public TreeNode PruneTree(TreeNode root)
+		{
+            if (ContainsOneValue(root))
+            {
+				return root;
+            }
+
+			return null;
+		}
+
+		private bool ContainsOneValue(TreeNode root)
+		{
+            if (root == null)
+            {
+				return false;
+            }
+
+			var leftRes = ContainsOneValue(root.left);
+			var rightRes = ContainsOneValue(root.right);
+            if (!leftRes)
+            {
+				root.left = null;
+            }
+
+            if (!rightRes)
+            {
+				root.right = null;
+            }
+
+			return root.val == 1 || leftRes | rightRes;
+		}
+	}
+}
+
