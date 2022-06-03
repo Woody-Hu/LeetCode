8e9482c4fbd8ba5a0652b62d0fe0bc5f8048e1e6
Stephen Hu <stephenhu@seismic.com>
2022-06-03 09:59:03 +0800

    Create number-of-subarrays-with-bounded-maximum.cs

---
+++ LeetCode/LeetCode/number-of-subarrays-with-bounded-maximum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace LeetCode
+{
+	public class number_of_subarrays_with_bounded_maximum
+	{
+		public int NumSubarrayBoundedMax(int[] nums, int left, int right)
+		{
+			return CalculateInBound(nums, right) - CalculateInBound(nums, left - 1);
+		}
+
+		private int CalculateInBound(int[] nums, int boun)
+		{
+			var res = 0;
+			var current = 0;
+            foreach (var item in nums)
+            {
+				if (item <= boun)
+				{
+					current = current + 1;
+				}
+				else
+				{
+					current = 0;
+				}
+
+				res += current;
+            }
+
+			return res;
+		}
+	}
+}
+
