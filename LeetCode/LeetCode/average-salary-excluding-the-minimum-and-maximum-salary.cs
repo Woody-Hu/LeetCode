ba98aad02409292ba77bbf46f82395d3f3ea3eb9
Stephen Hu <stephenhu@seismic.com>
2022-06-04 08:28:01 +0800

    Create average-salary-excluding-the-minimum-and-maximum-salary.cs

---
+++ LeetCode/LeetCode/average-salary-excluding-the-minimum-and-maximum-salary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class average_salary_excluding_the_minimum_and_maximum_salary
+	{
+		public double Average(int[] salary)
+		{
+            Array.Sort(salary);
+            var len = salary.Length;
+            var sum = 0.0d;
+            for (int i = 1; i < len - 1; i++)
+            {
+                sum += salary[i];
+            }
+
+            return sum / (len - 2);
+		}
+	}
+}
+
