192166e2040c525f3f442cf11d8400779cbbb70f
Stephen Hu <stephenhu@seismic.com>
2022-06-05 07:43:02 +0800

    Create delete-leaves-with-a-given-value.cs

---
+++ LeetCode/LeetCode/delete-leaves-with-a-given-value.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+	public class delete_leaves_with_a_given_value
+	{
+		public TreeNode RemoveLeafNodes(TreeNode root, int target)
+		{
+            if (root == null)
+            {
+				return null;
+            }
+
+			var left = RemoveLeafNodes(root.left, target);
+			var right = RemoveLeafNodes(root.right, target);
+			root.left = left;
+			root.right = right;
+            if (left == null && right == null && root.val == target)
+            {
+				return null;
+            }
+
+			return root;
+		}
+	}
+}
+
