599d25705f0b665ae77f85df1a8bd7bddc690bcb
Stephen Hu <stephenhu@seismic.com>
2022-06-05 09:37:40 +0800

    Create final-prices-with-a-special-discount-in-a-shop.cs

---
+++ LeetCode/LeetCode/final-prices-with-a-special-discount-in-a-shop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class final_prices_with_a_special_discount_in_a_shop
+	{
+		public int[] FinalPrices(int[] prices)
+		{
+			var stack = new Stack<int>();
+            for (int i = 0; i < prices.Length; i++)
+            {
+                while (stack.Any() && prices[i] <= prices[stack.Peek()] )
+                {
+					var j = stack.Pop();
+					prices[j] = prices[j] - prices[i];
+                }
+
+				stack.Push(i);
+            }
+
+			return prices;
+		}
+	}
+}
+
