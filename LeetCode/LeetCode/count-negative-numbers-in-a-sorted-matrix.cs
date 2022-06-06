ce2790a7aff3eaac59ab92d33d1268f49809a624
Stephen Hu <stephenhu@seismic.com>
2022-06-06 16:31:11 +0800

    Create count-negative-numbers-in-a-sorted-matrix.cs

---
+++ LeetCode/LeetCode/count-negative-numbers-in-a-sorted-matrix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LeetCode
+{
+    public class count_negative_numbers_in_a_sorted_matrix
+    {
+        public int CountNegatives(int[][] grid)
+        {
+            var rows = grid.Length;
+            var columns = grid[0].Length;
+            var res = 0;
+            var i = 0;
+            var j = columns - 1;
+            while (i < rows && j >= 0)
+            {
+                if (grid[i][j] < 0)
+                {
+                    res += rows - i;
+                    j--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
