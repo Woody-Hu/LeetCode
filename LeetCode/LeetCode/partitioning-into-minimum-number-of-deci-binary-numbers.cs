a6e4a106906332337cd366e48ad977ecaa97c76a
Stephen Hu <stephenhu@seismic.com>
2022-06-06 16:31:18 +0800

    Create partitioning-into-minimum-number-of-deci-binary-numbers.cs

---
+++ LeetCode/LeetCode/partitioning-into-minimum-number-of-deci-binary-numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LeetCode
+{
+    public class partitioning_into_minimum_number_of_deci_binary_numbers
+    {
+        public int MinPartitions(string n)
+        {
+            var res = 0;
+            foreach (var item in n)
+            {
+                res = Math.Max(res, item - '0');
+            }
+
+            return res;
+        }
+    }
+}
