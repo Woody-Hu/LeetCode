fd0d1aed68dbe71435db15152d824267e8ea2399
Stephen Hu <stephenhu@seismic.com>
2022-06-06 16:31:15 +0800

    Create deepest-leaves-sum.cs

---
+++ LeetCode/LeetCode/deepest-leaves-sum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace LeetCode
+{
+    public class deepest_leaves_sum
+    {
+        public int DeepestLeavesSum(TreeNode root)
+        {
+            var res = 0;
+            var maxDep = 0;
+            Visit(root, 0, ref maxDep, ref res);
+            return res;
+        }
+
+        private void Visit(TreeNode root, int currentDep, ref int maxDep, ref int res)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                if (currentDep > maxDep)
+                {
+                    maxDep = currentDep;
+                    res = root.val;
+                }
+                else if (currentDep == maxDep)
+                {
+                    res += root.val;
+                }
+            }
+            else
+            {
+                Visit(root.left, currentDep + 1, ref maxDep, ref res);
+                Visit(root.right, currentDep + 1, ref maxDep, ref res);
+            }
+        }
+    }
+}
