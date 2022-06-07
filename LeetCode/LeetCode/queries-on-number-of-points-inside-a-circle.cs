d1ff1e5e9fd931e96f2ee57d7adf10c7b07e02b5
Stephen Hu <stephenhu@seismic.com>
2022-06-07 17:15:34 +0800

    Create queries-on-number-of-points-inside-a-circle.cs

---
+++ LeetCode/LeetCode/queries-on-number-of-points-inside-a-circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+    public class queries_on_number_of_points_inside_a_circle
+    {
+        public int[] CountPoints(int[][] points, int[][] queries)
+        {
+            var array = new int[queries.Length];
+            var i = 0;
+            foreach (var query in queries)
+            {
+                int count = 0;
+                foreach (var point in points)
+                {
+                    var distance = Math.Sqrt(Math.Abs(query[0] - point[0]) * Math.Abs(query[0] - point[0]) + Math.Abs(query[1] - point[1]) * Math.Abs(query[1] - point[1]));
+                    if (distance <= query[2])
+                    {
+                        count++;
+                    }
+                }
+                array[i++] = count;
+            }
+            return array;
+        }
+    }
+}
