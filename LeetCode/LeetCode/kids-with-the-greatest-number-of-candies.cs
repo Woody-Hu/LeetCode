c7b78bb5b21f9382f985b9e0fe23defa3827fde0
Stephen Hu <stephenhu@seismic.com>
2022-06-07 17:07:05 +0800

    Create kids-with-the-greatest-number-of-candies.cs

---
+++ LeetCode/LeetCode/kids-with-the-greatest-number-of-candies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class kids_with_the_greatest_number_of_candies
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            var res = new List<bool>(candies.Length);
+            var max = candies.Max();
+            for (int i = 0; i < candies.Length; i++)
+            {
+                if (candies[i] + extraCandies >= max)
+                {
+                    res.Add(true);
+                }
+                else
+                {
+                    res.Add(false);
+                }
+            }
+
+            return res;
+        }
+    }
+}
