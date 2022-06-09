42b68bf3fea944a7817c84cd011516742e016c70
Stephen Hu <stephenhu@seismic.com>
2022-06-09 14:18:23 +0800

    Create minimize-maximum-pair-sum-in-array.cs

---
+++ LeetCode/LeetCode/minimize-maximum-pair-sum-in-array.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimize_maximum_pair_sum_in_array
+    {
+        public int MinPairSum(int[] nums)
+        {
+            Array.Sort(nums);
+            var res = 0;
+            for (int i = 0; i < nums.Length / 2; ++i)
+            {
+                res = Math.Max(res, nums[i] + nums[nums.Length - 1 - i]);
+            }
+
+            return res;
+        }
+    }
+}
