edf8598278f276e5df309eb4e296538168c249cd
Stephen Hu <stephenhu@seismic.com>
2022-06-09 16:26:44 +0800

    Create check-if-all-1s-are-at-least-length-k-places-away.cs

---
+++ LeetCode/LeetCode/check-if-all-1s-are-at-least-length-k-places-away.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+    public class check_if_all_1s_are_at_least_length_k_places_away
+    {
+        public bool KLengthApart(int[] nums, int k)
+        {
+            var pre = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    if (pre != -1 && i - pre - 1 < k)
+                    {
+                        return false;
+                    }
+
+                    pre = i;
+                }
+            }
+
+            return true;
+        }
+    }
+}
