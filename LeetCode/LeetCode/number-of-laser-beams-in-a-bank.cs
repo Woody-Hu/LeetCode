d18ec2bf96b66b5b07744a901feabc7316d74d5c
Stephen Hu <stephenhu@seismic.com>
2022-06-10 11:53:19 +0800

    Create number-of-laser-beams-in-a-bank.cs

---
+++ LeetCode/LeetCode/number-of-laser-beams-in-a-bank.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class number_of_laser_beams_in_a_bank
+    {
+        public int NumberOfBeams(string[] bank)
+        {
+            var res = 0;
+            var last = 0;
+            foreach (var item in bank)
+            {
+                var count = item.Count(k => k == '1');
+                if (count != 0)
+                {
+                    res += last * count;
+                    last = count;
+                }
+            }
+
+            return res;
+        }
+    }
+}
