4e71100f478ddadf20403aced70a56b525973f04
Stephen Hu <stephenhu@seismic.com>
2022-06-11 07:09:21 +0800

    Create number-of-students-doing-homework-at-a-given-time.cs

---
+++ LeetCode/LeetCode/number-of-students-doing-homework-at-a-given-time.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+	public class number_of_students_doing_homework_at_a_given_time
+	{
+		public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+		{
+			var res = 0;
+            for (int i = 0; i < startTime.Length; i++)
+            {
+                if (startTime[i] <= queryTime && endTime[i] >= queryTime)
+                {
+					res++;
+                }
+            }
+
+			return res;
+		}
+	}
+}
+
