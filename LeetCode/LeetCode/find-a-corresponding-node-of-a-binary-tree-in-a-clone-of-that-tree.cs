24befdef454f73a7c920b73dd3a0052cb6df15b4
Stephen Hu <stephenhu@seismic.com>
2022-06-11 07:00:49 +0800

    Create find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cs

---
+++ LeetCode/LeetCode/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeetCode
+{
+	public class find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree
+	{
+        public TreeNode GetTargetCopy(TreeNode original, TreeNode cloned, TreeNode target)
+        {
+            if (original == target)
+            {
+                return cloned;
+            }
+
+            TreeNode res = null;
+            if (original.left != null)
+            {
+                res = GetTargetCopy(original.left, cloned.left, target);
+            }
+
+            if (res == null && original.right != null)
+            {
+                res = GetTargetCopy(original.right, cloned.right, target);
+            }
+
+            return res;
+        }
+    }
+}
+
