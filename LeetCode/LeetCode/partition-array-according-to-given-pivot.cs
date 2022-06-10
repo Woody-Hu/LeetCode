689069b70fa96016bb510f475ef8e564b8ad2da2
Stephen Hu <stephenhu@seismic.com>
2022-06-10 11:37:44 +0800

    Create partition-array-according-to-given-pivot.cs

---
+++ LeetCode/LeetCode/partition-array-according-to-given-pivot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace LeetCode
+{
+    public class partition_array_according_to_given_pivot
+    {
+        public int[] PivotArray(int[] nums, int pivot)
+        {
+            var res = new int[nums.Length];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = pivot;
+            }
+
+            var left = 0;
+            var right = nums.Length - 1;
+            for (int i = 0; i < res.Length; i++)
+            {
+                if (nums[i] < pivot)
+                {
+                    res[left] = nums[i];
+                    left++;
+                }
+            }
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (nums[i] > pivot)
+                {
+                    res[right] = nums[i];
+                    right--;
+                }
+            }
+
+            return res;
+        }
+    }
+}
