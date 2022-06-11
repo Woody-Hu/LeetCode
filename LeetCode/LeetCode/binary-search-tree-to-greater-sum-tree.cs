e7acbabce90f673e63fef65cb862940bf41f26c6
Stephen Hu <stephenhu@seismic.com>
2022-06-12 07:58:30 +0800

    Create binary-search-tree-to-greater-sum-tree.cs

---
+++ LeetCode/LeetCode/binary-search-tree-to-greater-sum-tree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+	public class binary_search_tree_to_greater_sum_tree
+	{
+		public TreeNode BstToGst(TreeNode root)
+		{
+			var sum = 0;
+			Visit(root, ref sum);
+			return root;
+		}
+
+		private void Visit(TreeNode root, ref int sum)
+		{
+            if (root == null)
+            {
+				return;
+            }
+
+			Visit(root.right, ref sum);
+			sum += root.val;
+			root.val = sum;
+			Visit(root.left, ref sum);
+		}
+	}
+}
+
