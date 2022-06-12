9ec5e1bcca6d02d773e78a1bc3d037320cacc41e
Stephen Hu <stephenhu@seismic.com>
2022-06-12 08:01:19 +0800

    Create can-make-arithmetic-progression-from-sequence.cs

---
+++ LeetCode/LeetCode/can-make-arithmetic-progression-from-sequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+	public class can_make_arithmetic_progression_from_sequence
+	{
+		public bool CanMakeArithmeticProgression(int[] arr)
+		{
+			Array.Sort(arr);
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                if (arr[i] * 2 != arr[i - 1] + arr[i + 1])
+                {
+					return false;
+                }
+            }
+
+			return true;
+		}
+	}
+}
+
