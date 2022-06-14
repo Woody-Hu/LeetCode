2937a2889a505a168243976a4941198b41a7a34a
Stephen Hu <stephenhu@seismic.com>
2022-06-14 11:45:40 +0800

    Create richest-customer-wealth.cs

---
+++ LeetCode/LeetCode/richest-customer-wealth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class richest_customer_wealth
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            var max = 0;
+            foreach (var item in accounts)
+            {
+                var sum = item.Sum();
+                max = Math.Max(max, sum);
+            }
+
+            return max;
+        }
+    }
+}
