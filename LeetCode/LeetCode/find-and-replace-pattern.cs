f85f3bc144bfba4d4707a7850555c66d03fa73c5
Stephen Hu <stephenhu@seismic.com>
2022-06-14 11:25:25 +0800

    Create find-and-replace-pattern.cs

---
+++ LeetCode/LeetCode/find-and-replace-pattern.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class find_and_replace_pattern
+    {
+        public IList<string> FindAndReplacePattern(string[] words, string pattern)
+        {
+            var res = new List<string>();
+            foreach (var item in words)
+            {
+                if (Check(item, pattern) && Check(pattern, item))
+                {
+                    res.Add(item);
+                }
+            }
+
+            return res;
+        }
+
+        private bool Check(string input, string pattern)
+        {
+            var map = new Dictionary<char, char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                var x = input[i];
+                var y = pattern[i];
+                if (!map.ContainsKey(x))
+                {
+                    map[x] = y;
+                }
+                else if (map[x] != y)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
