d09732fa09c202d9f2b3385d30259c00f2833212
Stephen Hu <stephenhu@seismic.com>
2022-06-15 11:41:22 +0800

    Create percentage-of-letter-in-string.cs

---
+++ LeetCode/LeetCode/percentage-of-letter-in-string.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class percentage_of_letter_in_string
+    {
+        public int PercentageLetter(string s, char letter)
+        {
+            var count = s.Count(k => k == letter);
+            return count * 100 / s.Length;
+        }
+    }
+}
