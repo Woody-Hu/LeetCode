531224874f08b18ff309487cc9f58af9589bf2e0
Stephen Hu <stephenhu@seismic.com>
2022-06-16 11:50:45 +0800

    Create make-two-arrays-equal-by-reversing-sub-arrays.cs

---
+++ LeetCode/LeetCode/make-two-arrays-equal-by-reversing-sub-arrays.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class make_two_arrays_equal_by_reversing_sub_arrays
+    {
+        public bool CanBeEqual(int[] target, int[] arr)
+        {
+            Array.Sort(target);
+            Array.Sort(arr);
+            return target.SequenceEqual(arr);
+        }
+    }
+}
