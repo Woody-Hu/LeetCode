7882797486f64690e58d4c3a76a4c24017a42cb3
Stephen Hu <stephenhu@seismic.com>
2022-06-17 15:17:01 +0800

    Create root-equals-sum-of-children.cs

---
+++ LeetCode/LeetCode/root-equals-sum-of-children.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LeetCode
+{
+    public class root_equals_sum_of_children
+    {
+        public bool CheckTree(TreeNode root)
+        {
+            return root.val == root.left.val + root.right.val;
+        }
+    }
+}
