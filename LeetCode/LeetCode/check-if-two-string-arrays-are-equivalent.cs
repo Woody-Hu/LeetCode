984c0f4797619523fec2e6afca4c28e415f000ff
Stephen Hu <stephenhu@seismic.com>
2022-06-18 07:13:42 +0800

    Create check-if-two-string-arrays-are-equivalent.cs

---
+++ LeetCode/LeetCode/check-if-two-string-arrays-are-equivalent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LeetCode
+{
+	public class check_if_two_string_arrays_are_equivalent
+	{
+		public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+		{
+			var s1 = string.Concat(word1);
+			var s2 = string.Concat(word2);
+			return s1.Equals(s2);
+		}
+	}
+}
+
