8643eeb29813f44b2920051eeb87d3c63c8dff91
Stephen Hu <stephenhu@seismic.com>
2022-06-17 14:57:42 +0800

    Create watering-plants.cs

---
+++ LeetCode/LeetCode/watering-plants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+    public class watering_plants
+    {
+        public int WateringPlants(int[] plants, int capacity)
+        {
+            var res = 0;
+            var current = capacity;
+            for (int i = 0; i < plants.Length; i++)
+            {
+                if (current >= plants[i])
+                {
+                    res++;
+                    current -= plants[i];
+                }
+                else
+                {
+                    res += 2 * i + 1;
+                    current = capacity - plants[i];
+                }
+            }
+
+            return res;
+        }
+    }
+}
