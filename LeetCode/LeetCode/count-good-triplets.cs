f7afcc463b6665deaba14ac4f5ae0e25893b5030
Stephen Hu <stephenhu@seismic.com>
2022-06-17 15:11:58 +0800

    Create count-good-triplets.cs

---
+++ LeetCode/LeetCode/count-good-triplets.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+    public class count_good_triplets
+    {
+        public int CountGoodTriplets(int[] arr, int a, int b, int c)
+        {
+            var res = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    for (int k = j + 1; k < arr.Length; k++)
+                    {
+                        if (Math.Abs(arr[i] - arr[j]) <= a && Math.Abs(arr[j] - arr[k]) <= b && Math.Abs(arr[k] - arr[i]) <= c)
+                        {
+                            res++;
+                        }
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
