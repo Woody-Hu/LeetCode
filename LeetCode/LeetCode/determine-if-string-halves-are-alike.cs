270eceea7ff64ca9d3505dc53d12f344b5676d28
Stephen Hu <stephenhu@seismic.com>
2022-06-19 07:36:49 +0800

    Create determine-if-string-halves-are-alike.cs

---
+++ LeetCode/LeetCode/determine-if-string-halves-are-alike.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class determine_if_string_halves_are_alike
+	{
+		public bool HalvesAreAlike(string s)
+		{
+			var set = new HashSet<char>("aeiouAEIOU");
+			var lc = 0;
+			var rc = 0;
+            for (int i = 0; i < s.Length/2; i++)
+            {
+                if (set.Contains(s[i]))
+                {
+					lc++;
+                }
+
+                if (set.Contains(s[i + s.Length/2]))
+                {
+					rc++;
+                }
+            }
+
+			return lc == rc;	
+		}
+	}
+}
+
