5a0c6173bfe41dadba7c86786e6458c3058f85c9
Stephen Hu <stephenhu@seismic.com>
2022-06-19 07:36:54 +0800

    Create final-value-of-variable-after-performing-operations.cs

---
+++ LeetCode/LeetCode/final-value-of-variable-after-performing-operations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+	public class final_value_of_variable_after_performing_operations
+	{
+		public int FinalValueAfterOperations(string[] operations)
+		{
+			var res = 0;
+            foreach (var item in operations)
+            {
+				if (item[1] == '-')
+				{
+					res--;
+				}
+				else
+				{
+					res++;
+				}
+            }
+
+			return res;
+		}
+	}
+}
+
