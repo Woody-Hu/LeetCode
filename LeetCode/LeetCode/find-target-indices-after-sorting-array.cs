6ce8b146187a014e5b532c62f9d4a18371d0e6b5
Stephen Hu <stephenhu@seismic.com>
2022-06-19 07:37:04 +0800

    Create find-target-indices-after-sorting-array.cs

---
+++ LeetCode/LeetCode/find-target-indices-after-sorting-array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class find_target_indices_after_sorting_array
+	{
+		public IList<int> TargetIndices(int[] nums, int target)
+		{
+			Array.Sort(nums);
+			var res = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > target)
+                {
+					break;
+                }
+                else if (nums[i] == target)
+                {
+					res.Add(i);
+                }
+            }
+
+			return res;
+		}
+	}
+}
+
