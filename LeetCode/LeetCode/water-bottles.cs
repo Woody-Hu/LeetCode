bff8063ebc64b2c87ba0b21c163f69421e9041db
Stephen Hu <stephenhu@seismic.com>
2022-06-20 14:46:52 +0800

    Create water-bottles.cs

---
+++ LeetCode/LeetCode/water-bottles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LeetCode
+{
+    public class water_bottles
+    {
+        public int NumWaterBottles(int numBottles, int numExchange)
+        {
+            return numBottles >= numExchange ? (numBottles - numExchange) / (numExchange - 1) + 1 + numBottles : numBottles;
+        }
+    }
+}
