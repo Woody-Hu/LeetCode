d3bb0cfd8a6d061b6226836a79e0fb664982339a
Stephen Hu <stephenhu@seismic.com>
2022-06-20 14:43:22 +0800

    Create maximum-ice-cream-bars.cs

---
+++ LeetCode/LeetCode/maximum-ice-cream-bars.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_ice_cream_bars
+    {
+        public int MaxIceCream(int[] costs, int coins)
+        {
+            Array.Sort(costs);
+            var res = 0;
+            foreach (var item in costs)
+            {
+                if (item <= coins)
+                {
+                    res++;
+                    coins -= item;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return res;
+        }
+    }
+}
