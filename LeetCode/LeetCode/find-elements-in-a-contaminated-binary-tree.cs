af9a406847c02658b411d5e44fa830f191a64923
Stephen Hu <stephenhu@seismic.com>
2022-06-21 09:22:36 +0800

    Create find-elements-in-a-contaminated-binary-tree.cs

---
+++ LeetCode/LeetCode/find-elements-in-a-contaminated-binary-tree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class find_elements_in_a_contaminated_binary_tree
+    {
+        public class FindElements
+        {
+            private HashSet<int> _set = new HashSet<int>();
+
+            public FindElements(TreeNode root)
+            {
+                root.val = 0;
+                _set.Add(0);
+                Fix(root);
+            }
+
+            private void Fix(TreeNode root)
+            {
+                if (root.left != null)
+                {
+                    var leftValue = root.left.val;
+                    leftValue = (root.val << 1) + 1;
+                    _set.Add(leftValue);
+                    root.left.val = leftValue;
+                    Fix(root.left);
+                }
+
+                if (root.right != null)
+                {
+                    var rightValue = root.right.val;
+                    rightValue = (root.val << 1) + 2;
+                    _set.Add(rightValue);
+                    root.right.val = rightValue;
+                    Fix(root.right);
+                }
+            }
+
+            public bool Find(int target)
+            {
+                return _set.Contains(target);
+            }
+        }
+    }
+}
