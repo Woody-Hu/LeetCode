227eb78878f48cf3f13dbf5863c5351c8c22f578
Stephen Hu <stephenhu@seismic.com>
2022-06-22 10:09:16 +0800

    Create widest-vertical-area-between-two-points-containing-no-points.cs

---
+++ LeetCode/LeetCode/widest-vertical-area-between-two-points-containing-no-points.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+    public class widest_vertical_area_between_two_points_containing_no_points
+    {
+        public int MaxWidthOfVerticalArea(int[][] points)
+        {
+            var xPoints = new int[points.Length];
+            for (int i = 0; i < points.Length; i++)
+            {
+                xPoints[i] = points[i][0];
+            }
+
+            Array.Sort(xPoints);
+            var res = 0;
+            for (int i = 1; i < xPoints.Length; i++)
+            {
+                var dif = xPoints[i] - xPoints[i - 1];
+                res = Math.Max(res, dif);
+            }
+
+            return res;
+        }
+    }
+}
