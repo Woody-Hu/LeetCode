f022d42f1e6723e46aa9c8cf48312d51c586a0c5
Stephen Hu <stephenhu@seismic.com>
2022-06-22 10:36:30 +0800

    Create count-number-of-pairs-with-absolute-difference-k.cs

---
+++ LeetCode/LeetCode/count-number-of-pairs-with-absolute-difference-k.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LeetCode
+{
+    public class count_number_of_pairs_with_absolute_difference_k
+    {
+        public int CountKDifference(int[] nums, int k)
+        {
+            var res = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (Math.Abs(nums[i] - nums[j]) == k)
+                    {
+                        res++;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
