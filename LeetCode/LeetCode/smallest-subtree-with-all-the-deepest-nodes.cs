c040b7c3a6a7113940a94ce37f55ce30ce44b241
Stephen Hu <stephenhu@seismic.com>
2022-06-23 11:29:42 +0800

    Create smallest-subtree-with-all-the-deepest-nodes.cs

---
+++ LeetCode/LeetCode/smallest-subtree-with-all-the-deepest-nodes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace LeetCode
+{
+    public class smallest_subtree_with_all_the_deepest_nodes
+    {
+        public TreeNode SubtreeWithAllDeepest(TreeNode root)
+        {
+            var res = Visit(root);
+            return res.Item1;
+        }
+
+        private (TreeNode, int) Visit(TreeNode root)
+        {
+            if (root == null)
+            {
+                return (null, 0);
+            }
+
+            var left = Visit(root.left);
+            var right = Visit(root.right);
+            if (left.Item2 > right.Item2)
+            {
+                return (left.Item1, left.Item2 + 1);
+            }
+
+            if (left.Item2 < right.Item2)
+            {
+                return (right.Item1, right.Item2 + 1);
+            }
+
+            return (root, left.Item2 + 1);
+        }
+    }
+}
