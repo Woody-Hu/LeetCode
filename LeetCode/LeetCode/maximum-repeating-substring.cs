71d8dfe98e1cbd3f5d6eb4e90a19b621dfe83c29
Stephen Hu <stephenhu@seismic.com>
2022-06-23 11:29:39 +0800

    Create maximum-repeating-substring.cs

---
+++ LeetCode/LeetCode/maximum-repeating-substring.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    public class maximum_repeating_substring
+    {
+        public int MaxRepeating(string sequence, string word)
+        {
+            var res = 0;
+            var stringBuilder = new StringBuilder(word);
+            while (sequence.Contains(stringBuilder.ToString()))
+            {
+                res++;
+                stringBuilder.Append(word);
+            }
+
+            return res;
+        }
+    }
+}
