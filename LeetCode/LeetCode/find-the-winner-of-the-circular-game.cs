2334c8cb17339f31c1fdd1f37bb1bd4bfc24faf7
Stephen Hu <stephenhu@seismic.com>
2022-06-24 10:40:33 +0800

    Create find-the-winner-of-the-circular-game.cs

---
+++ LeetCode/LeetCode/find-the-winner-of-the-circular-game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LeetCode
+{
+    public class find_the_winner_of_the_circular_game
+    {
+        public int FindTheWinner(int n, int k)
+        {
+            var res = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                res = (k + res - 1) % i + 1;
+            }
+
+            return res;
+        }
+    }
+}
