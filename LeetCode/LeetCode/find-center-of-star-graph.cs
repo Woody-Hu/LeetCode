41d44ae4222cdabea610ab16674b15888a2dbceb
Stephen Hu <stephenhu@seismic.com>
2022-06-24 10:53:28 +0800

    Create find-center-of-star-graph.cs

---
+++ LeetCode/LeetCode/find-center-of-star-graph.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LeetCode
+{
+    public class find_center_of_star_graph
+    {
+        public int FindCenter(int[][] edges)
+        {
+            return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+        }
+    }
+}
