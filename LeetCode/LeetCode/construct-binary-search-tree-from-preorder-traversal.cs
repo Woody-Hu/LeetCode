92532b064a310649a4d572a1bf3af6d4ff38b94e
Stephen Hu <stephenhu@seismic.com>
2022-06-24 10:50:48 +0800

    Create construct-binary-search-tree-from-preorder-traversal.cs

---
+++ LeetCode/LeetCode/construct-binary-search-tree-from-preorder-traversal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class construct_binary_search_tree_from_preorder_traversal
+    {
+        public TreeNode BstFromPreorder(int[] preorder)
+        {
+            if (!preorder.Any())
+            {
+                return null;
+            }
+
+            var root = new TreeNode(preorder[0]);
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+            for (int i = 1; i < preorder.Length; i++)
+            {
+                var parent = stack.Peek();
+                var current = new TreeNode(preorder[i]);
+                while (stack.Any() && stack.Peek().val < current.val)
+                {
+                    parent = stack.Pop();
+                }
+
+                if (current.val < parent.val)
+                {
+                    parent.left = current;
+                }
+                else
+                {
+                    parent.right = current;
+                }
+
+                stack.Push(current);
+            }
+
+            return root;
+        }
+    }
+}
