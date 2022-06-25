b4579150d5f6423a209c5210b56c70c04373f5f8
Stephen Hu <stephenhu@seismic.com>
2022-06-25 10:50:13 +0800

    Create design-parking-system.cs

---
+++ LeetCode/LeetCode/design-parking-system.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCode
+{
+	public class design_parking_system
+	{
+        public class ParkingSystem
+        {
+            private int[] _data;
+
+            public ParkingSystem(int big, int medium, int small)
+            {
+                _data = new int[3];
+                _data[0] = big;
+                _data[1] = medium;
+                _data[2] = small;
+            }
+
+            public bool AddCar(int carType)
+            {
+                if (_data[carType] <= 0)
+                {
+                    return false;
+                }
+
+                _data[carType]--;
+                return true;
+            }
+        }
+    }
+}
+
