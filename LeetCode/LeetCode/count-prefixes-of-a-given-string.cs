e7f43c81882db23073f1fe316f441318159ba069
Stephen Hu <stephenhu@seismic.com>
2022-06-25 10:54:51 +0800

    Create count-prefixes-of-a-given-string.cs

---
+++ LeetCode/LeetCode/count-prefixes-of-a-given-string.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class count_prefixes_of_a_given_string
+	{
+        public int CountPrefixes(string[] words, string s)
+        {
+            return words.Count(k => s.StartsWith(k, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
+
