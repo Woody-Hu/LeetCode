85b5d2b73c3a77e2ee0c7e1581feebc5f2c29a27
Stephen Hu <stephenhu@seismic.com>
2022-06-25 10:50:09 +0800

    Create defuse-the-bomb.cs

---
+++ LeetCode/LeetCode/defuse-the-bomb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+	public class defuse_the_bomb
+	{
+        public int[] Decrypt(int[] code, int k)
+        {
+            var res = new int[code.Length];
+            var e = k >= 0 ? k == 0 ? 0 : 1 : -1;
+            for (int i = 0; i < code.Length; i++)
+            {
+                var sum = 0;
+                for (int j = e; j != k + e; j = j + e)
+                {
+                    sum += code[(i + j + code.Length) % code.Length];
+                }
+
+                res[i] = sum;
+            }
+
+            return res;
+        }
+    }
+}
+
