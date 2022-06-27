bbd22e386d2b1ddb1dd7c845e7b67bba5082a9c4
Stephen Hu <stephenhu@seismic.com>
2022-06-27 13:40:53 +0800

    Create rearrange-array-elements-by-sign.cs

---
+++ LeetCode/LeetCode/rearrange-array-elements-by-sign.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+    public class rearrange_array_elements_by_sign
+    {
+        public int[] RearrangeArray(int[] nums) {
+            var res = new int[nums.Length];
+            var evenI = 0;
+            var oddI = 1;
+            foreach (var item in nums)
+            {
+                if (item > 0)
+                {
+                    res[evenI] = item;
+                    evenI += 2;
+                }
+                else
+                {
+                    res[oddI] = item;
+                    oddI += 2;
+                }
+            }
+
+            return res;
+        }
+    }
+}
