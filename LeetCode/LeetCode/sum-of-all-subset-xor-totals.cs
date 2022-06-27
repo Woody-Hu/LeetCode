f541149e3548637682435ca81aca8a2069363abf
Stephen Hu <stephenhu@seismic.com>
2022-06-27 13:46:33 +0800

    Create sum-of-all-subset-xor-totals.cs

---
+++ LeetCode/LeetCode/sum-of-all-subset-xor-totals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LeetCode
+{
+    public class sum_of_all_subset_xor_totals
+    {
+        public int SubsetXORSum(int[] nums)
+        {
+            var res = 0;
+            foreach (var item in nums)
+            {
+                res |= item;
+            }
+
+            return res << (nums.Length - 1);
+        }
+    }
+}
