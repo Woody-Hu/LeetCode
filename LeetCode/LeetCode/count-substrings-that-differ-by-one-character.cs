1adf27a76572a08453f0c8c0f7306e99eef64339
Stephen Hu <stephenhu@seismic.com>
2022-06-27 13:40:46 +0800

    Create count-substrings-that-differ-by-one-character.cs

---
+++ LeetCode/LeetCode/count-substrings-that-differ-by-one-character.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class count_substrings_that_differ_by_one_character
+    {
+        public int CountSubstrings(string s, string t)
+        {
+            var m = s.Length;
+            var n = t.Length;
+            var res = 0;
+            for (int i = 0; i < m; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    int diff = 0;
+                    for (int k = 0; i + k < m && j + k < n; ++k)
+                    {
+                        if (s.ElementAt(i + k) != t.ElementAt(j + k))
+                        {
+                            diff++;
+                        }
+
+                        if (diff > 1)
+                        {
+                            break;
+                        }
+                        if (diff == 1)
+                        {
+                            ++res;
+                        }
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
