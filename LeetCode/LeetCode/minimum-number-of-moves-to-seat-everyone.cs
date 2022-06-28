77a20d803a49c6fb279dcc5ee99cb99dd5996105
Stephen Hu <stephenhu@seismic.com>
2022-06-28 17:16:10 +0800

    Create minimum-number-of-moves-to-seat-everyone.cs

---
+++ LeetCode/LeetCode/minimum-number-of-moves-to-seat-everyone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_number_of_moves_to_seat_everyone
+    {
+        public int MinMovesToSeat(int[] seats, int[] students)
+        {
+            Array.Sort(seats);
+            Array.Sort(students);
+            var res = 0;
+            for (int i = 0; i < seats.Length; i++)
+            {
+                res += Math.Abs(seats[i] - students[i]);
+            }
+
+            return res;
+        }
+    }
+}
