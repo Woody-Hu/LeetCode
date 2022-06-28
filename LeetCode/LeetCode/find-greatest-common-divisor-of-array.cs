507895e7d55f18d16dd8334bb46753fd3a0b43d1
Stephen Hu <stephenhu@seismic.com>
2022-06-28 17:05:57 +0800

    Create find-greatest-common-divisor-of-array.cs

---
+++ LeetCode/LeetCode/find-greatest-common-divisor-of-array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LeetCode
+{
+    public class find_greatest_common_divisor_of_array
+    {
+        public int FindGCD(int[] nums)
+        {
+            Array.Sort(nums);
+            int min = nums[0];
+            int max = nums[nums.Length - 1];
+            int ans = 1;
+            for (int i = min; i >= 1; i--)
+            {
+                if (min % i == 0 && max % i == 0)
+                {
+                    ans = i;
+                    break;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
