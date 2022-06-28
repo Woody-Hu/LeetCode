8b1e913f10f486b79aa0b0d75bff7a8e1c1c3f63
Stephen Hu <stephenhu@seismic.com>
2022-06-28 15:28:30 +0800

    Create reveal-cards-in-increasing-order.cs

---
+++ LeetCode/LeetCode/reveal-cards-in-increasing-order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class reveal_cards_in_increasing_order
+    {
+        public int[] DeckRevealedIncreasing(int[] deck)
+        {
+            var n = deck.Length;
+            var indexes = new Queue<int>();
+            for (int i = 0; i < n; i++)
+            {
+                indexes.Enqueue(i);
+            }
+
+            Array.Sort(deck);
+            var res = new int[n];
+            foreach (var item in deck)
+            {
+                res[indexes.Dequeue()] = item;
+                if (indexes.Any())
+                {
+                    indexes.Enqueue(indexes.Dequeue());
+                }
+            }
+
+            return res;  
+        }
+    }
+}
