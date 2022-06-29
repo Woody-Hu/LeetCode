403818389eaf2e2fec05d4c789fa97ce7767fd0c
Stephen Hu <stephenhu@seismic.com>
2022-06-29 11:25:50 +0800

    Create minimum-number-of-vertices-to-reach-all-nodes.cs

---
+++ LeetCode/LeetCode/minimum-number-of-vertices-to-reach-all-nodes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class minimum_number_of_vertices_to_reach_all_nodes
+    {
+        public IList<int> FindSmallestSetOfVertices(int n, IList<IList<int>> edges)
+        {
+            var res = new List<int>();
+            var set = new HashSet<int>();
+            foreach (var item in edges)
+            {
+                set.Add(item[1]);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!set.Contains(i))
+                {
+                    set.Add(i);
+                }
+            }
+
+            return res;
+        }
+    }
+}
