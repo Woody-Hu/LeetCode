521acf210b325d0f4503b61f071862369f06281c
Stephen Hu <stephenhu@seismic.com>
2022-07-01 15:52:49 +0800

    Create delete-middle-node-lcci.cs

---
+++ LeetCode/LeetCode/delete-middle-node-lcci.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LeetCode
+{
+    public class delete_middle_node_lcci
+    {
+        public void DeleteNode(ListNode node)
+        {
+            var next = node.next;
+            node.val = next.val;
+            node.next = next.next;
+        }
+    }
+}
