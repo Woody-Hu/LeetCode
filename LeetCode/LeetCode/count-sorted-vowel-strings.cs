fff59d7fa256e02f308638bce5cefb7d3a3ca210
Stephen Hu <stephenhu@seismic.com>
2022-07-04 10:36:24 +0800

    Create count-sorted-vowel-strings.cs

---
+++ LeetCode/LeetCode/count-sorted-vowel-strings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LeetCode
+{
+    public class count_sorted_vowel_strings
+    {
+        public int CountVowelStrings(int n)
+        {
+            return (n + 4) * (n + 3) * (n + 2) * (n + 1) / 24;
+        }
+    }
+}
