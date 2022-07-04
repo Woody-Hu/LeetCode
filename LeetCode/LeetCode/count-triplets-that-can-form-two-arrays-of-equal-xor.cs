5d87e76ac166c5f0f054b9496e833087ac523311
Stephen Hu <stephenhu@seismic.com>
2022-07-04 10:36:26 +0800

    Create count-triplets-that-can-form-two-arrays-of-equal-xor.cs

---
+++ LeetCode/LeetCode/count-triplets-that-can-form-two-arrays-of-equal-xor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LeetCode
+{
+    public class count_triplets_that_can_form_two_arrays_of_equal_xor
+    {
+        public int CountTriplets(int[] arr)
+        {
+            var s = new int[arr.Length + 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                s[i + 1] = s[i] ^ arr[i];
+            }
+
+            var res = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (s[i] == s[j + 1])
+                    {
+                        res += j - i;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
