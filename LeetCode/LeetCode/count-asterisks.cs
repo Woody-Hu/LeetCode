82122ee390cfcde2b09441f681f9faaa64ca5bd3
Stephen Hu <stephenhu@seismic.com>
2022-07-04 12:13:58 +0800

    Update count-asterisks.cs

--- LeetCode/LeetCode/count-asterisks.cs
+++ LeetCode/LeetCode/count-asterisks.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Linq;
+
 namespace LeetCode
 {
     public class count_asterisks
     {
-        public count_asterisks()
+        public int CountAsterisks(string s)
         {
+            var splited = s.Split('|');
+            var res = 0;
+            for (int i = 0; i < splited.Length; i = i +2)
+            {
+                res += splited[i].Count(k => k == '*');
+            }
+
+            return res;
         }
     }
 }
