2165e9c4a97ba97bd63cfb5d577c6ac316e886b4
Stephen Hu <stephenhu@seismic.com>
2022-07-05 09:25:26 +0800

    Create count-good-nodes-in-binary-tree.cs

---
+++ LeetCode/LeetCode/count-good-nodes-in-binary-tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCode
+{
+    public class count_good_nodes_in_binary_tree
+    {
+        public int GoodNodes(TreeNode root)
+        {
+            var res = 0;
+            Visit(root, root.val, ref res);
+            return res;
+        }
+
+        private void Visit(TreeNode root, int currentMax, ref int res)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            var currentValue = root.val;
+            if (currentValue >= currentMax)
+            {
+                res++;
+                currentMax = currentValue;
+            }
+
+            Visit(root.left, currentMax, ref res);
+            Visit(root.right, currentMax, ref res);
+        }
+    }
+}
