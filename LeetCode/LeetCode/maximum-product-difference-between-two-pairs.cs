95d5ad9bd6c361d78141b58eb1019cd21935b4da
Stephen Hu <stephenhu@seismic.com>
2022-07-05 09:25:27 +0800

    Create maximum-product-difference-between-two-pairs.cs

---
+++ LeetCode/LeetCode/maximum-product-difference-between-two-pairs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_product_difference_between_two_pairs
+    {
+        public int MaxProductDifference(int[] nums)
+        {
+            var n = nums.Length;
+            Array.Sort(nums);
+            return (nums[n - 1] * nums[n - 2]) - (nums[1] * nums[0]);
+        }
+    }
+}
