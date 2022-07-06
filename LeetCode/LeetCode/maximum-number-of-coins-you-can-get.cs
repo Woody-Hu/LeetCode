d7a2aed7d88ed75d21311a587186c189b88204aa
Stephen Hu <stephenhu@seismic.com>
2022-07-06 09:50:57 +0800

    Create maximum-number-of-coins-you-can-get.cs

---
+++ LeetCode/LeetCode/maximum-number-of-coins-you-can-get.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_number_of_coins_you_can_get
+    {
+        public int MaxCoins(int[] piles)
+        {
+            Array.Sort(piles);
+            var times = piles.Length / 3;
+            var i = piles.Length - 2;
+            var res = 0;
+            for (int j = 0; j < times; j++)
+            {
+                res += piles[i];
+                i = i - 2;
+            }
+
+            return res;
+        }
+    }
+}
