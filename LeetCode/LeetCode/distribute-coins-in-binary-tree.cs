25bf700117ac9147cbf00ed70570766016905445
Stephen Hu <stephenhu@seismic.com>
2022-07-06 09:50:54 +0800

    Create distribute-coins-in-binary-tree.cs

---
+++ LeetCode/LeetCode/distribute-coins-in-binary-tree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+    public class distribute_coins_in_binary_tree
+    {
+        public int DistributeCoins(TreeNode root)
+        {
+            var res = 0;
+            Visit(root, ref res);
+            return res;
+        }
+
+        private int Visit(TreeNode root, ref int res)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var l = Visit(root.left, ref res);
+            var r = Visit(root.right, ref res);
+            res += Math.Abs(l) + Math.Abs(r);
+            return root.val + l + r - 1;
+        }
+    }
+}
