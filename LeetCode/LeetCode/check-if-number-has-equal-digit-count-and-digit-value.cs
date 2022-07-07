bf4070ca5ae0348be4c5f608d7c52c671d253aae
Stephen Hu <stephenhu@seismic.com>
2022-07-07 10:24:36 +0800

    Create check-if-number-has-equal-digit-count-and-digit-value.cs

---
+++ LeetCode/LeetCode/check-if-number-has-equal-digit-count-and-digit-value.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace LeetCode
+{
+    public class check_if_number_has_equal_digit_count_and_digit_value
+    {
+        public check_if_number_has_equal_digit_count_and_digit_value()
+        {
+        }
+    }
+}
