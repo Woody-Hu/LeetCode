98291ee1b2f7de51eb374d7652e349c6ddb2c1c7
Stephen Hu <stephenhu@seismic.com>
2022-07-07 10:24:38 +0800

    Create maximum-xor-after-operations.cs

---
+++ LeetCode/LeetCode/maximum-xor-after-operations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_xor_after_operations
+    {
+        public int MaximumXOR(int[] nums)
+        {
+            var res = 0;
+            foreach (var item in nums)
+            {
+                res |= item;
+            }
+
+            return res;
+        }
+    }
+}
