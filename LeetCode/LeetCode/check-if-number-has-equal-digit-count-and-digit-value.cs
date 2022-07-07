cf03d780f57589a8a96f2ef3e667f70bdc3dde8c
Stephen Hu <stephenhu@seismic.com>
2022-07-07 12:00:09 +0800

    Update check-if-number-has-equal-digit-count-and-digit-value.cs

--- LeetCode/LeetCode/check-if-number-has-equal-digit-count-and-digit-value.cs
+++ LeetCode/LeetCode/check-if-number-has-equal-digit-count-and-digit-value.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace LeetCode
 {
     public class check_if_number_has_equal_digit_count_and_digit_value
     {
-        public check_if_number_has_equal_digit_count_and_digit_value()
+        public bool DigitCount(string num)
         {
+            var dic = new int[10];
+            foreach (var item in num)
+            {
+                var value = item - '0';
+                dic[value]++;
+            }
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (dic[i] != (num[i] - '0'))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
