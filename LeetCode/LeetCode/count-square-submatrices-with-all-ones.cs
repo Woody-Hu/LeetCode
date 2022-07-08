ffe1ba861df1735d2aa61785ee13c55d800abfb6
Stephen Hu <stephenhu@seismic.com>
2022-07-08 09:38:07 +0800

    Create count-square-submatrices-with-all-ones.cs

---
+++ LeetCode/LeetCode/count-square-submatrices-with-all-ones.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeetCode
+{
+    public class count_square_submatrices_with_all_ones
+    {
+        public int CountSquares(int[][] matrix)
+        {
+            var n = matrix.Length;
+            var m = matrix[0].Length;
+            var dp = new int[n, m];
+            var res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i][j] == 1)
+                    {
+                        if (i == 0 || j == 0)
+                        {
+                            res++;
+                            dp[i, j] = 1;
+                        }
+                        else
+                        {
+                            dp[i, j] = Math.Min(Math.Min(dp[i - 1, j], dp[i, j - 1]), dp[i - 1, j - 1]) + 1;
+                            res += dp[i, j];
+                        }
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
