b9fd5cd040cfe47c088bada4f169e1f811ac0e2e
Stephen Hu <stephenhu@seismic.com>
2022-07-08 09:39:52 +0800

    Update na-ying-bi.cs

--- LeetCode/LeetCode/na-ying-bi.cs
+++ LeetCode/LeetCode/na-ying-bi.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Linq;
+
 namespace LeetCode
 {
     public class na_ying_bi
     {
-        public na_ying_bi()
+        public int MinCount(int[] coins)
         {
+            return coins.Sum(k => (k + 1) / 2);
         }
     }
 }
