3ca94f90fec94b5d7a19dfca53f6591d41429ea0
Stephen Hu <stephenhu@seismic.com>
2022-07-11 10:12:16 +0800

    Create evaluate-boolean-binary-tree.cs

---
+++ LeetCode/LeetCode/evaluate-boolean-binary-tree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+    public class evaluate_boolean_binary_tree
+    {
+        public bool EvaluateTree(TreeNode root)
+        {
+            if (root.val <= 1)
+            {
+                return root.val == 1;
+            }
+
+            if (root.val == 2)
+            {
+                return EvaluateTree(root.left) || EvaluateTree(root.right);
+            }
+
+            return EvaluateTree(root.left) && EvaluateTree(root.right);
+        }
+    }
+}
