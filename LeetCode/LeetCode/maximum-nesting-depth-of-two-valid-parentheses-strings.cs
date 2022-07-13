05a64f362032f105ba754aa75813955530a8bbe0
Stephen Hu <stephenhu@seismic.com>
2022-07-13 12:05:08 +0800

    Create maximum-nesting-depth-of-two-valid-parentheses-strings.cs

---
+++ LeetCode/LeetCode/maximum-nesting-depth-of-two-valid-parentheses-strings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_nesting_depth_of_two_valid_parentheses_strings
+    {
+        public int[] MaxDepthAfterSplit(string seq)
+        {
+            var res = new int[seq.Length];
+            var d = 0;
+            for (int i = 0; i < seq.Length; i++)
+            {
+                if (seq[i] == '(')
+                {
+                    d++;
+                    res[i] = d % 2;
+                }
+                else
+                {
+                    res[i] = d % 2;
+                    d--;
+                }
+            }
+
+            return res;
+        }
+    }
+}
