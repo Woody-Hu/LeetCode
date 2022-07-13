afa51609d9964176f53acc856baea9a93876c324
Stephen Hu <stephenhu@seismic.com>
2022-07-13 12:09:07 +0800

    Create partition-array-such-that-maximum-difference-is-k.cs

---
+++ LeetCode/LeetCode/partition-array-such-that-maximum-difference-is-k.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class partition_array_such_that_maximum_difference_is_k
+    {
+        public int PartitionArray(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            var min = nums.First();
+            var res = 1;
+            foreach (var item in nums)
+            {
+                if (item - min > k)
+                {
+                    res++;
+                    min = item;
+                }
+            }
+
+            return res;
+        }
+    }
+}
