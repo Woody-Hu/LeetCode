fc5b87d1cfe53f3654b519f33c5723793a56c243
Stephen Hu <stephenhu@seismic.com>
2022-07-13 12:05:04 +0800

    Create flip-equivalent-binary-trees.cs

---
+++ LeetCode/LeetCode/flip-equivalent-binary-trees.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+    public class flip_equivalent_binary_trees
+    {
+        public bool FlipEquiv(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null && root2 == null)
+            {
+                return true;
+            }
+
+            if (root1 == null || root2 == null || root1.val != root2.val)
+            {
+                return false;
+            }
+
+            return (FlipEquiv(root1.left, root2.left) && FlipEquiv(root1.right, root2.right)) || (FlipEquiv(root1.left, root2.right) && FlipEquiv(root1.right, root2.left));
+        }
+    }
+}
