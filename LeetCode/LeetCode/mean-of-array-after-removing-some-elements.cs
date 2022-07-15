fb933624ef717a7566025926067722718d95b6da
Stephen Hu <stephenhu@seismic.com>
2022-07-15 09:55:10 +0800

    Create mean-of-array-after-removing-some-elements.cs

---
+++ LeetCode/LeetCode/mean-of-array-after-removing-some-elements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class mean_of_array_after_removing_some_elements
+    {
+        public double TrimMean(int[] arr)
+        {
+            Array.Sort(arr);
+            var l = arr.Length;
+            var skip = l / 20;
+            var count = (int)(arr.Count() * 0.9d);
+            var use = arr.Skip(skip).Take(count);
+            var useCount = use.Count();
+            var sum = 0.0d;
+            foreach (var item in use)
+            {
+                sum += item;
+            }
+
+            return sum / useCount;
+
+        }
+    }
+}
