918b335f70e1c67d564e069675836dda0e328e75
Stephen Hu <stephenhu@seismic.com>
2022-07-18 10:38:44 +0800

    Create delete-nodes-and-return-forest.cs

---
+++ LeetCode/LeetCode/delete-nodes-and-return-forest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class delete_nodes_and_return_forest
+    {
+        public IList<TreeNode> DelNodes(TreeNode root, int[] to_delete)
+        {
+            var res = new List<TreeNode>();
+            if (root == null)
+            {
+                return res;
+            }
+
+            var set = new HashSet<int>();
+            foreach (var item in to_delete)
+            {
+                set.Add(item);
+            }
+
+            Visit(root, null, set, res);
+            return res;
+        }
+
+        private TreeNode Visit(TreeNode root, TreeNode parent, HashSet<int> toDelete, List<TreeNode> res)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            var left = root.left;
+            var right = root.right;
+            if (toDelete.Contains(root.val))
+            {
+                root = null;
+            }
+            else if (parent == null)
+            {
+                res.Add(root);
+            }
+
+            var leftRes = Visit(left, root, toDelete, res);
+            var rightRes = Visit(right, root, toDelete, res);
+            if (root != null)
+            {
+                root.left = leftRes;
+                root.right = rightRes;
+            }
+
+            return root;
+        }
+    }
+}
