3a34740e9011d8b9fdb3d19999db9db4ac7ce27d
Stephen Hu <stephenhu@seismic.com>
2022-07-19 09:57:50 +0800

    Create minimum-difference-between-highest-and-lowest-of-k-scores.cs

---
+++ LeetCode/LeetCode/minimum-difference-between-highest-and-lowest-of-k-scores.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_difference_between_highest_and_lowest_of_k_scores
+    {
+        public minimum_difference_between_highest_and_lowest_of_k_scores()
+        {
+        }
+    }
+}
