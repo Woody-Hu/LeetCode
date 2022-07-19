0363378ec87c5e6a04d8611f5965281f3bb356d6
Stephen Hu <stephenhu@seismic.com>
2022-07-19 09:57:48 +0800

    Create beautiful-array.cs

---
+++ LeetCode/LeetCode/beautiful-array.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class beautiful_array
+    {
+        public int[] BeautifulArray(int n)
+        {
+            var dic = new Dictionary<int, int[]>();
+            return Calculate(n, dic);
+        }
+
+        private int[] Calculate(int n, Dictionary<int, int[]> dic)
+        {
+            if (dic.TryGetValue(n, out var exist))
+            {
+                return exist;
+            }
+
+            var res = new int[n];
+            if (n == 1)
+            {
+                res[0] = 1;
+            }
+            else
+            {
+                var t = 0;
+                foreach (var item in Calculate((n + 1)/2, dic))
+                {
+                    res[t++] = 2*item - 1;
+                }
+
+                foreach (var item in Calculate((n) / 2, dic))
+                {
+                    res[t++] = 2 * item;
+                }
+            }
+
+            dic[n] = res;
+            return res;
+        }
+
+    }
+}
