2e646ae31a3aeed46796958e2dbcd58596b84415
Stephen Hu <stephenhu@seismic.com>
2022-07-19 09:59:16 +0800

    Update minimum-difference-between-highest-and-lowest-of-k-scores.cs

--- LeetCode/LeetCode/minimum-difference-between-highest-and-lowest-of-k-scores.cs
+++ LeetCode/LeetCode/minimum-difference-between-highest-and-lowest-of-k-scores.cs
@@ -3,8 +3,16 @@ namespace LeetCode
 {
     public class minimum_difference_between_highest_and_lowest_of_k_scores
     {
-        public minimum_difference_between_highest_and_lowest_of_k_scores()
+        public int MinimumDifference(int[] nums, int k)
         {
+            Array.Sort(nums);
+            var res = int.MaxValue;
+            for (int i = 0; i < nums.Length - k + 1; i++)
+            {
+                res = Math.Min(nums[i + k - 1] - nums[i], res);
+            }
+
+            return res;
         }
     }
 }
