b947167ae98163c5a027131afd27d3a9a9adb90c
Stephen Hu <stephenhu@seismic.com>
2022-07-19 09:57:53 +0800

    Create simplified-fractions.cs

---
+++ LeetCode/LeetCode/simplified-fractions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class simplified_fractions
+    {
+        public IList<string> SimplifiedFractions(int n)
+        {
+            var ans = new List<string>();
+            for (int denominator = 2; denominator <= n; ++denominator)
+            {
+                for (int numerator = 1; numerator < denominator; ++numerator)
+                {
+                    if (Calculate(numerator, denominator) == 1)
+                    {
+                        ans.Add(numerator + "/" + denominator);
+                    }
+                }
+            }
+
+            return ans;
+        }
+
+        private int Calculate(int a, int b)
+        {
+            return b != 0 ? Calculate(b, a % b) : a;
+        }
+    }
+}
