729dd917b1a272be61fe91e6cf1bc5dd1bbd2748
Stephen Hu <stephenhu@seismic.com>
2022-07-20 09:00:41 +0800

    Create remove-all-occurrences-of-a-substring.cs

---
+++ LeetCode/LeetCode/remove-all-occurrences-of-a-substring.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LeetCode
+{
+    public class remove_all_occurrences_of_a_substring
+    {
+        public string RemoveOccurrences(string s, string part)
+        {
+            while (s.Contains(part))
+            {
+                s = s.Substring(0, s.IndexOf(part)) + s.Substring(s.IndexOf(part) + part.Length);
+            }
+
+            return s;
+        }
+    }
+}
