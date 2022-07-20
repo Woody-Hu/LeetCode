319e6b86a3fa5e9cca942adb6f41f25829d54fd4
Stephen Hu <stephenhu@seismic.com>
2022-07-20 09:00:38 +0800

    Create maximum-xor-for-each-query.cs

---
+++ LeetCode/LeetCode/maximum-xor-for-each-query.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_xor_for_each_query
+    {
+        public int[] GetMaximumXor(int[] nums, int maximumBit)
+        {
+            var temp = new int[nums.Length];
+            temp[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                temp[i] = nums[i] ^ temp[i - 1];
+            }
+
+            var res = new int[nums.Length];
+            var value = (int)(Math.Pow(2, maximumBit) - 1);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res[nums.Length - i - 1] = temp[i] ^ value;
+            }
+
+            return res;
+        }
+    }
+}
