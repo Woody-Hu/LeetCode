b8519b6a40a250c5cf60ecbac1a3921323081c04
Stephen Hu <stephenhu@seismic.com>
2022-07-20 09:00:36 +0800

    Update check-if-word-equals-summation-of-two-words.cs

--- LeetCode/LeetCode/check-if-word-equals-summation-of-two-words.cs
+++ LeetCode/LeetCode/check-if-word-equals-summation-of-two-words.cs
@@ -3,8 +3,21 @@ namespace LeetCode
 {
     public class check_if_word_equals_summation_of_two_words
     {
-        public check_if_word_equals_summation_of_two_words()
+        public bool IsSumEqual(string firstWord, string secondWord, string targetWord)
         {
+            return (Calculate(firstWord) + Calculate(secondWord)) == Calculate(targetWord);
+        }
+
+        private int Calculate(string firstWord)
+        {
+            var res = 0;
+            foreach (var item in firstWord)
+            {
+                res = res * 10;
+                res += item - 'a';
+            }
+
+            return res;
         }
     }
 }
