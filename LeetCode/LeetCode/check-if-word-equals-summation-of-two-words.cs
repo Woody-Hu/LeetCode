77b4e008d3f4d24b1b23aa5649f01a0699d4667b
Stephen Hu <stephenhu@seismic.com>
2022-07-20 09:00:26 +0800

    Create check-if-word-equals-summation-of-two-words.cs

---
+++ LeetCode/LeetCode/check-if-word-equals-summation-of-two-words.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace LeetCode
+{
+    public class check_if_word_equals_summation_of_two_words
+    {
+        public check_if_word_equals_summation_of_two_words()
+        {
+        }
+    }
+}
