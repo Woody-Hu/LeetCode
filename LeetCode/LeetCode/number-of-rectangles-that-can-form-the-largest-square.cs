89777902d983616c65a469aad5f736a9f664b61f
Stephen Hu <stephenhu@seismic.com>
2022-07-21 12:22:05 +0800

    Create number-of-rectangles-that-can-form-the-largest-square.cs

---
+++ LeetCode/LeetCode/number-of-rectangles-that-can-form-the-largest-square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class number_of_rectangles_that_can_form_the_largest_square
+    {
+        public int CountGoodRectangles(int[][] rectangles)
+        {
+            var res = 0;
+            var max = 0;
+            foreach (var item in rectangles)
+            {
+                var currentMin = item.Min();
+                if (currentMin > max)
+                {
+                    max = currentMin;
+                    res = 1;
+                }
+                else if (currentMin == max)
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
