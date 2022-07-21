1839e58c3edd323a14f5a0c94fb8ccfc0338d5ed
Stephen Hu <stephenhu@seismic.com>
2022-07-21 12:21:59 +0800

    Create execution-of-all-suffix-instructions-staying-in-a-grid.cs

---
+++ LeetCode/LeetCode/execution-of-all-suffix-instructions-staying-in-a-grid.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace LeetCode
+{
+    public class execution_of_all_suffix_instructions_staying_in_a_grid
+    {
+        public int[] ExecuteInstructions(int n, int[] startPos, string s)
+        {
+            var res = new int[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                var x = startPos[0];
+                var y = startPos[1];
+                var count = 0;
+                for (int j = i; j < s.Length; j++)
+                {
+                    var value = s[j];
+                    if (value == 'L')
+                    {
+                        --y;
+                    }
+                    else if (value == 'R')
+                    {
+                        ++y;
+                    }
+                    else if (value == 'U')
+                    {
+                        --x;
+                    }
+                    else
+                    {
+                        ++x;
+                    }
+
+                    if (x < 0 || x >= n || y < 0 || y >= n)
+                    {
+                        break;
+                    }
+
+                    count++;
+                }
+
+                res[i] = count;
+            }
+
+            return res;
+        }
+    }
+}
