6c453f1d1e6a2d4a8b68cd91577b1a2b3135fce5
Stephen Hu <stephenhu@seismic.com>
2022-07-22 10:17:24 +0800

    Create egg-drop-with-2-eggs-and-n-floors.cs

---
+++ LeetCode/LeetCode/egg-drop-with-2-eggs-and-n-floors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LeetCode
+{
+    public class egg_drop_with_2_eggs_and_n_floors
+    {
+        public int TwoEggDrop(int n)
+        {
+            var dp = new int[n + 1];
+            dp[0] = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                var min = int.MaxValue;
+                for (int j = 1; j <= i; j++)
+                {
+                    min = Math.Min(Math.Max(dp[i - j] + 1, j), min);
+                }
+
+                dp[i] = min;
+            }
+
+            return dp[n];
+        }
+    }
+}
