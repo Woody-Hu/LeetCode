e19a3e3a9971589c5a49701419989daa4a3d94ae
Stephen Hu <stephenhu@seismic.com>
2022-07-22 10:17:27 +0800

    Create maximum-difference-between-increasing-elements.cs

---
+++ LeetCode/LeetCode/maximum-difference-between-increasing-elements.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_difference_between_increasing_elements
+    {
+        public int MaximumDifference(int[] nums)
+        {
+            var res = -1;
+            var pre = nums[0];
+            foreach (var item in nums)
+            {
+                if (item > pre)
+                {
+                    res = Math.Max(item - pre, res);
+                }
+                else
+                {
+                    pre = item;
+                }
+            }
+
+            return res;
+        }
+    }
+}
