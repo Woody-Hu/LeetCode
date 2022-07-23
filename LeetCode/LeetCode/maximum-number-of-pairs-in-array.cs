728212bc4a97e5f2a83baca20ff7a528df1338c7
Stephen Hu <stephenhu@seismic.com>
2022-07-24 06:33:00 +0800

    Create maximum-number-of-pairs-in-array.cs

---
+++ LeetCode/LeetCode/maximum-number-of-pairs-in-array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_number_of_pairs_in_array
+    {
+        public int[] NumberOfPairs(int[] nums)
+        {
+            var temp = new int[101];
+            foreach (var item in nums)
+            {
+                temp[item]++;
+            }
+
+            var count = 0;
+            foreach (var item in temp)
+            {
+                count += item / 2;
+            }
+
+            return new int[] { count, nums.Length - 2 * count };
+        }
+    }
+}
+
