b177a830c9d2aca06037cdfa76119e13c89652b4
Stephen Hu <stephenhu@seismic.com>
2022-07-25 11:38:19 +0800

    Create queries-on-a-permutation-with-key.cs

---
+++ LeetCode/LeetCode/queries-on-a-permutation-with-key.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class queries_on_a_permutation_with_key
+    {
+        public int[] ProcessQueries(int[] queries, int m)
+        {
+            List<int> perm = new List<int>();
+            for (int i = 1; i <= m; i++)
+                perm.Add(i);
+            for (int i = 0; i < queries.Length; i++)
+            {
+                int query = queries[i], idx = perm.IndexOf(query);
+                queries[i] = idx;
+                perm.RemoveAt(idx);
+                perm.Insert(0, query);
+            }
+            return queries;
+        }
+    }
+}
