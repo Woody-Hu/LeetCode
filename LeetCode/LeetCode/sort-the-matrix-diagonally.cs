90d8284574ad80b0a5e3ad541bfe6f4cb5832e77
Stephen Hu <stephenhu@seismic.com>
2022-07-25 11:38:25 +0800

    Create sort-the-matrix-diagonally.cs

---
+++ LeetCode/LeetCode/sort-the-matrix-diagonally.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LeetCode
+{
+    public class sort_the_matrix_diagonally
+    {
+        public int[][] DiagonalSort(int[][] mat)
+        {
+            var m = mat.Length;
+            var n = mat[0].Length;
+            for (int k = 0; k < Math.Min(m,n) - 1; k++)
+            {
+                for (int i = 0; i < m - 1; i++)
+                {
+                    for (int j = 0; j < n - 1; j++)
+                    {
+                        if (mat[i][j] > mat[i + 1][j + 1])
+                        {
+                            var temp = mat[i][j];
+                            mat[i][j] = mat[i + 1][j + 1];
+                            mat[i + 1][j + 1] = temp;
+                        }
+                    }
+                }
+            }
+
+            return mat;
+        }
+    }
+}
