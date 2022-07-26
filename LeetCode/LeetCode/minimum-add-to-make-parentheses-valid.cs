6c7d45db47d0bd312418eaeb2a4f6dbf6d145235
Stephen Hu <stephenhu@seismic.com>
2022-07-26 10:53:53 +0800

    Create minimum-add-to-make-parentheses-valid.cs

---
+++ LeetCode/LeetCode/minimum-add-to-make-parentheses-valid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_add_to_make_parentheses_valid
+    {
+        public int MinAddToMakeValid(string s)
+        {
+            var res = 0;
+            var bal = 0;
+            foreach (var item in s)
+            {
+                if (item == '(')
+                {
+                    bal++;
+                }
+                else
+                {
+                    bal--;
+                }
+
+                if (bal == -1)
+                {
+                    res++;
+                    bal++;
+                }
+            }
+
+            return res + bal;
+        }
+    }
+}
