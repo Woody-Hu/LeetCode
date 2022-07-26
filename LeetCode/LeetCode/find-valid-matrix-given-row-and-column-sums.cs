a2a263008c7eb5967594bda7af9be6cbca810613
Stephen Hu <stephenhu@seismic.com>
2022-07-26 10:53:51 +0800

    Create find-valid-matrix-given-row-and-column-sums.cs

---
+++ LeetCode/LeetCode/find-valid-matrix-given-row-and-column-sums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+    public class find_valid_matrix_given_row_and_column_sums
+    {
+        public int[][] RestoreMatrix(int[] rowSum, int[] colSum)
+        {
+            int[][] res = new int[rowSum.Length][];
+            for (int i = 0; i < rowSum.Length; i++)
+            {
+                res[i] = new int[colSum.Length];
+            }
+
+            for (int i = 0; i < rowSum.Length; i++)
+            {
+                for (int j = 0; j < colSum.Length; j++)
+                {
+                    res[i][j] = Math.Min(rowSum[i], colSum[j]);
+                    rowSum[i] -= res[i][j];
+                    colSum[j] -= res[i][j];
+                }
+            }
+
+            return res;
+        }
+    }
+}
