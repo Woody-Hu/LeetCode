c60fec43f9f0a081990ee42897ed3ac2970f3ea3
Stephen Hu <stephenhu@seismic.com>
2022-07-28 10:31:56 +0800

    Create incremental-memory-leak.cs

---
+++ LeetCode/LeetCode/incremental-memory-leak.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace LeetCode
+{
+    public class incremental_memory_leak
+    {
+        public int[] MemLeak(int memory1, int memory2)
+        {
+            var time = 1;
+            while (true)
+            {
+				if (memory1 >= time && memory2 >= time)
+				{
+					if (memory1 == memory2 || memory1 > memory2)
+					{
+						memory1 -= time;
+					}
+					else
+					{
+						memory2 -= time;
+					}
+				}
+				else if (memory1 >= time)
+				{
+					memory1 -= time;
+				}
+				else if (memory2 >= time)
+				{
+					memory2 -= time;
+
+				}
+				else
+				{
+					return new int[] { time, memory1, memory2 };
+				}
+
+				time++;
+            }
+
+			return new int[0];
+        }
+    }
+}
