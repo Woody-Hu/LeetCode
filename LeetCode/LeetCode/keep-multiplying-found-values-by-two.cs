29a8660e92b4da169cdd24914f8daa4c334d8f6e
Stephen Hu <stephenhu@seismic.com>
2022-07-30 06:23:14 +0800

    Create keep-multiplying-found-values-by-two.cs

---
+++ LeetCode/LeetCode/keep-multiplying-found-values-by-two.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class keep_multiplying_found_values_by_two
+    {
+        public int FindFinalValue(int[] nums, int original)
+        {
+            var set = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                set.Add(item);
+            }
+
+            while (set.Contains(original))
+            {
+                original = original * 2;
+            }
+
+            return original;
+        }
+    }
+}
+
