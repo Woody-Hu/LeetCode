50843a6f147965d935364aa250d7dbc9881a660f
Stephen Hu <stephenhu@seismic.com>
2022-07-31 06:29:04 +0800

    Create find-nearest-point-that-has-the-same-x-or-y-coordinate.cs

---
+++ LeetCode/LeetCode/find-nearest-point-that-has-the-same-x-or-y-coordinate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LeetCode
+{
+    public class find_nearest_point_that_has_the_same_x_or_y_coordinate
+    {
+        public int NearestValidPoint(int x, int y, int[][] points)
+        {
+            var res = -1;
+            var min = int.MaxValue;
+            for (int i = 0; i < points.Length; i++)
+            {
+                var dx = x - points[i][0];
+                var dy = y - points[i][1];
+                if (dx * dy == 0)
+                {
+                    var temp = Math.Abs(dx + dy);
+                    if (temp < min)
+                    {
+                        res = i;
+                        min = temp;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
