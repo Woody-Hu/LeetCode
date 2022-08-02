5ad010f48743169a2a87d5191aaab62ac39a429e
Stephen Hu <stephenhu@seismic.com>
2022-08-02 09:34:04 +0800

    Create xor-queries-of-a-subarray.cs

---
+++ LeetCode/LeetCode/xor-queries-of-a-subarray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LeetCode
+{
+    public class xor_queries_of_a_subarray
+    {
+        public int[] XorQueries(int[] arr, int[][] queries)
+        {
+            var n = arr.Length;
+            var xorRes = new int[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                xorRes[i + 1] = xorRes[i] ^ arr[i];
+            }
+
+            var res = new int[queries.Length];
+            for (int i = 0; i < queries.Length; i++)
+            {
+                res[i] = xorRes[queries[i][0]] ^ xorRes[queries[i][1] + 1];
+            }
+
+            return res;
+        }
+    }
+}
