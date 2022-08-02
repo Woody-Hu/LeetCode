44566e29c33a65dedbe097973fdb16424832ab30
Stephen Hu <stephenhu@seismic.com>
2022-08-02 09:34:02 +0800

    Create minimum-number-of-steps-to-make-two-strings-anagram-ii.cs

---
+++ LeetCode/LeetCode/minimum-number-of-steps-to-make-two-strings-anagram-ii.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_number_of_steps_to_make_two_strings_anagram_ii
+    {
+        public int MinSteps(string s, string t)
+        {
+            var diff = new int[26];
+            foreach (var item in s)
+            {
+                diff[item - 'a']++;
+            }
+
+            foreach (var item in t)
+            {
+                diff[item - 'a']--;
+            }
+
+            var res = 0;
+            foreach (var item in diff)
+            {
+                res += Math.Abs(item);
+            }
+
+            return res;
+        }
+    }
+}
