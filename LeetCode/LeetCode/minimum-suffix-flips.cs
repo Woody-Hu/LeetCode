98236c1820b04c5b38006a6618b2620535e503bb
Stephen Hu <stephenhu@seismic.com>
2022-08-03 09:51:52 +0800

    Create minimum-suffix-flips.cs

---
+++ LeetCode/LeetCode/minimum-suffix-flips.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_suffix_flips
+    {
+        public int MinFlips(string target)
+        {
+            var res = 0;
+            var pre = '0';
+            foreach (var item in target)
+            {
+                if (pre != item)
+                {
+                    res++;
+                    pre = item;
+                }
+            }
+
+            return res;
+        }
+    }
+}
