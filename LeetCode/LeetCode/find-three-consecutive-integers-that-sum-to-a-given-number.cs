e44b0ed74d41c5711138788cc025f46a8f3f46bb
Stephen Hu <stephenhu@seismic.com>
2022-08-04 11:23:07 +0800

    Create find-three-consecutive-integers-that-sum-to-a-given-number.cs

---
+++ LeetCode/LeetCode/find-three-consecutive-integers-that-sum-to-a-given-number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LeetCode
+{
+    public class find_three_consecutive_integers_that_sum_to_a_given_number
+    {
+        public long[] SumOfThree(long num)
+        {
+            var target = num / 3;
+            if (target * 3 == num)
+            {
+                return new long[] { target - 1, target, target + 1 };
+            }
+            else
+            {
+                return new long[0];
+            }
+        }
+    }
+}
