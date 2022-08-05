3f905fc1f0a4dabf7e90f3a6b81bfdc0aaced45d
Stephen Hu <stephenhu@seismic.com>
2022-08-05 11:48:37 +0800

    Create escape-the-ghosts.cs

---
+++ LeetCode/LeetCode/escape-the-ghosts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+    public class escape_the_ghosts
+    {
+        public bool EscapeGhosts(int[][] ghosts, int[] target)
+        {
+            var start = new int[] { 0, 0 };
+            var distance = Distance(start, target);
+            foreach (var item in ghosts)
+            {
+                var tempDistance = Distance(item, target);
+                if (tempDistance <= distance)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private int Distance(int[] point1, int[] point2)
+        {
+            return Math.Abs(point1[0] - point2[0]) + Math.Abs(point1[1] - point2[1]);
+        }
+    }
+}
