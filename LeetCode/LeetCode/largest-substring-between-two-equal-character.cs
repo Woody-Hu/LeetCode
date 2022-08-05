b2e0b8a07682c17ef5bd03154698323473c38732
Stephen Hu <stephenhu@seismic.com>
2022-08-06 06:18:37 +0800

    Create largest-substring-between-two-equal-character.cs

---
+++ LeetCode/LeetCode/largest-substring-between-two-equal-character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class largest_substring_between_two_equal_character
+    {
+        public int MaxLengthBetweenEqualCharacters(string s)
+        {
+            var res = -1;
+            var dic = new Dictionary<int, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                var value = s[i] - '0';
+                if (dic.ContainsKey(value))
+                {
+                    res = Math.Max(res, i - dic[value] - 1);
+                }
+                else
+                {
+                    dic[value] = i;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
