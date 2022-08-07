d26a1a75a47162e4d489a3caee525950cf307150
Stephen Hu <stephenhu@seismic.com>
2022-08-07 14:45:46 +0800

    Create longer-contiguous-segments-of-ones-than-zeros.cs

---
+++ LeetCode/LeetCode/longer-contiguous-segments-of-ones-than-zeros.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace LeetCode
+{
+    public class longer_contiguous_segments_of_ones_than_zeros
+    {
+        public bool CheckZeroOnes(string s)
+        {
+            var maxZ = 0;
+            var maxO = 0;
+            var current = 0;
+            var continueZ = true;
+            foreach (var item in s)
+            {
+                if (item == '0')
+                {
+                    if (continueZ)
+                    {
+                        current++;
+                    }
+                    else
+                    {
+                        continueZ = true;
+                        current = 1;
+                    }
+
+                    maxZ = Math.Max(maxZ, current);
+                }
+                else
+                {
+                    if (!continueZ)
+                    {
+                        current++;
+                    }
+                    else
+                    {
+                        continueZ = false;
+                        current = 1;
+                    }
+
+                    maxO = Math.Max(maxO, current);
+                }
+            }
+
+            return maxO > maxZ;
+        }
+    }
+}
+
