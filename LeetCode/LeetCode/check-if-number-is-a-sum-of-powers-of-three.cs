2c66156fc47288c9f0b2a1bad4eaf63a26b7ee84
Stephen Hu <stephenhu@seismic.com>
2022-08-08 13:52:17 +0800

    Create check-if-number-is-a-sum-of-powers-of-three.cs

---
+++ LeetCode/LeetCode/check-if-number-is-a-sum-of-powers-of-three.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+    public class check_if_number_is_a_sum_of_powers_of_three
+    {
+        public bool CheckPowersOfThree(int n)
+        {
+            while (n > 0)
+            {
+                if (n%3 >1)
+                {
+                    return false;
+                }
+
+                n = n / 3;
+            }
+
+            return true;
+        }
+    }
+}
