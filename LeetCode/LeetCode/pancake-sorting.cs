f89138ed47350416ae32451b28e7f540657c1c49
Stephen Hu <stephenhu@seismic.com>
2022-08-09 13:30:41 +0800

    Create pancake-sorting.cs

---
+++ LeetCode/LeetCode/pancake-sorting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class pancake_sorting
+    {
+        public IList<int> PancakeSort(int[] arr)
+        {
+            var res = new List<int>();
+            for (int n = arr.Length; n > 1; n--)
+            {
+                int index = 0;
+                for (int i = 1; i < n; i++)
+                {
+                    if (arr[i] >= arr[index])
+                    {
+                        index = i;
+                    }
+                }
+                if (index == n - 1)
+                {
+                    continue;
+                }
+                Reverse(arr, index);
+                Reverse(arr, n - 1);
+                res.Add(index + 1);
+                res.Add(n);
+            }
+
+            return res;
+        }
+
+        private void Reverse(int[] arr, int end)
+        {
+            for (int i = 0, j = end; i < j; i++, j--)
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
