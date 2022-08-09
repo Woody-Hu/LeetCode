6c6b55e0be68a0b3e6c4704772392915ca6dd5a8
Stephen Hu <stephenhu@seismic.com>
2022-08-09 13:30:39 +0800

    Create first-letter-to-appear-twice.cs

---
+++ LeetCode/LeetCode/first-letter-to-appear-twice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class first_letter_to_appear_twice
+    {
+        public char RepeatedCharacter(string s)
+        {
+            var set = new HashSet<char>();
+            foreach (var item in s)
+            {
+                if (set.Contains(item))
+                {
+                    return item;
+                }
+
+                set.Add(item);
+            }
+
+            return ' ';
+        }
+    }
+}
