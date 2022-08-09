cff75f3004d484ed01e5345f148907b5bcc67311
Stephen Hu <stephenhu@seismic.com>
2022-08-09 13:30:44 +0800

    Create k-th-smallest-prime-fraction.cs

---
+++ LeetCode/LeetCode/k-th-smallest-prime-fraction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace LeetCode
+{
+    public class k_th_smallest_prime_fraction
+    {
+        public int[] KthSmallestPrimeFraction(int[] arr, int k)
+        {
+            var n = arr.Length;
+            var left = 0.0;
+            var right = 1.0;
+            while (true)
+            {
+                var mid = (left + right) / 2;
+                var i = -1;
+                var count = 0;
+                var x = 0;
+                var y = 1;
+                for (int j = 1; j < n; j++)
+                {
+                    while ((double)arr[i + 1] / arr[j] < mid)
+                    {
+                        i++;
+                        if (arr[i] * y > arr[j] * x)
+                        {
+                            x = arr[i];
+                            y = arr[j];
+                        }
+                    }
+
+                    count += i + 1;
+                }
+
+                if (count == k)
+                {
+                    return new int[] { x, y };
+                }
+                else if (count < k)
+                {
+                    left = mid;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+
+            return null;
+        }
+    }
+}
