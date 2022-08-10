bcf660ab77861a36e9e185c0491f64ecee10d283
Stephen Hu <stephenhu@seismic.com>
2022-08-10 11:50:09 +0800

    Create letter-tile-possibilities.cs

---
+++ LeetCode/LeetCode/letter-tile-possibilities.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class letter_tile_possibilities
+    {
+        public int NumTilePossibilities(string tiles)
+        {
+            var set = new HashSet<string>();
+            var used = new bool[tiles.Length];
+            var res = 0;
+            for (int i = 1; i <= tiles.Length; i++)
+            {
+                Calculate(tiles, string.Empty, i, used, set, ref res);
+            }
+
+            return res;
+        }
+
+        private void Calculate(string tiles, string curs, int expectLength, bool[] used, HashSet<string> set, ref int res)
+        {
+            if (curs.Length == expectLength && !set.Contains(curs))
+            {
+                set.Add(curs);
+                res++;
+                return;
+            }
+
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    Calculate(tiles, curs + tiles[i], expectLength, used, set, ref res);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
