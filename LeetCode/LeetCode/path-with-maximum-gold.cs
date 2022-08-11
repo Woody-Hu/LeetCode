7e2252c2561d572e6eb5fdc4fa0c2898449dba4d
Stephen Hu <stephenhu@seismic.com>
2022-08-11 09:41:05 +0800

    Create path-with-maximum-gold.cs

---
+++ LeetCode/LeetCode/path-with-maximum-gold.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace LeetCode
+{
+    public class path_with_maximum_gold
+    {
+        public int GetMaximumGold(int[][] grid)
+        {
+            int[][] dirs = { new int[] { -1, 0 }, new int[] { 1, 0 }, new int[] { 0, -1 }, new int[] { 0, 1 } };
+            var res = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] != 0)
+                    {
+                        Visit(grid, i, j, dirs, 0, ref res);
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        private void Visit(int[][] grid, int x, int y, int[][] dirs, int current, ref int res)
+        {
+            current += grid[x][y];
+            res = Math.Max(res, current);
+
+            var temp = grid[x][y];
+            grid[x][y] = 0;
+            foreach (var item in dirs)
+            {
+                int nx = x + item[0];
+                int ny = y + item[1];
+                if (nx >= 0 && nx < grid.Length && ny >= 0 && ny < grid[0].Length && grid[nx][ny] != 0)
+                {
+                    Visit(grid, nx, ny, dirs, current, ref res);
+                }
+            }
+
+            grid[x][y] = temp;
+        }
+    }
+}
