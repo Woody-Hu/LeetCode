77643b132c15a26657aceaf8a26c5505e211ec92
Stephen Hu <stephenhu@seismic.com>
2022-08-12 09:51:16 +0800

    Update airplane-seat-assignment-probability.cs

--- LeetCode/LeetCode/airplane-seat-assignment-probability.cs
+++ LeetCode/LeetCode/airplane-seat-assignment-probability.cs
@@ -3,8 +3,9 @@ namespace LeetCode
 {
     public class airplane_seat_assignment_probability
     {
-        public airplane_seat_assignment_probability()
+        public double NthPersonGetsNthSeat(int n)
         {
+            return n == 1 ? 1 : 0.5;
         }
     }
 }
