ae9f30b5fde06cbe7437bb116b7fc8f4ac7c52dc
Stephen Hu <stephenhu@seismic.com>
2022-08-12 09:47:11 +0800

    Create circular-permutation-in-binary-representation.cs

---
+++ LeetCode/LeetCode/circular-permutation-in-binary-representation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class circular_permutation_in_binary_representation
+    {
+        public IList<int> CircularPermutation(int n, int start)
+        {
+            var res = new List<int>();
+            var total = 1 << n;
+            for (int i = 0; i < total; i++)
+            {
+                res.Add((i >> 1) ^ i ^ start);
+            }
+
+            return res;
+        }
+    }
+}
