062794dd26332ceb4eff19d32bb5a5c6dd95fa89
Stephen Hu <stephenhu@seismic.com>
2022-08-12 09:47:09 +0800

    Create airplane-seat-assignment-probability.cs

---
+++ LeetCode/LeetCode/airplane-seat-assignment-probability.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace LeetCode
+{
+    public class airplane_seat_assignment_probability
+    {
+        public airplane_seat_assignment_probability()
+        {
+        }
+    }
+}
