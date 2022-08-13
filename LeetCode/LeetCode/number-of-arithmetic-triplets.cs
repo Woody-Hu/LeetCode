554bd96a3adaa50fa4300ce18a46e33494d329a3
Stephen Hu <stephenhu@seismic.com>
2022-08-13 14:30:52 +0800

    Create number-of-arithmetic-triplets.cs

---
+++ LeetCode/LeetCode/number-of-arithmetic-triplets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+    public class number_of_arithmetic_triplets
+    {
+        public int ArithmeticTriplets(int[] nums, int diff)
+        {
+            var array = new int[201];
+            var res = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var value = nums[i];
+                if (value >= 2 * diff)
+                {
+                    res += array[value - diff] * array[value - 2 * diff];
+                }
+
+                array[value] += 1;
+            }
+
+            return res;
+        }
+    }
+}
+
