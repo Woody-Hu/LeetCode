d7ac0f794af11216b02e882dc94af7bd22011f04
Stephen Hu <stephenhu@seismic.com>
2022-08-14 06:20:24 +0800

    Create minimum-bit-flips-to-convert-number.cs

---
+++ LeetCode/LeetCode/minimum-bit-flips-to-convert-number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_bit_flips_to_convert_number
+    {
+        public int MinBitFlips(int start, int goal)
+        {
+            var res = 0;
+            var temp = start ^ goal;
+            while (temp > 0)
+            {
+                res += temp & 1;
+                temp = temp >> 1;
+            }
+
+            return res;
+        }
+    }
+}
+
