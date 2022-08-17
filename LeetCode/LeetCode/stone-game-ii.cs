433ca3949f463ab81d842dc18d5be484742f9ffb
Stephen Hu <stephenhu@seismic.com>
2022-08-17 09:09:11 +0800

    Create stone-game-ii.cs

---
+++ LeetCode/LeetCode/stone-game-ii.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace LeetCode
+{
+    public class stone_game_ii
+    {
+        public int StoneGameII(int[] piles)
+        {
+            var len = piles.Length;
+            var sum = 0;
+            var dp = new int[len, len + 1];
+            for (int i = len - 1; i >= 0; i--)
+            {
+                sum += piles[i];
+                for (int m = 1; m <= len; m++)
+                {
+                    if (i + 2 * m >= len)
+                    {
+                        dp[i, m] = sum;
+                    }
+                    else
+                    {
+                        for (int x = 1; x <= 2*m; x++)
+                        {
+                            dp[i, m] = Math.Max(dp[i, m], sum - dp[i + x, Math.Max(m, x)]);
+                        }
+                    }
+                }
+            }
+
+            return dp[0, 1];
+        }
+    }
+}
