76f30a8c01fc3abe41a5668751e0c6785fa44447
Stephen Hu <stephenhu@seismic.com>
2022-08-17 09:09:09 +0800

    Create minimum-flips-to-make-a-or-b-equal-to-c.cs

---
+++ LeetCode/LeetCode/minimum-flips-to-make-a-or-b-equal-to-c.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_flips_to_make_a_or_b_equal_to_c
+    {
+        public int MinFlips(int a, int b, int c)
+        {
+            var res = 0;
+            for (int i = 0; i < 31; i++)
+            {
+                var bitA = (a >> i) & 1;
+                var bitB = (b >> i) & 1;
+                var bitC = (c >> i) & 1;
+                if (bitC == 0)
+                {
+                    res += bitA + bitB;
+                }
+                else
+                {
+                    if (bitA == 0 && bitB ==0)
+                    {
+                        res += 1;
+                    }
+                }
+
+            }
+
+            return res;
+        }
+    }
+}
