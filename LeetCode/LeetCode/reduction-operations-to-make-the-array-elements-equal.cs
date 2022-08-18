931680029cb56e6fb2423a20dde9499c149f53c9
Stephen Hu <stephenhu@seismic.com>
2022-08-18 08:40:17 +0800

    Create reduction-operations-to-make-the-array-elements-equal.cs

---
+++ LeetCode/LeetCode/reduction-operations-to-make-the-array-elements-equal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+    public class reduction_operations_to_make_the_array_elements_equal
+    {
+        public int ReductionOperations(int[] nums)
+        {
+            Array.Sort(nums);
+            var res = 0;
+            var step = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    step++;
+                }
+
+                res += step;
+            }
+
+            return res;
+
+        }
+    }
+}
