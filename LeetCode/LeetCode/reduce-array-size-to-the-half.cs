c6c0ac083301e13638a6a102827aaa30179f68ad
Stephen Hu <stephenhu@seismic.com>
2022-08-22 07:48:40 +0800

    Create reduce-array-size-to-the-half.cs

---
+++ LeetCode/LeetCode/reduce-array-size-to-the-half.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class reduce_array_size_to_the_half
+    {
+        public int MinSetSize(int[] arr)
+        {
+            var res = 0;
+            var dic = new Dictionary<int, int>();
+            foreach (var item in arr)
+            {
+                if (!dic.ContainsKey(item))
+                {
+                    dic[item] = 0;
+                }
+
+                dic[item]++;
+            }
+
+
+            var ordered = dic.OrderByDescending(k => k.Value);
+            var current = 0;
+            foreach (var item in ordered)
+            {
+                res++;
+                current += dic[item.Key];
+                if (current >= arr.Length/2)
+                {
+                    break;
+                }
+            }
+
+            return res;
+        }
+    }
+}
