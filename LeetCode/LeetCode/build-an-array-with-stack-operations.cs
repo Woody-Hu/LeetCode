e907c19c950fc9ad0884012e3da12b26051dcee7
Stephen Hu <stephenhu@seismic.com>
2022-08-22 07:48:28 +0800

    Create build-an-array-with-stack-operations.cs

---
+++ LeetCode/LeetCode/build-an-array-with-stack-operations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class build_an_array_with_stack_operations
+    {
+        public IList<string> BuildArray(int[] target, int n)
+        {
+            var res = new List<string>();
+            var index = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (target[target.Length - 1] <i)
+                {
+                    break;
+                }
+
+                res.Add("Push");
+                if (target[index] > i)
+                {
+                    res.Add("Pop");
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
