e792ea4df73892509a37a32eeabab23767902689
Stephen Hu <stephenhu@seismic.com>
2022-08-22 07:48:34 +0800

    Create minimum-number-of-operations-to-reinitialize-a-permutation.cs

---
+++ LeetCode/LeetCode/minimum-number-of-operations-to-reinitialize-a-permutation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_number_of_operations_to_reinitialize_a_permutation
+    {
+        public int ReinitializePermutation(int n)
+        {
+            if (n == 2)
+            {
+                return 1;
+            }
+
+            var res = 1;
+            var k = 2;
+            while (k != 1)
+            {
+                res++;
+                k = k * 2 % (n - 1);
+            }
+
+            return res;
+        }
+    }
+}
