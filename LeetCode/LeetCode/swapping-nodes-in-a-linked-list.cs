d086a9356f49669771964746760f5958f395333c
Stephen Hu <stephenhu@seismic.com>
2022-08-25 14:15:11 +0800

    Create swapping-nodes-in-a-linked-list.cs

---
+++ LeetCode/LeetCode/swapping-nodes-in-a-linked-list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeetCode
+{
+    public class swapping_nodes_in_a_linked_list
+    {
+        public ListNode SwapNodes(ListNode head, int k)
+        {
+            var left = head;
+            var right = head;
+            for (int i = 1; i < k; i++)
+            {
+                left = left.next;
+            }
+
+            var current = left;
+            while (current.next != null)
+            {
+                right = right.next;
+                current = current.next;
+            }
+
+            var value = right.val;
+            right.val = left.val;
+            left.val = value;
+            return head;
+        }
+    }
+}
