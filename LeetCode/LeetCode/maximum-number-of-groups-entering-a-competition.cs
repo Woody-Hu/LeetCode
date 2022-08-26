7f88414f1aff346a323e27f500b9a1ec40303b39
Stephen Hu <stephenhu@seismic.com>
2022-08-26 09:48:12 +0800

    Create maximum-number-of-groups-entering-a-competition.cs

---
+++ LeetCode/LeetCode/maximum-number-of-groups-entering-a-competition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_number_of_groups_entering_a_competition
+    {
+        public int MaximumGroups(int[] grades)
+        {
+            var n = grades.Length;
+            return (int)((Math.Sqrt(8 * n + 1) - 1) / 2);
+        }
+    }
+}
