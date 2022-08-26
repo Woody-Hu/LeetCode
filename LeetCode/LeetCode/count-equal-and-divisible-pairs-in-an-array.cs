c5c44c7796b05db1fa240de0e8843d72d0c776a7
Stephen Hu <stephenhu@seismic.com>
2022-08-27 06:50:09 +0800

    Create count-equal-and-divisible-pairs-in-an-array.cs

---
+++ LeetCode/LeetCode/count-equal-and-divisible-pairs-in-an-array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LeetCode
+{
+    public class count_equal_and_divisible_pairs_in_an_array
+    {
+        public int CountPairs(int[] nums, int k)
+        {
+            var res = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && (i*j)%k == 0)
+                    {
+                        res++;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
