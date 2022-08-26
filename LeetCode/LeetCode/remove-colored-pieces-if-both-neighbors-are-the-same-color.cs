f5230e9e4c878f647add0fabd89a32d5038e788f
Stephen Hu <stephenhu@seismic.com>
2022-08-26 09:48:14 +0800

    Create remove-colored-pieces-if-both-neighbors-are-the-same-color.cs

---
+++ LeetCode/LeetCode/remove-colored-pieces-if-both-neighbors-are-the-same-color.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace LeetCode
+{
+    public class remove_colored_pieces_if_both_neighbors_are_the_same_color
+    {
+        public bool WinnerOfGame(string colors)
+        {
+            var counts = new int[] { 0, 0 };
+            var current = 'C';
+            var count = 0;
+            for (int i = 0; i < colors.Length; i++)
+            {
+                var charValue = colors[i];
+                if (charValue != current)
+                {
+                    current = charValue;
+                    count = 1;
+                }
+                else
+                {
+                    count++;
+                    if (count >= 3)
+                    {
+                        counts[charValue - 'A'] += 1;
+                    }
+                }
+            }
+
+            return counts[0] > counts[1];
+        }
+    }
+}
