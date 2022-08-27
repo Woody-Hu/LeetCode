7a09cad46077d1b2f0e1c496ede6a83557552b01
Stephen Hu <stephenhu@seismic.com>
2022-08-28 06:27:13 +0800

    Create decode-the-message.cs

---
+++ LeetCode/LeetCode/decode-the-message.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class decode_the_message
+    {
+        public string DecodeMessage(string key, string message)
+        {
+            var dic = new Dictionary<char, char>();
+            dic.Add(' ', ' ');
+            var index = 'a';
+            foreach (var item in key)
+            {
+                if (!dic.ContainsKey(item))
+                {
+                    dic.Add(item, index);
+                    index++;
+                    if (index >= 'z' + 1)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            var stringBuilder = new StringBuilder();
+            foreach (var item in message)
+            {
+                stringBuilder.Append(dic[item]);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
+
