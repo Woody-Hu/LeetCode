6168e76c657fee132dbca134884f1925af414b2e
Stephen Hu <stephenhu@seismic.com>
2022-08-29 10:03:30 +0800

    Create minimum-number-of-swaps-to-make-the-string-balanced.cs

---
+++ LeetCode/LeetCode/minimum-number-of-swaps-to-make-the-string-balanced.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_number_of_swaps_to_make_the_string_balanced
+    {
+        public int MinSwaps(string s)
+        {
+            var count = 0;
+            var res = 0;
+            foreach (var item in s)
+            {
+                if (item == '[')
+                {
+                    count++;
+                }
+                else if (count > 0)
+                {
+                    count--;
+                }
+                else
+                {
+                    count++;
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
