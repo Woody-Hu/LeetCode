95170870999d0942fb80f5f8da401ab519209b05
Stephen Hu <stephenhu@seismic.com>
2022-08-29 10:03:33 +0800

    Create removing-stars-from-a-string.cs

---
+++ LeetCode/LeetCode/removing-stars-from-a-string.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    public class removing_stars_from_a_string
+    {
+        public string RemoveStars(string s)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var item in s)
+            {
+                if (item != '*')
+                {
+                    stringBuilder.Append(item);
+                }
+                else
+                {
+                    stringBuilder.Remove(stringBuilder.Length - 1, 1);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
