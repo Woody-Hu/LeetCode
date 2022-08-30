e31d0fb0557b32f31cbfb4b6f55170a25ed082e1
Stephen Hu <stephenhu@seismic.com>
2022-08-30 12:10:45 +0800

    Create score-of-parentheses.cs

---
+++ LeetCode/LeetCode/score-of-parentheses.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LeetCode
+{
+    public class score_of_parentheses
+    {
+        public int ScoreOfParentheses(string s)
+        {
+            var blance = 0;
+            var res = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var item = s[i];
+                if (item == '(')
+                {
+                    blance++;
+                }
+                else
+                {
+                    blance--;
+                    if (s[i - 1] == '(' )
+                    {
+                        res += 1 << blance;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
