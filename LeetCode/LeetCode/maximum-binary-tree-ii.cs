5af21473ea05ad3c200b5a81c4be33ab39ef8699
Stephen Hu <stephenhu@seismic.com>
2022-08-30 12:10:40 +0800

    Create maximum-binary-tree-ii.cs

---
+++ LeetCode/LeetCode/maximum-binary-tree-ii.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_binary_tree_ii
+    {
+        public TreeNode InsertIntoMaxTree(TreeNode root, int val)
+        {
+            TreeNode parent = null;
+            var current = root;
+            while (current !=  null)
+            {
+                if (val > current.val)
+                {
+                    if (parent == null)
+                    {
+                        return new TreeNode(val, root, null);
+                    }
+
+                    var node = new TreeNode(val, current, null);
+                    parent.right = node;
+                    return root;
+                }
+                else
+                {
+                    parent = current;
+                    current = current.right;
+                }
+            }
+
+            parent.right = new TreeNode(val);
+            return root;
+        }
+    }
+}
