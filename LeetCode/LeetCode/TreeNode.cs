1a0cd7c4faca79d2695f29aecb9d8b1f7fb258a4
Stephen Hu <stephenhu@seismic.com>
2022-08-30 12:10:47 +0800

    Update TreeNode.cs

--- LeetCode/LeetCode/TreeNode.cs
+++ LeetCode/LeetCode/TreeNode.cs
@@ -7,9 +7,16 @@ using System.Threading.Tasks;
 namespace LeetCode
 {
   public class TreeNode {
-     public int val;
-     public TreeNode left;
-     public TreeNode right;
-     public TreeNode(int x) { val = x; }
- }
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+    }
 }
