8f6e7b829a82e7d4f6c2519fa4d59892aa1a78a4
Stephen Hu <stephenhu@seismic.com>
2022-08-31 09:51:01 +0800

    Create maximum-score-from-removing-stones.cs

---
+++ LeetCode/LeetCode/maximum-score-from-removing-stones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_score_from_removing_stones
+    {
+        public int MaximumScore(int a, int b, int c)
+        {
+            var array = new int[] { a, b, c };
+            Array.Sort(array);
+            if (array[0] + array[1] <= array[2])
+            {
+                return array[0] + array[1];
+            }
+            else
+            {
+                return (array[0] + array[1] + array[2]) / 2;
+            }
+        }
+    }
+}
