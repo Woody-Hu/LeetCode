79d5d3742aa4680ac535f1b35afa1319c730dc3b
Stephen Hu <stephenhu@seismic.com>
2022-08-31 09:50:59 +0800

    Create maximum-element-after-decreasing-and-rearranging.cs

---
+++ LeetCode/LeetCode/maximum-element-after-decreasing-and-rearranging.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_element_after_decreasing_and_rearranging
+    {
+        public int MaximumElementAfterDecrementingAndRearranging(int[] arr)
+        {
+            Array.Sort(arr);
+            arr[0] = 1;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                arr[i] = Math.Min(arr[i], arr[i - 1] + 1);
+            }
+
+            return arr[arr.Length - 1];
+        }
+    }
+}
