a70c8313ef39f70c8045fcc41faba79906a2d50c
Stephen Hu <stephenhu@seismic.com>
2022-09-01 09:05:54 +0800

    Create minimum-numbers-of-function-calls-to-make-target-array.cs

---
+++ LeetCode/LeetCode/minimum-numbers-of-function-calls-to-make-target-array.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_numbers_of_function_calls_to_make_target_array
+    {
+        public int MinOperations(int[] nums)
+        {
+            var res = 0;
+            var maxV = 0;
+            foreach (var item in nums)
+            {
+                var cur = item;
+                maxV = Math.Max(maxV, item);
+                while (cur != 0)
+                {
+                    if ((cur & 1 ) != 0)
+                    {
+                        res++;
+                    }
+
+                    cur = cur / 2;
+                }
+            }
+
+            if (maxV != 0)
+            {
+                while (maxV != 0)
+                {
+                    res++;
+                    maxV >>= 1;
+                }
+                res--;
+            }
+
+            return res;
+        }
+    }
+}
