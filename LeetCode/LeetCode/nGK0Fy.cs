d79eb36700d8db645f260e3c565897f57e4fddf5
Stephen Hu <stephenhu@seismic.com>
2022-09-02 09:13:59 +0800

    Update nGK0Fy.cs

--- LeetCode/LeetCode/nGK0Fy.cs
+++ LeetCode/LeetCode/nGK0Fy.cs
@@ -3,8 +3,23 @@ namespace LeetCode
 {
     public class nGK0Fy
     {
-        public nGK0Fy()
+        public int Calculate(string s)
         {
+            var x = 1;
+            var y = 0;
+            foreach (var item in s)
+            {
+                if (item == 'A')
+                {
+                    x = 2 * x + y;
+                }
+                else
+                {
+                    y = 2 * y + x;
+                }
+            }
+
+            return x + y;
         }
     }
 }
