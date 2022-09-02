e51b17099abbb5e5ff816f17c21b51f55580a183
Stephen Hu <stephenhu@seismic.com>
2022-09-03 05:24:45 +0800

    Create sZ59z6.cs

---
+++ LeetCode/LeetCode/sZ59z6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class sZ59z6
+    {
+        public int NumColor(TreeNode root)
+        {
+            var set = new HashSet<int>();
+            Visit(root, ref set);
+            return set.Count;
+        }
+
+        private void Visit(TreeNode root, ref HashSet<int> set)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            set.Add(root.val);
+            Visit(root.left, ref set);
+            Visit(root.right, ref set);
+        }
+    }
+}
+
