2e437640da54fd7324e7517e44091a3ab7da2437
Stephen Hu <stephenhu@seismic.com>
2022-09-04 15:33:40 +0800

    Create find-first-palindromic-string-in-the-array.cs

---
+++ LeetCode/LeetCode/find-first-palindromic-string-in-the-array.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace LeetCode
+{
+    public class find_first_palindromic_string_in_the_array
+    {
+        public string FirstPalindrome(string[] words)
+        {
+            foreach (var item in words)
+            {
+                if (Check(item))
+                {
+                    return item;
+                }
+            }
+
+            return String.Empty;
+        }
+
+        private bool Check(string input)
+        {
+            var l = 0;
+            var r = input.Length - 1;
+            while (l < r)
+            {
+                if (input[l] != input[r])
+                {
+                    return false;
+                }
+
+                l++;
+                r--;
+            }
+
+            return true;
+
+        }
+    }
+}
+
