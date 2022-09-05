a881febc2733773e9cc31fefa153d0cfe93f9564
Stephen Hu <stephenhu@seismic.com>
2022-09-05 10:13:35 +0800

    Create strictly-palindromic-number.cs

---
+++ LeetCode/LeetCode/strictly-palindromic-number.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LeetCode
+{
+    public class strictly_palindromic_number
+    {
+        public bool IsStrictlyPalindromic(int n)
+        {
+            return false;
+        }
+    }
+}
