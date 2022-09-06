3fbbaa931cef179aae2e005367eaed6fd0bd7381
Stephen Hu <stephenhu@seismic.com>
2022-09-06 09:02:33 +0800

    Create minimum-swaps-to-make-strings-equal.cs

---
+++ LeetCode/LeetCode/minimum-swaps-to-make-strings-equal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_swaps_to_make_strings_equal
+    {
+        public int MinimumSwap(string s1, string s2)
+        {
+            var xy = 0;
+            var yx = 0;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                var c1 = s1[i];
+                var c2 = s2[i];
+                if (c1 == c2)
+                {
+                    continue;
+                }
+                else if (c1 == 'x')
+                {
+                    xy++;
+                }
+                else
+                {
+                    yx++;
+                }
+            }
+
+            if (((xy + yx) & 1) != 0)
+            {
+                return -1;
+            }
+
+            return xy / 2 + yx / 2 + ((xy % 2) * 2);
+        }
+    }
+}
