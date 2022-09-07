e6f0a01a596f200d0f75e84a567cf0ba9b4c032c
Stephen Hu <stephenhu@seismic.com>
2022-09-07 09:01:10 +0800

    Create qiu-12n-lcof.cs

---
+++ LeetCode/LeetCode/qiu-12n-lcof.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeetCode
+{
+    public class qiu_12n_lcof
+    {
+        public int SumNums(int n)
+        {
+            bool flag = n > 0 && (n += SumNums(n-1)) > 0;
+            return n;
+        }
+    }
+}
