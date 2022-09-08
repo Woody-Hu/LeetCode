0988590f6575f060368421ad8601127b6e861b31
Stephen Hu <stephenhu@seismic.com>
2022-09-08 08:55:37 +0800

    Create beautiful-arrangement-ii.cs

---
+++ LeetCode/LeetCode/beautiful-arrangement-ii.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+    public class beautiful_arrangement_ii
+    {
+        public int[] ConstructArray(int n, int k)
+        {
+            var res = new int[n];
+            var index = 0;
+            for (int i = 1; i < n - k; i++)
+            {
+                res[index++] = i;
+            }
+
+            for (int i = n -k , j = n; i <=j; i++, j--)
+            {
+                res[index++] = i;
+                if (i != j)
+                {
+                    res[index++] = j;
+                }
+            }
+
+            return res;
+        }
+    }
+}
