f635a745f1716dad76a87c74b6f2e2b0e1869123
Stephen Hu <stephenhu@seismic.com>
2022-09-09 08:59:21 +0800

    Create pseudo-palindromic-paths-in-a-binary-tree.cs

---
+++ LeetCode/LeetCode/pseudo-palindromic-paths-in-a-binary-tree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeetCode
+{
+    public class pseudo_palindromic_paths_in_a_binary_tree
+    {
+        public int PseudoPalindromicPaths(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var res = 0;
+            Visit(root, 0, ref res);
+            return res;
+        }
+
+        private void Visit(TreeNode root, int current, ref int res)
+        {
+            if (root == null)
+            {
+                if (current == 0 || (current & (current - 1)) == 0)
+                {
+                    res++;
+                }
+
+                return;
+            }
+
+            current = current ^ (1 << root.val);
+            Visit(root.left, current, ref res);
+            Visit(root.right, current, ref res);
+        }
+    }
+}
