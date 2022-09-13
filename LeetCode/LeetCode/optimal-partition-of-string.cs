268df85bb6bdf54df0913d2ded4483953863bc91
Stephen Hu <stephenhu@seismic.com>
2022-09-13 09:00:55 +0800

    Create optimal-partition-of-string.cs

---
+++ LeetCode/LeetCode/optimal-partition-of-string.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class optimal_partition_of_string
+    {
+        public int PartitionString(string s)
+        {
+            var set = new HashSet<char>();
+            var res = 1;
+            foreach (var item in s)
+            {
+                if (set.Contains(item))
+                {
+                    res++;
+                    set.Clear();
+                }
+
+                set.Add(item);
+            }
+
+            return res;
+        }
+    }
+}
