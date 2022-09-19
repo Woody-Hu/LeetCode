30be28db2646869872718958cb1d929920e03d06
Stephen Hu <stephenhu@seismic.com>
2022-09-19 08:52:51 +0800

    Create reverse-odd-levels-of-binary-tree.cs

---
+++ LeetCode/LeetCode/reverse-odd-levels-of-binary-tree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LeetCode
+{
+    public class reverse_odd_levels_of_binary_tree
+    {
+        public TreeNode ReverseOddLevels(TreeNode root)
+        {
+            Visit(root.left, root.right, 1);
+            return root;
+        }
+
+        private void Visit(TreeNode l, TreeNode r, int level)
+        {
+            if (l == null)
+            {
+                return;
+            }
+
+            if (level % 2 != 0)
+            {
+                var temp = l.val;
+                l.val = r.val;
+                r.val = temp;
+            }
+
+            Visit(l.left, r.right, level + 1);
+            Visit(l.right, r.left, level + 1);
+        }
+    }
+}
