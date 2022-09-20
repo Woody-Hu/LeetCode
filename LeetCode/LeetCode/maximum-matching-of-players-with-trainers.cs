6977a5a3cb2c3f19518c40e455f1fe754a8148b3
Stephen Hu <stephenhu@seismic.com>
2022-09-20 09:53:57 +0800

    Create maximum-matching-of-players-with-trainers.cs

---
+++ LeetCode/LeetCode/maximum-matching-of-players-with-trainers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_matching_of_players_with_trainers
+    {
+        public int MatchPlayersAndTrainers(int[] players, int[] trainers)
+        {
+            var pn = players.Length;
+            var tn = trainers.Length;
+            Array.Sort(players);
+            Array.Sort(trainers);
+            var res = 0;
+            var ti = 0;
+            for (int pi = 0; pi < pn; pi++)
+            {
+                while (ti < tn && players[pi] > trainers[ti])
+                {
+                    ti++;
+                }
+
+                if (ti < tn && players[pi] <= trainers[ti])
+                {
+                    res++;
+                    ti++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
