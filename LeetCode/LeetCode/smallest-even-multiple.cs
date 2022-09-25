cc6670a04912afabd698ba0ab6933939a22cfa11
Stephen Hu <stephenhu@seismic.com>
2022-09-25 16:38:24 +0800

    Create smallest-even-multiple.cs

---
+++ LeetCode/LeetCode/smallest-even-multiple.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+    public class smallest_even_multiple
+    {
+        public int SmallestEvenMultiple(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return n;
+            }
+            else
+            {
+                return n * 2;
+            }
+        }
+    }
+}
