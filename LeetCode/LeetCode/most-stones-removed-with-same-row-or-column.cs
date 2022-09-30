8d2b9f1623c22f8b9db412147126d9c5768e846d
Stephen Hu <stephenhu@seismic.com>
2022-09-30 16:15:33 +0800

    Create most-stones-removed-with-same-row-or-column.cs

---
+++ LeetCode/LeetCode/most-stones-removed-with-same-row-or-column.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class most_stones_removed_with_same_row_or_column
+    {
+        public int RemoveStones(int[][] stones)
+        {
+            var find = new UnionFind();
+            foreach (var item in stones)
+            {
+                find.Union(item[0] + 10001, item[1]);
+            }
+
+            return stones.Length - find.Count;
+        }
+
+        private class UnionFind
+        {
+            private readonly Dictionary<int, int> _dict = new Dictionary<int, int>();
+
+            internal int Count { get; set; }
+
+            internal int Find(int input)
+            {
+                if (!_dict.ContainsKey(input))
+                {
+                    _dict.Add(input, input);
+                    Count++;
+                }
+
+                if (input != _dict[input])
+                {
+                    _dict[input] = Find(_dict[input]);
+                }
+
+                return _dict[input];
+            }
+
+            internal void Union(int l, int r)
+            {
+                var rl = Find(l);
+                var rr = Find(r);
+                if (rl == rr)
+                {
+                    return;
+                }
+
+                _dict[rl] = rr;
+                Count--;
+                return;
+            }
+        }
+    }
+}
+
