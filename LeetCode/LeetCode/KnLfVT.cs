b09509d2cfddcaf9b0cd07af85d81a19c76d3dc1
Stephen Hu <stephenhu@seismic.com>
2022-10-10 07:30:48 +0800

    Create KnLfVT.cs

---
+++ LeetCode/LeetCode/KnLfVT.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+    public class KnLfVT
+    {
+        public TreeNode ExpandBinaryTree(TreeNode root)
+        {
+            if (root.left != null)
+            {
+                root.left = new TreeNode(-1, ExpandBinaryTree(root.left), null);
+            }
+
+            if (root.right != null)
+            {
+                root.right = new TreeNode(-1, null, ExpandBinaryTree(root.right));
+            }
+
+            return root;
+        }
+    }
+}
