8c0b7f40a4ce511cb3add041977372ff4d454bf1
Stephen Hu <stephenhu@seismic.com>
2022-10-11 16:00:12 +0800

    Create find-the-original-array-of-prefix-xor.cs

---
+++ LeetCode/LeetCode/find-the-original-array-of-prefix-xor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LeetCode
+{
+    public class find_the_original_array_of_prefix_xor
+    {
+        public int[] FindArray(int[] pref)
+        {
+            var res = new int[pref.Length];
+            res[0] = pref[0];
+            for (int i = 1; i < pref.Length; i++)
+            {
+                res[i] = pref[i] ^ pref[i - 1];
+            }
+
+            return res;
+        }
+    }
+}
+
