46ed277d26653a3f957f1e00a1490bbf20175420
Stephen Hu <stephenhu@seismic.com>
2022-10-13 01:48:35 +0800

    Create number-of-common-factors.cs

---
+++ LeetCode/LeetCode/number-of-common-factors.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+    public class number_of_common_factors
+    {
+        public int CommonFactors(int a, int b)
+        {
+            var min = Math.Min(a, b);
+            var res = 0;
+            for (int i = 1; i <= min; i++)
+            {
+                if (a % i == 0 && b % i ==0)
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
