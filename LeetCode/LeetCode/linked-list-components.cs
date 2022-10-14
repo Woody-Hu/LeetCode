2fa9d0f08843fa593211d251fb5c7440180a57e5
Stephen Hu <stephenhu@seismic.com>
2022-10-14 08:40:04 +0800

    Create linked-list-components.cs

---
+++ LeetCode/LeetCode/linked-list-components.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class linked_list_components
+    {
+        public int NumComponents(ListNode head, int[] nums)
+        {
+            var res = 0;
+            var set = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                set.Add(item);
+            }
+
+            var inSet = false;
+            while (head != null)
+            {
+                if (set.Contains(head.val))
+                {
+                    if (!inSet)
+                    {
+                        inSet = true;
+                        res++;
+                    }
+                }
+                else
+                {
+                    inSet = false;
+                }
+
+                head = head.next;
+            }
+
+            return res;
+        }
+    }
+}
