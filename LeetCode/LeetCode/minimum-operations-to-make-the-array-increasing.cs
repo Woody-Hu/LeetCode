3be2a646a64c788b7b1a01aa0299aeff72b819c1
Stephen Hu <stephenhu@seismic.com>
2022-10-15 07:53:36 +0800

    Create minimum-operations-to-make-the-array-increasing.cs

---
+++ LeetCode/LeetCode/minimum-operations-to-make-the-array-increasing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_operations_to_make_the_array_increasing
+    {
+        public int MinOperations(int[] nums)
+        {
+            var res = 0;
+            for (int i = 0; nums.Length > 1 && i < nums.Length - 1; i++)
+            {
+                if (nums[i] >= nums[i+ 1])
+                {
+                    res += nums[i] - nums[i + 1] + 1;
+                    nums[i + 1] = nums[i] + 1;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
