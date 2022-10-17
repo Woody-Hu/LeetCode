ee49205950d18c57beb0af609ec085885b82fc1a
Stephen Hu <stephenhu@seismic.com>
2022-10-17 18:58:54 +0800

    Create sign-of-the-product-of-an-array.cs

---
+++ LeetCode/LeetCode/sign-of-the-product-of-an-array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LeetCode
+{
+    public class sign_of_the_product_of_an_array
+    {
+        public int ArraySign(int[] nums)
+        {
+            var count = 0;
+            foreach (var item in nums)
+            {
+                if (item == 0)
+                {
+                    return 0;
+                }
+                else if (item < 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count % 2 == 0)
+            {
+                return 1;
+            }
+
+            return -1;
+        }
+    }
+}
