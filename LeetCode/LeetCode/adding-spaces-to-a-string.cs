7d5985f9fb6ff9c4065441442bb3f4f26b8750cd
Stephen Hu <stephenhu@seismic.com>
2022-10-19 05:13:58 +0800

    Create adding-spaces-to-a-string.cs

---
+++ LeetCode/LeetCode/adding-spaces-to-a-string.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+    public class adding_spaces_to_a_string
+    {
+        public string AddSpaces(string s, int[] spaces)
+        {
+            var n = s.Length;
+            var m = spaces.Length;
+            var res = new char[m + n];
+            var i = 0;
+            var j = 0;
+            while (i < n)
+            {
+                while (j <m && spaces[j] <= i)
+                {
+                    res[i + j] = ' ';
+                    j++;
+                }
+
+                res[i + j] = s[i++];
+            }
+
+            return new string(res);
+        }
+    }
+}
