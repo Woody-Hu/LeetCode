415da88d94998a637d013e0fa049742a6ae4ec0a
Stephen Hu <stephenhu@seismic.com>
2022-10-18 08:24:55 +0800

    Create count-number-of-distinct-integers-after-reverse-operations.cs

---
+++ LeetCode/LeetCode/count-number-of-distinct-integers-after-reverse-operations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class count_number_of_distinct_integers_after_reverse_operations
+    {
+        public int CountDistinctIntegers(int[] nums)
+        {
+            var set = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                set.Add(item);
+                set.Add(Revert(item));
+            }
+
+            return set.Count;
+        }
+
+        private int Revert(int input)
+        {
+            var res = 0;
+            while (input > 0)
+            {
+                res = res * 10 + input % 10;
+                input = input / 10;
+            }
+
+            return res;
+        }
+    }
+}
