908badaea8251fd2f89ae21331ee40315c5185b9
Stephen Hu <stephenhu@seismic.com>
2022-10-20 15:50:40 +0800

    Create zuo-xuan-zhuan-zi-fu-chuan-lcof.cs

---
+++ LeetCode/LeetCode/zuo-xuan-zhuan-zi-fu-chuan-lcof.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    public class zuo_xuan_zhuan_zi_fu_chuan_lcof
+    {
+        public string ReverseLeftWords(string s, int n)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(s.Substring(n));
+            stringBuilder.Append(s.Substring(0, n));
+            return stringBuilder.ToString();
+        }
+    }
+}
