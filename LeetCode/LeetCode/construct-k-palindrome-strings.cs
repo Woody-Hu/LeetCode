ae28892251de2fa41c717f42b7932140a5c1eeca
Stephen Hu <stephenhu@seismic.com>
2022-10-24 09:49:10 +0800

    Create construct-k-palindrome-strings.cs

---
+++ LeetCode/LeetCode/construct-k-palindrome-strings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LeetCode
+{
+    public class construct_k_palindrome_strings
+    {
+        public bool CanConstruct(string s, int k)
+        {
+            var right = s.Length;
+            var array = new int[26];
+            foreach (var item in s)
+            {
+                array[item - 'a']++;
+            }
+
+            var left = 0;
+            foreach (var item in array)
+            {
+                if (item % 2 == 1)
+                {
+                    left++;
+                }
+            }
+
+            left = Math.Max(left, 1);
+            return left <= k && k <= right;
+        }
+    }
+}
+
