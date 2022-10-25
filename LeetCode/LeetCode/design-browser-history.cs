e88229c843327791a61fa1c5fccc3f82b65ad794
Stephen Hu <stephenhu@seismic.com>
2022-10-25 12:44:33 +0800

    Create design-browser-history.cs

---
+++ LeetCode/LeetCode/design-browser-history.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace LeetCode
+{
+    public class design_browser_history
+    {
+        public class BrowserHistory
+        {
+            private string[] _history;
+            private int _current;
+            private int _range;
+
+            public BrowserHistory(string homepage)
+            {
+                _history = new string[5000];
+                _current = -1;
+                Visit(homepage);
+            }
+
+            public void Visit(string url)
+            {
+                _history[++_current] = url;
+                _range = _current;
+            }
+
+            public string Back(int steps)
+            {
+                _current = Math.Max(0, _current - steps);
+                return _history[_current];
+            }
+
+            public string Forward(int steps)
+            {
+                _current = Math.Min(_range, _current + steps);
+                return _history[_current];
+            }
+        }
+    }
+}
