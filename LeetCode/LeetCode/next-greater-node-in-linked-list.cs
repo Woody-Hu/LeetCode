1e6fd6c3b8978766e28df248f5a306f9264e4f71
Stephen Hu <stephenhu@seismic.com>
2022-10-26 10:54:34 +0800

    Create next-greater-node-in-linked-list.cs

---
+++ LeetCode/LeetCode/next-greater-node-in-linked-list.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class next_greater_node_in_linked_list
+    {
+        public int[] NextLargerNodes(ListNode head)
+        {
+            var list = new List<int>();
+            var cur = head;
+            while (cur != null)
+            {
+                list.Add(cur.val);
+                cur = cur.next;
+            }
+
+            var stack = new Stack<int>();
+            var res = new int[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                while (stack.Any() && list[stack.Peek()] < list[i])
+                {
+                    var pop = stack.Pop();
+                    res[pop] = list[i];
+                }
+
+                stack.Push(i);
+            }
+
+            return res;
+        }
+    }
+}
