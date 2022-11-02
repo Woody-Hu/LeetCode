5c05f67f0ee5264fcffd6f7243452a121f7960c0
Stephen Hu <stephenhu@seismic.com>
2022-11-02 10:43:51 +0800

    Create average-waiting-time.cs

---
+++ LeetCode/LeetCode/average-waiting-time.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode
+{
+    public class average_waiting_time
+    {
+        public double AverageWaitingTime(int[][] customers)
+        {
+            var sumWait = 0.0d;
+            var start = 0;
+            var n = customers.Length;
+            foreach (var item in customers)
+            {
+                var arrival = item[0];
+                start = Math.Max(start, arrival) + item[1];
+                sumWait += start - arrival;
+            }
+
+            return sumWait / n;
+        }
+    }
+}
+
