69a3f38ee71d07f59a72f95e7220fabbc248091e
Stephen Hu <stephenhu@seismic.com>
2022-11-07 11:23:56 +0800

    Create angle-between-hands-of-a-clock.cs

---
+++ LeetCode/LeetCode/angle-between-hands-of-a-clock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+    public class angle_between_hands_of_a_clock
+    {
+        public double AngleClock(int hour, int minutes)
+        {
+            var a1 = 6;
+            var a2 = 30;
+            double minutesAngle = a1 * minutes;
+            double hourAngle = (hour % 12 + minutes / 60.0d) * a2;
+            double diff = Math.Abs(hourAngle - minutesAngle);
+            return Math.Min(diff, 360 - diff);
+
+        }
+    }
+}
+
