50ade87aeaa88a1aa82a8eab6adf0b74670025b9
Stephen Hu <stephenhu@seismic.com>
2022-11-10 09:47:45 +0800

    2022 11 10

---
+++ LeetCode/LeetCode/find-all-groups-of-farmland.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class find_all_groups_of_farmland
+    {
+        public int[][] FindFarmland(int[][] land)
+        {
+            var res = new List<int[]>();
+            var m = land.Length;
+            var n = land[0].Length;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (land[i][j] == 0)
+                    {
+                        continue;
+                    }
+
+                    var row = i;
+                    var col = j;
+                    while (row + 1 < m && land[row+1][j] == 1)
+                    {
+                        row++;
+                    }
+
+                    while (col + 1 <n && land[i][col + 1] == 1)
+                    {
+                        col++;
+                    }
+
+                    for (int x = i; x <= row; x++)
+                    {
+                        for (int y = j; y <= col; y++)
+                        {
+                            land[x][y] = 0;
+                        }
+                    }
+
+                    res.Add(new int[] { i, j, row, col });
+                }
+            }
+
+            return res.ToArray();
+        }
+    }
+}
---
+++ LeetCode/LeetCode/length-of-the-longest-alphabetical-continuous-substring.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+    public class length_of_the_longest_alphabetical_continuous_substring
+    {
+        public int LongestContinuousSubstring(string s)
+        {
+            var res = 1;
+            var count = 1;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] - s[i - 1] == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 1;
+                }
+
+                res = Math.Max(res, count);
+            }
+
+            return res;
+        }
+    }
+}
