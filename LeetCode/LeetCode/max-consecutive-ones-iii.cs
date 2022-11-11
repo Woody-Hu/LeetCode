0203e5c4db67d821b51f19b3ef7716990bfee21b
Stephen Hu <stephenhu@seismic.com>
2022-11-11 11:41:42 +0800

    Create max-consecutive-ones-iii.cs

---
+++ LeetCode/LeetCode/max-consecutive-ones-iii.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+    public class max_consecutive_ones_iii
+    {
+        public int LongestOnes(int[] nums, int k)
+        {
+            var res = 0;
+            var left = 0;
+            var leftSum = 0;
+            var rightSum = 0;
+            for (int r = 0; r < nums.Length; r++)
+            {
+                rightSum += 1 - nums[r];
+                while (leftSum < rightSum - k)
+                {
+                    leftSum += 1 - nums[left++];
+                }
+
+                res = Math.Max(res, r - left + 1);
+            }
+
+            return res;
+        }
+    }
+}
