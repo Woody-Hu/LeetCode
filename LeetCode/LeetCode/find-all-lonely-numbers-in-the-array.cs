d2020da262bdc480066d6d2f69220b559bce7742
Stephen Hu <stephenhu@seismic.com>
2022-11-15 09:11:24 +0800

    Create find-all-lonely-numbers-in-the-array.cs

---
+++ LeetCode/LeetCode/find-all-lonely-numbers-in-the-array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class find_all_lonely_numbers_in_the_array
+    {
+        public IList<int> FindLonely(int[] nums)
+        {
+            var res = new List<int>();
+            var dic = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (!dic.ContainsKey(item))
+                {
+                    dic.Add(item, 0);
+                }
+
+                dic[item]++;
+            }
+
+            foreach (var item in nums)
+            {
+                if (dic[item] == 1 && !dic.ContainsKey(item - 1) && !dic.ContainsKey(item + 1))
+                {
+                    res.Add(item);
+                }
+            }
+
+            return res;
+        }
+    }
+}
