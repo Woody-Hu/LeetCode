f4746c7fabbf1434d65d0e858582fc7c506ba511
Stephen Hu <stephenhu@seismic.com>
2022-11-16 09:58:12 +0800

    Create convert-the-temperature.cs

---
+++ LeetCode/LeetCode/convert-the-temperature.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LeetCode
+{
+    public class convert_the_temperature
+    {
+        public double[] ConvertTemperature(double celsius)
+        {
+            return new double[] { celsius + 273.15d, celsius * 1.80d + 32.00d };
+        }
+    }
+}
