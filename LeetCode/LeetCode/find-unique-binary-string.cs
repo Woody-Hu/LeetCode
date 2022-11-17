23ed4f6476030a6f1b1ff82fa7bce0189f5eb1a7
Stephen Hu <stephenhu@seismic.com>
2022-11-17 09:29:43 +0800

    Create find-unique-binary-string.cs

---
+++ LeetCode/LeetCode/find-unique-binary-string.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    public class find_unique_binary_string
+    {
+        public string FindDifferentBinaryString(string[] nums)
+        {
+            var stringBuilder = new StringBuilder();
+            var n = nums.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i][i] == '0')
+                {
+                    stringBuilder.Append('1');
+                }
+                else
+                {
+                    stringBuilder.Append('0');
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
