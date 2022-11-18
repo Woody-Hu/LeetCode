d711ce5d84a3ca975586974beea86d24a8a04cfa
Stephen Hu <stephenhu@seismic.com>
2022-11-18 09:17:26 +0800

    Create w6cpku.cs

---
+++ LeetCode/LeetCode/w6cpku.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+    public class w6cpku
+    {
+        public TreeNode ConvertBST(TreeNode root)
+        {
+            var sum = 0;
+            return ConvertBST(root, ref sum);
+        }
+
+        private TreeNode ConvertBST(TreeNode root, ref int sum)
+        {
+            if (root != null)
+            {
+                ConvertBST(root.right, ref sum);
+                sum += root.val;
+                root.val = sum;
+                ConvertBST(root.left, ref sum);
+            }
+
+            return root;
+        }
+    }
+}
