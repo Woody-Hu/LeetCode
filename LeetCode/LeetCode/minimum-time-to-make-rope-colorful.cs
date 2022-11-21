90f941de3d264ddd685ee4718be038058bdba6cc
Stephen Hu <stephenhu@seismic.com>
2022-11-21 14:24:04 +0800

    Create minimum-time-to-make-rope-colorful.cs

---
+++ LeetCode/LeetCode/minimum-time-to-make-rope-colorful.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LeetCode
+{
+	public class minimum_time_to_make_rope_colorful
+	{
+        public int MinCost(string colors, int[] neededTime)
+        {
+            var l = colors.Length;
+            var i = 0;
+            var res = 0;
+            while (i < l)
+            {
+                var cur = colors[i];
+                var max = 0;
+                var sum = 0;
+                while (i < l && colors[i] == cur)
+                {
+                    max = Math.Max(max, neededTime[i]);
+                    sum += neededTime[i];
+                    i++;
+                }
+
+                res += sum - max;
+            }
+
+            return res;
+        }
+    }
+}
+
