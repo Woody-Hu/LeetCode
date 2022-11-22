daf0839c41d0d64ef38286d9b4ecfb9b7bc55435
Stephen Hu <stephenhu@seismic.com>
2022-11-22 09:23:12 +0800

    Create maximum-sum-of-two-non-overlapping-subarrays.cs

---
+++ LeetCode/LeetCode/maximum-sum-of-two-non-overlapping-subarrays.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_sum_of_two_non_overlapping_subarrays
+    {
+        public int MaxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen)
+        {
+            var l = nums.Length;
+            var res = 0;
+            var sum = new int[l + 1];
+            for (int i = 0; i < l; i++)
+            {
+                sum[i + 1] = sum[i] + nums[i];
+            }
+
+            if (firstLen > secondLen)
+            {
+                var temp = firstLen;
+                firstLen = secondLen;
+                secondLen = temp;
+            }
+
+            var dp = new int[l + 1, 2];
+            for (int i = firstLen; i <= l; i++)
+            {
+                var s1 = sum[i] - sum[i - firstLen];
+                dp[i, 0] = Math.Max(dp[i - 1, 0], s1);
+                res = Math.Max(res, s1 + dp[i - firstLen, 1]);
+                if (i >= secondLen)
+                {
+                    var s2 = sum[i] - sum[i - secondLen];
+                    dp[i, 1] = Math.Max(dp[i - 1, 1], s2);
+                    res = Math.Max(res, s2 + dp[i - secondLen, 0]);
+                }
+            }
+
+            return res;
+
+        }
+    }
+}
