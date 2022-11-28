235088d8e1e7530fd0ddbd6f20c55f91dbe3fd3f
Stephen Hu <stephenhu@seismic.com>
2022-11-28 09:21:07 +0800

    Create difference-between-ones-and-zeros-in-row-and-column.cs

---
+++ LeetCode/LeetCode/difference-between-ones-and-zeros-in-row-and-column.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace LeetCode
+{
+    public class difference_between_ones_and_zeros_in_row_and_column
+    {
+        public int[][] OnesMinusZeros(int[][] grid)
+        {
+            var m = grid.Length;
+            var n = grid[0].Length;
+            var r = new int[m];
+            var c = new int[n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[i][j] == 0)
+                    {
+                        r[i]--;
+                        c[j]--;
+                    }
+                    else
+                    {
+                        r[i]++;
+                        c[j]++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    grid[i][j] = r[i] + c[j];
+                }
+            }
+
+            return grid;
+        }
+    }
+}
