2fc22aa8339fa83da9a26847dc1bb553b7ada5b1
Stephen Hu <stephenhu@seismic.com>
2022-11-29 09:16:19 +0800

    Create remove-nodes-from-linked-list.cs

---
+++ LeetCode/LeetCode/remove-nodes-from-linked-list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeetCode
+{
+    public class remove_nodes_from_linked_list
+    {
+        public ListNode RemoveNodes(ListNode head)
+        {
+            return RemoveNodesInternal(head);
+        }
+
+        private ListNode RemoveNodesInternal(ListNode head)
+        {
+            if (head.next == null)
+            {
+                return head;
+            }
+
+            head.next = RemoveNodesInternal(head.next);
+            if (head.val < head.next.val)
+            {
+                return head.next;
+            }
+
+            return head;
+
+        }
+    }
+}
