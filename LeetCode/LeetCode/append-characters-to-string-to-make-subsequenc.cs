c3b4ff34574b9ab768880d45876fef60f28c7f4f
Stephen Hu <stephenhu@seismic.com>
2022-11-30 09:31:01 +0800

    Create append-characters-to-string-to-make-subsequenc.cs

---
+++ LeetCode/LeetCode/append-characters-to-string-to-make-subsequenc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+	public class append_characters_to_string_to_make_subsequenc
+	{
+        public int AppendCharacters(string s, string t)
+        {
+            var p = 0;
+            foreach (var item in s)
+            {
+                if (item == t[p])
+                {
+                    p++;
+                }
+
+                if (p == t.Length)
+                {
+                    break;
+                }
+            }
+
+            return t.Length - p;
+        }
+    }
+}
+
