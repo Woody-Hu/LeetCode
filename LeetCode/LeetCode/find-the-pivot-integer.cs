925b8f5709900a98a4c7c9264954eaa94ea24abe
Stephen Hu <stephenhu@seismic.com>
2022-12-01 08:01:18 +0800

    Create find-the-pivot-integer.cs

---
+++ LeetCode/LeetCode/find-the-pivot-integer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+	public class find_the_pivot_integer
+	{
+        public int PivotInteger(int n)
+        {
+            var m = n * (n + 1) / 2;
+            var x = (int)Math.Sqrt(m);
+            if (x * x == m)
+            {
+                return x;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
+
