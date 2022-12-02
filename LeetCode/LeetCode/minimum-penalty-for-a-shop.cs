a6d9d6c5f325f4a8898ea74db3f9b18f9ec352b3
Stephen Hu <stephenhu@seismic.com>
2022-12-02 10:27:49 +0800

    Create minimum-penalty-for-a-shop.cs

---
+++ LeetCode/LeetCode/minimum-penalty-for-a-shop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCode
+{
+    public class minimum_penalty_for_a_shop
+    {
+        public int BestClosingTime(string customers)
+        {
+            var sum = 0;
+            var max = 0;
+            var res = customers.Length;
+            for (int i = customers.Length - 1; i >= 0; i--)
+            {
+                if (customers[i] == 'N')
+                {
+                    sum++;
+                    if (sum >= max)
+                    {
+                        max = sum;
+                        res = i;
+                    }
+                }
+                else
+                {
+                    sum--;
+                }
+            }
+
+            return res;
+        }
+    }
+}
