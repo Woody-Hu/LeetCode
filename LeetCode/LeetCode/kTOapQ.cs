f5fb16aa5d4c4708cfb157620632bffe298abcf0
Stephen Hu <stephenhu@seismic.com>
2022-12-06 11:37:50 +0800

    Create kTOapQ.cs

---
+++ LeetCode/LeetCode/kTOapQ.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class kTOapQ
+    {
+        public class BSTIterator
+        {
+            private TreeNode _cur;
+            private Stack<TreeNode> _stack;
+
+            public BSTIterator(TreeNode root)
+            {
+                _cur = root;
+                _stack = new Stack<TreeNode>();
+            }
+
+            public int Next()
+            {
+                while (_cur != null)
+                {
+                    _stack.Push(_cur);
+                    _cur = _cur.left;
+                }
+
+                _cur = _stack.Pop();
+                var res = _cur.val;
+                _cur = _cur.right;
+                return res;
+            }
+
+            public bool HasNext()
+            {
+                return _cur != null || _stack.Any();
+            }
+        }
+    }
+}
