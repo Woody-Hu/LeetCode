588f4b131a5c082f370378aabc3ee82c469cd808
Stephen Hu <stephenhu@seismic.com>
2022-12-08 07:49:40 +0800

    Create number-of-times-binary-string-is-prefix-aligned.cs

---
+++ LeetCode/LeetCode/number-of-times-binary-string-is-prefix-aligned.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode
+{
+    public class number_of_times_binary_string_is_prefix_aligned
+    {
+        public int NumTimesAllBlue(int[] flips)
+        {
+            var res = 0;
+            var max = 0;
+            for (int i = 0; i < flips.Length; i++)
+            {
+                max = Math.Max(max, flips[i]);
+                if (max == i + 1)
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
