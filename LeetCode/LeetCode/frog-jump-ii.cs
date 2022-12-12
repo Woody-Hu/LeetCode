8ffc714a91d7ff1b96cf1971503640b9046c4b2f
Stephen Hu <stephenhu@seismic.com>
2022-12-12 15:03:31 +0800

    Create frog-jump-ii.cs

---
+++ LeetCode/LeetCode/frog-jump-ii.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LeetCode
+{
+    public class frog_jump_ii
+    {
+        public int MaxJump(int[] stones)
+        {
+            var res = stones[1] - stones[0];
+            for (int i = 2; i < stones.Length; i++)
+            {
+                res = Math.Max(res, stones[i] - stones[i - 2]);
+            }
+
+            return res;
+        }
+    }
+}
