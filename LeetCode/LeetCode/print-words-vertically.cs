fe987a822ea6f3d5bfa7a844a0048d4904bdfe74
Stephen Hu <stephenhu@seismic.com>
2022-12-20 09:32:10 +0800

    Create print-words-vertically.cs

---
+++ LeetCode/LeetCode/print-words-vertically.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class print_words_vertically
+    {
+        public IList<string> PrintVertically(string s)
+        {
+            var res = new List<string>();
+            var array = s.Split(' ');
+            var len = array.Select(k => k.Count()).Max();
+            for (int i = 0; i < len; i++)
+            {
+                var sb = new StringBuilder();
+                foreach (var item in array)
+                {
+                    if (i < item.Length)
+                    {
+                        sb.Append(item[i]);
+                    }
+                    else
+                    {
+                        sb.Append(' ');
+                    }
+                }
+
+                res.Add(sb.ToString().TrimEnd());
+            }
+
+            return res;
+        }
+    }
+}
