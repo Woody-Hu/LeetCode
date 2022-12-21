e4030211d2bf8e556a08daa5f8856a7ee697943f
Stephen Hu <stephenhu@seismic.com>
2022-12-21 09:38:34 +0800

    Create minimum-limit-of-balls-in-a-bag.cs

---
+++ LeetCode/LeetCode/minimum-limit-of-balls-in-a-bag.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class minimum_limit_of_balls_in_a_bag
+    {
+        public int MinimumSize(int[] nums, int maxOperations)
+        {
+            var left = 0;
+            var right = nums.Max();
+            var res = 0;
+            while (left <= right)
+            {
+                var y = (left + right) / 2;
+                var count = 0l;
+                foreach (var item in nums)
+                {
+                    count += (item - 1) / y;
+                }
+
+                if (count <= maxOperations)
+                {
+                    res = y;
+                    right = y - 1;
+                }
+                else
+                {
+                    left = y + 1;
+                }
+            }
+
+            return res;
+        }
+    }
+}
