eeb36e68f1cfd9fd2df5b1d48a61c2310278d64e
Stephen Hu <stephenhu@seismic.com>
2022-12-26 11:10:16 +0800

    Create longest-subarray-of-1s-after-deleting-one-element.cs

---
+++ LeetCode/LeetCode/longest-subarray-of-1s-after-deleting-one-element.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace LeetCode
+{
+    public class longest_subarray_of_1s_after_deleting_one_element
+    {
+        public int LongestSubarray(int[] nums)
+        {
+            var res = 0;
+            var p0 = 0;
+            var p1 = 0;
+            foreach (var item in nums)
+            {
+                if (item == 0)
+                {
+                    p1 = p0;
+                    p0 = 0;
+                }
+                else
+                {
+                    ++p0;
+                    ++p1;
+                }
+
+                res = Math.Max(res, p1);
+            }
+
+            if (res == nums.Length)
+            {
+                res--;
+            }
+
+            return res;
+
+            
+        }
+    }
+}
