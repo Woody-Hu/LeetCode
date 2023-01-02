9efdb420c2c5a93702f5db688c5865e31361ee0b
Stephen Hu <stephenhu@seismic.com>
2023-01-03 07:58:25 +0800

    Create maximum-tastiness-of-candy-basket.cs

---
+++ LeetCode/LeetCode/maximum-tastiness-of-candy-basket.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_tastiness_of_candy_basket
+    {
+        public int MaximumTastiness(int[] price, int k)
+        {
+            var n = price.Length;
+            Array.Sort(price);
+            if (k == 2)
+            {
+                return price[n - 1] - price[0];
+            }
+
+            var l = 0;
+            var r = (price[n - 1] - price[0]) / (k - 1) + 1;
+            while (l + 1 < r)
+            {
+                var mid = (l + r) / 2;
+                if (Check(price, mid, k))
+                {
+                    l = mid;
+                }
+                else
+                {
+                    r = mid;
+                }
+            }
+
+            return l;
+        }
+
+        private bool Check(int[] price, int value, int k)
+        {
+            var count = 1;
+            var pre = price[0];
+
+            for (int i = 1; i < price.Length; i++)
+            {
+                if (price[i] >= pre + value)
+                {
+                    count++;
+                    pre = price[i];
+                }
+
+                if (count == k)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
