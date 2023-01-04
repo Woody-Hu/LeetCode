2c63cb7d755c2d5849547592a6184c11f91ca74a
Stephen Hu <stephenhu@seismic.com>
2023-01-04 09:21:08 +0800

    Create distinct-prime-factors-of-product-of-array.cs

---
+++ LeetCode/LeetCode/distinct-prime-factors-of-product-of-array.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class distinct_prime_factors_of_product_of_array
+    {
+        public int DistinctPrimeFactors(int[] nums)
+        {
+            var primeSet = new HashSet<int>();
+            var primeFactorSet = new HashSet<int>();
+            foreach (var n in nums)
+            {
+                if (primeFactorSet.Contains(n))
+                {
+                    continue;
+                }
+                if (IsPrime(n, primeSet))
+                {
+                    primeFactorSet.Add(n);
+                }
+
+                var temp = n;
+                for (int i = 2; i <= Math.Sqrt(temp); i++)
+                {
+                    if (IsPrime(i, primeSet))
+                    {
+                        while (temp % i == 0)
+                        {
+                            primeFactorSet.Add(i);
+                            temp /= i;
+                            if (IsPrime(temp, primeSet))
+                            {
+                                primeFactorSet.Add(temp);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            return primeFactorSet.Count;
+        }
+
+        private bool IsPrime(int n, HashSet<int> primeSet)
+        {
+            if (primeSet.Contains(n))
+            {
+                return true;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            primeSet.Add(n);
+            return true;
+        }
+    }
+}
