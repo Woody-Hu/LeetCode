c1b2d1ac626551213a4c691ecb549eaa07931c6b
Stephen Hu <stephenhu@seismic.com>
2023-01-09 09:16:02 +0800

    Create find-xor-beauty-of-array.cs

---
+++ LeetCode/LeetCode/find-xor-beauty-of-array.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LeetCode
+{
+    public class find_xor_beauty_of_array
+    {
+        public int XorBeauty(int[] nums)
+        {
+            var res = 0;
+            foreach (var item in nums)
+            {
+                res ^= item;
+            }
+
+            return res;
+        }
+    }
+}
