d092283d95712f8857c8a7f1619bf57f6443c80c
Stephen Hu <stephenhu@seismic.com>
2023-01-10 09:33:02 +0800

    Create find-kth-bit-in-nth-binary-string.cs

---
+++ LeetCode/LeetCode/find-kth-bit-in-nth-binary-string.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeetCode
+{
+    public class find_kth_bit_in_nth_binary_string
+    {
+        public char FindKthBit(int n, int k)
+        {
+            if (k == 1)
+            {
+                return '0';
+            }
+
+            var mid = 1 << (n - 1);
+            if (k == mid)
+            {
+                return '1';
+            }
+            else if (k < mid)
+            {
+                return FindKthBit(n - 1, k);
+            }
+            else
+            {
+                k = mid * 2 - k;
+                return Invert(FindKthBit(n - 1, k));
+            }
+
+        }
+
+        public char Invert(char bit)
+        {
+            return (char)('0' + '1' - bit);
+        }
+    }
+}
