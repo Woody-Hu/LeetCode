4b75e87b3364680b7162c98292c8b2eceb6c5923
Stephen Hu <stephenhu@seismic.com>
2023-01-12 11:06:20 +0800

    Create check-if-word-is-valid-after-substitutions.cs

---
+++ LeetCode/LeetCode/check-if-word-is-valid-after-substitutions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class check_if_word_is_valid_after_substitutions
+    {
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (var item in s)
+            {
+                if (item == 'c')
+                {
+                    if (!stack.Any() || stack.Pop() != 'b')
+                    {
+                        return false;
+                    }
+
+                    if (!stack.Any() || stack.Pop() != 'a')
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Push(item);
+                }
+            }
+
+            return !stack.Any();
+        }
+    }
+}
