27b86b6756781f4f85fe27a47f7bd42755fcb0d2
Stephen Hu <stephenhu@seismic.com>
2023-01-13 09:07:56 +0800

    Create maximum-alternating-subsequence-sum.cs

---
+++ LeetCode/LeetCode/maximum-alternating-subsequence-sum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_alternating_subsequence_sum
+    {
+        public long MaxAlternatingSum(int[] nums)
+        {
+            long odd = 0l;
+            long even = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                odd = Math.Max(odd, even - nums[i]);
+                even = Math.Max(even, odd + nums[i]);
+            }
+
+            return even;
+        }
+    }
+}
