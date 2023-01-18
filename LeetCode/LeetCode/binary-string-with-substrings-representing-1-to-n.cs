6facd8266e38bab77cc0a37bf92e1381fa30d4af
Stephen Hu <stephenhu@seismic.com>
2023-01-18 09:21:59 +0800

    Create binary-string-with-substrings-representing-1-to-n.cs

---
+++ LeetCode/LeetCode/binary-string-with-substrings-representing-1-to-n.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class binary_string_with_substrings_representing_1_to_n
+    {
+        public bool QueryString(string s, int n)
+        {
+            var l = s.Length;
+            if (n > l*l)
+            {
+                return false;
+            }
+
+            return Enumerable.Range(1, n).All(c => s.Contains(Convert.ToString(c, 2)));
+        }
+    }
+}
