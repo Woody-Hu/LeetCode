2794cbcfbb51293847832492b13aedef326c7dd0
Stephen Hu <stephenhu@seismic.com>
2023-01-19 09:36:25 +0800

    Create difference-between-element-sum-and-digit-sum-of-an-array.cs

---
+++ LeetCode/LeetCode/difference-between-element-sum-and-digit-sum-of-an-array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LeetCode
+{
+    public class difference_between_element_sum_and_digit_sum_of_an_array
+    {
+        public int DifferenceOfSum(int[] nums)
+        {
+            var res1 = 0;
+            var res2 = 0;
+            foreach (var item in nums)
+            {
+                res1 += item;
+                var temp = item;
+                while (temp > 0)
+                {
+                    res2 += temp % 10;
+                    temp = temp / 10;
+                }
+            }
+
+            return Math.Abs(res1 - res2);
+        }
+    }
+}
