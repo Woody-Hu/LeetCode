d2ab91f982e121453679243da143f7ad99d529c8
Stephen Hu <stephenhu@seismic.com>
2023-01-28 08:59:06 +0800

    Create sort-the-students-by-their-kth-score.cs

---
+++ LeetCode/LeetCode/sort-the-students-by-their-kth-score.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeetCode
+{
+    public class sort_the_students_by_their_kth_score
+    {
+        public int[][] SortTheStudents(int[][] score, int k)
+        {
+            Array.Sort(score, (a, b) => b[k] - a[k]);
+            return score;
+        }
+    }
+}
