c10b1244ef91d5c45d2828631b13705138ab86d9
Stephen Hu <stephenhu@seismic.com>
2023-01-29 09:17:51 +0800

    Create ways-to-make-a-fair-array.cs

---
+++ LeetCode/LeetCode/ways-to-make-a-fair-array.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace LeetCode
+{
+    public class ways_to_make_a_fair_array
+    {
+        public int WaysToMakeFair(int[] nums)
+        {
+            var odd1 = 0;
+            var even1 = 0;
+            var odd2 = 0;
+            var even2 = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if ((i & 1) != 0)
+                {
+                    odd1 += nums[i];
+                }
+                else
+                {
+                    even1 += nums[i];
+                }
+            }
+
+            var res = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if ((i & 1) != 0)
+                {
+                    odd1 -= nums[i];
+                }
+                else
+                {
+                    even1 -= nums[i];
+                }
+
+                if (even1 + odd2 == odd1 + even2)
+                {
+                    res++;
+                }
+
+                if ((i & 1) != 0)
+                {
+                    odd2 += nums[i];
+                }
+                else
+                {
+                    even2 += nums[i];
+                }
+            }
+
+            return res;
+        }
+    }
+}
