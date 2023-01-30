bc16893c21fff87d43c15b2ce581b6648089c343
Stephen Hu <stephenhu@seismic.com>
2023-01-30 09:24:25 +0800

    Create smallest-string-with-a-given-numeric-value.cs

---
+++ LeetCode/LeetCode/smallest-string-with-a-given-numeric-value.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    public class smallest_string_with_a_given_numeric_value
+    {
+        public string GetSmallestString(int n, int k)
+        {
+            var stringBuilder = new StringBuilder();
+            for (int i = 1; i <= n; i++)
+            {
+                var temp = Math.Max(1, k - (n - i) * 26);
+                k -= temp;
+                stringBuilder.Append((char)('a' + temp - 1));
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
