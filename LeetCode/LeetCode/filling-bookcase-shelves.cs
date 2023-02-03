74a92908382669e3a1831454af1506d4e4502c0c
Stephen Hu <stephenhu@seismic.com>
2023-02-03 09:36:23 +0800

    Create filling-bookcase-shelves.cs

---
+++ LeetCode/LeetCode/filling-bookcase-shelves.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LeetCode
+{
+	public class filling_bookcase_shelves
+	{
+        public int MinHeightShelves(int[][] books, int shelfWidth)
+        {
+            var n = books.Length;
+            var dp = new int[n + 1];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                var ch = 0;
+                var leftWidth = shelfWidth;
+                var mh = int.MaxValue;
+                for (int j = i; j < n && leftWidth >= books[j][0]; j++)
+                {
+                    leftWidth -= books[j][0];
+                    ch = Math.Max(ch, books[j][1]);
+                    mh = Math.Min(mh, ch + dp[j + 1]);
+                }
+
+                dp[i] = mh;
+            }
+
+            return dp[0];
+        }
+    }
+}
+
