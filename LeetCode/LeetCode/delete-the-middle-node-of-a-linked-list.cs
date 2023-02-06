3aa30f8bfd8a00f12acd547f80880b86694a8930
Stephen Hu <stephenhu@seismic.com>
2023-02-06 08:20:44 +0800

    Create delete-the-middle-node-of-a-linked-list.cs

---
+++ LeetCode/LeetCode/delete-the-middle-node-of-a-linked-list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeetCode
+{
+	public class delete_the_middle_node_of_a_linked_list
+	{
+        public ListNode DeleteMiddle(ListNode head)
+        {
+            if (head.next == null)
+            {
+                return null;
+            }
+
+            var slow = head;
+            var fast = head;
+            ListNode pre = null;
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                pre = slow;
+                slow = slow.next;
+            }
+
+            pre.next = pre.next.next;
+            return head;
+        }
+    }
+}
+
