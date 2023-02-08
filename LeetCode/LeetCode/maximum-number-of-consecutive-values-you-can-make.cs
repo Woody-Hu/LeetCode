8d3eeebd2e25433468ab58ffccba5cd8461fd40d
Stephen Hu <stephenhu@seismic.com>
2023-02-08 08:57:56 +0800

    Create maximum-number-of-consecutive-values-you-can-make.cs

---
+++ LeetCode/LeetCode/maximum-number-of-consecutive-values-you-can-make.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LeetCode
+{
+	public class maximum_number_of_consecutive_values_you_can_make
+	{
+        public int GetMaximumConsecutive(int[] coins)
+        {
+            var res = 1;
+            Array.Sort(coins);
+            foreach (var item in coins)
+            {
+                if (item > res)
+                {
+                    break;
+                }
+
+                res += item;
+            }
+
+            return res;
+        }
+    }
+}
+
