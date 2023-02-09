a7407913a8e475c4de4d1fc45b9333702b4df6fb
Stephen Hu <stephenhu@seismic.com>
2023-02-09 09:30:46 +0800

    Create remove-sub-folders-from-the-filesystem.cs

---
+++ LeetCode/LeetCode/remove-sub-folders-from-the-filesystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class remove_sub_folders_from_the_filesystem
+	{
+        public IList<string> RemoveSubfolders(string[] folder)
+        {
+            Array.Sort(folder);
+            var ans = new List<string>();
+            ans.Add(folder[0]);
+            for (int i = 1; i < folder.Length; ++i)
+            {
+                int pre = ans[ans.Count - 1].Length;
+                if (!(pre < folder[i].Length && folder[i].StartsWith(ans[ans.Count - 1] + "/")))
+                {
+                    ans.Add(folder[i]);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
+
