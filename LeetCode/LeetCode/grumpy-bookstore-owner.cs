5dbca40665647674383b876f3d63d50ece0cf874
Stephen Hu <stephenhu@seismic.com>
2023-02-13 07:45:43 +0800

    Create grumpy-bookstore-owner.cs

---
+++ LeetCode/LeetCode/grumpy-bookstore-owner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace LeetCode
+{
+    public class grumpy_bookstore_owner
+    {
+        public int MaxSatisfied(int[] customers, int[] grumpy, int minutes)
+        {
+            var total = 0;
+            for (int i = 0; i < customers.Length; i++)
+            {
+                if (grumpy[i] == 0)
+                {
+                    total += customers[i];
+                }
+            }
+
+            var add = 0;
+            for (int i = 0; i < minutes; i++)
+            {
+                add += customers[i] * grumpy[i];
+            }
+
+            var max = add;
+            for (int i = minutes; i < customers.Length; i++)
+            {
+                add = add - customers[i - minutes] * grumpy[i - minutes] + customers[i] * grumpy[i];
+                max = Math.Max(add, max);
+            }
+
+            return total + max;
+        }
+    }
+}
