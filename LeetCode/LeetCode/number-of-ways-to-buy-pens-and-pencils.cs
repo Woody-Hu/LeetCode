9a4bc9df6f6781a760645c800352cd38fbc73123
Stephen Hu <stephenhu@seismic.com>
2023-02-16 09:20:45 +0800

    Create number-of-ways-to-buy-pens-and-pencils.cs

---
+++ LeetCode/LeetCode/number-of-ways-to-buy-pens-and-pencils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LeetCode
+{
+	public class number_of_ways_to_buy_pens_and_pencils
+	{
+        public long WaysToBuyPensPencils(int total, int cost1, int cost2)
+        {
+            var max1 = total / cost1;
+            var res = 0l;
+            for (int i = 0; i <= max1; i++)
+            {
+                var rest = total - i * cost1;
+                res += rest / cost2 + 1;
+            }
+
+            return res;
+        }
+    }
+}
+
