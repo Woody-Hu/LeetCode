f98bfefe0d6d2a39899e9d0000f53026101261ac
Stephen Hu <stephenhu@seismic.com>
2023-02-20 09:39:05 +0800

    Create bracket-lcci.cs

---
+++ LeetCode/LeetCode/bracket-lcci.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+	public class bracket_lcci
+	{
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var res = new List<string>();
+            GenerateInternal(res, new StringBuilder(), 0, 0, n);
+            return res;
+        }
+
+        private void GenerateInternal(List<string> res, StringBuilder stringBuilder, int open, int close, int max)
+        {
+            if (stringBuilder.Length == max * 2)
+            {
+                res.Add(stringBuilder.ToString());
+                return;
+            }
+
+            if (open < max)
+            {
+                stringBuilder.Append("(");
+                GenerateInternal(res, stringBuilder, open + 1, close, max);
+                stringBuilder.Remove(stringBuilder.Length - 1, 1);
+            }
+
+            if (close < open)
+            {
+                stringBuilder.Append(")");
+                GenerateInternal(res, stringBuilder, open, close + 1, max);
+                stringBuilder.Remove(stringBuilder.Length - 1, 1);
+            }
+        }
+    }
+}
+
