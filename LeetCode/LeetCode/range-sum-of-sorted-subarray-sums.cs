0e5d9b74a05cec6a56fc2c988317d55f2c541f64
Stephen Hu <stephenhu@seismic.com>
2023-02-22 09:19:03 +0800

    Create range-sum-of-sorted-subarray-sums.cs

---
+++ LeetCode/LeetCode/range-sum-of-sorted-subarray-sums.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+
+namespace LeetCode
+{
+	public class range_sum_of_sorted_subarray_sums
+	{
+        public int RangeSum(int[] nums, int n, int left, int right)
+        {
+            const int modulo = 1000000007;
+            var sumLength = n * (n + 1) / 2;
+            var sums = new int[sumLength];
+            var index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < n; j++)
+                {
+                    sum += nums[j];
+                    sums[index++] = sum;
+                }
+            }
+
+            Array.Sort(sums);
+            var ans = 0;
+            for (int i = left - 1; i < right; i++)
+            {
+                ans = (ans + sums[i]) % modulo;
+            }
+
+            return ans;
+        }
+    }
+}
+
