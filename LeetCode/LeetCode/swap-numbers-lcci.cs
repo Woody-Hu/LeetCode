cccedd9270df69f03d204cc57cfdf99f4c0958c7
Stephen Hu <stephenhu@seismic.com>
2023-02-24 08:50:45 +0800

    Create swap-numbers-lcci.cs

---
+++ LeetCode/LeetCode/swap-numbers-lcci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LeetCode
+{
+	public class swap_numbers_lcci
+	{
+        public int[] SwapNumbers(int[] numbers)
+        {
+            numbers[0] = numbers[0] ^ numbers[1];
+            numbers[1] = numbers[0] ^ numbers[1];
+            numbers[0] = numbers[0] ^ numbers[1];
+            return numbers;
+        }
+    }
+}
+
