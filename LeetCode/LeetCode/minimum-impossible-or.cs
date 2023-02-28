071e3d2666ec6b09f88be7a0573c4996c57a3dd1
Stephen Hu <stephenhu@seismic.com>
2023-02-28 09:20:35 +0800

    Create minimum-impossible-or.cs

---
+++ LeetCode/LeetCode/minimum-impossible-or.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class minimum_impossible_or
+	{
+        public int MinImpossibleOR(int[] nums)
+        {
+            var set = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                set.Add(item);
+            }
+
+            var temp = 1;
+            while (true)
+            {
+                if (!set.Contains(temp))
+                {
+                    return temp;
+                }
+
+                temp <<= 1;
+            }
+        }
+    }
+}
+
