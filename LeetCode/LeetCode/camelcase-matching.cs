0590af03e5b43472c1b2388d1b15b9fdd06a557e
Stephen Hu <stephenhu@seismic.com>
2023-03-03 09:52:47 +0800

    Create camelcase-matching.cs

---
+++ LeetCode/LeetCode/camelcase-matching.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class camelcase_matching
+	{
+        public IList<bool> CamelMatch(string[] queries, string pattern)
+        {
+            var res = new List<bool>();
+            var pArray = pattern.ToCharArray();
+            var length = pArray.Length;
+            foreach (var queryItem in queries)
+            {
+                var i = 0;
+                var current = true;
+                foreach (var item in queryItem)
+                {
+                    if (i < length && item == pArray[i])
+                    {
+                        i++;
+                    }
+                    else if (item >= 'A' && item <= 'Z')
+                    {
+                        current = false;
+                        break;
+                    }
+                }
+
+                res.Add(current ? i == length : current);
+            }
+
+            return res;
+        }
+    }
+}
+
