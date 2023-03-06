d278778a8505ab3007b2f83892efb6b0a14f71bb
Stephen Hu <stephenhu@seismic.com>
2023-03-06 08:55:01 +0800

    Create count-total-number-of-colored-cells.cs

---
+++ LeetCode/LeetCode/count-total-number-of-colored-cells.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeetCode
+{
+	public class count_total_number_of_colored_cells
+	{
+        public long ColoredCells(int n)
+        {
+            return 1L + (long)2 * n * (n - 1);
+        }
+    }
+}
+
