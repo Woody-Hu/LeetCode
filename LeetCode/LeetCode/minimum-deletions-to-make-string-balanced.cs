9a9de6ef19f8ba39ace8f3de7e7c1eb0141eeb0a
Stephen Hu <stephenhu@seismic.com>
2023-03-07 09:07:05 +0800

    Create minimum-deletions-to-make-string-balanced.cs

---
+++ LeetCode/LeetCode/minimum-deletions-to-make-string-balanced.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+	public class minimum_deletions_to_make_string_balanced
+	{
+        public int MinimumDeletions(string s)
+        {
+            var ca = 0;
+            foreach (var item in s)
+            {
+                ca += 'b' - item;
+            }
+
+            var res = ca;
+            foreach (var item in s)
+            {
+                ca += (item - 'a') * 2 - 1;
+                res = Math.Min(ca, res);
+            }
+
+            return res;
+        }
+    }
+}
+
