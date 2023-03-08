605006840851c6b9f71ee5e238f08b501953a236
Stephen Hu <stephenhu@seismic.com>
2023-03-08 09:05:34 +0800

    Create replace-elements-in-an-array.cs

---
+++ LeetCode/LeetCode/replace-elements-in-an-array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class replace_elements_in_an_array
+	{
+        public int[] ArrayChange(int[] nums, int[][] operations)
+        {
+            var dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dic[nums[i]] = i;
+            }
+
+            foreach (var item in operations)
+            {
+                var index = dic[item[0]];
+                nums[index] = item[1];
+                dic.Remove(item[0]);
+                dic[item[1]] = index;
+            }
+
+            return nums;
+        }
+    }
+}
+
