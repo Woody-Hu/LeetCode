1e7c4e3b6fd1ea8bfca9f947a12d042d04c71ee4
Stephen Hu <stephenhu@seismic.com>
2023-03-10 09:10:02 +0800

    Create maximum-total-importance-of-roads.cs

---
+++ LeetCode/LeetCode/maximum-total-importance-of-roads.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+    public class maximum_total_importance_of_roads
+    {
+        public long MaximumImportance(int n, int[][] roads)
+        {
+            var values = new long[n];
+            foreach (var item in roads)
+            {
+                values[item[0]]++;
+                values[item[1]]++;
+            }
+
+            var cityValue = 1;
+            Array.Sort(values);
+            var res = 0l;
+            foreach (var item in values)
+            {
+                res += item * cityValue;
+                cityValue++;
+            }
+
+            return res;
+        }
+    }
+}
