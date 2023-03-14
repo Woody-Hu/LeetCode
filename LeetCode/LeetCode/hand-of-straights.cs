8cc5ad92d2ca56e2b3e2077ef07270ecd220db3c
Stephen Hu <stephenhu@seismic.com>
2023-03-14 09:14:23 +0800

    Create hand-of-straights.cs

---
+++ LeetCode/LeetCode/hand-of-straights.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class hand_of_straights
+    {
+        public bool IsNStraightHand(int[] hand, int groupSize)
+        {
+            int n = hand.Length;
+            if (n % groupSize != 0)
+            {
+                return false;
+            }
+
+            Array.Sort(hand);
+            var cnt = new Dictionary<int, int>();
+            foreach (int x in hand)
+            {
+                if (!cnt.ContainsKey(x))
+                {
+                    cnt.Add(x, 0);
+                }
+                cnt[x]++;
+            }
+
+            foreach (var item in hand)
+            {
+                if (!cnt.ContainsKey(item))
+                {
+                    continue;
+                }
+
+                for (int i = 0; i < groupSize; i++)
+                {
+                    var index = item + i;
+                    if (!cnt.ContainsKey(index))
+                    {
+                        return false;
+                    }
+
+                    cnt[index]--;
+                    if (cnt[index] == 0)
+                    {
+                        cnt.Remove(index);
+                    }
+                }
+            }
+
+            return true;
+
+        }
+    }
+}
