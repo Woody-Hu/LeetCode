278839898b4ee3a9b8b491b3209b916811f38dad
Stephen Hu <stephenhu@seismic.com>
2023-03-21 09:42:13 +0800

    Create mirror-reflection.cs

---
+++ LeetCode/LeetCode/mirror-reflection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LeetCode
+{
+	public class mirror_reflection
+	{
+        public int MirrorReflection(int p, int q)
+        {
+            var g = GCD(p, q);
+            p /= g; g %= 2;
+            q /= g; q %= 2;
+            if (p == 1 && q == 1)
+            {
+                return 1;
+            }
+
+            return p == 1 ? 0 : 2;
+        }
+
+        private int GCD(int a, int b)
+        {
+            if (a == 0)
+            {
+                return b;
+            }
+
+            return GCD(b % a, a);
+        }
+    }
+}
+
