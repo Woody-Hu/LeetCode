4d017123d0b8aebadc7bbacd6947fa01b6bfe47d
Stephen Hu <stephenhu@seismic.com>
2023-03-23 09:36:45 +0800

    Create maximize-greatness-of-an-array.cs

---
+++ LeetCode/LeetCode/maximize-greatness-of-an-array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode
+{
+	public class maximize_greatness_of_an_array
+	{
+        public int MaximizeGreatness(int[] nums)
+        {
+            Array.Sort(nums);
+            var i = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] > nums[i])
+                {
+                    i++;
+                }
+            }
+
+            return i;
+        }
+    }
+}
+
