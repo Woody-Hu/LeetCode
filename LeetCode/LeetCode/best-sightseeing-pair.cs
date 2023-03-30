fbfccac17de3227f3d81f316d112f740ecbec97e
Stephen Hu <stephenhu@seismic.com>
2023-03-30 09:05:04 +0800

    Create best-sightseeing-pair.cs

---
+++ LeetCode/LeetCode/best-sightseeing-pair.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+	public class best_sightseeing_pair
+	{
+        public int MaxScoreSightseeingPair(int[] values)
+        {
+            var res = 0;
+            var max = values[0] - 0;
+
+            for (int i = 1; i < values.Length; i++)
+            {
+                res = Math.Max(res, max + values[i] - i);
+                max = Math.Max(max, values[i] + i);
+            }
+
+            return res;
+        }
+    }
+}
+
