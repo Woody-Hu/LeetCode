bfb08a34128159be5c59dc818b26239452daa903
Stephen Hu <stephenhu@seismic.com>
2023-03-31 08:56:25 +0800

    Create left-and-right-sum-differences.cs

---
+++ LeetCode/LeetCode/left-and-right-sum-differences.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class left_and_right_sum_differences
+	{
+        public int[] LeftRigthDifference(int[] nums)
+        {
+            var total = nums.Sum();
+            var leftSum = 0;
+            var rightSum = total;
+            var res = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var value = nums[i];
+                rightSum -= value;
+                res[i] = Math.Abs(leftSum - rightSum);
+                leftSum += value;
+            }
+
+            return res;
+        }
+    }
+}
+
