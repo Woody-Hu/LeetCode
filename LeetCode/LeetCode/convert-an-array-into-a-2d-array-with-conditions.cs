02f81689262baff125eeecc5a79ab0575127fd17
Stephen Hu <stephenhu@seismic.com>
2023-04-03 09:09:58 +0800

    Create convert-an-array-into-a-2d-array-with-conditions.cs

---
+++ LeetCode/LeetCode/convert-an-array-into-a-2d-array-with-conditions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class convert_an_array_into_a_2d_array_with_conditions
+	{
+        public IList<IList<int>> FindMatrix(int[] nums)
+        {
+            var dic = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (!dic.ContainsKey(item))
+                {
+                    dic.Add(item, 0);
+                }
+
+                dic[item]++;
+            }
+
+            var res = new List<IList<int>>();
+            while (dic.Any())
+            {
+                var list = new List<int>();
+                var keys = dic.Keys;
+                foreach (var item in keys)
+                {
+                    list.Add(item);
+                    dic[item]--;
+                    if (dic[item] == 0)
+                    {
+                        dic.Remove(item);
+                    }
+                }
+
+                res.Add(list);
+            }
+
+            return res;
+        }
+    }
+}
+
