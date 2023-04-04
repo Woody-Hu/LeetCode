074805c3a87b336daf3d281bf8cdfafb118cb1fe
Stephen Hu <stephenhu@seismic.com>
2023-04-04 09:18:40 +0800

    Create minimum-rounds-to-complete-all-tasks.cs

---
+++ LeetCode/LeetCode/minimum-rounds-to-complete-all-tasks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class minimum_rounds_to_complete_all_tasks
+	{
+        public int MinimumRounds(int[] tasks)
+        {
+            var res = 0;
+            var dic = new Dictionary<int, int>();
+            foreach (var item in tasks)
+            {
+                if (!dic.ContainsKey(item))
+                {
+                    dic.Add(item, 0);
+                }
+
+                dic[item]++;
+            }
+
+            foreach (var item in dic)
+            {
+                if (item.Value < 2)
+                {
+                    return -1;
+                }
+
+                res += (item.Value + 2) / 3;
+            }
+
+            return res;
+        }
+    }
+}
+
