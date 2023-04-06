3dcb3df905d50a630909c69a70f0174d9531c278
Stephen Hu <stephenhu@seismic.com>
2023-04-06 09:34:41 +0800

    Create sender-with-largest-word-count.cs

---
+++ LeetCode/LeetCode/sender-with-largest-word-count.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class sender_with_largest_word_count
+	{
+        public string LargestWordCount(string[] messages, string[] senders)
+        {
+            var res = string.Empty;
+            var max = 0;
+            var dic = new Dictionary<string, int>();
+            for (int i = 0; i < messages.Length; i++)
+            {
+                var num = messages[i].Split(' ').Length;
+                if (!dic.ContainsKey(senders[i])) dic.Add(senders[i], num);
+                else dic[senders[i]] += num;
+
+                if (dic[senders[i]] > max || (dic[senders[i]] == max && StringComparer.Ordinal.Compare(senders[i], res) > 0))
+                {
+                    max = dic[senders[i]];
+                    res = senders[i];
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
