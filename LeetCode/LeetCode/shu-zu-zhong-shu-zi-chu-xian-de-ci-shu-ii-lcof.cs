e390017c259619d264dfddd878a11e15241324bf
Stephen Hu <stephenhu@seismic.com>
2023-04-10 07:24:44 +0800

    Create shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof.cs

---
+++ LeetCode/LeetCode/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+	public class shu_zu_zhong_shu_zi_chu_xian_de_ci_shu_ii_lcof
+	{
+        public int SingleNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length; i+= 3)
+            {
+                if (i >= nums.Length - 1|| nums[i] != nums[i + 1])
+                {
+                    return i >= nums.Length ? nums[nums.Length - 1] : nums[i];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
+
