bec62b71c21d4a2be893a1073f47b90b5b1e841e
Stephen Hu <stephenhu@seismic.com>
2023-04-11 10:00:15 +0800

    Create moving-stones-until-consecutive-ii.cs

---
+++ LeetCode/LeetCode/moving-stones-until-consecutive-ii.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace LeetCode
+{
+	public class moving_stones_until_consecutive_ii
+	{
+        public int[] NumMovesStonesII(int[] stones)
+        {
+            var n = stones.Length;
+            Array.Sort(stones);
+            if (stones[n-1] - stones[0] + 1 == n)
+            {
+                return new int[] { 0, 0 };
+            }
+
+            var max = Math.Max(stones[n - 2] - stones[0], stones[n - 1] - stones[1]) + 1 - (n - 1);
+            var min = n;
+            for (int i = 0, j = 0; i < n && j + 1 <n; i++)
+            {
+                while (j + 1 < n && stones[j + 1] - stones[i] + 1 <= n)
+                {
+                    ++j;
+                }
+
+                if (j - i + 1 == n - 1 && stones[j] - stones[i] + 1 == n - 1)
+                {
+                    min = Math.Min(min, 2);
+                }
+                else
+                {
+                    min = Math.Min(min, n - (j - i + 1));
+                }
+
+            }
+
+            return new int[] { min, max };
+        }
+    }
+}
+
