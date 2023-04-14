efa64691f2a9bdf76fc588fac5a054d88cddc7f6
Stephen Hu <stephenhu@seismic.com>
2023-04-14 08:43:08 +0800

    2023 04 14

---
+++ LeetCode/LeetCode/Ygoe9J.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class Ygoe9J
+	{
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            var res = new List<IList<int>>();
+            var list = new List<int>();
+            DFS(candidates, target, 0, list, res);
+            return res;
+        }
+
+        private void DFS(int[] input, int target, int i, List<int> list, IList<IList<int>> res)
+        {
+            if (i == input.Length)
+            {
+                return;
+            }
+
+            if (target <= 0)
+            {
+                res.Add(new List<int>(list));
+                return;
+            }
+
+            DFS(input, target, i + 1, list, res);
+            if (target - input[i] >= 0)
+            {
+                list.Add(input[i]);
+                DFS(input, target - input[i], i, list, res);
+                list.RemoveAt(list.Count - 1);
+            }
+        }
+    }
+}
+
---
+++ LeetCode/LeetCode/detect-squares.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class detect_squares
+	{
+        public class DetectSquares
+        {
+            private readonly Dictionary<int, Dictionary<int, int>> _cnt;
+
+            public DetectSquares()
+            {
+                _cnt = new Dictionary<int, Dictionary<int, int>>();
+            }
+
+            public void Add(int[] point)
+            {
+                int x = point[0], y = point[1];
+                if (!_cnt.ContainsKey(y))
+                {
+                    _cnt.Add(y, new Dictionary<int, int>());
+                }
+
+                var yCnt = _cnt[y];
+                if (!yCnt.ContainsKey(x))
+                {
+                    yCnt.Add(x, 0);
+                }
+                yCnt[x]++;
+            }
+
+            public int Count(int[] point)
+            {
+                int res = 0;
+                int x = point[0], y = point[1];
+                if (!_cnt.ContainsKey(y))
+                {
+                    return 0;
+                }
+
+                var yCnt = _cnt[y];
+                foreach (var pair in _cnt)
+                {
+                    int col = pair.Key;
+                    var colCnt = pair.Value;
+                    if (col != y)
+                    {
+                        var d = col - y;
+                        res += (colCnt.ContainsKey(x) ? colCnt[x] : 0) * (yCnt.ContainsKey(x + d) ? yCnt[x + d] : 0) * (colCnt.ContainsKey(x + d) ? colCnt[x + d] : 0);
+                        res += (colCnt.ContainsKey(x) ? colCnt[x] : 0) * (yCnt.ContainsKey(x - d) ? yCnt[x - d] : 0) * (colCnt.ContainsKey(x - d) ? colCnt[x - d] : 0);
+                    }
+                }
+
+                return res;
+            }
+        }
+    }
+}
+
