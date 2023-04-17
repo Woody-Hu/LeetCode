375300251d821632b28457151e645077c71d1f5e
Stephen Hu <stephenhu@seismic.com>
2023-04-17 09:10:53 +0800

    Create LwUNpT.cs

---
+++ LeetCode/LeetCode/LwUNpT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace LeetCode
+{
+	public class LwUNpT
+	{
+        public int FindBottomLeftValue(TreeNode root)
+        {
+            var maxHeight = 0;
+            var res = 0;
+            DFS(root, 0, ref maxHeight, ref res);
+            return res;
+        }
+
+        private void DFS(TreeNode root, int height, ref int maxHeight, ref int res)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            height++;
+            DFS(root.left, height, ref maxHeight, ref res);
+            DFS(root.right, height, ref maxHeight, ref res);
+            if (height> maxHeight)
+            {
+                maxHeight = height;
+                res = root.val;
+            }
+        }
+    }
+}
+
