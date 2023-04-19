65a427c36f17b7a62873f558fb007c62e0c99c87
Stephen Hu <stephenhu@seismic.com>
2023-04-19 08:52:01 +0800

    Create find-the-score-of-all-prefixes-of-an-arra.cs

---
+++ LeetCode/LeetCode/find-the-score-of-all-prefixes-of-an-arra.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeetCode
+{
+	public class find_the_score_of_all_prefixes_of_an_arra
+	{
+        public long[] FindPrefixScore(int[] nums)
+        {
+            var len = nums.Length;
+            var arr = new long[len];
+            var max = 0;
+            for (int i = 0; i < len; i++)
+            {
+                max = Math.Max(max, nums[i]);
+                arr[i] = nums[i] + max;
+            }
+
+            var res = new long[len];
+            for (int i = 0; i < len; i++)
+            {
+                if (i == 0)
+                {
+                    res[i] = arr[i];
+                }
+                else
+                {
+                    res[i] = arr[i] + res[i - 1];
+                }
+
+            }
+
+            return res;
+        }
+    }
+}
+
