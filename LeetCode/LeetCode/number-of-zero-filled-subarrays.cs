df99185c7bb1376df610f586b24e15ed1be0d431
Stephen Hu <stephenhu@seismic.com>
2023-04-24 11:46:08 +0800

    Create number-of-zero-filled-subarrays.cs

---
+++ LeetCode/LeetCode/number-of-zero-filled-subarrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+	public class number_of_zero_filled_subarrays
+	{
+        public long ZeroFilledSubarray(int[] nums)
+        {
+            var res = 0l;
+            var count = 0;
+            foreach (var item in nums)
+            {
+                if (item != 0)
+                {
+                    count = 0;
+                }
+                else
+                {
+                    res += count;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
