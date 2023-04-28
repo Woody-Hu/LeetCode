bae375978e13dde7b89e38a003b4c9db2e22d57b
Stephen Hu <stephenhu@seismic.com>
2023-04-28 08:43:18 +0800

    Create find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cs

---
+++ LeetCode/LeetCode/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace LeetCode
+{
+	public class find_the_minimum_and_maximum_number_of_nodes_between_critical_points
+	{
+        public int[] NodesBetweenCriticalPoints(ListNode head)
+        {
+            var pre = head;
+            var cur = head.next;
+            var firstIndex = 0;
+            var preIndex = 0;
+            var curIndex = 1;
+            var min = int.MaxValue;
+            var max = -1;
+            while (cur.next != null)
+            {
+                var next = cur.next;
+                var less = pre.val > cur.val && next.val > cur.val;
+                var great = pre.val < cur.val && next.val < cur.val;
+                if (less || great)
+                {
+                    if (firstIndex == 0)
+                    {
+                        firstIndex = curIndex;
+                    }
+                    else
+                    {
+                        max = curIndex - firstIndex;
+                        min = Math.Min(min, curIndex - preIndex);
+                    }
+
+                    preIndex = curIndex;
+                }
+
+                curIndex++;
+                pre = cur;
+                cur = next;
+            }
+
+            return max == -1 ? new int[] { -1, -1 } : new int[] { min, max };
+        }
+    }
+}
+
