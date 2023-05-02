08cee91c6b43de36f165c2511b8174a598688e11
Stephen Hu <stephenhu@seismic.com>
2023-05-02 09:17:46 +0800

    Create find-the-prefix-common-array-of-two-arrays.cs

---
+++ LeetCode/LeetCode/find-the-prefix-common-array-of-two-arrays.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class find_the_prefix_common_array_of_two_arrays
+	{
+        public int[] FindThePrefixCommonArray(int[] A, int[] B)
+        {
+            var setA = new HashSet<int>();
+            var setB = new HashSet<int>();
+            var c = 0;
+            var res = new int[A.Length];
+            for (int i = 0; i < A.Length; i++)
+            {
+                setA.Add(A[i]);
+                setB.Add(B[i]);
+                if (setA.Contains(B[i]))
+                {
+                    c++;
+                }
+
+                if (setB.Contains(A[i]))
+                {
+                    c++;
+                }
+
+                if (A[i] == B[i])
+                {
+                    c--;
+                }
+
+                res[i] = c;
+            }
+
+            return res;
+        }
+    }
+}
+
