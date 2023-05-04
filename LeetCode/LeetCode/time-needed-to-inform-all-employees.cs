179a5bf9e74afbd1f190db31971f439f9150f4df
Stephen Hu <stephenhu@seismic.com>
2023-05-04 10:44:56 +0800

    Create time-needed-to-inform-all-employees.cs

---
+++ LeetCode/LeetCode/time-needed-to-inform-all-employees.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class time_needed_to_inform_all_employees
+	{
+        public int NumOfMinutes(int n, int headID, int[] manager, int[] informTime)
+        {
+            var dic = new Dictionary<int, List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                if (!dic.ContainsKey(manager[i]))
+                {
+                    dic.Add(manager[i], new List<int>());
+                }
+
+                dic[manager[i]].Add(i);
+            }
+
+            return Calculate(headID, informTime, dic);
+        }
+
+        private int Calculate(int i, int[] informTime, Dictionary<int, List<int>> dic)
+        {
+            var res = 0;
+            var children = dic.GetValueOrDefault(i, new List<int>());
+            foreach (var item in children)
+            {
+                res = Math.Max(res, Calculate(item, informTime, dic));
+            }
+
+            return informTime[i] + res;
+
+        }
+    }
+}
+
