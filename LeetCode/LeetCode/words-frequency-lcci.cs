38a6ff56dccab496dd8d3c8e6641f4bff1bd7dcf
Stephen Hu <stephenhu@seismic.com>
2023-05-05 09:07:15 +0800

    Create words-frequency-lcci.cs

---
+++ LeetCode/LeetCode/words-frequency-lcci.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class words_frequency_lcci
+	{
+        public class WordsFrequency
+        {
+            private readonly Dictionary<string, int> _dic = new Dictionary<string, int>();
+
+            public WordsFrequency(string[] book)
+            {
+                foreach (var item in book)
+                {
+                    if (!_dic.ContainsKey(item))
+                    {
+                        _dic.Add(item, 0);
+                    }
+
+                    _dic[item]++;
+                }
+            }
+
+            public int Get(string word)
+            {
+                if (_dic.TryGetValue(word, out var exist))
+                {
+                    return exist;
+                }
+
+                return 0;
+            }
+        }
+    }
+}
+
