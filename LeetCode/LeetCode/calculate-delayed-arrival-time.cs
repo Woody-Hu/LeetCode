e025ee8025accb3258c315621f7e5ca23198b9b4
Stephen Hu <stephenhu@seismic.com>
2023-05-08 01:38:41 +0800

    Create calculate-delayed-arrival-time.cs

---
+++ LeetCode/LeetCode/calculate-delayed-arrival-time.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeetCode
+{
+	public class calculate_delayed_arrival_time
+	{
+        public int FindDelayedArrivalTime(int arrivalTime, int delayedTime)
+        {
+            return (arrivalTime + delayedTime) % 24;
+        }
+    }
+}
+
