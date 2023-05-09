93e50863c031f4a2620847e48e5c50a4e2a73c42
Stephen Hu <stephenhu@seismic.com>
2023-05-09 08:59:53 +0800

    Create binary-number-to-string-lcci.cs

---
+++ LeetCode/LeetCode/binary-number-to-string-lcci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+	public class binary_number_to_string_lcci
+	{
+        public string PrintBin(double num)
+        {
+            var stringBuilder = new StringBuilder("0.");
+            while (stringBuilder.Length <= 32 && num != 0)
+            {
+                num *= 2;
+                var intPart = (int)num;
+                stringBuilder.Append(intPart);
+                num -= intPart;
+            }
+
+            return stringBuilder.Length <= 32 ? stringBuilder.ToString() : "ERROR";
+        }
+    }
+}
+
