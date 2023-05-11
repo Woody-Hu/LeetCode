97b77a08f4d4d62a75918475d93e5b77d490b79b
Stephen Hu <stephenhu@seismic.com>
2023-05-11 09:01:27 +0800

    Create make-costs-of-paths-equal-in-a-binary-tree.cs

---
+++ LeetCode/LeetCode/make-costs-of-paths-equal-in-a-binary-tree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LeetCode
+{
+	public class make_costs_of_paths_equal_in_a_binary_tree
+	{
+        public int MinIncrements(int n, int[] cost)
+        {
+            var res = 0;
+            for (int i = n/2; i > 0; i--)
+            {
+                res += Math.Abs(cost[i * 2 - 1] - cost[i * 2]);
+                cost[i - 1] = Math.Max(cost[i * 2 - 1], cost[i * 2]);
+            }
+
+            return res;
+        }
+    }
+}
+
