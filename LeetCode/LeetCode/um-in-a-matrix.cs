157d21da20aaef782b74219eaf8eb39e7d20206e
Stephen Hu <stephenhu@seismic.com>
2023-05-15 09:08:43 +0800

    Create um-in-a-matrix.cs

---
+++ LeetCode/LeetCode/um-in-a-matrix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LeetCode
+{
+	public class um_in_a_matrix
+	{
+        public int MatrixSum(int[][] nums)
+        {
+            foreach (var item in nums)
+            {
+                Array.Sort(item);
+            }
+
+            var res = 0;
+            for (int i = 0; i < nums[0].Length; i++)
+            {
+                var max = 0;
+                foreach (var item in nums)
+                {
+                    max = Math.Max(item[i], max);
+                }
+
+                res += max;
+            }
+
+            return res;
+        }
+    }
+}
+
