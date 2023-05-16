b5c53340899221273c35f33cfa286d1fb1e45337
Stephen Hu <stephenhu@seismic.com>
2023-05-16 09:16:08 +0800

    Create neighboring-bitwise-xor.cs

---
+++ LeetCode/LeetCode/neighboring-bitwise-xor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+	public class neighboring_bitwise_xor
+	{
+        public bool DoesValidArrayExist(int[] derived)
+        {
+            var res = 0;
+            for (int i = 0; i < derived.Length; i++)
+            {
+                res = res ^ derived[i];
+            }
+
+            return res == 0;
+        }
+    }
+}
+
