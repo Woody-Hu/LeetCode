2d2065b460e363624e01659ee65646f54cb60894
Stephen Hu <stephenhu@seismic.com>
2023-05-22 10:34:22 +0800

    Create minimum-additions-to-make-valid-string.cs

---
+++ LeetCode/LeetCode/minimum-additions-to-make-valid-string.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode
+{
+	public class minimum_additions_to_make_valid_string
+	{
+        public int AddMinimum(string word)
+        {
+            var n = word.Length;
+            var res = 1;
+            for (int i = 1; i < n; i++)
+            {
+                if (word[i] <= word[i - 1])
+                {
+                    res++;
+                }
+            }
+
+            return 3 * res - n;
+        }
+    }
+}
+
