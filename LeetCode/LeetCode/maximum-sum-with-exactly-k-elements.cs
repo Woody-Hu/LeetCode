0aab33696090b8bb3511354ae40d7fe3d3e75ece
Stephen Hu <stephenhu@seismic.com>
2023-05-23 15:51:06 +0800

    Create maximum-sum-with-exactly-k-elements.cs

---
+++ LeetCode/LeetCode/maximum-sum-with-exactly-k-elements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class maximum_sum_with_exactly_k_elements
+    {
+        public int MaximizeSum(int[] nums, int k)
+        {
+            var max = nums.Max();
+            return (2 * max + k - 1) * k / 2;
+        }
+    }
+}
