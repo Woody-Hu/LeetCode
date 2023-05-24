1a9e1e1ddfed18f6faeffceed343e7fd09be1570
Stephen Hu <stephenhu@seismic.com>
2023-05-24 09:24:09 +0800

    Create insufficient-nodes-in-root-to-leaf-paths.cs

---
+++ LeetCode/LeetCode/insufficient-nodes-in-root-to-leaf-paths.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace LeetCode
+{
+	public class insufficient_nodes_in_root_to_leaf_paths
+	{
+        public TreeNode SufficientSubset(TreeNode root, int limit)
+        {
+            var res = Calculate(root, 0, limit);
+            return res ? root : null;
+        }
+
+        private bool Calculate(TreeNode root, int currentSum, int limit)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                return root.val + currentSum >= limit;
+            }
+
+            var cl = Calculate(root.left, currentSum + root.val, limit);
+            var cr = Calculate(root.right, currentSum + root.val, limit);
+            if (!cl)
+            {
+                root.left = null;
+            }
+
+            if (!cr)
+            {
+                root.right = null;
+            }
+
+            return cl || cr;
+        }
+    }
+}
+
