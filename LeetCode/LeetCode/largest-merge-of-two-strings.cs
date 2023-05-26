1d3d46e2ed59ce987d01c116872f6b335c2d5824
Stephen Hu <stephenhu@seismic.com>
2023-05-26 09:22:57 +0800

    Create largest-merge-of-two-strings.cs

---
+++ LeetCode/LeetCode/largest-merge-of-two-strings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+	public class largest_merge_of_two_strings
+	{
+        public string LargestMerge(string word1, string word2)
+        {
+            var res = new StringBuilder();
+            var i = 0;
+            var j = 0;
+            while (i < word1.Length || j < word2.Length)
+            {
+                if (i < word1.Length && word1.Substring(i).CompareTo(word2.Substring(j)) > 0)
+                {
+                    res.Append(word1[i]);
+                    i++;
+                }
+                else
+                {
+                    res.Append(word2[j]);
+                    j++;
+                }
+            }
+
+            return res.ToString();
+        }
+    }
+}
+
