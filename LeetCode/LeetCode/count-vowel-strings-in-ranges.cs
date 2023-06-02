7c462e4135a5b62f688bf4775b3b4ee49b291f84
Stephen Hu <stephenhu@seismic.com>
2023-06-02 09:24:34 +0800

    Create count-vowel-strings-in-ranges.cs

---
+++ LeetCode/LeetCode/count-vowel-strings-in-ranges.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace LeetCode
+{
+	public class count_vowel_strings_in_ranges
+	{
+        public int[] VowelStrings(string[] words, int[][] queries)
+        {
+            var n = words.Length;
+            var pSum = new int[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                var value = IsVowelString(words[i]) ? 1 : 0;
+                pSum[i + 1] = pSum[i] + value;
+            }
+
+            var res = new int[queries.Length];
+            for (int i = 0; i < queries.Length; i++)
+            {
+                res[i] = pSum[queries[i][1] + 1] - pSum[queries[i][0]];
+            }
+
+            return res;
+        }
+
+        private bool IsVowelString(string s)
+        {
+            return IsVowelLetter(s[0]) && IsVowelLetter(s[s.Length - 1]);
+        }
+
+        private bool IsVowelLetter(char c)
+        {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        }
+    }
+}
+
