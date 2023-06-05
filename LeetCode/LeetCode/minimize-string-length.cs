6f15433efd1a61582272eceac00ecb47c528e486
Stephen Hu <stephenhu@seismic.com>
2023-06-05 08:04:52 +0800

    Create minimize-string-length.cs

---
+++ LeetCode/LeetCode/minimize-string-length.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class minimize_string_length
+	{
+        public int MinimizedStringLength(string s)
+        {
+            var set = new HashSet<char>();
+            foreach (var item in s)
+            {
+                set.Add(item);
+            }
+
+            return set.Count;
+        }
+    }
+}
+
