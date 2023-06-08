c531812fa0734b625c5584d67c66e6cf95ee7a16
Stephen Hu <stephenhu@seismic.com>
2023-06-08 08:22:15 +0800

    Create time-needed-to-rearrange-a-binary-string.cs

---
+++ LeetCode/LeetCode/time-needed-to-rearrange-a-binary-string.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+	public class time_needed_to_rearrange_a_binary_string
+	{
+        public int SecondsToRemoveOccurrences(string s)
+        {
+            var f = 0;
+            var pre0 = 0;
+            foreach (var item in s)
+            {
+                if (item == '0')
+                {
+                    pre0++;
+                }
+                else if (pre0 > 0)
+                {
+                    f = Math.Max(f + 1, pre0);
+                }
+
+            }
+
+            return f;
+        }
+    }
+}
+
