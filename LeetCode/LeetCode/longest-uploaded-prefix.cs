bc4e77a3be09ba651b1271447ac72f0732c563f3
Stephen Hu <stephenhu@seismic.com>
2023-06-09 08:44:00 +0800

    Create longest-uploaded-prefix.cs

---
+++ LeetCode/LeetCode/longest-uploaded-prefix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class longest_uploaded_prefix
+	{
+        public class LUPrefix
+        {
+            private int _x = 0;
+
+            private HashSet<int> _set = new HashSet<int>();
+
+            public LUPrefix(int n)
+            {
+
+            }
+
+            public void Upload(int video)
+            {
+                _set.Add(video);
+            }
+
+            public int Longest()
+            {
+                while (_set.Contains(_x + 1))
+                {
+                    _x++;
+                }
+
+                return _x;
+            }
+        }
+    }
+}
+
