636e8371bc26bcb8e2d85878a00f67e5dd90350f
Stephen Hu <stephenhu@seismic.com>
2023-06-12 10:03:56 +0800

    Create mice-and-cheese.cs

---
+++ LeetCode/LeetCode/mice-and-cheese.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+	public class mice_and_cheese
+	{
+        public int MiceAndCheese(int[] reward1, int[] reward2, int k)
+        {
+            var res = 0;
+            var n = reward1.Length;
+            var diff = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                res += reward2[i];
+                diff[i] = reward1[i] - reward2[i];
+            }
+
+            Array.Sort(diff);
+            for (int i = 1; i <= k ; i++)
+            {
+                res += diff[n - i];
+            }
+
+            return res;
+        }
+    }
+}
+
