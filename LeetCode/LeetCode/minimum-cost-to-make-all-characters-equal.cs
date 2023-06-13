aa380aa3f9974d9a657e80a31823084e12f03829
Stephen Hu <stephenhu@seismic.com>
2023-06-13 08:47:50 +0800

    Create minimum-cost-to-make-all-characters-equal.cs

---
+++ LeetCode/LeetCode/minimum-cost-to-make-all-characters-equal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LeetCode
+{
+	public class minimum_cost_to_make_all_characters_equal
+	{
+        public long MinimumCost(string s)
+        {
+            long res = 0;
+            var array = s.ToCharArray();
+            var n = s.Length;
+            for (int i = 1; i < n; i++)
+            {
+                if (s[i - 1] != s[i])
+                {
+                    res += Math.Min(i, n - i);
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
