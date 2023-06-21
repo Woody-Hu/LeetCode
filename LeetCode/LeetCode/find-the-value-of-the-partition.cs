56b68734bc2d579b37f9f8466bfe2dc95e20ea1d
Stephen Hu <stephenhu@seismic.com>
2023-06-21 10:21:56 +0800

    Create find-the-value-of-the-partition.cs

---
+++ LeetCode/LeetCode/find-the-value-of-the-partition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LeetCode
+{
+	public class find_the_value_of_the_partition
+	{
+        public int FindValueOfPartition(int[] nums)
+        {
+            Array.Sort(nums);
+            var res = int.MaxValue;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                var diff = nums[i] - nums[i - 1];
+                res = Math.Min(res, diff);
+            }
+
+            return res;
+        }
+    }
+}
+
