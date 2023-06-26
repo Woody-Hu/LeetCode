28cd48ec28b0640c3e5b2d1cbd01d51d12b4b9a5
Stephen Hu <stephenhu@seismic.com>
2023-06-26 18:02:14 +0800

    Create maximum-points-you-can-obtain-from-cards.cs

---
+++ LeetCode/LeetCode/maximum-points-you-can-obtain-from-cards.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class maximum_points_you_can_obtain_from_cards
+	{
+        public int MaxScore(int[] cardPoints, int k)
+        {
+            var n = cardPoints.Length;
+            var ws = n - k;
+            var sum = 0;
+            for (int i = 0; i < ws; i++)
+            {
+                sum += cardPoints[i];
+            }
+
+            var minSum = sum;
+            for (int i = ws; i < n; i++)
+            {
+                sum += cardPoints[i] - cardPoints[i - ws];
+                minSum = Math.Min(minSum, sum);
+            }
+
+            return cardPoints.Sum() - minSum;
+        }
+    }
+}
+
