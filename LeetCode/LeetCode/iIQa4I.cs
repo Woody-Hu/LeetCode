042a324ef817300afd2c0bee3ff221f1d73b5316
Stephen Hu <stephenhu@seismic.com>
2023-06-29 08:57:31 +0800

    Create iIQa4I.cs

---
+++ LeetCode/LeetCode/iIQa4I.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class iIQa4I
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            var length = temperatures.Length;
+            var res = new int[length];
+            var stack = new Stack<int>();
+            for (int i = 0; i < length; i++)
+            {
+                var t = temperatures[i];
+                while (stack.Any() && t > temperatures[stack.Peek()])
+                {
+                    var p = stack.Pop();
+                    res[p] = i - p;
+                }
+
+                stack.Push(i);
+            }
+
+            return res;
+        }
+    }
+}
