7ee7ff9b1ed8367756fb5947c51dca11d855c2f7
Stephen Hu <stephenhu@seismic.com>
2023-07-03 09:16:52 +0800

    Create construct-the-longest-new-string.cs

---
+++ LeetCode/LeetCode/construct-the-longest-new-string.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeetCode
+{
+	public class construct_the_longest_new_string
+	{
+        public int LongestString(int x, int y, int z)
+        {
+            return (Math.Min(x, y) * 2+ (x != y ? 1 : 0) + z) * 2;
+        }
+    }
+}
+
