6b2f12d4df320e4134650eaaec3f4be7a3980f3d
Stephen Hu <stephenhu@seismic.com>
2023-07-11 09:14:59 +0800

    Create minimum-difference-between-largest-and-smallest-value-in-three-moves.cs

---
+++ LeetCode/LeetCode/minimum-difference-between-largest-and-smallest-value-in-three-moves.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+	public class minimum_difference_between_largest_and_smallest_value_in_three_moves
+	{
+        public int MinDifference(int[] nums)
+        {
+            if (nums.Length <= 4)
+            {
+                return 0;
+            }
+
+            var n = nums.Length;
+            Array.Sort(nums);
+            var res = int.MaxValue;
+            for (int i = 0; i < 4; i++)
+            {
+                res = Math.Min(res, nums[n - 4 + i] - nums[i]);
+            }
+
+            return res;
+        }
+    }
+}
+
