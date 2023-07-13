1b4e16554291c8d55c3ef7980aa293160b784b97
Stephen Hu <stephenhu@seismic.com>
2023-07-13 13:27:15 +0800

    Create divide-players-into-teams-of-equal-skill.cs

---
+++ LeetCode/LeetCode/divide-players-into-teams-of-equal-skill.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class divide_players_into_teams_of_equal_skill
+	{
+        public long DividePlayers(int[] skill)
+        {
+            Array.Sort(skill);
+            var s = skill.First() + skill.Last();
+            long res = 0;
+            for (int i = 0; i < skill.Length / 2; i++)
+            {
+                var x = skill[i];
+                var y = skill[skill.Length - 1 - i];
+                if (x + y != s)
+                {
+                    return -1;
+                }
+
+                res += x * y;
+            }
+
+            return res;
+        }
+    }
+}
+
