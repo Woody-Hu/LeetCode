5003072f020bd74897e5a4eab88af5c8f7a1f1fe
Stephen Hu <stephenhu@seismic.com>
2023-07-14 13:07:11 +0800

    Create relocate-marbles.cs

---
+++ LeetCode/LeetCode/relocate-marbles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class relocate_marbles
+	{
+        public IList<int> RelocateMarbles(int[] nums, int[] moveFrom, int[] moveTo)
+        {
+            var set = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                set.Add(item);
+            }
+
+            for (int i = 0; i < moveFrom.Length; i++)
+            {
+                set.Remove(moveFrom[i]);
+                set.Add(moveTo[i]);
+            }
+
+            var res = set.ToList();
+            res.Sort();
+            return res;
+        }
+    }
+}
+
