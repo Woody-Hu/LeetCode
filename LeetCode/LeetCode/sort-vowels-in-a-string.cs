597056ee368a379827824fc36e3e98a4f24bc227
Stephen Hu <stephenhu@seismic.com>
2023-07-25 11:43:14 +0800

    Create sort-vowels-in-a-string.cs

---
+++ LeetCode/LeetCode/sort-vowels-in-a-string.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+	public class sort_vowels_in_a_string
+	{
+        public string SortVowels(string s)
+        {
+            var vowelSet = new HashSet<char>() { 'a', 'e', 'i', 'o', 'u' };
+            var list = new List<char>();
+            foreach (var item in s)
+            {
+                if (vowelSet.Contains(Char.ToLower(item)))
+                {
+                    list.Add(item);
+                }
+            }
+
+            list.Sort();
+            var i = 0;
+            var res = new StringBuilder();
+            foreach (var item in s)
+            {
+                var c = item;
+                if (vowelSet.Contains(Char.ToLower(item)))
+                {
+                    c = list[i++];
+                }
+
+                res.Append(c);
+            }
+
+            return res.ToString();
+        }
+    }
+}
+
