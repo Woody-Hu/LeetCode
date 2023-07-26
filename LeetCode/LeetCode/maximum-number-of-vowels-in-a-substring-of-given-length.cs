b956fd13db37bde6c14bf58964c8e925de9a6b7d
Stephen Hu <stephenhu@seismic.com>
2023-07-26 09:42:44 +0800

    Create maximum-number-of-vowels-in-a-substring-of-given-length.cs

---
+++ LeetCode/LeetCode/maximum-number-of-vowels-in-a-substring-of-given-length.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCode
+{
+	public class maximum_number_of_vowels_in_a_substring_of_given_length
+	{
+        public int MaxVowels(string s, int k)
+        {
+            var n = s.Length;
+            var c = 0;
+            for (int i = 0; i < k; i++)
+            {
+                c += IsVowel(s[i]);
+            }
+
+            var res = c;
+            for (int i = k; i < n; i++)
+            {
+                c += IsVowel(s[i]) - IsVowel(s[i - k]);
+                res = Math.Max(res, c);
+            }
+
+            return res;
+        }
+
+        private int IsVowel(char ch)
+        {
+            return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ? 1 : 0;
+        }
+    }
+}
+
