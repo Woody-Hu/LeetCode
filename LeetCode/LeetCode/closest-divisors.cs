48d4e1ee7afab3d3bb31aff47aad742c6dbac25b
Stephen Hu <stephenhu@seismic.com>
2023-08-02 09:42:59 +0800

    Create closest-divisors.cs

---
+++ LeetCode/LeetCode/closest-divisors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LeetCode
+{
+	public class closest_divisors
+	{
+        public int[] ClosestDivisors(int num)
+        {
+            var divisor = num == 1 ? num + 1 : num + 2;
+            var i = (int)Math.Sqrt(divisor);
+            while (divisor % i > 1)
+            {
+                i--;
+            }
+
+            return new int[] { i, divisor / i };
+        }
+    }
+}
+
