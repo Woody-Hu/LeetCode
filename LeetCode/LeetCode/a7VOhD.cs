b903d2255cd9360cd7230c5437490601441175e8
Stephen Hu <stephenhu@seismic.com>
2023-08-03 10:48:13 +0800

    Create a7VOhD.cs

---
+++ LeetCode/LeetCode/a7VOhD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+	public class a7VOhD
+	{
+        public int CountSubstrings(string s)
+        {
+            var n = s.Length;
+            var res = 0;
+            for (int i = 0; i < 2* n - 1; i++)
+            {
+                var l = i / 2;
+                var r = i / 2 + i % 2;
+                while (l >= 0 && r < n && s[l] == s[r])
+                {
+                    --l;
+                    ++r;
+                    ++res;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
