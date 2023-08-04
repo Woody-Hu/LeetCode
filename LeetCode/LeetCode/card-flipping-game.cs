02f34e0bc4debacec39b17be5584b5827343f25d
Stephen Hu <stephenhu@seismic.com>
2023-08-04 09:50:03 +0800

    Create card-flipping-game.cs

---
+++ LeetCode/LeetCode/card-flipping-game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class card_flipping_game
+	{
+        public int Flipgame(int[] fronts, int[] backs)
+        {
+            var set = new HashSet<int>();
+            for (int i = 0; i < fronts.Length; i++)
+            {
+                if (fronts[i] == backs[i])
+                {
+                    set.Add(fronts[i]);
+                }
+            }
+
+            var res = 3000;
+            foreach (var item in fronts)
+            {
+                if (item < res && !set.Contains(item))
+                {
+                    res = item;
+                }
+            }
+
+            foreach (var item in backs)
+            {
+                if (item < res && !set.Contains(item))
+                {
+                    res = item;
+                }
+            }
+
+            return res % 3000;
+        }
+    }
+}
+
