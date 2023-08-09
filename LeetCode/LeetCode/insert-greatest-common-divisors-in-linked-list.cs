6ae2e38d433847baf82f60e7e883fdc013e5b08c
Stephen Hu <stephenhu@seismic.com>
2023-08-09 10:24:37 +0800

    2023 08 09

---
+++ LeetCode/LeetCode/insert-greatest-common-divisors-in-linked-list.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace LeetCode
+{
+	public class insert_greatest_common_divisors_in_linked_list
+	{
+        public ListNode InsertGreatestCommonDivisors(ListNode head)
+        {
+            var root = head;
+            var preValue = head.val;
+            var preNode = head;
+            head = head.next;
+            while (head != null)
+            {
+                var current = head.val;
+                var newValue = CalGCD(preValue, current);
+                var newNode = new ListNode(newValue);
+                preNode.next = newNode;
+                newNode.next = head;
+                preValue = current;
+                preNode = head;
+                head = head.next;
+            }
+
+            return root;
+        }
+
+        private int CalGCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                var r = a % b;
+                a = b;
+                b = r;
+            }
+
+            return a;  
+        }
+    }
+}
+
---
+++ LeetCode/LeetCode/maximum-absolute-sum-of-any-subarray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeetCode
+{
+	public class maximum_absolute_sum_of_any_subarray
+	{
+        public int MaxAbsoluteSum(int[] nums)
+        {
+            var s = 0;
+            var max = 0;
+            var min = 0;
+            foreach (var item in nums)
+            {
+                s += item;
+                if (s > max)
+                {
+                    max = s;
+                }
+                else if (s < min)
+                {
+                    min = s;
+                }
+            }
+
+            return max - min;
+        }
+    }
+}
+
