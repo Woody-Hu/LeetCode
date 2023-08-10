4d645d03e85ee0033018a66c3b0017d9f2b3f82e
Stephen Hu <stephenhu@seismic.com>
2023-08-10 09:13:14 +0800

    Create max-number-of-k-sum-pairs.cs

---
+++ LeetCode/LeetCode/max-number-of-k-sum-pairs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace LeetCode
+{
+	public class max_number_of_k_sum_pairs
+	{
+        public int MaxOperations(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            var res = 0;
+            var l = 0;
+            var r = nums.Length - 1;
+            var sum = 0;
+            while (l < r)
+            {
+                sum = nums[l] + nums[r];
+                if (sum == k)
+                {
+                    res++;
+                    r--;
+                    l++;
+                }
+                else if (sum > k)
+                {
+                    r--;
+                }
+                else
+                {
+                    l++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
