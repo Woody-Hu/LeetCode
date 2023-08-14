ecf9d6a60ad391283f512425b5bee33ab6454a6f
Stephen Hu <stephenhu@seismic.com>
2023-08-14 09:38:54 +0800

    Create find-the-substring-with-maximum-cost.cs

---
+++ LeetCode/LeetCode/find-the-substring-with-maximum-cost.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCode
+{
+	public class find_the_substring_with_maximum_cost
+	{
+        public int MaximumCostSubstring(string s, string chars, int[] vals)
+        {
+            var dic = new int[26];
+            for (int i = 0; i < 26; i++)
+            {
+                dic[i] = i + 1;
+            }
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                dic[chars[i] - 'a'] = vals[i];
+            }
+
+            var res = 0;
+            var temp = 0;
+            foreach (var item in s)
+            {
+                temp = Math.Max(temp, 0) + dic[item - 'a'];
+                res = Math.Max(res, temp);
+            }
+
+            return res;
+        }
+    }
+}
+
