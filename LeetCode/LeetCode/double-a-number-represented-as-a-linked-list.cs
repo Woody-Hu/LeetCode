6553da1685c3a8e6ed60eb5acc9b50d0543b889d
Stephen Hu <stephenhu@seismic.com>
2023-08-16 09:27:46 +0800

    Create double-a-number-represented-as-a-linked-list.cs

---
+++ LeetCode/LeetCode/double-a-number-represented-as-a-linked-list.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCode
+{
+	public class double_a_number_represented_as_a_linked_list
+	{
+        public ListNode DoubleIt(ListNode head)
+        {
+            if (head.val > 4)
+            {
+                var temp = head;
+                head = new ListNode(0);
+                head.next = temp;
+            }
+
+            var current = head;
+            while (current != null)
+            {
+                current.val = current.val * 2 % 10;
+                if (current.next != null && current.next.val > 4)
+                {
+                    current.val++;
+                }
+
+                current = current.next;
+            }
+
+            return head;
+        }
+    }
+}
+
