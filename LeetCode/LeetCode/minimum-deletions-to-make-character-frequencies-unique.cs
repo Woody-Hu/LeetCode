47a48535fb1f323e495ce54fd4d1a5a252a571d4
Stephen Hu <stephenhu@seismic.com>
2023-08-18 09:06:24 +0800

    Create minimum-deletions-to-make-character-frequencies-unique.cs

---
+++ LeetCode/LeetCode/minimum-deletions-to-make-character-frequencies-unique.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class minimum_deletions_to_make_character_frequencies_unique
+	{
+        public int MinDeletions(string s)
+        {
+            var count = new int[26];
+            foreach (var item in s)
+            {
+                count[item - 'a']++;
+            }
+
+            var list = new List<int>();
+            foreach (var item in count)
+            {
+                list.Add(item);
+            }
+
+            list.Sort();
+            var set = new HashSet<int>();
+            var res = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                var cur = list[i];
+                while (set.Contains(cur))
+                {
+                    res++;
+                    cur--;
+                }
+
+                if (cur !=0)
+                {
+                    set.Add(cur);
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
