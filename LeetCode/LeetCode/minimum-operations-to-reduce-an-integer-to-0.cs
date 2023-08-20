cf60be60f84155f5f875deeca1cb794ca949dc30
Stephen Hu <stephenhu@seismic.com>
2023-08-21 07:44:00 +0800

    Create minimum-operations-to-reduce-an-integer-to-0.cs

---
+++ LeetCode/LeetCode/minimum-operations-to-reduce-an-integer-to-0.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeetCode
+{
+	public class minimum_operations_to_reduce_an_integer_to_0
+	{
+        public int MinOperations(int n)
+        {
+            var res = 1;
+            while ((n & (n - 1)) > 0)
+            {
+                var lb = n & -n;
+                if ((n & lb << 1) > 0)
+                {
+                    n += lb;
+                }
+                else
+                {
+                    n -= lb;
+                }
+
+                res++;
+            }
+
+            return res;
+        }
+    }
+}
+
