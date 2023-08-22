14754319ca29e6483ff175a120c3b8db8966869a
Stephen Hu <stephenhu@seismic.com>
2023-08-22 17:28:40 +0800

    Create determine-the-minimum-sum-of-a-k-avoiding-array.cs

---
+++ LeetCode/LeetCode/determine-the-minimum-sum-of-a-k-avoiding-array.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LeetCode
+{
+	public class determine_the_minimum_sum_of_a_k_avoiding_array
+	{
+        public int MinimumSum(int n, int k)
+        {
+            var m = Math.Min(k / 2, n);
+            return (m * (m + 1) + (k * 2 + n - m - 1) * (n - m)) / 2;
+        }
+    }
+}
+
