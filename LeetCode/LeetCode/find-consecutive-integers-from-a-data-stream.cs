749185f82b5aa3e2696c99d84cd912d26147091c
Stephen Hu <stephenhu@seismic.com>
2023-08-24 09:11:44 +0800

    Create find-consecutive-integers-from-a-data-stream.cs

---
+++ LeetCode/LeetCode/find-consecutive-integers-from-a-data-stream.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace LeetCode
+{
+	public class find_consecutive_integers_from_a_data_stream
+	{
+        public class DataStream
+        {
+            private int _current;
+
+            private int _k;
+
+            private int _count;
+
+            public DataStream(int value, int k)
+            {
+                _current = value;
+                _k = k;
+                _count = 0;
+            }
+
+            public bool Consec(int num)
+            {
+                if (num == _current)
+                {
+                    _count++;
+                }
+                else
+                {
+                    _count = 0;
+                }
+
+                return _count >= _k;
+            }
+        }
+    }
+}
+
