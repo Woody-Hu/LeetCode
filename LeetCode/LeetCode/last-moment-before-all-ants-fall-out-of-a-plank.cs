9e3e17ec0ec8775849fb552ee637d3bcc3661901
Stephen Hu <stephenhu@seismic.com>
2023-08-28 06:59:05 +0800

    Create last-moment-before-all-ants-fall-out-of-a-plank.cs

---
+++ LeetCode/LeetCode/last-moment-before-all-ants-fall-out-of-a-plank.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LeetCode
+{
+	public class last_moment_before_all_ants_fall_out_of_a_plank
+	{
+		public last_moment_before_all_ants_fall_out_of_a_plank()
+		{
+		}
+	}
+}
+
