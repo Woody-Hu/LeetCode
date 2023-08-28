408b4c05dd8dbfa66d0ac5a08a073507b3400ec8
Stephen Hu <stephenhu@seismic.com>
2023-08-28 13:28:35 +0800

    Update last-moment-before-all-ants-fall-out-of-a-plank.cs

--- LeetCode/LeetCode/last-moment-before-all-ants-fall-out-of-a-plank.cs
+++ LeetCode/LeetCode/last-moment-before-all-ants-fall-out-of-a-plank.cs
@@ -3,9 +3,21 @@ namespace LeetCode
 {
 	public class last_moment_before_all_ants_fall_out_of_a_plank
 	{
-		public last_moment_before_all_ants_fall_out_of_a_plank()
-		{
-		}
-	}
+        public int GetLastMoment(int n, int[] left, int[] right)
+        {
+            var res = 0;
+            foreach (var item in left)
+            {
+                res = Math.Max(res, item);
+            }
+
+            foreach (var item in right)
+            {
+                res = Math.Max(res, n - item);
+            }
+
+            return res;
+        }
+    }
 }
 
