aeb410ec823e42a2400dc9cc99c63cef8f55822b
Stephen Hu <stephenhu@seismic.com>
2023-08-30 12:32:21 +0800

    Create boats-to-save-people.cs

---
+++ LeetCode/LeetCode/boats-to-save-people.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+	public class boats_to_save_people
+	{
+        public int NumRescueBoats(int[] people, int limit)
+        {
+            var res = 0;
+            Array.Sort(people);
+            var l = 0;
+            var r = people.Length - 1;
+            while (l <= r)
+            {
+                if (people[l] + people[r] <= limit)
+                {
+                    l++;
+                }
+
+                r--;
+                res++;
+            }
+
+            return res;
+        }
+    }
+}
+
