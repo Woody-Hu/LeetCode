0e5634514ccabecb1410f1ee7be710d2264f8f4e
Stephen Hu <stephenhu@seismic.com>
2023-08-31 08:18:03 +0800

    Create sequential-digits.cs

---
+++ LeetCode/LeetCode/sequential-digits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class sequential_digits
+	{
+        public IList<int> SequentialDigits(int low, int high)
+        {
+            var res = new List<int>();
+            for (int i = 1; i <= 9; i++)
+            {
+                var cur = i;
+                for (int j = i + 1; j <= 9; j++)
+                {
+                    cur = cur * 10 + j;
+                    if (cur >= low && cur <= high)
+                    {
+                        res.Add(cur);
+                    }
+                }
+
+            }
+
+            res.Sort();
+            return res;
+        }
+    }
+}
+
