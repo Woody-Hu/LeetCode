10746346e633b114ff2c28e2794bc7ba1d0cf005
Stephen Hu <stephenhu@seismic.com>
2023-09-01 09:27:18 +0800

    Create masking-personal-information.cs

---
+++ LeetCode/LeetCode/masking-personal-information.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+	public class masking_personal_information
+	{
+        public string MaskPII(string s)
+        {
+            string[] country = { "", "+*-", "+**-", "+***-" };
+            var atIndex = s.IndexOf("@");
+            if (atIndex > 0)
+            {
+                s = s.ToLower();
+                return (s[0] + "*****" + s.Substring(atIndex - 1)).ToLower();
+            }
+
+            var sb = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (char.IsDigit(c))
+                {
+                    sb.Append(c);
+                }
+            }
+            s = sb.ToString();
+            return country[s.Length - 10] + "***-***-" + s.Substring(s.Length - 4);
+        }
+    }
+}
+
