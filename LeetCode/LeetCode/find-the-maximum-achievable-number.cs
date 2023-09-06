b64828214b0f63aa054ba51a69c711d0b2729bd7
Stephen Hu <stephenhu@seismic.com>
2023-09-06 09:07:50 +0800

    Create find-the-maximum-achievable-number.cs

---
+++ LeetCode/LeetCode/find-the-maximum-achievable-number.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeetCode
+{
+	public class find_the_maximum_achievable_number
+	{
+        public int TheMaximumAchievableX(int num, int t)
+        {
+            return num + t * 2;
+        }
+    }
+}
+
