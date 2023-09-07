9fce480eb8a225c3bd57b949bf7a3a6c0cd53a35
Stephen Hu <stephenhu@seismic.com>
2023-09-07 09:10:04 +0800

    Create number-of-employees-who-met-the-target.cs

---
+++ LeetCode/LeetCode/number-of-employees-who-met-the-target.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class number_of_employees_who_met_the_target
+	{
+        public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+        {
+            return hours.Where(k => k >= target).Count();
+        }
+    }
+}
+
