eead4a91447489b7008dfb7001d25d0ad0a294dd
Stephen Hu <stephenhu@seismic.com>
2023-09-19 09:11:57 +0800

    Create faulty-keyboard.cs

---
+++ LeetCode/LeetCode/faulty-keyboard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+	public class faulty_keyboard
+	{
+        public string FinalString(string s)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var item in s)
+            {
+                if (item == 'i')
+                {
+                    var array = stringBuilder.ToString().ToCharArray();
+                    Array.Reverse(array);
+                    stringBuilder = new StringBuilder(new string(array));
+                }
+                else
+                {
+                    stringBuilder.Append(item);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
+
