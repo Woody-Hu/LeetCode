32cbfbb3c41ec4d2c7db7b624697f915d2d2ab28
Stephen Hu <stephenhu@seismic.com>
2023-09-21 09:36:01 +0800

    Create lexicographically-smallest-palindrome.cs

---
+++ LeetCode/LeetCode/lexicographically-smallest-palindrome.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+	public class lexicographically_smallest_palindrome
+	{
+        public string MakeSmallestPalindrome(string s)
+        {
+            var arr = s.ToCharArray();
+            var n = s.Length;
+            for (int i = 0; i < n/2; i++)
+            {
+                var j = n - i - 1;
+                if (arr[i] != arr[j])
+                {
+                    var c = arr[i] < arr[j] ? arr[i] : arr[j];
+                    arr[i] = c;
+                    arr[j] = c;
+                }
+            }
+
+            return new string(arr);
+        }
+    }
+}
+
