28c84f456a207a72e0d851b0b5413d62af4d02d4
Stephen Hu <stephenhu@seismic.com>
2023-09-22 06:46:21 +0800

    Create remove-trailing-zeros-from-a-string.cs

---
+++ LeetCode/LeetCode/remove-trailing-zeros-from-a-string.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeetCode
+{
+	public class remove_trailing_zeros_from_a_string
+	{
+        public string RemoveTrailingZeros(string num)
+        {
+            return num.TrimEnd('0');
+        }
+    }
+}
+
