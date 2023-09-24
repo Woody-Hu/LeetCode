2db0f3ca8ffbc16fe21615490e1a1e66694dee3c
Stephen Hu <stephenhu@seismic.com>
2023-09-25 07:10:29 +0800

    Create H8086Q.cs

---
+++ LeetCode/LeetCode/H8086Q.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class H8086Q
+	{
+        public class RecentCounter
+        {
+            private readonly Queue<int> _queue = new Queue<int>();
+
+            public RecentCounter()
+            {
+
+            }
+
+            public int Ping(int t)
+            {
+                _queue.Enqueue(t);
+                while (_queue.Peek() < t - 3000)
+                {
+                    _queue.Dequeue();
+                }
+
+                return _queue.Count;
+            }
+        }
+    }
+}
+
