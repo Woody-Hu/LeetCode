b4fe0de1cb218797ccda55f5b18e88440986a8d4
Stephen Hu <stephenhu@seismic.com>
2023-09-26 09:23:50 +0800

    Create number-of-senior-citizens.cs

---
+++ LeetCode/LeetCode/number-of-senior-citizens.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+	public class number_of_senior_citizens
+	{
+        public int CountSeniors(string[] details)
+        {
+            var res = 0;
+            foreach (var item in details)
+            {
+                if (int.Parse(item.Substring(11,2)) > 60)
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
