6c13413b3cd0434dc23f8fd6575b4edb6bcbfacb
Stephen Hu <stephenhu@seismic.com>
2023-09-27 09:21:11 +0800

    Create separate-the-digits-in-an-array.cs

---
+++ LeetCode/LeetCode/separate-the-digits-in-an-array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+	public class separate_the_digits_in_an_array
+	{
+        public int[] SeparateDigits(int[] nums)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var item in nums)
+            {
+                stringBuilder.Append(item);
+            }
+
+            var length = stringBuilder.Length;
+            var res = new int[length];
+            var str = stringBuilder.ToString();
+            for (int i = 0; i < length; i++)
+            {
+                res[i] = str[i] - '0';
+            }
+
+            return res;
+        }
+    }
+}
+
