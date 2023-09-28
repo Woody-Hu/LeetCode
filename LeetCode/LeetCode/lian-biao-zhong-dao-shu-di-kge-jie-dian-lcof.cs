13c8043c455faa0addc3db7b6f49d9853ddada05
Stephen Hu <stephenhu@seismic.com>
2023-09-28 09:19:45 +0800

    Create lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.cs

---
+++ LeetCode/LeetCode/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+	public class lian_biao_zhong_dao_shu_di_kge_jie_dian_lcof
+	{
+        public ListNode TrainingPlan(ListNode head, int cnt)
+        {
+            var front = head;
+            var back = head;
+            for (int i = 0; i < cnt; i++)
+            {
+                front = front.next;
+            }
+
+            while (front != null)
+            {
+                front = front.next;
+                back = back.next;
+            }
+
+            return back;
+        }
+    }
+}
+
