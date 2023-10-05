7cf031f8c1aac4646bb7796b5e43871ea33199e7
Stephen Hu <stephenhu@seismic.com>
2023-10-05 08:12:48 +0800

    Create maximum-odd-binary-number.cs

---
+++ LeetCode/LeetCode/maximum-odd-binary-number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+	public class maximum_odd_binary_number
+	{
+        public string MaximumOddBinaryNumber(string s)
+        {
+            var count1 = s.Where(c => c == '1').Count();
+            var pre = count1 - 1;
+            var stringBuilder = new StringBuilder();
+            for (int i = 0; i < pre; i++)
+            {
+                stringBuilder.Append("1");
+            }
+
+            for (int i = 0; i < s.Length - count1; i++)
+            {
+                stringBuilder.Append("0");
+            }
+
+            stringBuilder.Append("1");
+            return stringBuilder.ToString();
+
+        }
+    }
+}
+
