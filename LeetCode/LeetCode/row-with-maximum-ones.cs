6e742e38972f424a07c235521564488deeda2cdb
Stephen Hu <stephenhu@seismic.com>
2023-10-09 06:56:41 +0800

    Create row-with-maximum-ones.cs

---
+++ LeetCode/LeetCode/row-with-maximum-ones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LeetCode
+{
+	public class row_with_maximum_ones
+	{
+        public int[] RowAndMaximumOnes(int[][] mat)
+        {
+            var count = 0;
+            var rowI = 0;
+            for (int i = 0; i < mat.Length; i++)
+            {
+                var temp = 0;
+                foreach (var item in mat[i])
+                {
+                    temp += item;
+                }
+
+                if (temp > count)
+                {
+                    rowI = i;
+                    count = temp;
+                }
+            }
+
+            return new int[] { rowI, count };
+        }
+    }
+}
+
