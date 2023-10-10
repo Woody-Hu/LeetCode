a4f72c0a52523dd787a92d61ac0d365c01627692
Stephen Hu <stephenhu@seismic.com>
2023-10-10 09:07:38 +0800

    Create alternating-digit-sum.cs

---
+++ LeetCode/LeetCode/alternating-digit-sum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+	public class alternating_digit_sum
+	{
+        public int AlternateDigitSum(int n)
+        {
+            var res = 0;
+            var sign = 1;
+            while (n > 0)
+            {
+                res += n % 10 * sign;
+                sign = -sign;
+                n /= 10;
+            }
+
+            return res * -sign;
+        }
+    }
+}
+
