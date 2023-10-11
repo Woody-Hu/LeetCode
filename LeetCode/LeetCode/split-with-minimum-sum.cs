27b9cbd3f4b8b0ca14087c5689c29868465aba5b
Stephen Hu <stephenhu@seismic.com>
2023-10-11 08:52:54 +0800

    Create split-with-minimum-sum.cs

---
+++ LeetCode/LeetCode/split-with-minimum-sum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeetCode
+{
+	public class split_with_minimum_sum
+	{
+        public int SplitNum(int num)
+        {
+            var array = num.ToString().ToCharArray();
+            Array.Sort(array);
+            var num1 = 0;
+            var num2 = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    num1 = num1 * 10 + (array[i] - '0');
+                }
+                else
+                {
+                    num2 = num2 * 10 + (array[i] - '0');
+                }
+            }
+
+            return num1 + num2;
+        }
+    }
+}
+
