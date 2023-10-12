673feef2ea261cb58cfd9364bf6a37fc7bee50dd
Stephen Hu <stephenhu@seismic.com>
2023-10-12 08:55:27 +0800

    Create sum-of-squares-of-special-elements.cs

---
+++ LeetCode/LeetCode/sum-of-squares-of-special-elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode
+{
+	public class sum_of_squares_of_special_elements
+	{
+        public int SumOfSquares(int[] nums)
+        {
+            var res = 0;
+            var n = nums.Length;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    res += nums[i - 1] * nums[i - 1];
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
