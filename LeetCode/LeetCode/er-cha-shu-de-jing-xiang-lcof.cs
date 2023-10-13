831177095e842cb92619108468487435ddb3b848
Stephen Hu <stephenhu@seismic.com>
2023-10-13 09:34:01 +0800

    Create er-cha-shu-de-jing-xiang-lcof.cs

---
+++ LeetCode/LeetCode/er-cha-shu-de-jing-xiang-lcof.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LeetCode
+{
+	public class er_cha_shu_de_jing_xiang_lcof
+	{
+        public TreeNode MirrorTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+
+            var temp = root.left;
+            root.left = MirrorTree(root.right);
+            root.right = MirrorTree(temp);
+            return root;
+        }
+    }
+}
+
