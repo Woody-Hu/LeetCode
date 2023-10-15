b72dd95675494d4d822102d64a960859afc4bb65
Stephen Hu <stephenhu@seismic.com>
2023-10-16 07:47:52 +0800

    Create divisible-and-non-divisible-sums-difference.cs

---
+++ LeetCode/LeetCode/divisible-and-non-divisible-sums-difference.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeetCode
+{
+	public class divisible_and_non_divisible_sums_difference
+	{
+        public int DifferenceOfSums(int n, int m)
+        {
+            return n * (n + 1) / 2 - n / m * (n / m + 1) * m;
+        }
+    }
+}
+
