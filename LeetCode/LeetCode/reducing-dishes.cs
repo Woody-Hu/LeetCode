eb1fae5b1aa50c19828ef6395265669ddd8cf43a
Stephen Hu <stephenhu@seismic.com>
2023-10-24 09:05:20 +0800

    Create reducing-dishes.cs

---
+++ LeetCode/LeetCode/reducing-dishes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeetCode
+{
+	public class reducing_dishes
+	{
+        public int MaxSatisfaction(int[] satisfaction)
+        {
+            Array.Sort(satisfaction, (a, b) => b - a);
+            var pre = 0;
+            var res = 0;
+            foreach (var item in satisfaction)
+            {
+                if (pre + item > 0)
+                {
+                    pre += item;
+                    res += pre;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
