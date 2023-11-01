77f5a3fa8323b07febff62e597d0ea4afb3e2683
Stephen Hu <stephenhu@seismic.com>
2023-11-01 09:49:45 +0800

    Create subarrays-distinct-element-sum-of-squares-i.cs

---
+++ LeetCode/LeetCode/subarrays-distinct-element-sum-of-squares-i.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class subarrays_distinct_element_sum_of_squares_i
+	{
+        public int SumCounts(IList<int> nums)
+        {
+            int mod = 1000000007;
+            long sum = 0l;
+            var set = new HashSet<int>();
+            for (int i = 0; i < nums.Count; i++)
+            {
+                set.Clear();
+                set.Add(nums[i]);
+                for (int j = i; j < nums.Count; j++)
+                {
+                    set.Add(nums[j]);
+                    sum += set.Count * set.Count;
+                    sum %= mod;
+                }
+            }
+
+            return (int)sum;
+        }
+    }
+}
+
