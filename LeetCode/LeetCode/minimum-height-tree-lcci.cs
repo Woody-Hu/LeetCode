eb4fba1aeafd6ac6dc9ad0785b7d19b854fb79eb
Stephen Hu <stephenhu@seismic.com>
2023-11-02 09:05:08 +0800

    Create minimum-height-tree-lcci.cs

---
+++ LeetCode/LeetCode/minimum-height-tree-lcci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+	public class minimum_height_tree_lcci
+	{
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            return SortedArrayToBSTInternal(nums, 0, nums.Length - 1);
+        }
+
+        private TreeNode SortedArrayToBSTInternal(int[] nums, int left, int right)
+        {
+            if (left > right)
+            {
+                return null;
+            }
+
+            var mid = (left + right) / 2;
+
+            var root = new TreeNode(nums[mid]);
+            root.left = SortedArrayToBSTInternal(nums, left, mid - 1);
+            root.right = SortedArrayToBSTInternal(nums, mid + 1, right);
+            return root;
+        }
+    }
+}
+
