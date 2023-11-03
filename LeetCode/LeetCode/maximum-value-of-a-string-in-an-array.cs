8c4ad85498f16c33f6e204d1123f2ddf2fcfce2a
Stephen Hu <stephenhu@seismic.com>
2023-11-03 09:23:08 +0800

    Create maximum-value-of-a-string-in-an-array.cs

---
+++ LeetCode/LeetCode/maximum-value-of-a-string-in-an-array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+	public class maximum_value_of_a_string_in_an_array
+	{
+        public int MaximumValue(string[] strs)
+        {
+            var res = 0;
+            foreach (var item in strs)
+            {
+                var isDigits = true;
+                var n = item.Length;
+                for (int i = 0; i < n; i++)
+                {
+                    isDigits = isDigits && Char.IsDigit(item[i]);
+                }
+
+                res = Math.Max(res, isDigits ? int.Parse(item) : n);
+            }
+
+            return res;
+        }
+    }
+}
+
