55a6c44c49e75cbf2a12b549ac6ff3b2ccde409f
Stephen Hu <stephenhu@seismic.com>
2023-11-06 08:55:29 +0800

    Create furthest-point-from-origin.cs

---
+++ LeetCode/LeetCode/furthest-point-from-origin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace LeetCode
+{
+	public class furthest_point_from_origin
+	{
+        public int FurthestDistanceFromOrigin(string moves)
+        {
+            var lc = 0;
+            var lr = 0;
+            var dic = new int[3];
+            var res = 0;
+            foreach (var item in moves)
+            {
+                if (item == 'L')
+                {
+                    dic[0]++;
+                }
+                else if (item == 'R')
+                {
+                    dic[1]++;
+                }
+                else
+                {
+                    dic[2]++;
+                }
+            }
+
+            if (dic[0] >= dic[1])
+            {
+                dic[0] += dic[2];
+                res = dic[0] - dic[1];
+            }
+            else
+            {
+                dic[1] += dic[2];
+                res = dic[1] - dic[0];
+            }
+
+            return res;
+        }
+    }
+}
+
