1b21e0d0c4b43cb9267f296e214f0f340c371f56
Stephen Hu <stephenhu@seismic.com>
2023-11-07 17:46:22 +0800

    Create w3tCBm.cs

---
+++ LeetCode/LeetCode/w3tCBm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LeetCode
+{
+	public class w3tCBm
+	{
+        public int[] CountBits(int n)
+        {
+            var res = new int[n + 1];
+            var highBit = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if ((i & (i - 1)) == 0)
+                {
+                    highBit = i;
+                }
+
+                res[i] = res[i - highBit] + 1;
+            }
+
+            return res;
+        }
+    }
+}
+
