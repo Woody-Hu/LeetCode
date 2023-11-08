a2f2fb31251ee786ba809fa57dc95cd4319a1945
Stephen Hu <stephenhu@seismic.com>
2023-11-08 10:48:51 +0800

    Create neither-minimum-nor-maximum.cs

---
+++ LeetCode/LeetCode/neither-minimum-nor-maximum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+	public class neither_minimum_nor_maximum
+	{
+        public int FindNonMinOrMax(int[] nums)
+        {
+            if (nums.Length < 3)
+            {
+                return -1;
+            }
+
+            Array.Sort(nums, 0, 3);
+            return nums[1];
+        }
+    }
+}
+
