2d014d57c46f790f52ac63a4d0b3b90416e80db4
Stephen Hu <stephenhu@seismic.com>
2023-11-10 09:13:29 +0800

    Create minimum-processing-time.cs

---
+++ LeetCode/LeetCode/minimum-processing-time.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+	public class minimum_processing_time
+	{
+        public int MinProcessingTime(IList<int> processorTime, IList<int> tasks)
+        {
+            var processorTimeList = processorTime.ToList();
+            var tasksList = tasks.ToList();
+            tasksList.Reverse();
+            processorTimeList.Sort();
+            tasksList.Sort();
+            var result = 0;
+            for (var i = 0; i < tasksList.Count; i++)
+            {
+                result = Math.Max(result, processorTimeList[i] + tasksList[i * 4]);
+            }
+
+            return result;
+        }
+    }
+}
+
