87a1f4c0de65f79048f6ad5a5f32e033d4d0be74
Stephen Hu <stephenhu@seismic.com>
2023-11-10 09:13:26 +0800

    Create kth-node-from-end-of-list-lcci.cs

---
+++ LeetCode/LeetCode/kth-node-from-end-of-list-lcci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+	public class kth_node_from_end_of_list_lcci
+	{
+        public int KthToLast(ListNode head, int k)
+        {
+            var pre = head;
+            var cur = head;
+            for (int i = 0; i < k; i++)
+            {
+                cur = cur.next;
+            }
+
+            while (cur != null)
+            {
+                cur = cur.next;
+                pre = pre.next;
+            }
+
+            return pre.val;
+        }
+    }
+}
+
