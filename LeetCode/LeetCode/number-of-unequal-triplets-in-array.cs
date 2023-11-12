e2d9d557154bf3ad721a987c604b6b48302934ea
Stephen Hu <stephenhu@seismic.com>
2023-11-13 07:13:53 +0800

    Create number-of-unequal-triplets-in-array.cs

---
+++ LeetCode/LeetCode/number-of-unequal-triplets-in-array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+	public class number_of_unequal_triplets_in_array
+	{
+        public int UnequalTriplets(int[] nums)
+        {
+            Array.Sort(nums);
+            var res = 0;
+            var n = nums.Length;
+            for (int i = 0, j = 0; i < n; i=j)
+            {
+                while (j < n && nums[i] == nums[j])
+                {
+                    j++;
+                }
+
+                res += i * (j - i) * (n - j);
+            }
+
+            return res;
+
+        }
+    }
+}
+
