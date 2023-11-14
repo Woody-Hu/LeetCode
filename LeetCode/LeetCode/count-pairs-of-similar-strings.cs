4b9a622145659910b4ce9f5d91a6b3d195650e2d
Stephen Hu <stephenhu@seismic.com>
2023-11-14 08:32:55 +0800

    Create count-pairs-of-similar-strings.cs

---
+++ LeetCode/LeetCode/count-pairs-of-similar-strings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace LeetCode
+{
+	public class count_pairs_of_similar_strings
+	{
+        public int SimilarPairs(string[] words)
+        {
+            var len = words.Length;
+            var masks = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                foreach (var item in words[i])
+                {
+                    masks[i] = masks[i] | (1 << (item - 'a'));
+                }
+            }
+
+            var res = 0;
+            for (int i = 0; i < len - 1; i++)
+            {
+                for (int j = i + 1; j < len; j++)
+                {
+                    if (masks[i] == masks[j])
+                    {
+                        res++;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
