2fbf4d6d456d777c6a89f7cbb26352e99ecbd69c
Stephen Hu <stephenhu@seismic.com>
2023-11-17 08:59:17 +0800

    Create da-yin-cong-1dao-zui-da-de-nwei-shu-lcof.cs

---
+++ LeetCode/LeetCode/da-yin-cong-1dao-zui-da-de-nwei-shu-lcof.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LeetCode
+{
+	public class da_yin_cong_1dao_zui_da_de_nwei_shu_lcof
+	{
+        public int[] CountNumbers(int cnt)
+        {
+            var end = (int)Math.Pow(10, cnt) - 1;
+            var res = new int[end];
+            for (int i = 0; i < end; i++)
+            {
+                res[i] = i + 1;
+            }
+
+            return res;
+        }
+    }
+}
+
