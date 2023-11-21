5f069dc84867f7dc68c1d8ad126b73383acec911
Stephen Hu <stephenhu@seismic.com>
2023-11-21 09:29:11 +0800

    Create find-the-width-of-columns-of-a-grid.cs

---
+++ LeetCode/LeetCode/find-the-width-of-columns-of-a-grid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+	public class find_the_width_of_columns_of_a_grid
+	{
+        public int[] FindColumnWidth(int[][] grid)
+        {
+            var m = grid.Length;
+            var n = grid[0].Length;
+            var res = new int[m];
+            var max = 0;
+            for (int i = 0; i < m; i++)
+            {
+                max = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    max = Math.Max(grid[i][j].ToString().Length, max);
+                }
+
+                res[i] = max;
+            }
+
+            return res;
+        }
+    }
+}
+
