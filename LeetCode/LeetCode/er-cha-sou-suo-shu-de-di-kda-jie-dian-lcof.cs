e3525322cbabf2b1dcb9728549b88fbb013a7ba4
Stephen Hu <stephenhu@seismic.com>
2023-11-22 09:35:35 +0800

    Create er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof.cs

---
+++ LeetCode/LeetCode/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeetCode
+{
+	public class er_cha_sou_suo_shu_de_di_kda_jie_dian_lcof
+	{
+        public int FindTargetNode(TreeNode root, int cnt)
+        {
+            var res = 0;
+            FindTargetNode(root, ref cnt, ref res);
+            return res;
+        }
+
+        private void FindTargetNode(TreeNode root, ref int cnt, ref int res)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            FindTargetNode(root.right, ref cnt, ref res);
+            if (cnt == 0)
+            {
+                return;
+            }
+
+            if (--cnt == 0)
+            {
+                res = root.val;
+            }
+
+            FindTargetNode(root.left, ref cnt, ref res);
+        }
+    }
+}
+
