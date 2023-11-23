999f68182740827540dff67593fbf41aa9334e01
Stephen Hu <stephenhu@seismic.com>
2023-11-23 16:01:23 +0800

    Create find-the-distinct-difference-array.cs

---
+++ LeetCode/LeetCode/find-the-distinct-difference-array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class find_the_distinct_difference_array
+	{
+        public int[] DistinctDifferenceArray(int[] nums)
+        {
+            var n = nums.Length;
+            var suf = new int[n + 1];
+            var set = new HashSet<int>();
+            for (int i = n - 1; i > 0; i--)
+            {
+                set.Add(nums[i]);
+                suf[i] = set.Count;
+            }
+
+            set.Clear();
+            var res = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                set.Add(nums[i]);
+                res[i] = set.Count - suf[i + 1];
+            }
+
+            return res;
+        }
+    }
+}
+
