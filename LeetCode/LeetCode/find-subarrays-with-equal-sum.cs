2f5fa04ed324b0b58fee96adc6edc01df6c1b5c3
Stephen Hu <stephenhu@seismic.com>
2023-11-24 09:32:10 +0800

    Create find-subarrays-with-equal-sum.cs

---
+++ LeetCode/LeetCode/find-subarrays-with-equal-sum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class find_subarrays_with_equal_sum
+	{
+        public bool FindSubarrays(int[] nums)
+        {
+            var set = new HashSet<int>();
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                set.Add(nums[i] + nums[i + 1]);
+            }
+
+            return set.Count < nums.Length - 1;
+        }
+    }
+}
+
