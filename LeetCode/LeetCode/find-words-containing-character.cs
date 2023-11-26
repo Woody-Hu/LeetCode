3d9b5ea1ab52c8693d43f90d81fcb9782080eead
Stephen Hu <stephenhu@seismic.com>
2023-11-27 07:34:35 +0800

    Create find-words-containing-character.cs

---
+++ LeetCode/LeetCode/find-words-containing-character.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class find_words_containing_character
+	{
+        public IList<int> FindWordsContaining(string[] words, char x)
+        {
+            var res = new List<int>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Contains(x))
+                {
+                    res.Add(i);
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
