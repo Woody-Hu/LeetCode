389607ac46071108fae55d5c262df8f3f1b7e48d
Stephen Hu <stephenhu@seismic.com>
2023-11-28 09:28:02 +0800

    Create 2AoeFn.cs

---
+++ LeetCode/LeetCode/2AoeFn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+	public class _AoeFn
+	{
+        public int UniquePaths(int m, int n)
+        {
+            var res = 1l;
+            for (int x = n, y = 1; y < m; x++, y++)
+            {
+                res = res * x / y;
+            }
+
+            return (int)res;
+        }
+    }
+}
+
