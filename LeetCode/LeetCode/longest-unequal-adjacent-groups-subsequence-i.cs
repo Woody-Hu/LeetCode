d74cc342d90d7597615e9db86947cc8a98019264
Stephen Hu <stephenhu@seismic.com>
2023-11-30 09:07:32 +0800

    Create longest-unequal-adjacent-groups-subsequence-i.cs

---
+++ LeetCode/LeetCode/longest-unequal-adjacent-groups-subsequence-i.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class longest_unequal_adjacent_groups_subsequence_i
+	{
+        public IList<string> GetWordsInLongestSubsequence(int n, string[] words, int[] groups)
+        {
+            var res = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                if (i == n - 1 || groups[i] != groups[i + 1])
+                {
+                    res.Add(words[i]);
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
