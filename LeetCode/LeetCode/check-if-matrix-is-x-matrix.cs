f164c6f34e0d26861fe64d7b1c53335fc3a90f0c
Stephen Hu <stephenhu@seismic.com>
2023-11-30 09:07:30 +0800

    Create check-if-matrix-is-x-matrix.cs

---
+++ LeetCode/LeetCode/check-if-matrix-is-x-matrix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCode
+{
+	public class check_if_matrix_is_x_matrix
+	{
+        public bool CheckXMatrix(int[][] grid)
+        {
+            var n = grid.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j || (i + j) == (n - 1))
+                    {
+                        if (grid[i][j] == 0)
+                        {
+                            return false;
+                        }
+                    }
+                    else if (grid[i][j] != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
