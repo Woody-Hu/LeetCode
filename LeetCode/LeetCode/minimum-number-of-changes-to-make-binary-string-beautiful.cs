2c79703fe3947a71e89ff50fe9a174311b3a02d6
Stephen Hu <stephenhu@seismic.com>
2023-12-04 09:17:50 +0800

    Create minimum-number-of-changes-to-make-binary-string-beautiful.cs

---
+++ LeetCode/LeetCode/minimum-number-of-changes-to-make-binary-string-beautiful.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+	public class minimum_number_of_changes_to_make_binary_string_beautiful
+	{
+        public int MinChanges(string s)
+        {
+            var res = 0;
+            for (int i = 0; i < s.Length; i = i + 2)
+            {
+                if (s[i] != s[i + 1])
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
