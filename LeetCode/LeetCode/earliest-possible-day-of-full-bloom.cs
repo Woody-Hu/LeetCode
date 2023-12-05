4f63a9cc8a14956ca7507be40b0e2e21039f3370
Stephen Hu <stephenhu@seismic.com>
2023-12-05 08:57:45 +0800

    Create earliest-possible-day-of-full-bloom.cs

---
+++ LeetCode/LeetCode/earliest-possible-day-of-full-bloom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeetCode
+{
+	public class earliest_possible_day_of_full_bloom
+	{
+        public int EarliestFullBloom(int[] plantTime, int[] growTime)
+        {
+            var n = plantTime.Length;
+            var dic = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                dic[i] = i;
+            }
+
+            Array.Sort(dic, (i, j) => growTime[j] - growTime[i]);
+            var res = 0;
+            var sum = 0;
+            foreach (var item in dic)
+            {
+                sum += plantTime[item];
+                res = Math.Max(res, sum + growTime[item]);
+            }
+
+            return res;
+        }
+    }
+}
+
