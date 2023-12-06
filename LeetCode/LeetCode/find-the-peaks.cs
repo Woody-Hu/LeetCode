70d924592ecb87056bd73a597f7eba48fe2adbcd
Stephen Hu <stephenhu@seismic.com>
2023-12-06 09:09:07 +0800

    Create find-the-peaks.cs

---
+++ LeetCode/LeetCode/find-the-peaks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class find_the_peaks
+	{
+        public IList<int> FindPeaks(int[] mountain)
+        {
+            var res = new List<int>();
+            for (int i = 2; i < mountain.Length - 1; i++)
+            {
+                if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1])
+                {
+                    res.Add(i);
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
