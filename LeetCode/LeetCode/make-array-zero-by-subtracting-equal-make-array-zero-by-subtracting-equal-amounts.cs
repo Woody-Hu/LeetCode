a095055ef9db23e24fc8b3ec5a4dc07f47320b7b
Stephen Hu <stephenhu@seismic.com>
2023-12-08 06:05:15 +0800

    Create make-array-zero-by-subtracting-equal-make-array-zero-by-subtracting-equal-amounts.cs

---
+++ LeetCode/LeetCode/make-array-zero-by-subtracting-equal-make-array-zero-by-subtracting-equal-amounts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class make_array_zero_by_subtracting_equal_make_array_zero_by_subtracting_equal_amounts
+	{
+        public int MinimumOperations(int[] nums)
+        {
+            var set = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                if (item > 0)
+                {
+                    set.Add(item);
+                }
+            }
+
+            return set.Count;
+        }
+    }
+}
+
