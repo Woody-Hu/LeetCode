e10c0698ca1a7cfee3a0bc918606ffc2862e51e8
Stephen Hu <stephenhu@seismic.com>
2023-12-07 09:02:51 +0800

    Create check-distances-between-same-letters.cs

---
+++ LeetCode/LeetCode/check-distances-between-same-letters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LeetCode
+{
+	public class check_distances_between_same_letters
+	{
+        public bool CheckDistances(string s, int[] distance)
+        {
+            var firstIndex = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                var index = s[i] - 'a';
+                if (firstIndex[index] != 0 && i - firstIndex[index] != distance[index])
+                {
+                    return false;
+                }
+
+                firstIndex[index] = i + 1;
+            }
+
+            return true;
+        }
+    }
+}
+
