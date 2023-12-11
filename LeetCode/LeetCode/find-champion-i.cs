b7f6f6e649e56a7079fb64a288e8bf6b9b095a9d
Stephen Hu <stephenhu@seismic.com>
2023-12-11 09:35:07 +0800

    Create find-champion-i.cs

---
+++ LeetCode/LeetCode/find-champion-i.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LeetCode
+{
+	public class find_champion_i
+	{
+        public int FindChampion(int[][] grid)
+        {
+            var n = grid.Length;
+            for (int j = 0; ; j++)
+            {
+                var res = true;
+                for (int i = 0; i < n; i++)
+                {
+                    if (grid[i][j] != 0)
+                    {
+                        res = false;
+                        break;
+                    }
+                }
+
+                if (res)
+                {
+                    return j;
+                }
+            }
+        }
+    }
+}
+
