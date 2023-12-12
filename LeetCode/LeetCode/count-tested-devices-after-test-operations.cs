f9ddae3b165b8b4cf9fb2c2baefb8e2d23127cd2
Stephen Hu <stephenhu@seismic.com>
2023-12-12 09:22:26 +0800

    Create count-tested-devices-after-test-operations.cs

---
+++ LeetCode/LeetCode/count-tested-devices-after-test-operations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+	public class count_tested_devices_after_test_operations
+	{
+        public int CountTestedDevices(int[] batteryPercentages)
+        {
+            var res = 0;
+            foreach (var item in batteryPercentages)
+            {
+                if (item - res > 0)
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
