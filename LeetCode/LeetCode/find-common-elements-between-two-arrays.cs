de201d3dab9639848e54aeb4bc48b3ba6e1ff3fd
Stephen Hu <stephenhu@seismic.com>
2023-12-13 09:06:12 +0800

    Create find-common-elements-between-two-arrays.cs

---
+++ LeetCode/LeetCode/find-common-elements-between-two-arrays.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class find_common_elements_between_two_arrays
+	{
+        public int[] FindIntersectionValues(int[] nums1, int[] nums2)
+        {
+            var set1 = new HashSet<int>();
+            var set2 = new HashSet<int>();
+            foreach (var item in nums1)
+            {
+                set1.Add(item);
+            }
+
+            foreach (var item in nums2)
+            {
+                set2.Add(item);
+            }
+
+            var res = new int[2];
+            foreach (var item in nums1)
+            {
+                if (set2.Contains(item))
+                {
+                    res[0]++;
+                }
+            }
+
+            foreach (var item in nums2)
+            {
+                if (set1.Contains(item))
+                {
+                    res[1]++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
