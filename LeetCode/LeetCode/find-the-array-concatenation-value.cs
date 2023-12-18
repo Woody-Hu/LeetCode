8d5b59a661949a3d02ef15672e07ac9a33a64298
Stephen Hu <stephenhu@seismic.com>
2023-12-18 09:23:56 +0800

    Create find-the-array-concatenation-value.cs

---
+++ LeetCode/LeetCode/find-the-array-concatenation-value.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode
+{
+	public class find_the_array_concatenation_value
+	{
+        public long FindTheArrayConcVal(int[] nums)
+        {
+            var res = 0l;
+            var i = 0;
+            var j = nums.Length - 1;
+            for (; i <= j; i++, j--)
+            {
+                if (i != j)
+                {
+                    res += int.Parse(nums[i].ToString() + nums[j].ToString());
+                }
+                else
+                {
+                    res += nums[i];
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
