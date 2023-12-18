e3f5771df8d251daaf03b88dd6202ffe1edde368
Stephen Hu <stephenhu@seismic.com>
2023-12-18 09:23:54 +0800

    Create er-jin-zhi-zhong-1de-ge-shu-lcof.cs

---
+++ LeetCode/LeetCode/er-jin-zhi-zhong-1de-ge-shu-lcof.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+    public class er_jin_zhi_zhong_1de_ge_shu_lcof
+    {
+        public int HammingWeight(uint n)
+        {
+            var res = 0;
+            while (n != 0)
+            {
+                n = n & (n - 1);
+                res++;
+            }
+
+            return res;
+        }
+    }
+}
