a69fbb5423e1ca8d79f3d0a5487cb5c2979cb48a
Stephen Hu <stephenhu@seismic.com>
2023-12-19 09:16:58 +0800

    Create find-missing-and-repeated-values.cs

---
+++ LeetCode/LeetCode/find-missing-and-repeated-values.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeetCode
+{
+	public class find_missing_and_repeated_values
+	{
+        public int[] FindMissingAndRepeatedValues(int[][] grid)
+        {
+            var n = grid.Length;
+            var dic = new int[n * n + 1];
+            foreach (var item in grid)
+            {
+                foreach (var x in item)
+                {
+                    dic[x]++;
+                }
+            }
+
+            var res = new int[2];
+            for (int i = 1; i <= n * n; i++)
+            {
+                if (dic[i] == 2)
+                {
+                    res[0] = i;
+                }
+                else if (dic[i] == 0)
+                {
+                    res[1] = i;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
