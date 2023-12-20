19c5a77c3d537c06b2e4dc6a99cc4f83a38f3bce
Stephen Hu <stephenhu@seismic.com>
2023-12-20 09:45:00 +0800

    Create sfvd7V.cs

---
+++ LeetCode/LeetCode/sfvd7V.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class sfvd7V
+	{
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dic = new Dictionary<string, List<string>>();
+            foreach (var item in strs)
+            {
+                var array = item.ToCharArray();
+                Array.Sort(array);
+                var temp = new string(array);
+                if (!dic.ContainsKey(temp))
+                {
+                    dic.Add(temp, new List<string>());
+                }
+
+                dic[temp].Add(item);
+            }
+
+            var res = new List<IList<string>>();
+            foreach (var item in dic)
+            {
+                res.Add(item.Value);
+            }
+
+            return res;
+        }
+    }
+}
+
