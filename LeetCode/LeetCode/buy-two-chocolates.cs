d921e5d48e41710c4ebcd4cc98377a72e67f0829
Stephen Hu <stephenhu@seismic.com>
2023-12-21 09:41:29 +0800

    Create buy-two-chocolates.cs

---
+++ LeetCode/LeetCode/buy-two-chocolates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+	public class buy_two_chocolates
+	{
+        public int BuyChoco(int[] prices, int money)
+        {
+            Array.Sort(prices);
+            if (prices[0] + prices[1] > money)
+            {
+                return money;
+            }
+
+            return money - (prices[0] + prices[1]);
+        }
+    }
+}
+
