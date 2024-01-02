eb336b8635f3122c6f0d315aa6f59f3c0e77e58a
Stephen Hu <stephenhu@seismic.com>
2024-01-02 09:10:35 +0800

    Create minimum-number-game.cs

---
+++ LeetCode/LeetCode/minimum-number-game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LeetCode
+{
+	public class minimum_number_game
+	{
+        public int[] NumberGame(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length; i+=2)
+            {
+                var temp = nums[i - 1];
+                nums[i - 1] = nums[i];
+                nums[i] = temp;
+            }
+
+            return nums;
+        }
+    }
+}
+
