dd4bc4843258edaf37c1f5023a189a2463144a22
Stephen Hu <stephenhu@seismic.com>
2024-01-05 10:13:56 +0800

    Create check-if-bitwise-or-has-trailing-zeros.cs

---
+++ LeetCode/LeetCode/check-if-bitwise-or-has-trailing-zeros.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+	public class check_if_bitwise_or_has_trailing_zeros
+	{
+        public bool HasTrailingZeros(int[] nums)
+        {
+            var even = nums.Length;
+            foreach (var item in nums)
+            {
+                even -= item % 2;
+            }
+
+            return even >= 2;
+        }
+    }
+}
+
