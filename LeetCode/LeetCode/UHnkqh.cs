a5ff41106a5f4418a54e8bdeb56facf157a88d1a
Stephen Hu <stephenhu@seismic.com>
2024-01-12 09:37:06 +0800

    Create UHnkqh.cs

---
+++ LeetCode/LeetCode/UHnkqh.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode
+{
+	public class UHnkqh
+	{
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode pre = null;
+            var curr = head;
+            while (curr != null)
+            {
+                var next = curr.next;
+                curr.next = pre;
+                pre = curr;
+                curr = next;
+            }
+
+            return pre;
+        }
+    }
+}
+
