c2dedd7776a6742d45d523d9cdee0ef56a266900
Stephen Hu <stephenhu@seismic.com>
2024-01-12 09:37:04 +0800

    Create minimum-string-length-after-removing-substrings.cs

---
+++ LeetCode/LeetCode/minimum-string-length-after-removing-substrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class minimum_string_length_after_removing_substrings
+	{
+        public int MinLength(string s)
+        {
+            var list = new List<char>();
+            foreach (var item in s)
+            {
+                list.Add(item);
+                var n = list.Count;
+                if (n >= 2 && (list[n - 2] == 'A' && list[n - 1] == 'B' ||
+                list[n - 2] == 'C' && list[n - 1] == 'D'))
+                {
+                    list.RemoveAt(n - 1);
+                    list.RemoveAt(n - 2);
+                }
+            }
+
+            return list.Count;
+        }
+    }
+}
+
