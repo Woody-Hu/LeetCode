3429b41a3c6d5eadebd91b7e506ff5e0193d695f
Stephen Hu <stephenhu@seismic.com>
2024-01-15 09:00:26 +0800

    Create count-elements-with-maximum-frequency.cs

---
+++ LeetCode/LeetCode/count-elements-with-maximum-frequency.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class count_elements_with_maximum_frequency
+	{
+        public int MaxFrequencyElements(int[] nums)
+        {
+            var max = 0;
+            var res = 0;
+            var dic = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (!dic.ContainsKey(item))
+                {
+                    dic[item] = 0;
+                }
+
+                dic[item]++;
+                max = Math.Max(max, dic[item]);
+            }
+
+            foreach (var item in nums)
+            {
+                if (dic[item] == max)
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
