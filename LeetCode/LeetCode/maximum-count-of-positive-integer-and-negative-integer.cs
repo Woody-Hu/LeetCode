db41fec5a868bf8d13ffa0da8f063f49ebfe01ac
Stephen Hu <stephenhu@seismic.com>
2024-01-17 09:15:31 +0800

    Create maximum-count-of-positive-integer-and-negative-integer.cs

---
+++ LeetCode/LeetCode/maximum-count-of-positive-integer-and-negative-integer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+	public class maximum_count_of_positive_integer_and_negative_integer
+	{
+        public int MaximumCount(int[] nums)
+        {
+            var count1 = 0;
+            var count2 = 0;
+            foreach (var item in nums)
+            {
+                if (item > 0)
+                {
+                    count1++;
+                }
+                else if (item < 0)
+                {
+                    count2++;
+                }
+            }
+
+            return Math.Max(count1, count2);
+        }
+    }
+}
+
