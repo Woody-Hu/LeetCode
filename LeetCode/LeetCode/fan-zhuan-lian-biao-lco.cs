cef382d697eac06b605076e6943f6b9b25eebbd2
Stephen Hu <stephenhu@seismic.com>
2024-01-19 04:15:38 +0800

    Create fan-zhuan-lian-biao-lco.cs

---
+++ LeetCode/LeetCode/fan-zhuan-lian-biao-lco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode
+{
+	public class fan_zhuan_lian_biao_lco
+	{
+        public ListNode TrainningPlan(ListNode head)
+        {
+            var cur = head;
+            ListNode pre = null;
+            while (cur != null)
+            {
+                var temp = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = temp;
+            }
+
+            return pre;
+        }
+    }
+}
+
