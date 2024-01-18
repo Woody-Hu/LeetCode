6239a31effd2ec79302f97cb122849caaa622e25
Stephen Hu <stephenhu@seismic.com>
2024-01-19 04:15:35 +0800

    Create distribute-candies-among-children-i.cs

---
+++ LeetCode/LeetCode/distribute-candies-among-children-i.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LeetCode
+{
+	public class distribute_candies_among_children_i
+	{
+        public int DistributeCandies(int n, int limit)
+        {
+            return C(n + 2) - 3 * C(n - limit + 1) + 3 * C(n - 2 * limit) - C(n - 3 * limit - 1);
+        }
+
+        private int C(int n)
+        {
+            return n > 1 ? n * (n - 1) / 2 : 0;
+        }
+    }
+}
+
