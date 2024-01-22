0d69fbc814c208f3302abd72103c2e9aa4165493
Stephen Hu <stephenhu@seismic.com>
2024-01-22 10:26:21 +0800

    Create split-strings-by-separator.cs

---
+++ LeetCode/LeetCode/split-strings-by-separator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class split_strings_by_separator
+	{
+        public IList<string> SplitWordsBySeparator(IList<string> words, char separator)
+        {
+            var res = new List<string>(words.Count);
+            foreach (var item in words)
+            {
+                var splited = item.Split(separator);
+                foreach (var subStr in splited)
+                {
+                    if (!string.IsNullOrWhiteSpace(subStr))
+                    {
+                        res.Add(subStr);
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
