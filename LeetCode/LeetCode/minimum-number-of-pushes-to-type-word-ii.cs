4a2a6d944b444f0c239c223ee36221d4df58dbb5
Stephen Hu <stephenhu@seismic.com>
2024-01-23 08:27:59 +0800

    Create minimum-number-of-pushes-to-type-word-ii.cs

---
+++ LeetCode/LeetCode/minimum-number-of-pushes-to-type-word-ii.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LeetCode
+{
+	public class minimum_number_of_pushes_to_type_word_ii
+	{
+        public int MinimumPushes(string word)
+        {
+            var cnt = new int[26];
+            foreach (var item in word.ToCharArray())
+            {
+                cnt[item - 'a']++;
+            }
+
+            Array.Sort(cnt);
+
+            var res = 0;
+            for (int i = 0; i < 26; i++)
+            {
+                res += cnt[25 - i] * (i / 8 + 1);
+            }
+
+            return res;
+        }
+    }
+}
+
