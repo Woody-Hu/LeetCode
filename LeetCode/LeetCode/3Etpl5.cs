b86dc1dd011772cd74a509c5cbfa8e76a854c6aa
Stephen Hu <stephenhu@seismic.com>
2024-01-24 09:48:47 +0800

    Create 3Etpl5.cs

---
+++ LeetCode/LeetCode/3Etpl5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LeetCode
+{
+	public class _Etpl5
+	{
+        public int SumNumbers(TreeNode root)
+        {
+            return DFS(root, 0);
+        }
+
+        private int DFS(TreeNode node, int preSum)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            var sum = preSum * 10 + node.val;
+            if (node.left == null && node.right == null)
+            {
+                return sum;
+            }
+            else
+            {
+                return DFS(node.left, sum) + DFS(node.right, sum);
+            }
+        }
+    }
+}
+
