a950bd48c07771892595f460d4c04ac9df1f0196
Stephen Hu <stephenhu@seismic.com>
2024-01-25 09:04:17 +0800

    Create cong-wei-dao-tou-da-yin-lian-biao-lcof.cs

---
+++ LeetCode/LeetCode/cong-wei-dao-tou-da-yin-lian-biao-lcof.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class cong_wei_dao_tou_da_yin_lian_biao_lcof
+	{
+        public int[] ReverseBookList(ListNode head)
+        {
+            var list = new List<int>();
+            while (head != null)
+            {
+                list.Add(head.val);
+                head = head.next;
+            }
+
+            list.Reverse();
+            return list.ToArray();
+        }
+    }
+}
+
