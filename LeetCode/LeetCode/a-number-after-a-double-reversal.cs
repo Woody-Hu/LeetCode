a738373d6dc549ebe14cf6509232eb4b4a5456da
Stephen Hu <stephenhu@seismic.com>
2024-01-26 09:45:29 +0800

    Create a-number-after-a-double-reversal.cs

---
+++ LeetCode/LeetCode/a-number-after-a-double-reversal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeetCode
+{
+	public class a_number_after_a_double_reversal
+	{
+        public bool IsSameAfterReversals(int num)
+        {
+            return num == 0 || num % 10 != 0;
+        }
+    }
+}
+
