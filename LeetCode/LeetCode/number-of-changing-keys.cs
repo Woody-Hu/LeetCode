c027eb3a0b3cc27ad1ffcd02fa86b568c05f5234
Stephen Hu <stephenhu@seismic.com>
2024-01-29 07:56:02 +0800

    Create number-of-changing-keys.cs

---
+++ LeetCode/LeetCode/number-of-changing-keys.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode
+{
+	public class number_of_changing_keys
+	{
+        public int CountKeyChanges(string s)
+        {
+            var res = 0;
+            var pre = Char.ToLower(s[0]);
+            for (int i = 1; i < s.Length; i++)
+            {
+                var cur = Char.ToLower(s[i]);
+                if (cur != pre)
+                {
+                    res++;
+                }
+
+                pre = cur;
+            }
+
+            return res;
+        }
+    }
+}
+
