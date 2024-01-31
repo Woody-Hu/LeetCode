9d2f82915fc444a6d89a5078e728c50ae440c458
Stephen Hu <stephenhu@seismic.com>
2024-02-01 06:33:41 +0800

    Create semi-ordered-permutation.cs

---
+++ LeetCode/LeetCode/semi-ordered-permutation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace LeetCode
+{
+	public class semi_ordered_permutation
+	{
+        public int SemiOrderedPermutation(int[] nums)
+        {
+            var len = nums.Length;
+            var head = -1;
+            var tail = -1;
+            for (int i = 0; i < len; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    head = i;
+                }
+                else if (nums[i] == len)
+                {
+                    tail = i;
+                }
+            }
+
+            var res = head + (len - 1 - tail);
+            if (head > tail)
+            {
+                res--;
+            }
+
+            return res;
+        }
+    }
+}
+
