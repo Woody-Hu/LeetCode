7e085087a34cb2dfd5f5e23110a1db542c92d17b
Stephen Hu <stephenhu@seismic.com>
2024-02-01 06:33:39 +0800

    Create number-of-even-and-odd-bits.cs

---
+++ LeetCode/LeetCode/number-of-even-and-odd-bits.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+	public class number_of_even_and_odd_bits
+	{
+        public int[] EvenOddBit(int n)
+        {
+            var res = new int[2];
+            var i = 0;
+            while (n != 0)
+            {
+                res[i % 2] += n % 2;
+                n /= 2;
+                i++;
+            }
+
+            return res;
+        }
+    }
+}
+
