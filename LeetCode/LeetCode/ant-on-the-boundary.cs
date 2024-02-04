859711b29af7a7b8c7875d9caca531622cc6b2ac
Stephen Hu <stephenhu@seismic.com>
2024-02-05 07:02:09 +0800

    Create ant-on-the-boundary.cs

---
+++ LeetCode/LeetCode/ant-on-the-boundary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LeetCode
+{
+	public class ant_on_the_boundary
+	{
+        public int ReturnToBoundaryCount(int[] nums)
+        {
+            var res = 0;
+            var sum = 0;
+            foreach (var item in nums)
+            {
+                sum += item;
+                if (sum == 0)
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
