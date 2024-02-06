920d8ab470a16e8ef2813c281cadcf3b423b574e
Stephen Hu <stephenhu@seismic.com>
2024-02-06 12:17:42 +0800

    Create PTXy4P.cs

---
+++ LeetCode/LeetCode/PTXy4P.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeetCode
+{
+	public class PTXy4P
+	{
+        public int GetMinimumTime(int[] time, int[][] fruits, int limit)
+        {
+            var res = 0;
+            foreach (var item in fruits)
+            {
+                res += time[item[0]] * (item[1] / limit + (item[1] % limit != 0 ? 1 : 0));
+            }
+
+            return res;
+        }
+    }
+}
+
