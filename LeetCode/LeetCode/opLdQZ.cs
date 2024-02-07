4165d7b00aa26a4b8c1601a19f82ee7890a73aef
Stephen Hu <stephenhu@seismic.com>
2024-02-07 13:53:32 +0800

    Create opLdQZ.cs

---
+++ LeetCode/LeetCode/opLdQZ.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class opLdQZ
+	{
+        public bool FindTarget(TreeNode root, int k)
+        {
+            var set = new HashSet<int>();
+            return FindTargetInternal(root, k, set);
+        }
+
+        private bool FindTargetInternal(TreeNode root, int k, HashSet<int> set)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (set.Contains(k - root.val))
+            {
+                return true;
+            }
+
+            set.Add(root.val);
+
+            return FindTargetInternal(root.left, k, set) || FindTargetInternal(root.right, k, set);
+        }
+    }
+}
+
