92a9c5084ad980a3c9d24f62aade8b7892602f72
Stephen Hu <stephenhu@seismic.com>
2024-02-19 07:05:00 +0800

    Create NYBBNL.cs

---
+++ LeetCode/LeetCode/NYBBNL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LeetCode
+{
+	public class NYBBNL
+	{
+        public TreeNode IncreasingBST(TreeNode root)
+        {
+            var pre = new TreeNode(-1);
+            var resNode = pre;
+            InOrder(root, ref resNode);
+            return pre.right;
+        }
+
+        private void InOrder(TreeNode node, ref TreeNode res)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            InOrder(node.left, ref res);
+            res.right = node;
+            node.left = null;
+            res = node;
+
+            InOrder(node.right, ref res);
+        }
+    }
+}
+
