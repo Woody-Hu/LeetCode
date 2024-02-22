51ed801c6cb89cefcb78ecd7e8367c589e53e171
Stephen Hu <stephenhu@seismic.com>
2024-02-22 09:21:05 +0800

    Create two-out-of-three.cs

---
+++ LeetCode/LeetCode/two-out-of-three.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class two_out_of_three
+	{
+        public IList<int> TwoOutOfThree(int[] nums1, int[] nums2, int[] nums3)
+        {
+            var dic = new Dictionary<int, int>();
+            foreach (var item in nums1)
+            {
+                dic.TryAdd(item, 1);
+            }
+
+            foreach (var item in nums2)
+            {
+                dic.TryAdd(item, 0);
+                dic[item] |= 2;
+            }
+
+            foreach (var item in nums3)
+            {
+                dic.TryAdd(item, 0);
+                dic[item] |= 4;
+            }
+
+            var res = new List<int>();
+            foreach (var item in dic)
+            {
+                var key = item.Key;
+                var val = item.Value;
+                if ((val & (val - 1)) != 0)
+                {
+                    res.Add(key);
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
