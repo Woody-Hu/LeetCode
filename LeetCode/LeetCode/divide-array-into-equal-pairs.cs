a3e58a4582c68a5307d41b9e0d1694ad331a768f
Stephen Hu <stephenhu@seismic.com>
2024-02-22 09:21:01 +0800

    Create divide-array-into-equal-pairs.cs

---
+++ LeetCode/LeetCode/divide-array-into-equal-pairs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class divide_array_into_equal_pairs
+	{
+        public bool DivideArray(int[] nums)
+        {
+            var dic = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (!dic.ContainsKey(item))
+                {
+                    dic.Add(item, 0);
+                }
+
+                dic[item]++;
+            }
+
+            var res = true;
+            foreach (var item in dic)
+            {
+                res = res && item.Value % 2 == 0;
+                if (!res)
+                {
+                    break;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
