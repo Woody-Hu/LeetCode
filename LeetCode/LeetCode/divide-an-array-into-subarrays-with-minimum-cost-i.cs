febde173b84083f8791677d3bc51c01f0261b03a
Stephen Hu <stephenhu@seismic.com>
2024-02-23 17:29:11 +0800

    Create divide-an-array-into-subarrays-with-minimum-cost-i.cs

---
+++ LeetCode/LeetCode/divide-an-array-into-subarrays-with-minimum-cost-i.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LeetCode
+{
+	public class divide_an_array_into_subarrays_with_minimum_cost_i
+	{
+        public int MinimumCost(int[] nums)
+        {
+            var first = nums[0];
+            var subArray = new int[nums.Length - 1];
+            Array.Copy(nums, 1, subArray, 0, nums.Length - 1);
+            Array.Sort(subArray);
+            return first + subArray[0] + subArray[1];
+        }
+    }
+}
+
