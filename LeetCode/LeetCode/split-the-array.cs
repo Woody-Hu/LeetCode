08d846ea64b76953e97e3f263d021af5ab6b6bc3
Stephen Hu <stephenhu@seismic.com>
2024-02-26 09:06:32 +0800

    Create split-the-array.cs

---
+++ LeetCode/LeetCode/split-the-array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public class split_the_array
+	{
+        public bool IsPossibleToSplit(int[] nums)
+        {
+            var dic = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (!dic.ContainsKey(item))
+                {
+                    dic.Add(item, 0);
+                }
+
+                dic[item]++;
+                if (dic[item] > 2)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
