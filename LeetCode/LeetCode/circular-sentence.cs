f636c9727eac197098b119dd858da93e16c62c7d
Stephen Hu <stephenhu@seismic.com>
2024-02-28 09:18:34 +0800

    Create circular-sentence.cs

---
+++ LeetCode/LeetCode/circular-sentence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LeetCode
+{
+	public class circular_sentence
+	{
+        public bool IsCircularSentence(string sentence)
+        {
+            var array = sentence.Split();
+            var first = array[0];
+            var last = array[array.Length - 1];
+            if (first[0] != last[last.Length - 1])
+            {
+                return false;
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                var pre = array[i - 1];
+                var cur = array[i];
+                if (pre[pre.Length - 1] != cur[0])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
