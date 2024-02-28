ca38611c5fb58468d43d7b9640716ebec0fb23b7
Stephen Hu <stephenhu@seismic.com>
2024-02-28 09:18:37 +0800

    Create min-max-game.cs

---
+++ LeetCode/LeetCode/min-max-game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LeetCode
+{
+	public class min_max_game
+	{
+        public int MinMaxGame(int[] nums)
+        {
+            var n = nums.Length;
+            if (n == 1)
+            {
+                return nums[0];
+            }
+
+            var newNums = new int[n / 2];
+            for (int i = 0; i < newNums.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    newNums[i] = Math.Min(nums[2 * i], nums[2 * i + 1]);
+                }
+                else
+                {
+                    newNums[i] = Math.Max(nums[2 * i], nums[2 * i + 1]);
+                }
+            }
+
+            return MinMaxGame(newNums);
+        }
+    }
+}
+
