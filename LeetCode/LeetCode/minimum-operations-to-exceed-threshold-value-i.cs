bb0f2b9b9e5a952003b7967711fd5d8d30e72b49
Stephen Hu <stephenhu@seismic.com>
2024-03-05 14:52:59 +0800

    Create minimum-operations-to-exceed-threshold-value-i.cs

---
+++ LeetCode/LeetCode/minimum-operations-to-exceed-threshold-value-i.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCode
+{
+	public class minimum_operations_to_exceed_threshold_value_i
+	{
+        public int MinOperations(int[] nums, int k)
+        {
+            var res = 0;
+            foreach (var item in nums)
+            {
+                if (item < k)
+                {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+
