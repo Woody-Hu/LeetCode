9e346ee92f0c7ef4428b399e8d795d8c401dc266
Stephen Hu <stephenhu@seismic.com>
2025-01-08 09:11:13 +0800

    Create pOCWxh.cs

---
+++ LeetCode/LeetCode/pOCWxh.cs
@@ -0,0 +1,17 @@
+namespace LeetCode;
+
+public class pOCWxh
+{
+    public TreeNode PruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        root.left = PruneTree(root.left);
+        root.right = PruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0) {
+            return null;
+        }
+        
+        return root;
+    }
+}
