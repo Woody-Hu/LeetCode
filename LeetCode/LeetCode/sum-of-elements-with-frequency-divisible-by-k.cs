4ee3af1b451e926e2f36b70e0d46cea0b9e02ca4
Stephen Hu <812791840@qq.com>
2025-10-22 10:12:48 +0800

    Update sum-of-elements-with-frequency-divisible-by-k.cs

--- LeetCode/LeetCode/sum-of-elements-with-frequency-divisible-by-k.cs
+++ LeetCode/LeetCode/sum-of-elements-with-frequency-divisible-by-k.cs
@@ -8,5 +8,31 @@ namespace LeetCode
 {
     internal class sum_of_elements_with_frequency_divisible_by_k
     {
+        public int SumDivisibleByK(int[] nums, int k)
+        {
+            int sum = 0;
+            var frequencyMap = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                if (frequencyMap.ContainsKey(num))
+                {
+                    frequencyMap[num]++;
+                }
+                else
+                {
+                    frequencyMap[num] = 1;
+                }
+            }
+            foreach (var kvp in frequencyMap)
+            {
+                if (kvp.Value % k == 0)
+                {
+                    sum += kvp.Key * kvp.Value;
+                }
+            }
+
+            return sum;
+
+        }
     }
 }
