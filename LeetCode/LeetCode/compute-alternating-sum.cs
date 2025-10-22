2f48e57e20bf754a05046eda59f58a0dda982cf5
Stephen Hu <812791840@qq.com>
2025-10-22 10:12:46 +0800

    Update compute-alternating-sum.cs

--- LeetCode/LeetCode/compute-alternating-sum.cs
+++ LeetCode/LeetCode/compute-alternating-sum.cs
@@ -8,5 +8,23 @@ namespace LeetCode
 {
     internal class compute_alternating_sum
     {
+        public int AlternatingSum(int[] nums)
+        {
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenSum += nums[i];
+                }
+                else
+                {
+                    oddSum += nums[i];
+                }
+            }
+            return evenSum - oddSum;
+
+        }
     }
 }
