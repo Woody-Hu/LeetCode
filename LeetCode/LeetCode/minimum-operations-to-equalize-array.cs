00333ad8cbd5953cc0a5e8c7ed8c698b2404df57
Stephen Hu <812791840@qq.com>
2025-10-23 10:20:00 +0800

    Update minimum-operations-to-equalize-array.cs

--- LeetCode/LeetCode/minimum-operations-to-equalize-array.cs
+++ LeetCode/LeetCode/minimum-operations-to-equalize-array.cs
@@ -8,5 +8,18 @@ namespace LeetCode
 {
     internal class minimum_operations_to_equalize_array
     {
+        public int MinOperations(int[] nums)
+        {
+
+            int n = nums.Length;
+            for (int i = 1; i < n; i++)
+            {
+                if (nums[i] != nums[0])
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
     }
 }
