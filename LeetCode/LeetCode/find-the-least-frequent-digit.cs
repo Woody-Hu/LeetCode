6abcd3002cd8ba724e9613d68d128ff9303d9f48
Stephen Hu <812791840@qq.com>
2025-10-24 10:58:15 +0800

    Create find-the-least-frequent-digit.cs

---
+++ LeetCode/LeetCode/find-the-least-frequent-digit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class find_the_least_frequent_digit
+    {
+        public int GetLeastFrequentDigit(int n)
+        {
+            IDictionary<int, int> counts = new Dictionary<int, int>();
+            while (n > 0)
+            {
+                int digit = n % 10;
+                counts.TryAdd(digit, 0);
+                counts[digit]++;
+                n /= 10;
+            }
+            int leastFrequentDigit = -1, minCount = int.MaxValue;
+            foreach (KeyValuePair<int, int> pair in counts)
+            {
+                int digit = pair.Key, count = pair.Value;
+                if (count < minCount || (count == minCount && digit < leastFrequentDigit))
+                {
+                    leastFrequentDigit = digit;
+                    minCount = count;
+                }
+            }
+            return leastFrequentDigit;
+
+        }
+    }
+}
