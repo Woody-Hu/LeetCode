1912d93834d5c66adc1128a906d7b2628e8b6f17
Stephen Hu <812791840@qq.com>
2025-10-31 08:16:31 +0800

    Update remove-zeros-in-decimal-representation.cs

--- LeetCode/LeetCode/remove-zeros-in-decimal-representation.cs
+++ LeetCode/LeetCode/remove-zeros-in-decimal-representation.cs
@@ -8,5 +8,20 @@ namespace LeetCode
 {
     internal class remove_zeros_in_decimal_representation
     {
+        public long RemoveZeros(long n)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (n > 0)
+            {
+                long digit = n % 10;
+                if (digit != 0)
+                {
+                    sb.Insert(0, digit);
+                }
+                n /= 10;
+            }
+            return sb.Length == 0 ? 0 : long.Parse(sb.ToString());
+
+        }
     }
 }
