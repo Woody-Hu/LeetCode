dd3df45070678173676936dbd29f61942d9486e7
Stephen Hu <812791840@qq.com>
2025-10-31 08:16:29 +0800

    Create maximum-alternating-sum-of-squares.cs

---
+++ LeetCode/LeetCode/maximum-alternating-sum-of-squares.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class maximum_alternating_sum_of_squares
+    {
+        public long MaxAlternatingSum(int[] nums)
+        {
+            int n = nums.Length;
+            int[] squares = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                squares[i] = nums[i] * nums[i];
+            }
+            Array.Sort(squares);
+            long sum = 0;
+            int negativeCount = n / 2;
+            for (int i = 0; i < n; i++)
+            {
+                if (i < negativeCount)
+                {
+                    sum -= squares[i];
+                }
+                else
+                {
+                    sum += squares[i];
+                }
+            }
+            return sum;
+
+        }
+    }
+}
