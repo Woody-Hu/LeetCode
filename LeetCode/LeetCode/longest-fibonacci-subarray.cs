f5975e2e0cfd30f30df14e7364c249a78bdb2674
Stephen Hu <812791840@qq.com>
2025-11-05 10:02:46 +0800

    Update longest-fibonacci-subarray.cs

--- LeetCode/LeetCode/longest-fibonacci-subarray.cs
+++ LeetCode/LeetCode/longest-fibonacci-subarray.cs
@@ -8,5 +8,27 @@ namespace LeetCode
 {
     internal class longest_fibonacci_subarray
     {
+        public int LongestSubarray(int[] nums)
+        {
+            int n = nums.Length;
+            if (n < 3) return n;
+            int maxLength = 2;
+            int currentLength = 2;
+            for (int i = 2; i < n; i++)
+            {
+                if (nums[i] == nums[i - 1] + nums[i - 2])
+                {
+                    currentLength++;
+                    maxLength = Math.Max(maxLength, currentLength);
+                }
+                else
+                {
+                    currentLength = 2;
+                }
+            }
+            return maxLength;
+
+
+        }
     }
 }
