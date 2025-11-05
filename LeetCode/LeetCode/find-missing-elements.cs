ccd4805e8524f4e5f1572771ade00c2994da0f32
Stephen Hu <812791840@qq.com>
2025-11-05 10:02:42 +0800

    Create find-missing-elements.cs

---
+++ LeetCode/LeetCode/find-missing-elements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class find_missing_elements
+    {
+        public IList<int> FindMissingElements(int[] nums)
+        {
+            int minNum = int.MaxValue, maxNum = int.MinValue;
+            ISet<int> numsSet = new HashSet<int>();
+            foreach (int num in nums)
+            {
+                minNum = Math.Min(minNum, num);
+                maxNum = Math.Max(maxNum, num);
+                numsSet.Add(num);
+            }
+            IList<int> missing = new List<int>();
+            for (int i = minNum + 1; i < maxNum; i++)
+            {
+                if (!numsSet.Contains(i))
+                {
+                    missing.Add(i);
+                }
+            }
+            return missing;
+        }
+    }
+}
