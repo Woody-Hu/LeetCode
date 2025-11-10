148caa1caa2902977bd4ccba8f22ab57e41223c1
Stephen Hu <812791840@qq.com>
2025-11-10 08:46:07 +0800

    Create minimum-moves-to-equal-array-elements-iii.cs

---
+++ LeetCode/LeetCode/minimum-moves-to-equal-array-elements-iii.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class minimum_moves_to_equal_array_elements_iii
+    {
+        public int MinMoves(int[] nums)
+        {
+            int maxNum = nums[0];
+            foreach (int num in nums)
+            {
+                maxNum = Math.Max(maxNum, num);
+            }
+            int moves = 0;
+            foreach (int num in nums)
+            {
+                moves += maxNum - num;
+            }
+
+            return moves;
+        }
+    }
+}
