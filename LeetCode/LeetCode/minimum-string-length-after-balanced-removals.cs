0d6be3555a217825dc8ebdc8146eda2f082b2477
Stephen Hu <812791840@qq.com>
2025-11-19 16:51:16 +0800

    Update minimum-string-length-after-balanced-removals.cs

--- LeetCode/LeetCode/minimum-string-length-after-balanced-removals.cs
+++ LeetCode/LeetCode/minimum-string-length-after-balanced-removals.cs
@@ -8,5 +8,23 @@ namespace LeetCode
 {
     internal class minimum_string_length_after_balanced_removals
     {
+        public int MinLengthAfterRemovals(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (stack.Count > 0 && c != stack.Peek())
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+            return stack.Count;
+
+
+        }
     }
 }
