e15a90770c7e592ff4a4607e83200bc8a936ed00
“hu <812791840@qq.com>
2025-12-02 13:23:12 +0800

    Create maximum-substrings-with-distinct-start.cs

---
+++ LeetCode/LeetCode/maximum-substrings-with-distinct-start.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class maximum_substrings_with_distinct_start
+    {
+        public int MaxDistinct(string s)
+        {
+            bool[] vis = new bool[26];
+            int ans = 0;
+            foreach (char c in s)
+            {
+                int index = c - 'a';
+                if (!vis[index])
+                {
+                    vis[index] = true;
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
