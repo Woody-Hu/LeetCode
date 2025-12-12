ab701b2d6d203c6ad195e0d51c5814bc61d46010
“hu <812791840@qq.com>
2025-12-12 08:25:46 +0800

    Create minimum-number-of-flips-to-reverse-binary-string.cs

---
+++ LeetCode/LeetCode/minimum-number-of-flips-to-reverse-binary-string.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class minimum_number_of_flips_to_reverse_binary_string
+    {
+        public int MinimumFlips(int n)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (n > 0)
+            {
+                sb.Append(n % 2);
+                n /= 2;
+            }
+            int flips = 0;
+            int length = sb.Length;
+            for (int i = 0, j = length - 1; i < length; i++, j--)
+            {
+                if (sb[i] != sb[j])
+                {
+                    flips++;
+                }
+            }
+            return flips;
+        }
+    }
+}
