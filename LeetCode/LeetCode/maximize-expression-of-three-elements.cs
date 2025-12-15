63260dfec46294cf1b9d80e079b3f2f064cf7537
“hu <812791840@qq.com>
2025-12-15 08:05:55 +0800

    Update maximize-expression-of-three-elements.cs

--- LeetCode/LeetCode/maximize-expression-of-three-elements.cs
+++ LeetCode/LeetCode/maximize-expression-of-three-elements.cs
@@ -8,5 +8,12 @@ namespace LeetCode
 {
     internal class maximize_expression_of_three_elements
     {
+        public int MaximizeExpressionOfThree(int[] nums)
+        {
+            Array.Sort(nums);
+            var res = nums[nums.Length - 1] + nums[nums.Length - 2] - nums[0];
+            return res;
+
+        }
     }
 }
