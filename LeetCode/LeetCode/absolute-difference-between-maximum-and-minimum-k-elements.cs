a2948c85771324f40e05bc650300c11c08570bc7
“hu <812791840@qq.com>
2025-12-19 08:21:19 +0800

    Create absolute-difference-between-maximum-and-minimum-k-elements.cs

---
+++ LeetCode/LeetCode/absolute-difference-between-maximum-and-minimum-k-elements.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class absolute_difference_between_maximum_and_minimum_k_elements
+    {
+    }
+}
