5b6219490a4810ef311952a01892f2bb6b3c42c2
“hu <812791840@qq.com>
2025-12-19 08:21:29 +0800

    Update absolute-difference-between-maximum-and-minimum-k-elements.cs

--- LeetCode/LeetCode/absolute-difference-between-maximum-and-minimum-k-elements.cs
+++ LeetCode/LeetCode/absolute-difference-between-maximum-and-minimum-k-elements.cs
@@ -8,5 +8,18 @@ namespace LeetCode
 {
     internal class absolute_difference_between_maximum_and_minimum_k_elements
     {
+        public int AbsDifference(int[] nums, int k)
+        {
+            int difference = 0;
+            Array.Sort(nums);
+            int n = nums.Length;
+            for (int i = n - 1, j = k - 1; j >= 0; i--, j--)
+            {
+                difference += nums[i];
+                difference -= nums[j];
+            }
+
+            return difference;
+        }
     }
 }
