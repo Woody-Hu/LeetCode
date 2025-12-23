c1bd9b61a0254d598128c58853af6bd5066214cb
“hu <812791840@qq.com>
2025-12-23 08:30:55 +0800

    Create mirror-distance-of-an-integer.cs

---
+++ LeetCode/LeetCode/mirror-distance-of-an-integer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class mirror_distance_of_an_integer
+    {
+        public int MirrorDistance(int n)
+        {
+            return Math.Abs(n - Reverse(n));
+        }
+
+        public int Reverse(int n)
+        {
+            int reversed = 0;
+            while (n != 0)
+            {
+                reversed = reversed * 10 + n % 10;
+                n /= 10;
+            }
+            return reversed;
+        }
+    }
+}
