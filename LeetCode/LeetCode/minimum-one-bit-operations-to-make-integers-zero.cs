49fad4dca0664a8d20720e05953154c5180716a9
“hu <812791840@qq.com>
2025-12-31 08:25:21 +0800

    Create minimum-one-bit-operations-to-make-integers-zero.cs

---
+++ LeetCode/LeetCode/minimum-one-bit-operations-to-make-integers-zero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class minimum_one_bit_operations_to_make_integers_zero
+    {
+    }
+}
