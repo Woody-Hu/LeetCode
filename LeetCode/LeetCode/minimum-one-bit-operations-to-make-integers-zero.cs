5c47f5ab3acb41338f4a70399efd5eb2c0a29690
“hu <812791840@qq.com>
2025-12-31 08:25:38 +0800

    Update minimum-one-bit-operations-to-make-integers-zero.cs

--- LeetCode/LeetCode/minimum-one-bit-operations-to-make-integers-zero.cs
+++ LeetCode/LeetCode/minimum-one-bit-operations-to-make-integers-zero.cs
@@ -8,5 +8,15 @@ namespace LeetCode
 {
     internal class minimum_one_bit_operations_to_make_integers_zero
     {
+        public int MinimumOneBitOperations(int n)
+        {
+            int ans = 0;
+            while (n != 0)
+            {
+                ans ^= n;
+                n >>= 1;
+            }
+            return ans;
+        }
     }
 }
