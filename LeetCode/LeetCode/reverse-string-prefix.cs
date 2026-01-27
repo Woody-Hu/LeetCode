0de8b97af99c60de057e1332d81bbc9650793dbc
“hu <812791840@qq.com>
2026-01-27 17:52:18 +0800

    Create reverse-string-prefix.cs

---
+++ LeetCode/LeetCode/reverse-string-prefix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class reverse_string_prefix
+    {
+        public string ReversePrefix(string s, int k)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr, 0, k);
+            return new string(arr);
+
+        }
+    }
+}
