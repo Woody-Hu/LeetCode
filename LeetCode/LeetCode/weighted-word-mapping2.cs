e858b1c639a5796f6457a45e21384852cd8d079b
Hu Di <812791840@qq.com>
2026-03-05 16:47:00 +0800

    Create weighted-word-mapping2.cs

---
+++ LeetCode/LeetCode/weighted-word-mapping2.cs
@@ -0,0 +1,20 @@
+using System.Text;
+
+namespace LeetCode;
+
+public class weighted_word_mapping2
+{
+    public string MapWordWeights(string[] words, int[] weights) {
+        StringBuilder sb = new StringBuilder();
+        foreach (string word in words) {
+            int sum = 0;
+            foreach (char c in word) {
+                sum += weights[c - 'a'];
+            }
+            int remainder = sum % 26;
+            sb.Append((char) ('a' + (25 - remainder)));
+        }
+        return sb.ToString();
+        
+    }
+}
